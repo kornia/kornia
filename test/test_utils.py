3ad7d0518365a0f7f12c3e6f5d29e3b2605974c2
edgarriba <edgar.riba@gmail.com>
2018-09-06 17:54:08 +0200

    add tensor_to_image, image_to_tensor

---
+++ test/test_utils.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+
+import torch
+import torchgeometry as dgm
+
+
+class Tester(unittest.TestCase):
+
+    def test_tensor_to_image(self):
+        tensor = torch.ones(3, 4, 4)
+        image = dgm.utils.tensor_to_image(tensor)
+
+        self.assertTrue(image.shape == (4, 4, 3))
+        self.assertIsInstance(image, np.ndarray)
+
+    def test_image_to_tensor(self):
+        image = np.ones((4, 4, 3))
+        tensor = dgm.utils.image_to_tensor(image)
+
+        self.assertTrue(tensor.shape == (3, 4, 4))
+        self.assertIsInstance(tensor, torch.Tensor)
+
+
+if __name__ == '__main__':
+    unittest.main()
