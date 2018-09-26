33dcc8554a6a187ee33762b1e572a252aa65e698
edgarriba <edgar.riba@gmail.com>
2018-09-26 12:17:41 +0200

    fix depth warper and add tests

--- test/test_depth_warper.py
+++ test/test_depth_warper.py
@@ -17,14 +17,14 @@ class Tester(unittest.TestCase):
         fx, fy = 1., 1.
         rx, ry, rz = 0., 0., 0.
         tx, ty, tz = 0., 0., 0.
-        offset_x = 0.  # we will apply a 10units offset to `i` camera
+        offset = 1.  # we will apply a 1unit offset to `i` camera
 
         pinhole_src = utils.create_pinhole(fx, fy, cx, cy, \
             height, width, rx, ry, rx, tx, ty, tz)
         pinhole_src = pinhole_src.expand(batch_size, -1)
 
         pinhole_dst = utils.create_pinhole(fx, fy, cx, cy, \
-            height, width, rx, ry, rx, tx + offset_x, ty, tz)
+            height, width, rx, ry, rx, tx + offset, ty + offset, tz)
         pinhole_dst = pinhole_dst.expand(batch_size, -1)
 
         # create checkerboard
@@ -32,15 +32,22 @@ class Tester(unittest.TestCase):
         patch_src = torch.from_numpy(board).view(
             1, 1, height, width).expand(batch_size, 1, height, width)
 
-        # instantiate warper
+        # instantiate warper and compute relative homographies
         warper = dgm.DepthWarper(pinhole_src, height, width)
         warper.compute_homographies(pinhole_dst, scale=torch.ones(batch_size, 1))
 
         # generate synthetic inverse depth
         inv_depth_src = torch.ones(batch_size, 1, height, width)
 
-        import pdb;pdb.set_trace()
+        # warpd source patch by depth
         patch_dst = warper(inv_depth_src, patch_src)
+
+        # compute error
+        res = utils.check_equal_torch( \
+            patch_src[..., :-int(offset), :-int(offset)], \
+            patch_dst[..., int(offset):, int(offset):])
+        self.assertTrue(res)
+      
         pass
 
 
