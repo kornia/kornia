b9a7539e6c8eab706189da76b8b341f388881669
edgarriba <edgar.riba@gmail.com>
2018-10-01 22:38:40 -0700

    fix linter test

--- torchgeometry/conversions.py
+++ torchgeometry/conversions.py
@@ -119,7 +119,7 @@ def convert_points_to_homogeneous(points):
 def transform_points(dst_pose_src, points_src):
     """Applies batch of transformations to batch of sets of points.
 
-    Args: 
+    Args:
         dst_pose_src (Tensor): tensor for transformations of size (B, D+1, D+1).
         points_src (Tensor): tensor of points of size (B, N, D).
 
@@ -152,11 +152,12 @@ def angle_axis_to_rotation_matrix_numpy(angle_axis):
     Convert 3d vector of axis-angle rotation to 4x4 rotation matrix
 
     Args:
-        angle_axis (numpy.ndarray): array of 3d vector of axis-angle rotations of size (1, 3).
-    
+        angle_axis (numpy.ndarray): array of 3d vector of axis-angle rotations
+                                    of size (1, 3).
+
     Returns:
         numpy.ndarray: array of 4x4 rotation matrices of size (4, 4).
-        
+
     """
     # stolen from ceres/rotation.h
     k_one = 1.0
@@ -201,8 +202,9 @@ def angle_axis_to_rotation_matrix_torch(angle_axis, eps=1e-6):
     Convert 3d vector of axis-angle rotation to 4x4 rotation matrix
 
     Args:
-        angle_axis (Tensor): tensor of 3d vector of axis-angle rotations of size (N, 3).
-    
+        angle_axis (Tensor): tensor of 3d vector of axis-angle rotations
+                             of size (N, 3).
+
     Returns:
         Tensor: tensor of 4x4 rotation matrices of size (N, 4, 4).
 
@@ -280,7 +282,7 @@ def rtvec_to_pose(rtvec):
     """
     Convert axis-angle rotation and translation vector to 4x4 pose matrix
 
-    Args: 
+    Args:
         rtvec (Tensor): Rodrigues vector transformations of size (N, 6).
 
     Returns:
@@ -297,7 +299,8 @@ def rotation_matrix_to_quaternion(rotation_matrix):
     if isinstance(rotation_matrix, np.ndarray):
         return rotation_matrix_to_quaternion_numpy(rotation_matrix)
     elif isinstance(rotation_matrix, torch.Tensor):
-        if not (len(rotation_matrix.shape) == 3 and rotation_matrix.shape[1] == 4):
+        if not (len(rotation_matrix.shape) == 3 and  # noqa
+                rotation_matrix.shape[1] == 4):
             raise ValueError("Input must be a three dimensional torch.Tensor.")
         return rotation_matrix_to_quaternion_torch(rotation_matrix)
     else:
@@ -312,7 +315,7 @@ def quaternion_to_angle_axis(quaternion):
     elif isinstance(quaternion, torch.Tensor):
         if not (len(quaternion.shape) == 2 and quaternion.shape[1] == 4):
             raise ValueError("Input must be a two dimensional torch.Tensor.")
-        return  quaternion_to_angle_axis_torch(quaternion)
+        return quaternion_to_angle_axis_torch(quaternion)
     else:
         raise NotImplementedError(
             'Not suported type {}'.format(
@@ -330,7 +333,7 @@ def rotation_matrix_to_angle_axis(rotation_matrix):
         Tensor: Rodrigues vector transformation of size (1, 6).
 
     '''
-    #todo add check that matrix is a valid rotation matrix
+    # todo add check that matrix is a valid rotation matrix
     quaternion = rotation_matrix_to_quaternion(rotation_matrix)
     return quaternion_to_angle_axis(quaternion)
 
@@ -374,8 +377,8 @@ def rotation_matrix_to_quaternion_numpy(rotation_matrix):
 
         j = (i + 1) % 3
         k = (j + 1) % 3
-        t = np.sqrt(rotation_matrix[i, i] - rotation_matrix[j, j] -
-                       rotation_matrix[k, k] + 1.0)
+        t = np.sqrt(rotation_matrix[i, i] - rotation_matrix[j, j] - # noqa
+                    rotation_matrix[k, k] + 1.0)
         quaternion[i + 1] = 0.5 * t
         t = 0.5 / t
         quaternion[0] = (rotation_matrix[k, j] - rotation_matrix[j, k]) * t
@@ -389,13 +392,13 @@ def quaternion_to_angle_axis_numpy(quaternion):
     '''
     Convert quaternion vector to angle axis of rotation
     Adapted from ceres C++ library: ceres-solver/include/ceres/rotation.h
-    
+
     Args:
         quaternion (Tensor): quaternion rotation vector of length 4
 
     Returns:
         Tensor: axis-angle rotation of size (1, 3).
-        
+
     '''
     assert quaternion.shape[-1] == 4, 'Input must be a vector of length 4'
     normalizer = 1 / np.linalg.norm(quaternion)
@@ -410,7 +413,7 @@ def quaternion_to_angle_axis_numpy(quaternion):
         sin_theta = np.sqrt(sin_squared)
         cos_theta = quaternion[0] * normalizer
         theta = np.arctan2(-sin_theta, -cos_theta) if cos_theta < 0.0\
-                else np.arctan2(sin_theta, cos_theta)
+            else np.arctan2(sin_theta, cos_theta)
         two_theta = 2 * theta
         k = two_theta / sin_theta
         angle_axis[0] = q1 * k
@@ -434,33 +437,32 @@ def rotation_matrix_to_quaternion_torch(rotation_matrix, eps=1e-6):
 
     mask_d2 = rmat_t[:, 2, 2] < eps
 
-    mask_d0_d1 = rmat_t[:, 0, 0] > rmat_t[:,1, 1]
-    mask_d0_nd1 = rmat_t[:, 0, 0] < -rmat_t[:,1, 1]
+    mask_d0_d1 = rmat_t[:, 0, 0] > rmat_t[:, 1, 1]
+    mask_d0_nd1 = rmat_t[:, 0, 0] < -rmat_t[:, 1, 1]
 
-
-    t0 = 1 + rmat_t[:,0, 0] - rmat_t[:,1, 1] - rmat_t[:,2, 2]
-    q0 = torch.stack([rmat_t[:,1, 2] - rmat_t[:, 2, 1],
-                   t0,  rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
-                   rmat_t[:, 2, 0] + rmat_t[:,0, 2]], -1)
-    t0_rep = t0.repeat(4,1).t()
+    t0 = 1 + rmat_t[:, 0, 0] - rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
+    q0 = torch.stack([rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+                      t0, rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2]], -1)
+    t0_rep = t0.repeat(4, 1).t()
 
     t1 = 1 - rmat_t[:, 0, 0] + rmat_t[:, 1, 1] - rmat_t[:, 2, 2]
-    q1 = torch.stack([rmat_t[:, 2,0] - rmat_t[:,0, 2],
-                    rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
-                    t1,  rmat_t[:, 1, 2] + rmat_t[:, 2, 1]], -1)
-    t1_rep = t1.repeat(4,1).t()
+    q1 = torch.stack([rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
+                      rmat_t[:, 0, 1] + rmat_t[:, 1, 0],
+                      t1, rmat_t[:, 1, 2] + rmat_t[:, 2, 1]], -1)
+    t1_rep = t1.repeat(4, 1).t()
 
     t2 = 1 - rmat_t[:, 0, 0] - rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
     q2 = torch.stack([rmat_t[:, 0, 1] - rmat_t[:, 1, 0],
-                     rmat_t[:, 2, 0] + rmat_t[:, 0, 2],
-                     rmat_t[:, 1, 2] + rmat_t[:, 2, 1], t2], -1)
-    t2_rep = t2.repeat(4,1).t()
+                      rmat_t[:, 2, 0] + rmat_t[:, 0, 2],
+                      rmat_t[:, 1, 2] + rmat_t[:, 2, 1], t2], -1)
+    t2_rep = t2.repeat(4, 1).t()
 
     t3 = 1 + rmat_t[:, 0, 0] + rmat_t[:, 1, 1] + rmat_t[:, 2, 2]
-    q3 = torch.stack([t3,  rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
+    q3 = torch.stack([t3, rmat_t[:, 1, 2] - rmat_t[:, 2, 1],
                       rmat_t[:, 2, 0] - rmat_t[:, 0, 2],
                       rmat_t[:, 0, 1] - rmat_t[:, 1, 0]], -1)
-    t3_rep = t3.repeat(4,1).t()
+    t3_rep = t3.repeat(4, 1).t()
 
     mask_c0 = mask_d2 * mask_d0_d1
     mask_c1 = mask_d2 * (1 - mask_d0_d1)
@@ -472,7 +474,8 @@ def rotation_matrix_to_quaternion_torch(rotation_matrix, eps=1e-6):
     mask_c3 = mask_c3.view(-1, 1).type_as(q3)
 
     q = q0 * mask_c0 + q1 * mask_c1 + q2 * mask_c2 + q3 * mask_c3
-    q /= torch.sqrt(t0_rep * mask_c0 + t1_rep * mask_c1 + t2_rep * mask_c2 + t3_rep * mask_c3)
+    q /= torch.sqrt(t0_rep * mask_c0 + t1_rep * mask_c1 +  # noqa
+                    t2_rep * mask_c2 + t3_rep * mask_c3)
     q *= 0.5
     return q
 
@@ -492,19 +495,21 @@ def quaternion_to_angle_axis_torch(quaternion, eps=1e-6):
 
     sin_squared = q1 * q1 + q2 * q2 + q3 * q3
     mask = (sin_squared > eps).to(sin_squared.device)
-    mask_pos = (mask == True).type_as(sin_squared)
-    mask_neg = (mask == False).type_as(sin_squared)
+    mask_pos = (mask).type_as(sin_squared)
+    mask_neg = (mask == False).type_as(sin_squared)  # noqa
     batch_size = quaternion.size(0)
-    angle_axis = torch.zeros(batch_size, 3, dtype=quaternion.dtype).to(quaternion.device)
+    angle_axis = torch.zeros(
+        batch_size, 3, dtype=quaternion.dtype).to(
+        quaternion.device)
 
     sin_theta = torch.sqrt(sin_squared)
     cos_theta = quaternion[:, 0] * normalizer
     mask_theta = (cos_theta < eps).view(1, -1)
-    mask_theta_neg = (mask_theta == True).type_as(cos_theta)
-    mask_theta_pos = (mask_theta == False).type_as(cos_theta)
+    mask_theta_neg = (mask_theta).type_as(cos_theta)
+    mask_theta_pos = (mask_theta == False).type_as(cos_theta)  # noqa
 
     theta = torch.atan2(-sin_theta, -cos_theta) * mask_theta_neg \
-            + torch.atan2(sin_theta, cos_theta) * mask_theta_pos
+        + torch.atan2(sin_theta, cos_theta) * mask_theta_pos
 
     two_theta = 2 * theta
     k_pos = two_theta / sin_theta
