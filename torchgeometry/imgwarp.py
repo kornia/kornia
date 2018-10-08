e6f4848820f1f5a2f1f0765c7eba7c5f1bced113
edgarriba <edgar.riba@gmail.com>
2018-10-07 21:03:20 -0700

    fix linter errors

--- torchgeometry/imgwarp.py
+++ torchgeometry/imgwarp.py
@@ -12,39 +12,51 @@ __all__ = [
 def center_transform(transform, height, width):
     assert len(transform.shape) == 3, transform.shape
     # move points to origin
-    center_mat_origin = torch.unsqueeze(torch.eye(3,
-        device=transform.device, dtype=transform.dtype), dim=0)
+    center_mat_origin = torch.unsqueeze(
+        torch.eye(
+            3,
+            device=transform.device,
+            dtype=transform.dtype),
+        dim=0)
     center_mat_origin[..., 0, 2] = float(width) / 2
     center_mat_origin[..., 1, 2] = float(height) / 2
     # move points from origin
-    origin_mat_center = torch.unsqueeze(torch.eye(3,
-        device=transform.device, dtype=transform.dtype), dim=0)
+    origin_mat_center = torch.unsqueeze(
+        torch.eye(
+            3,
+            device=transform.device,
+            dtype=transform.dtype),
+        dim=0)
     origin_mat_center[..., 0, 2] = -float(width) / 2
     origin_mat_center[..., 1, 2] = -float(height) / 2
     return torch.matmul(center_mat_origin,
-        torch.matmul(transform, origin_mat_center))
+                        torch.matmul(transform, origin_mat_center))
 
 
 def normalize_transform_to_pix(transform, height, width):
     assert len(transform.shape) == 3, transform.shape
     normal_trans_pix = torch.tensor([[
-        [2. / (width - 1),                 0., -1.],
-        [               0., 2. / (height - 1), -1.],
-        [               0.,                0.,  1.]]],
+        [2. / (width - 1), 0., -1.],
+        [0., 2. / (height - 1), -1.],
+        [0., 0., 1.]]],
         device=transform.device, dtype=transform.dtype)  # 1x3x3
     pix_trans_normal = inverse(normal_trans_pix)         # 1x3x3
     return torch.matmul(normal_trans_pix,
-        torch.matmul(transform, pix_trans_normal))                   
+                        torch.matmul(transform, pix_trans_normal))
 
 
 def warp_perspective(src, M, dsize, flags='bilinear', border_mode=None,
                      border_value=0):
     """Applies a perspective transformation to an image.
 
-    The function warpPerspective transforms the source image using the specified matrix:
+    The function warpPerspective transforms the source image using
+    the specified matrix:
 
     .. math::
-        dst(x, y) = src \left(\\frac{M_{11} x + M_{12} y + M_{33}}{M_{31} x + M_{32} y + M_{33}} , \\frac{M_{21} x + M_{22} y + M_{23}}{M_{31} x + M_{32} y + M_{33}}\\right)
+        dst(x, y) = src \left(
+        \\frac{M_{11} x + M_{12} y + M_{33}}{M_{31} x + M_{32} y + M_{33}} ,
+        \\frac{M_{21} x + M_{22} y + M_{23}}{M_{31} x + M_{32} y + M_{33}}
+        \\right)
 
     Args:
         src (Tensor): input image.
@@ -59,7 +71,8 @@ def warp_perspective(src, M, dsize, flags='bilinear', border_mode=None,
         - Output: :math:`(B, C, H, W)`
 
     .. note::
-       See a working example `here <https://github.com/arraiy/torchgeometry/blob/master/examples/warp_perspective.ipynb>`_.
+       See a working example `here <https://github.com/arraiy/torchgeometry/
+       blob/master/examples/warp_perspective.ipynb>`_.
     """
     if not torch.is_tensor(src):
         raise TypeError("Input src type is not a torch.Tensor. Got {}"
