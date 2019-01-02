6eddcc04c917f7abd126143e974338297e24bfae
edgarriba <edgar.riba@gmail.com>
2019-01-02 08:43:56 +0100

    update test and docs for convert_points_to_homogeneous

--- torchgeometry/conversions.py
+++ torchgeometry/conversions.py
@@ -99,25 +99,21 @@ def convert_points_from_homogeneous(points, eps=1e-6):
 
 
 def convert_points_to_homogeneous(points):
-    """Converts points from Euclidean to homogeneous space.
+    r"""Function that converts points from Euclidean to homogeneous space.
 
-    Args:
-        points (Tensor): tensor of N-dimensional points of size.
+    See :class:`~torchgeometry.ConvertPointsToHomogeneous` for details.
 
-    Returns:
-        Tensor: tensor of N+1-dimensional points.
+    Examples::
 
-    Shape:
-        - Input: :math:`(B, D, N)`
-        - Output: :math:`(B, D, N + 1)`
-
-    Example:
         >>> input = torch.rand(2, 4, 3)  # BxNx3
         >>> output = tgm.convert_points_to_homogeneous(input)  # BxNx4
     """
     if not torch.is_tensor(points):
         raise TypeError("Input type is not a torch.Tensor. Got {}".format(
             type(points)))
+    if len(points.shape) < 2:
+        raise ValueError("Input must be at least a 2D tensor. Got {}".format(
+            points.shape))
 
     # create shape for ones tensor: Nx(...)xD-1
     new_shape = points.shape[:-1] + (points.shape[-1].bit_length() - 1,)
@@ -467,6 +463,25 @@ class ConvertPointsFromHomogeneous(nn.Module):
 
 
 class ConvertPointsToHomogeneous(nn.Module):
+    r"""Creates a transformation to convert points from Euclidean to
+    homogeneous space.
+
+    Args:
+        points (Tensor): tensor of N-dimensional points.
+
+    Returns:
+        Tensor: tensor of N+1-dimensional points.
+
+    Shape:
+        - Input: :math:`(B, D, N)` or :math:`(D, N)`
+        - Output: :math:`(B, D, N + 1)` or :math:`(D, N + 1)`
+
+    Examples::
+
+        >>> input = torch.rand(2, 4, 3)  # BxNx3
+        >>> transform = tgm.ConvertPointsToHomogeneous()
+        >>> output = transform(input)  # BxNx4
+    """
     def __init__(self):
         super(ConvertPointsToHomogeneous, self).__init__()
 
