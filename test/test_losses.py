132577d973a39767738e10b3c75e45867593f813
edgarriba <edgar.riba@gmail.com>
2019-01-10 23:31:10 +0100

    apply autopep8

--- test/test_losses.py
+++ test/test_losses.py
@@ -12,7 +12,7 @@ from common import TEST_DEVICES
 @pytest.mark.parametrize("reduction_type", ['none', 'mean', 'sum'])
 @pytest.mark.parametrize("device_type", TEST_DEVICES)
 @pytest.mark.parametrize("batch_shape",
-                         [(1, 1, 10, 16), (2, 4, 8, 15),])
+                         [(1, 1, 10, 16), (2, 4, 8, 15), ])
 def test_ssim(batch_shape, device_type, window_size, reduction_type):
     # input data
     device = torch.device(device_type)
@@ -21,20 +21,20 @@ def test_ssim(batch_shape, device_type, window_size, reduction_type):
 
     ssim = tgm.losses.SSIM(window_size, reduction_type)
     ssim_loss_val = ssim(img1, img2)
-    
+
     if reduction_type == 'none':
         assert ssim_loss_val.shape == batch_shape
     else:
         assert ssim_loss_val.dim() == 0
 
-    assert ssim(img1, img1).sum().item() == 0
-    assert ssim(img2, img2).sum().item() == 0
+    assert pytest.approx(ssim(img1, img1).sum().item(), 0.0)
+    assert pytest.approx(ssim(img2, img2).sum().item(), 0.0)
 
     # functional
     ssim_loss_val2 = tgm.losses.ssim(img1, img2, window_size, reduction_type)
     assert bool((ssim_loss_val == ssim_loss_val2).all())
 
-    # evaluate function gradient                           
-    img1 = utils.tensor_to_gradcheck_var(img1)  # to var 
-    img2 = utils.tensor_to_gradcheck_var(img2, requires_grad=False)  # to var 
+    # evaluate function gradient
+    img1 = utils.tensor_to_gradcheck_var(img1)  # to var
+    img2 = utils.tensor_to_gradcheck_var(img2, requires_grad=False)  # to var
     assert gradcheck(ssim, (img1, img2,), raise_exception=True)
