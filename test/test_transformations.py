45c18c10d723754d4e2e3b784eced652fd9c9685
edgarriba <edgar.riba@gmail.com>
2019-01-23 20:54:34 +0100

    apply autopep8 format

--- test/test_transformations.py
+++ test/test_transformations.py
@@ -7,12 +7,13 @@ from torch.autograd import gradcheck
 import utils  # test utilities
 from common import TEST_DEVICES
 
+
 class TestTransformPose:
     def test_identity(self):
         pose_1 = torch.eye(4).unsqueeze(0)
         pose_2 = torch.eye(4).unsqueeze(0)
         pose_21 = tgm.relative_pose(pose_1, pose_2)
-        assert utils.check_equal_torch(pose_21, torch.eye(4).unsqueeze(0)) 
+        assert utils.check_equal_torch(pose_21, torch.eye(4).unsqueeze(0))
 
     def test_translation(self):
         offset = 10.
@@ -22,7 +23,7 @@ class TestTransformPose:
 
         # compute relative pose
         pose_21 = tgm.relative_pose(pose_1, pose_2)
-        assert utils.check_equal_torch(pose_21[...,:3,  -1:], offset)
+        assert utils.check_equal_torch(pose_21[..., :3, -1:], offset)
 
     def test_rotation(self):
         pose_1 = torch.eye(4).unsqueeze(0)
@@ -39,33 +40,33 @@ class TestTransformPose:
     @pytest.mark.parametrize("device_type", TEST_DEVICES)
     @pytest.mark.parametrize("batch_size", [1, 2, 5])
     def test_integration(self, batch_size, device_type):
-       pose_1 = torch.eye(4).repeat(batch_size, 1, 1)
-       pose_1 = pose_1.to(torch.device(device_type))
+        pose_1 = torch.eye(4).repeat(batch_size, 1, 1)
+        pose_1 = pose_1.to(torch.device(device_type))
 
-       pose_2 = torch.eye(4).repeat(batch_size, 1, 1)
-       pose_2[..., :3, :3] = torch.rand(batch_size, 3, 3)
-       pose_2[..., :3, -1:] = torch.rand(batch_size, 3, 1)
-       pose_2 = pose_2.to(torch.device(device_type))
+        pose_2 = torch.eye(4).repeat(batch_size, 1, 1)
+        pose_2[..., :3, :3] = torch.rand(batch_size, 3, 3)
+        pose_2[..., :3, -1:] = torch.rand(batch_size, 3, 1)
+        pose_2 = pose_2.to(torch.device(device_type))
 
-       pose_21 = tgm.relative_pose(pose_1, pose_2)
-       assert utils.check_equal_torch(
-           torch.matmul(pose_21, pose_1), pose_2)
+        pose_21 = tgm.relative_pose(pose_1, pose_2)
+        assert utils.check_equal_torch(
+            torch.matmul(pose_21, pose_1), pose_2)
 
     @pytest.mark.skip("Converting a tensor to a Python boolean ...")
     def test_jit(self):
-       pose_1 = torch.eye(4).unsqueeze(0)
-       pose_2 = torch.eye(4).unsqueeze(0)
+        pose_1 = torch.eye(4).unsqueeze(0)
+        pose_2 = torch.eye(4).unsqueeze(0)
 
-       pose_21 = tgm.relative_pose(pose_1, pose_2)
-       pose_21_jit = torch.jit.trace(
-           tgm.relative_pose, (pose_1, pose_2,))(pose_1, pose_2)
-       assert utils.check_equal_torch(pose_21, pose_21_jit)
+        pose_21 = tgm.relative_pose(pose_1, pose_2)
+        pose_21_jit = torch.jit.trace(
+            tgm.relative_pose, (pose_1, pose_2,))(pose_1, pose_2)
+        assert utils.check_equal_torch(pose_21, pose_21_jit)
 
     def test_gradcheck(self):
-       pose_1 = torch.eye(4).unsqueeze(0)
-       pose_2 = torch.eye(4).unsqueeze(0)
+        pose_1 = torch.eye(4).unsqueeze(0)
+        pose_2 = torch.eye(4).unsqueeze(0)
 
-       pose_1 = utils.tensor_to_gradcheck_var(pose_1)  # to var
-       pose_2 = utils.tensor_to_gradcheck_var(pose_2)  # to var
-       assert gradcheck(tgm.relative_pose, (pose_1, pose_2,),
-                        raise_exception=True)
+        pose_1 = utils.tensor_to_gradcheck_var(pose_1)  # to var
+        pose_2 = utils.tensor_to_gradcheck_var(pose_2)  # to var
+        assert gradcheck(tgm.relative_pose, (pose_1, pose_2,),
+                         raise_exception=True)
