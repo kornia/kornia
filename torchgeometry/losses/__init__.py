4e5d53e5d4b143ff6b83edcd298f1f9ac7b0a8bd
edgarriba <edgar.riba@gmail.com>
2019-02-04 16:32:17 +0100

    move apart one_hot function

--- torchgeometry/losses/__init__.py
+++ torchgeometry/losses/__init__.py
@@ -1,3 +1,4 @@
+from .one_hot import one_hot
 from .ssim import SSIM, ssim
-from .dice import DiceLoss, dice_loss, one_hot
+from .dice import DiceLoss, dice_loss
 from .depth_smooth import DepthSmoothnessLoss, depth_smoothness_loss
