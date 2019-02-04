bc3f738b54e607dd8f05c79bcb9f229b7425475e
edgarriba <edgar.riba@gmail.com>
2019-02-04 15:10:04 +0100

    add docs and example to dice_loss

--- torchgeometry/losses/dice.py
+++ torchgeometry/losses/dice.py
@@ -5,17 +5,29 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def one_hot(labels: torch.Tensor, num_classes: int, device: torch.device=None, dtype: torch.dtype=None) -> torch.Tensor:
-    r"""Converts an integer label tensor to a one-hot tensor.
+def one_hot(labels: torch.Tensor, num_classes: int, device: Optional[torch.device] = None, dtype: Optional[torch.dtype] = None) -> torch.Tensor:
+    r"""Converts an integer label 2D tensor to a one-hot 3D tensor.
 
     Args:
         labels (torch.Tensor) : tensor with labels of shape :math:`(N, H, W)`,
                                 where N is batch siz. Each value is an integer
                                 representing correct classification.
         num_classes (int): number of classes in labels.
+        device (Optional[torch.device]): the desired device of returned tensor. Default: if None, uses the current device for the default tensor type (see torch.set_default_tensor_type()). device will be the CPU for CPU tensor types and the current CUDA device for CUDA tensor types.
+        dtype (Optional[torch.dtype]): the desired data type of returned tensor. Default: if None, infers data type from values.
 
     Returns:
         torch.Tensor: the labels in one hot tensor.
+
+    Examples::
+        >>> labels = torch.LongTensor([[[0, 1], [2, 0]]])
+        >>> tgm.losses.one_hot(labels, num_classes=3)
+        tensor([[[[1., 0.],
+                  [0., 1.]],
+                 [[0., 1.],
+                  [0., 0.]],
+                 [[0., 0.],
+                  [1., 0.]]]]
     """
     if not torch.is_tensor(labels):
         raise TypeError("Input labels type is not a torch.Tensor. Got {}"
@@ -34,6 +46,10 @@ def one_hot(labels: torch.Tensor, num_classes: int, device: torch.device=None, d
     one_hot = torch.zeros(batch_size, num_classes, height, width,
                           device=device, dtype=dtype)
     return one_hot.scatter_(1, labels.unsqueeze(1), 1.0)
+
+
+# based on:
+# https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
 
 class DiceLoss(nn.Module):
     r"""Criterion that computes Sørensen-Dice Coefficient loss.
