f7dddabbbec423d385013db1cfe98fff86155007
edgarriba <edgar.riba@gmail.com>
2019-02-04 14:55:32 +0100

    expose tgm.one_hot

--- torchgeometry/losses/dice.py
+++ torchgeometry/losses/dice.py
@@ -4,12 +4,39 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Based on
-# https://github.com/tensorflow/models/blob/master/research/struct2depth/model.py#L625-L641
 
+def one_hot(labels: torch.Tensor, num_classes: int, device: torch.device=None, dtype: torch.dtype=None) -> torch.Tensor:
+    r"""Converts an integer label tensor to a one-hot tensor.
+
+    Args:
+        labels (torch.Tensor) : tensor with labels of shape :math:`(N, H, W)`,
+                                where N is batch siz. Each value is an integer
+                                representing correct classification.
+        num_classes (int): number of classes in labels.
+
+    Returns:
+        torch.Tensor: the labels in one hot tensor.
+    """
+    if not torch.is_tensor(labels):
+        raise TypeError("Input labels type is not a torch.Tensor. Got {}"
+                        .format(type(labels)))
+    if not len(labels.shape) == 3:
+        raise ValueError("Invalid depth shape, we expect BxHxW. Got: {}"
+                         .format(labels.shape))
+    if not labels.dtype == torch.int64:
+        raise ValueError(
+            "labels must be of the same dtype torch.int64. Got: {}" .format(
+                labels.dtype))
+    if num_classes < 1:
+        raise ValueError("The number of classes must be bigger than one. Got: {}"
+                         .format(num_classes))
+    batch_size, height, width = labels.shape
+    one_hot = torch.zeros(batch_size, num_classes, height, width,
+                          device=device, dtype=dtype)
+    return one_hot.scatter_(1, labels.unsqueeze(1), 1.0)
 
 class DiceLoss(nn.Module):
-    r"""Criterion that computes Dice Coefficient loss.
+    r"""Criterion that computes Sørensen-Dice Coefficient loss.
     """
     def __init__(self,
             smooth: Optional[float] = 1.0,
@@ -19,34 +46,6 @@ class DiceLoss(nn.Module):
         self.reduction: str = reduction
         self.eps: float = 1e-6
 
-    @staticmethod
-    def make_one_hot(labels: torch.Tensor, num_classes: int) -> torch.Tensor:
-        r"""Converts an integer label tensor a one-hot labels tensor.                                                                                        
-        Args:
-            labels (torch.Tensor) : tensor with labels of shape :math:`(N, H, W)`,
-                                    where N is batch siz. Each value is an integer
-                                    representing correct classification.       
-            num_classes (int): number of classes in labels.                                        
-        Returns:
-            torch.Tensor: the labels in one hot tensor.
-        """
-        if not torch.is_tensor(labels):
-            raise TypeError("Input labels type is not a torch.Tensor. Got {}"
-                            .format(type(labels)))
-        if not len(labels.shape) == 3:
-            raise ValueError("Invalid depth shape, we expect BxHxW. Got: {}"
-                             .format(labels.shape))
-        if not labels.dtype == torch.int64:
-            raise ValueError(
-                "labels must be of the same dtype torch.int64. Got: {}" .format(
-                    labels.dtype))
-        if num_classes < 1:
-            raise ValueError("The number of classes must be bigger than one. Got: {}"
-                             .format(num_classes))
-        batch_size, height, width = labels.shape
-        one_hot = torch.zeros(
-            batch_size, num_classes, height, width, device=labels.device)       
-        return one_hot.scatter_(1, labels.unsqueeze(1), 1.0)                    
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         if not torch.is_tensor(input):
@@ -66,8 +65,8 @@ class DiceLoss(nn.Module):
         input_soft = F.softmax(input, dim=1)
 
         # create the labels one hot tensor
-        target_one_hot = self.make_one_hot(target, num_classes=input.shape[1])
-        target_one_hot = target_one_hot.to(input.dtype)
+        target_one_hot = one_hot(target, num_classes=input.shape[1],
+                                 device=input.device, dtype=input.dtype)
 
         # compute the actual dice score
         numerator = torch.sum(input_soft * target_one_hot, dim=(1, 2, 3))
