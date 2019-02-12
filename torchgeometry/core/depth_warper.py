62a65fdadcf79d2a4a94328efd7693a6d3c59dd0
edgarriba <edgar.riba@gmail.com>
2019-02-12 17:58:00 +0100

    refactor DepthWarper.warp_grid and add tests

--- torchgeometry/core/depth_warper.py
+++ torchgeometry/core/depth_warper.py
@@ -1,8 +1,13 @@
+from typing import Union, Optional
+
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
-from .pinhole import scale_pinhole, homography_i_H_ref
+from torchgeometry.utils import create_meshgrid
+from torchgeometry.core.conversions import transform_points
+from torchgeometry.core.conversions import convert_points_to_homogeneous
+from torchgeometry.core.pinhole import scale_pinhole, homography_i_H_ref
 
 
 __all__ = [
@@ -20,22 +25,42 @@ class DepthWarper(nn.Module):
         I_{ref} = \\omega(I_{i}, H_{ref}^{i}, \\xi_{ref})
 
     Args:
-        pinholes (Tensor): The pinhole models for ith frame with shape `[Nx12]`.
+        pinholes (torch.Tensor): The pinhole models for ith frame with shape `[Nx12]`.
         width (int): The width of the image to warp. Optional.
         height (int): The height of the image to warp. Optional.
 
     """
 
-    def __init__(self, pinholes, width=None, height=None):
+    def __init__(self,
+                 pinholes: torch.Tensor,
+                 height: Optional[Union[None, int]] = None,
+                 width:  Optional[Union[None, int]] = None,
+                 normalized_coordinates: Optional[bool] = True):
         super(DepthWarper, self).__init__()
         # TODO: add type and value checkings
-        self.width = width if width is None else torch.tensor(width)
-        self.height = height if height is None else torch.tensor(height)
+        #self.width = width if width is None else torch.tensor(width)
+        #self.height = height if height is None else torch.tensor(height)
+        self.width = width
+        self.height = height
         self._pinholes = pinholes
         self._i_Hs_ref = None  # to be filled later
         self._pinhole_ref = None  # to be filled later
         self.eps = 1e-6
+        self.normalized_coordinates = normalized_coordinates
 
+        self.grid = None  # to be filled later
+        if height is not None and width is not None:
+            self.grid: torch.Tensor = create_meshgrid(height, width,
+                normalized_coordinates=self.normalized_coordinates)
+    
+    # TODO(edgar): refactor this and potentially and potentially accept the
+    # pinholes models as Nx2x4x4, where first will be the pinhole_matrix in the
+    # 4x4 shape, and second the Rt matrix in the 4x4 shape. For this, we need
+    # to update inverse_pinhole, scale_pinhole, etc. But seems the more generic
+    # thing to do. Additionally, I wouldn't force the output shape(self.height/width)
+    # to be the pinhole camera content. Instead I would stick to the values from
+    # the class signature which I would force to the user to introduce (now is
+    # optional and could be None).
     def compute_homographies(self, pinhole, scale=None):
         if scale is None:
             batch_size = pinhole.shape[0]
@@ -81,10 +106,26 @@ class DepthWarper(nn.Module):
 
     # compute grids
 
-    def warp(self, inv_depth_ref, roi=None):
+    def warp_grid(self, inv_depth_ref: torch.Tensor, roi=None) -> torch.Tensor:
+        """Computes a grid for warping a given the inverse depth from
+        a reference pinhole camera.
+        
+        The function `compute_homographies` has to be called beforehand in
+        order to have precomputed the relative transformations encoding the
+        relative pose and the intrinsics between the reference and a non
+        reference camera.
+        """
         # TODO: add type and value checkings
-        assert self._i_Hs_ref is not None, 'call compute_homographies'
-        if roi is None:
+        if self._i_Hs_ref is None:
+            raise ValueError("Please, call compute_homographies.")
+
+        if len(inv_depth_ref.shape) != 4:
+            raise ValueError("Input inv_depth_ref has to be in the shape of"
+                             "Bx1xHxW. Got {}".format(inv_depth_ref.shape))
+
+        # NOTE: exclude this by now. We will figure out later if we want to
+        # support this feature again.
+        '''if roi is None:
             roi = (0, int(self.height), 0, int(self.width))
         start_row, end_row, start_col, end_col = roi
         assert start_row < end_row
@@ -94,42 +135,50 @@ class DepthWarper(nn.Module):
 
         # take sub region
         inv_depth_ref = inv_depth_ref[..., start_row:end_row,
-                                      start_col:end_col].contiguous()
+                                      start_col:end_col].contiguous()'''
 
-        device = inv_depth_ref.device
-        ones_x = torch.ones(height).to(device)
-        ones_y = torch.ones(width).to(device)
-        ones = torch.ones(area).to(device)
+        batch_size, _, height, width = inv_depth_ref.shape
+        device: torch.device = inv_depth_ref.device
 
-        x = torch.linspace(start_col, end_col - 1, width).to(device)
-        y = torch.linspace(start_row, end_row - 1, height).to(device)
+        # compute the base grid to transform in case it does not exist,
+        # otherwise, use the existing one.
+        if self.grid is None:
+            grid: torch.Tensor = create_meshgrid(height, width,
+                normalized_coordinates=self.normalized_coordinates)
+        else:
+            grid: torch.Tensor = self.grid
 
-        xv = torch.ger(ones_x, x).view(area)
-        yv = torch.ger(y, ones_y).view(area)
+        # expand the base according to the number of the input batch size
+        # and concatenate it with the inverse depth [x, y, inv_depth]'
+        grid = grid.expand(batch_size, -1, -1, -1)
+        grid = grid.to(device).to(inv_depth_ref.dtype)
+        grid = torch.cat([grid, inv_depth_ref.permute(0, 2, 3, 1)], dim=-1)
+        grid = grid.view(batch_size, 1, 1, height, width, 3)  # Nx1x1xHxWx3
 
-        grid = torch.stack([xv, yv, ones], 0)
-        batch_size = inv_depth_ref.shape[0]
-        grid = grid.unsqueeze(0).expand(batch_size, -1, -1)
-        grid = torch.cat([grid, inv_depth_ref.view(batch_size, 1, -1)], 1)
+        # reshape the transforms to match with the grid shape: Nx1x1x4x4
+        i_trans_ref: torch.Tensor = torch.unsqueeze(
+            torch.unsqueeze(self._i_Hs_ref, dim=1), dim=1).to(device)
 
-        flow = torch.matmul(self._i_Hs_ref.to(device), grid)
-        assert len(flow.shape) == 3, flow.shape
+        # transform the actual grid
+        flow: torch.Tensor = transform_points(i_trans_ref, grid).view(
+            batch_size, height, width, 3)
 
-        factor_x = (self.width - 1) / 2
-        factor_x = factor_x.to(device)
+        # recover the the coordinates
+        z: torch.Tensor = 1. / (flow[..., 2] + self.eps)  # NxHxW
+        x: torch.Tensor = flow[..., 0] * z
+        y: torch.Tensor = flow[..., 1] * z
 
-        factor_y = (self.height - 1) / 2
-        factor_y = factor_y.to(device)
+        # in case we are using normalized coordinates in the range of [-1, 1]
+        # we have to move the coordinates to the grid center.
 
-        z = 1. / (flow[:, 2] + self.eps)  # Nx(H*W)
-        x = (flow[:, 0] * z - factor_x) / factor_x
-        y = (flow[:, 1] * z - factor_y) / factor_y
+        if self.normalized_coordinates:
+            factor_x: float = (self.width - 1.) / 2
+            factor_y: float = (self.height - 1.) / 2
+            x = (x - factor_x) / factor_x
+            y = (y - factor_y) / factor_y
 
-        flow = torch.stack([x, y], 1)  # Nx2x(H*W)
-
-        n, c, a = flow.shape
-        flows = flow.view(n, c, height, width)  # Nx2xHxW
-        return flows.permute(0, 2, 3, 1)  # NxHxWx2
+        # stack and return the values
+        return torch.stack([x, y], dim=-1)  # NxHxWx2
 
     def forward(self, inv_depth_ref, patch_i):
         """Warps an image or tensor from ith frame to reference given the
@@ -161,7 +210,7 @@ class DepthWarper(nn.Module):
         """
         # TODO: add type and value checkings
         return torch.nn.functional.grid_sample(
-            patch_i, self.warp(inv_depth_ref))
+            patch_i, self.warp_grid(inv_depth_ref))
 
 
 # functional api
