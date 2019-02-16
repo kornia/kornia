f55051bc82a5861cbdc2ae7c0e535be857d8cef0
edgarriba <edgar.riba@gmail.com>
2019-02-16 15:19:26 +0100

    move PinholeCamera and PinholeCamerasList to pinhole.py and add tests

--- test/test_depth_warper.py
+++ test/test_depth_warper.py
@@ -5,7 +5,6 @@ import torchgeometry as tgm
 from torch.autograd import gradcheck
 
 import utils  # test utils
-from torchgeometry.core.depth_warper import PinholeCamera
 from torchgeometry.core.depth_warper import normalize_pixel_coordinates
 
 
@@ -20,9 +19,9 @@ class TestDepthWarper:
         tx, ty, tz = 0, 0, 0
 
         # create pinhole cameras
-        pinhole_src = PinholeCamera.from_parameters(
+        pinhole_src = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx, ty, tz)
-        pinhole_dst = PinholeCamera.from_parameters(
+        pinhole_dst = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx + 1., ty, tz)
 
         # create warper
@@ -49,9 +48,9 @@ class TestDepthWarper:
         tx, ty, tz = 0, 0, 0
 
         # create pinhole cameras
-        pinhole_src = PinholeCamera.from_parameters(
+        pinhole_src = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx, ty, tz)
-        pinhole_dst = PinholeCamera.from_parameters(
+        pinhole_dst = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx + 1., ty, tz)
 
         # initialize depth to one
@@ -81,10 +80,44 @@ class TestDepthWarper:
         tx, ty, tz = 0, 0, 0
 
         # create pinhole cameras
-        pinhole_src = PinholeCamera.from_parameters(
+        pinhole_src = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx, ty, tz)
-        pinhole_dst = PinholeCamera.from_parameters(
+        pinhole_dst = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx + 1., ty + 1., tz)
+
+        # initialize depth to one
+        depth_src = torch.ones(batch_size, 1, height, width)
+
+        # create warper, initialize projection matrices and warp grid
+        warper = tgm.DepthWarper(None, height, width, pinhole_dst)
+        warper.compute_projection_matrix(pinhole_src)
+        grid_warped = warper.warp_grid(depth_src)
+
+        # normalize base meshgrid
+        grid = warper.grid[..., :2]
+        grid_norm = normalize_pixel_coordinates(grid, height, width)
+
+        # check offset in x-axis
+        assert utils.check_equal_torch(
+            grid_norm[..., -1, 0], grid_warped[..., -2, 0])
+        # check that y-axis remain the same
+        assert utils.check_equal_torch(
+            grid_norm[..., -1, :, 1], grid_warped[..., -2, :, 1])
+
+    def _test_warp_grid_offset_x1y1_depth1_batch2(self):
+        # prepare data
+        batch_size, height, width = 1, 3, 5
+        fx, fy = 1., 1.
+        cx, cy = width / 2, height / 2
+        tx, ty, tz = 0, 0, 0
+
+        # create pinhole cameras
+        pinhole_src = tgm.PinholeCamera.from_parameters(
+            fx, fy, cx, cy, height, width, tx, ty, tz)
+        pinhole_dst = tgm.PinholeCamera.from_parameters(
+            fx, fy, cx, cy, height, width, tx + 1., ty + 1., tz)
+        import pdb
+        pdb.set_trace()
 
         # initialize depth to one
         depth_src = torch.ones(batch_size, 1, height, width)
@@ -113,9 +146,9 @@ class TestDepthWarper:
         tx, ty, tz = 0, 0, 0
 
         # create pinhole cameras
-        pinhole_src = PinholeCamera.from_parameters(
+        pinhole_src = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx, ty, tz)
-        pinhole_dst = PinholeCamera.from_parameters(
+        pinhole_dst = tgm.PinholeCamera.from_parameters(
             fx, fy, cx, cy, height, width, tx + 1., ty + 1., tz)
 
         # initialize depth to one
@@ -150,4 +183,5 @@ class TestDepthWarper:
         self._test_compute_projection_matrix()
         self._test_warp_grid_offset_x1_depth1()
         self._test_warp_grid_offset_x1y1_depth1()
+        # self._test_warp_grid_offset_x1y1_depth1_batch2()
         self._test_depth_warper_offset_tensor()
