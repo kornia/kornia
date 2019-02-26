d23b7d6ec45d238dd9e511bd342e9641110bfca8
edgarriba <edgar.riba@gmail.com>
2019-02-27 00:13:00 +0100

    improve transformations module docs

--- test/test_transformations.py
+++ test/test_transformations.py
@@ -20,24 +20,24 @@ def euler_angles_to_rotation_matrix(x, y, z):
     ones, zeros = torch.ones_like(x), torch.zeros_like(x)
     # the rotation matrix for the x-axis
     rx_tmp = [
-          ones,        zeros,         zeros, zeros,
-         zeros, torch.cos(x), -torch.sin(x), zeros,
-         zeros, torch.sin(x),  torch.cos(x), zeros,
-         zeros,        zeros,         zeros,  ones]
+        ones, zeros, zeros, zeros,
+        zeros, torch.cos(x), -torch.sin(x), zeros,
+        zeros, torch.sin(x), torch.cos(x), zeros,
+        zeros, zeros, zeros, ones]
     rx = torch.stack(rx_tmp, dim=-1).view(-1, 4, 4)
     # the rotation matrix for the y-axis
     ry_tmp = [
-         torch.cos(y), zeros, torch.sin(y), zeros,
-                zeros,  ones,        zeros, zeros,
+        torch.cos(y), zeros, torch.sin(y), zeros,
+        zeros, ones, zeros, zeros,
         -torch.sin(y), zeros, torch.cos(y), zeros,
-                zeros, zeros,        zeros,  ones]
+        zeros, zeros, zeros, ones]
     ry = torch.stack(ry_tmp, dim=-1).view(-1, 4, 4)
     # the rotation matrix for the z-axis
     rz_tmp = [
         torch.cos(z), -torch.sin(z), zeros, zeros,
-        torch.sin(z),  torch.cos(z), zeros, zeros,
-               zeros,         zeros,  ones, zeros,
-               zeros,         zeros, zeros,  ones]
+        torch.sin(z), torch.cos(z), zeros, zeros,
+        zeros, zeros, ones, zeros,
+        zeros, zeros, zeros, ones]
     rz = torch.stack(rz_tmp, dim=-1).view(-1, 4, 4)
     return torch.matmul(rz, torch.matmul(ry, rx))  # Bx4x4
 
