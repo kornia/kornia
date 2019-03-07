f464e18ec60e40c8eae3b15a84ea92a636bb4a89
Carlos B <mcflurry0@gmail.com>
2019-03-07 17:33:28 +0100

    Added first tests checking cpu / gpu

--- test/common.py
+++ test/common.py
@@ -1,4 +1,5 @@
 import torch
+import pytest
 
 
 def get_test_devices():
@@ -17,3 +18,9 @@ def get_test_devices():
 # setup the devices to test the source code
 
 TEST_DEVICES = get_test_devices()
+
+
+@pytest.fixture()
+def device_type(request):
+    typ = request.config.getoption('--typetest')
+    return typ
