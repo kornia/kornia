3649af4ea95023661fa460b2e5f29ad6956e3fff
edgarriba <edgar.riba@gmail.com>
2019-03-07 17:07:15 +0100

    generate version.py by command line

--- setup.py
+++ setup.py
@@ -1,7 +1,54 @@
-import os
-import io
-import re
+# Welcome to the PyTorch setup.py.
+#
+# Environment variables you are probably interested in:
+#
+# Environment variables for feature toggles:
+#
+#   PYTORCH_BUILD_VERSION
+#   PYTORCH_BUILD_NUMBER
+#     specify the version of PyTorch, rather than the hard-coded version
+#     in this file; used when we're building binaries for distribution
+
+from __future__ import print_function
 from setuptools import setup, find_packages
+import subprocess
+import os
+
+
+# Constant known variables used throughout this file
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+
+################################################################################
+# Version, create_version_file, and package_name
+#
+# Example for release (0.1.2):
+#  TORCHGEOMETRY_BUILD_VERSION=0.1.2 \
+#  TORCHGEOMETRY_BUILD_NUMBER=1 python setup.py install
+################################################################################
+package_name = os.getenv('TORCHGEOMETRY_PACKAGE_NAME', 'torchgeometry')
+version = '0.1.2'  # NOTE: modify this variable each time we do a release
+if os.getenv('TORCHGEOMETRY_BUILD_VERSION'):
+    assert os.getenv('TORCHGEOMETRY_BUILD_NUMBER') is not None
+    build_number = int(os.getenv('TORCHGEOMETRY_BUILD_NUMBER'))
+    version = os.getenv('TORCHGEOMETRY_BUILD_VERSION')
+    if build_number > 1:
+        version += '.post' + str(build_number)
+else:
+    try:
+        sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).decode('ascii').strip()
+        version += '+' + sha[:7]
+    except Exception:
+        pass
+print("Building wheel {}-{}".format(package_name, version))
+
+
+# all the work we need to do _before_ setup runs
+def build_deps():
+    print('-- Building version ' + version)
+    version_path = os.path.join(cwd, 'torchgeometry', 'version.py')
+    with open(version_path, 'w') as f:
+        f.write("__version__ = '{}'\n".format(version))
 
 
 def read(*names, **kwargs):
@@ -12,21 +59,9 @@ def read(*names, **kwargs):
         return fp.read()
 
 
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
+# open readme file and remove logo
 readme = open('README.rst').read()
-# remove logo
 long_description = '\n'.join(readme.split('\n')[7:])
-
-
-VERSION = find_version('torchgeometry', '__init__.py')
 
 
 requirements = [
@@ -34,24 +69,26 @@ requirements = [
 ]
 
 
-setup(
-    # Metadata
-    name='torchgeometry',
-    version=VERSION,
-    author='Edgar Riba',
-    author_email='edgar.riba@gmail.com',
-    url='https://github.com/arraiyopensource/torchgeometry',
-    description='differential geometric computer vision for deep learning',
-    long_description=long_description,
-    license='BSD',
+if __name__ == '__main__':
+    build_deps()
+    setup(
+        # Metadata
+	name=package_name,
+	version=version,
+	author='Edgar Riba',
+	author_email='edgar.riba@gmail.com',
+	url='https://github.com/arraiyopensource/torchgeometry',
+	description='differential geometric computer vision for deep learning',
+	long_description=long_description,
+	license='BSD',
 
-    # Test
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+	# Test
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest'],
 
-    # Package info
-    packages=find_packages(exclude=('docs', 'test', 'examples',)),
+	# Package info
+	packages=find_packages(exclude=('docs', 'test', 'examples',)),
 
-    zip_safe=True,
-    install_requires=requirements,
-)
+	zip_safe=True,
+	install_requires=requirements,
+    )
