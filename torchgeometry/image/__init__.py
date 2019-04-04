44a8505226d1c9fe292a4a7cf2b4e458a3280487
pulkit-khandelwal <pulkit.khandelwal@mail.mcgill.ca>
2019-04-04 01:26:00 -0400

    fixes tests and laplacian

--- torchgeometry/image/__init__.py
+++ torchgeometry/image/__init__.py
@@ -1,4 +1,4 @@
 from .gaussian import get_gaussian_kernel, get_gaussian_kernel2d
 from .gaussian import GaussianBlur, gaussian_blur
 from .laplacian import get_laplacian_kernel, get_laplacian_kernel2d
-from .laplacian import LaplacianBlur, laplacian_blur
+from .laplacian import Laplacian, laplacian
