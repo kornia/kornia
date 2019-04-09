fix rotation tests

test/test_augmentation.py
@@ -106,85 +106,87 @@ class TestGrayscale:
 class TestRotate:
     def test_smoke(self):
         angle = 0.0
-        angle_t = torch.Tensor([angle])
+        angle_t = torch.tensor([angle])
         assert str(taug.Rotate(angle=angle_t)) == 'Rotate(angle=0.0, center=None)'
 
     def test_angle90(self):
         # prepare input data
-        inp = torch.FloatTensor([[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
+        inp = torch.tensor([[
+            [1., 2.],
+            [3., 4.],
+            [5., 6.],
+            [7., 8.],
         ]])
-        expected = torch.FloatTensor([[
-            [0, 0],
-            [4, 6],
-            [3, 5],
-            [0, 0],
+        expected = torch.tensor([[
+            [0., 0.],
+            [4., 6.],
+            [3., 5.],
+            [0., 0.],
         ]])
         # prepare transformation
-        angle_t = torch.Tensor([90])
+        angle_t = torch.tensor([90.])
         transform = taug.Rotate(angle_t)
         assert_allclose(transform(inp), expected)
         
     def test_angle90_batch2(self):
         # prepare input data
-        inp = torch.FloatTensor([[
-            [1, 2],
-            [3, 4],
-            [5, 6],
-            [7, 8],
+        inp = torch.tensor([[
+            [1., 2.],
+            [3., 4.],
+            [5., 6.],
+            [7., 8.],
         ]]).repeat(2, 1, 1, 1)
-        expected = torch.FloatTensor([[[
-            [0, 0],
-            [4, 6],
-            [3, 5],
-            [0, 0],
+        expected = torch.tensor([[[
+            [0., 0.],
+            [4., 6.],
+            [3., 5.],
+            [0., 0.],
         ]],[[
-            [0, 0],
-            [5, 3],
-            [6, 4],
-            [0, 0],
+            [0., 0.],
+            [5., 3.],
+            [6., 4.],
+            [0., 0.],
         ]]])
         # prepare transformation
-        angle_t = torch.Tensor([90, -90])
+        angle_t = torch.tensor([90., -90.])
         transform = taug.Rotate(angle_t)
         assert_allclose(transform(inp), expected)
 
-    def test_random_rotate_value(self):
-        transform = taug.RandomRotation(degrees=90)
-        inp = torch.rand(1, 3, 4)
-        assert transform(inp).shape == (1, 3, 4)
-
-    def test_random_rotate_minmax_value(self):
-        transform = taug.RandomRotation(degrees=(-45, 0))
-        inp = torch.rand(1, 3, 4)
-        assert transform(inp).shape == (1, 3, 4)
-
-    def test_random_rotate_minmax_value_batch(self):
-        transform = taug.RandomRotation(degrees=(-45, 0))
-        inp = torch.rand(2, 3, 3, 4)
-        assert transform(inp).shape == (2, 3, 3, 4)
-
     def test_compose_transforms(self):
-        c, h, w = 3, 4, 2  # channels, height, width
-        inp = torch.rand(c, h, w)
-        angle = torch.Tensor([90])
-        center = torch.Tensor([[(w - 1) / 2, (h - 1) / 2]])
+        h, w = 4, 2  # height, width
+        center = torch.tensor([[(w - 1) / 2, (h - 1) / 2]])
+
+        # prepare input data
+        inp = torch.tensor([[
+            [1., 2.],
+            [3., 4.],
+            [5., 6.],
+            [7., 8.],
+        ]])
+        expected = torch.tensor([[
+            [0., 0.],
+            [4., 6.],
+            [3., 5.],
+            [0., 0.],
+        ]])
 
         # compose the transforms
+        angle = torch.tensor([90.])
+        random_rotation = taug.RandomRotationMatrix(angle, center)
         compose_matrix = nn.Sequential(
             taug.RotationMatrix(angle, center),
+            random_rotation,
         )
         matrix = compose_matrix(taug.identity_matrix())
 
+        # rotation with obtained random angle
+        angle_composed = angle + random_rotation.angle
         transforms = nn.Sequential(
-            taug.Rotate(angle, center),
+            taug.Rotate(angle_composed, center),
         )
 
         # apply transforms
         out_affine = taug.affine(inp, matrix[..., :2, :3])
-        out_rotate = taug.rotate(inp, angle, center)
+        out_rotate = taug.rotate(inp, angle_composed, center)
         assert_allclose(out_affine, out_rotate)
         assert_allclose(out_affine, transforms(inp))

torchgeometry/augmentation/affine.py
@@ -27,50 +27,73 @@ def identity_matrix() -> torch.Tensor:
     return torch.eye(3)[None]
 
 
-# TODO: implement me
-class RandomRotationMatrix(nn.Module):
-    pass
+def rotation_matrix(angle: torch.Tensor, center: torch.Tensor) -> torch.Tensor:
+    scale: torch.Tensor = torch.ones_like(angle)
+    matrix: torch.Tensor = convert_matrix2d_to_homogeneous(
+        get_rotation_matrix2d(center, angle, scale))
+    return matrix
 
 
 class RotationMatrix(nn.Module):
     def __init__(self,
             angle: torch.Tensor, center: torch.Tensor) -> None:
         super(RotationMatrix, self).__init__()
-        scale: torch.Tensor = torch.ones_like(angle)
-        self.matrix: torch.Tensor = convert_matrix2d_to_homogeneous(
-            get_rotation_matrix2d(center, angle, scale))
+        self.angle: torch.Tensor = angle
+        self.center: torch.Tensor = center
+        self.matrix: torch.Tensor = self._generate_matrix()
+
+    def _generate_matrix(self) -> torch.Tensor:
+        assert self.angle is not None
+        assert self.center is not None
+        return rotation_matrix(self.angle, self.center)
+
+    def affine(self) -> torch.Tensor:
+        assert self.matrix is not None
+        return self.matrix[..., :2, :3]
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        assert input.shape == self.matrix.shape
+        assert self.matrix is not None
         return torch.matmul(input, self.matrix)
 
 
-class RandomRotation(object):
+class RandomRotationMatrix(nn.Module):
     def __init__(self,
-            degrees: Union[float, Tuple[float]],
-            center: Union[None, Tuple[float]] = None) -> None:
-        super(RandomRotation, self).__init__()
-        if isinstance(degrees, numbers.Number):
-            if degrees < 0:
+            degrees: torch.Tensor,
+            center: Union[None, torch.Tensor] = None) -> None:
+        super(RandomRotationMatrix, self).__init__()
+        if len(degrees.shape) == 1:
+            if bool(degrees < 0):
                  raise ValueError("If degrees is a single number, it must be positive.")
-            self.degrees = (-degrees, degrees)
+            self.degrees = torch.cat([-degrees, degrees])
         else:
-            if len(degrees) != 2:
+            if len(degrees.shape) != 2:
                 raise ValueError("If degrees is a sequence, it must be of len 2.")
             self.degrees = degrees
         self.center = center
+        self.angle = None
+        self.matrix = None
     
-    @staticmethod
-    def get_params(degrees: Tuple[float], num_samples: int) -> torch.Tensor:
-        return torch.FloatTensor(num_samples).uniform_(degrees[0], degrees[1])
+    def _generate_params(self, num_samples: int) -> torch.Tensor:
+        degrees: Tuple[float] = self.degrees.tolist()
+        angle: torch.Tensor = torch.tensor(
+            [float(num_samples)]).uniform_(degrees[0], degrees[1])
+        return angle
 
-    def __call__(self, tensor: torch.Tensor) -> torch.Tensor:
-        self.center = compute_rotation_center(tensor)
-        num_samples: int = 1
-        if len(tensor.shape) == 4:
-            num_samples = tensor.shape[0]
-        angle: torch.Tensor = self.get_params(self.degrees, num_samples)
-        return rotate(tensor, angle, self.center)
+    def _generate_matrix(self, num_samples: int) -> torch.Tensor:
+        assert self.center is not None
+        self.angle: torch.Tensor = self._generate_params(num_samples)
+        return rotation_matrix(self.angle, self.center)
+
+    def affine(self) -> torch.Tensor:
+        assert self.matrix is not None
+        return self.matrix[..., :2, :3]
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        assert len(input.shape) == 3, input.shape
+        assert self.center is not None, self.center
+        num_samples: int = input.shape[0]
+        self.matrix: torch.Tensor = self._generate_matrix(num_samples)
+        return torch.matmul(input, self.matrix)
 
 
 class Rotate(nn.Module):
@@ -90,7 +113,6 @@ class Rotate(nn.Module):
         self.center: Union[None, torch.Tensor] = center
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        self.center = compute_rotation_center(input)
         return rotate(input, self.angle, self.center)
 
     def __repr__(self):
@@ -110,6 +132,9 @@ def rotate(tensor: torch.Tensor, angle: torch.Tensor,
     if not torch.is_tensor(angle):
         raise TypeError("Input angle type is not a torch.Tensor. Got {}"
                         .format(type(angle)))
+    if center is not None and not torch.is_tensor(angle):
+        raise TypeError("Input center type is not a torch.Tensor. Got {}"
+                        .format(type(center)))
     if len(tensor.shape) not in (3, 4,):
         raise ValueError("Invalid tensor shape, we expect CxHxW or BxCxHxW. "
                          "Got: {}".format(tensor.shape))
@@ -126,12 +151,10 @@ def rotate(tensor: torch.Tensor, angle: torch.Tensor,
     # compute the rotation matrix
     # TODO: add broadcasting to get_rotation_matrix2d for center
     center = center.expand(angle.shape[0], -1)
-    scale: torch.Tensor = torch.ones_like(angle)
-    rotation_matrix: torch.Tensor = get_rotation_matrix2d(
-        center, angle, scale)
+    rotation_matrix = RotationMatrix(angle, center)
 
     # warp using the affine transform
-    return affine(tensor, rotation_matrix)
+    return affine(tensor, rotation_matrix.affine())
 
 # based on:
 # https://github.com/anibali/tvl/blob/master/src/tvl/transforms.py#L166