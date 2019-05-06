622e0e1a9ed3099994697f86ae34f83d056626d8
diegovd0296 <diegovd0296@gmail.com>
2019-05-06 20:06:48 +0200

    rgb to bgr and jit tests

--- test/test_image.py
+++ test/test_image.py
@@ -109,6 +109,23 @@ class TestNormalize:
         f = image.Normalize(mean, std)
         assert_allclose(f(data), expected)
 
+    def test_jit(self):
+        @torch.jit.script
+        def op_script(data: torch.Tensor, mean: torch.Tensor,
+                      std: torch.Tensor) -> torch.Tensor:
+
+            return image.normalize(data, mean, std)
+
+            data = torch.ones(2, 3, 1, 1)
+            data += 2
+
+            mean = torch.tensor([0.5, 1.0, 2.0]).repeat(2, 1)
+            std = torch.tensor([2.0, 2.0, 2.0]).repeat(2, 1)
+
+            actual = op.script(data, mean, std)
+            expected = image.normalize(data, mean, std)
+            assert_allclose(actual, expected)
+
     def test_gradcheck(self):
 
         # prepare input data
@@ -172,32 +189,105 @@ class TestBgRToRgb:
     def test_bgr_to_rgb(self):
 
         # prepare input data
-        data = torch.ones(3, 2, 2)
+        data = torch.tensor([[[1., 1.],
+                              [1., 1.]],
+
+                             [[2., 2.],
+                              [2., 2.]],
+
+                             [[3., 3.],
+                              [3., 3.]]])  # 3x2x2
+
+        expected = torch.tensor([[[3., 3.],
+                                  [3., 3.]],
+
+                                 [[2., 2.],
+                                  [2., 2.]],
+
+                                 [[1., 1.],
+                                  [1., 1.]]])  # 3x2x2
 
         f = image.BgrToRgb()
-        assert_allclose(f(data)[0], data[-1])
-        assert_allclose(f(data)[-1], data[0])
+        assert_allclose(f(data), expected)
 
     def test_batch_bgr_to_rgb(self):
 
         # prepare input data
-        data = torch.ones(5, 3, 2, 2)
+        data = torch.tensor([[[[1., 1.],
+                              [1., 1.]],
+
+                             [[2., 2.],
+                              [2., 2.]],
+
+                             [[3., 3.],
+                              [3., 3.]]],
+
+                             [[[1., 1.],
+                               [1., 1.]],
+
+                              [[2., 2.],
+                               [2., 2.]],
+
+                              [[3., 3.],
+                               [3., 3.]]]])  # 2x3x2x2
+
+        expected = torch.tensor([[[[3., 3.],
+                                  [3., 3.]],
+
+                                 [[2., 2.],
+                                  [2., 2.]],
+
+                                 [[1., 1.],
+                                  [1., 1.]]],
+
+                                [[[3., 3.],
+                                  [3., 3.]],
+
+                                 [[2., 2.],
+                                  [2., 2.]],
+
+                                 [[1., 1.],
+                                  [1., 1.]]]])  # 2x3x2x2
 
         f = image.BgrToRgb()
         out = f(data)
-        for i in range(out.shape[0]):
-            assert_allclose(out[i][0], data[i][-1])
-            assert_allclose(out[i][-1], out[i][0])
+        assert_allclose(out, expected)
 
     def test_gradcheck(self):
 
         # prepare input data
-        data = torch.ones(2, 3, 1, 1)
+        data = torch.tensor([[[1., 1.],
+                              [1., 1.]],
+
+                             [[2., 2.],
+                              [2., 2.]],
+
+                             [[3., 3.],
+                              [3., 3.]]])  # 3x2x2
 
         data = utils.tensor_to_gradcheck_var(data)  # to var
 
         assert gradcheck(image.BgrToRgb(), (data,),
                          raise_exception=True)
+
+    def test_jit(self):
+        @torch.jit.script
+        def op_script(data: torch.Tensor) -> torch.Tensor:
+
+            return image.bgr_to_rgb(data)
+
+            data = torch.tensor([[[1., 1.],
+                                  [1., 1.]],
+
+                                 [[2., 2.],
+                                  [2., 2.]],
+
+                                 [[3., 3.],
+                                  [3., 3.]]])  # 3x2x2
+
+            actual = op.script(data)
+            expected = image.bgr_to_rgb(data)
+            assert_allclose(actual, expected)
 
 
 class TestRgbToGrayscale:
