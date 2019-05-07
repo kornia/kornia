555110259943eae091077581b01cfbb7831d2bda
edgarriba <edgar.riba@gmail.com>
2019-05-07 16:57:43 +0200

    implement SobelEdges amd Sobel operators

--- torchgeometry/image/__init__.py
+++ torchgeometry/image/__init__.py
@@ -5,3 +5,4 @@ from .laplacian import get_laplacian_kernel, get_laplacian_kernel2d
 from .laplacian import Laplacian, laplacian
 from .color import rgb_to_grayscale, RgbToGrayscale
 from .pyramid import pyrdown, pyrup, PyrDown, PyrUp
+from .sobel import SobelEdges, Sobel, sobel, sobel_edges
