fb3894a95249c5f2c36757292a5e9cbfe624ac6a
edgarriba <edgar.riba@gmail.com>
2019-05-12 11:03:48 +0200

    rename sobel_edges -> spatial_gradient

--- torchgeometry/image/__init__.py
+++ torchgeometry/image/__init__.py
@@ -5,6 +5,6 @@ from .laplacian import get_laplacian_kernel, get_laplacian_kernel2d
 from .laplacian import Laplacian, laplacian
 from .color import rgb_to_grayscale, RgbToGrayscale
 from .pyramid import pyrdown, pyrup, PyrDown, PyrUp
-from .sobel import SobelEdges, Sobel, sobel, sobel_edges
+from .sobel import SpatialGradient, Sobel, sobel, spatial_gradient
 from .blur import BoxBlur, box_blur
 from .median import MedianBlur, median_blur
