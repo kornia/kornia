928b03864044c154c4a206287f028258a58b5465
S-aiueo32 <18kmc06@ms.dendai.ac.jp>
2019-06-25 07:58:15 +0000

    add YCbCr operations to __init__.py

--- kornia/color/__init__.py
+++ kornia/color/__init__.py
@@ -3,6 +3,8 @@ from .rgb import BgrToRgb, bgr_to_rgb
 from .rgb import RgbToBgr, rgb_to_bgr
 from .hsv import RgbToHsv, rgb_to_hsv
 from .hsv import HsvToRgb, hsv_to_rgb
+from .ycbcr import RgbToYcrcb, rgb_to_ycrcb
+from .ycbcr import YcbcrToRgb, ycbcr_to_rgb
 from .normalize import Normalize, normalize
 
 __all__ = [
@@ -11,12 +13,15 @@ __all__ = [
     "rgb_to_bgr",
     "rgb_to_hsv",
     "hsv_to_rgb",
-    "normalize"
+    "rgb_to_ycbcr",
+    "ycbcr_to_rgb",
+    "normalize",
     "RgbToGrayscale",
     "BgrToRgb",
     "RgbToBgr",
     "RgbToHsv",
     "HsvToRgb",
+    "RgbToYcrc",
+    "YcbcrToRgb",
     "Normalize",
-
 ]
