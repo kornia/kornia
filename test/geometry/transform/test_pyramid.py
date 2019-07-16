7e25f687e38e48f6212757b67293599900c2c11c
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-07-16 16:03:24 +0200

    fix mypy

--- test/geometry/transform/test_pyramid.py
+++ test/geometry/transform/test_pyramid.py
@@ -64,3 +64,32 @@ class TestPyrDown:
         actual = op_script(img)
         expected = kornia.geometry.pyrdown(img)
         assert_allclose(actual, expected)
+
+class TestScalePyramid:
+    def test_shape(self):
+        inp = torch.zeros(1, 2, 4, 4)
+        pyr = kornia.geometry.PyrUp()
+        assert pyr(inp).shape == (1, 2, 8, 8)
+
+    def test_shape_batch(self):
+        inp = torch.zeros(2, 2, 4, 4)
+        pyr = kornia.geometry.PyrUp()
+        assert pyr(inp).shape == (2, 2, 8, 8)
+
+    def test_gradcheck(self):
+        batch_size, channels, height, width = 1, 2, 5, 4
+        img = torch.rand(batch_size, channels, height, width)
+        img = utils.tensor_to_gradcheck_var(img)  # to var
+
+        assert gradcheck(kornia.geometry.pyrup, (img,), raise_exception=True)
+
+    @pytest.mark.skip("")
+    def test_jit(self):
+        @torch.jit.script
+        def op_script(input):
+            return kornia.geometry.pyrup(input)
+        img = torch.rand(2, 3, 4, 5)
+        actual = op_script(img)
+        expected = kornia.geometry.pyrup(img)
+        assert_allclose(actual, expected)
+
