46cc3bf23fe2b1bf7685e4dbe15a1dacd6578c45
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-07-29 20:31:11 +0200

    mypy fix

--- kornia/filters/sobel.py
+++ kornia/filters/sobel.py
@@ -53,8 +53,8 @@ class SpatialGradient(nn.Module):
         # convolve input tensor with sobel kernel
         kernel_flip: torch.Tensor = kernel.flip(-3)
         # Pad with "replicate for spatial dims, but with zeros for channel dim
-        padded_inp = F.pad(F.pad(input, (1, 1, 1, 1), 'replicate')[:, :, None],
-                           (0, 0, 0, 0, 1, 1), 'constant')
+        padded_inp = F.pad(F.pad(input, [1, 1, 1, 1], 'replicate')[:, :, None],
+                           [0, 0, 0, 0, 1, 1], 'constant')
         return F.conv3d(padded_inp, kernel_flip, padding=0, groups=c)
 
 
