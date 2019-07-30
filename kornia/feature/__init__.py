75727996ef83c5edbe195823da719af48adce4c5
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-07-30 11:15:13 +0200

    fix typo in function name

--- kornia/feature/__init__.py
+++ kornia/feature/__init__.py
@@ -4,6 +4,6 @@ from .nms import NonMaximaSuppression2d, non_maxima_suppression2d
 __all__ = [
     "non_maxima_suppression2d",
     "corner_harris",
-    "NonMaximaSupression2d",
+    "NonMaximaSuppression2d",
     "CornerHarris",
 ]
