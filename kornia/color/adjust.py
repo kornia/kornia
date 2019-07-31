360cc90189a0abc6bcc2ea24cef5b25b58953318
Pau Riba <priba@cvc.uab.cat>
2019-07-31 11:46:09 +0200

    Modify AdjustBrightness to accept a factor per element in the batch

--- kornia/color/adjust.py
+++ kornia/color/adjust.py
@@ -9,9 +9,10 @@ class AdjustBrightness(nn.Module):
 
     Args:
         image (torch.Tensor): Image to be adjusted.
-        brightness_factor (torch.Tensor): Brightness adjust factor. 0 generates
-          a compleatly black image, 1 does not modify the input image while any
-          other non-negative number modify the brightness by this factor.
+        brightness_factor (torch.Tensor): Brightness adjust factor per element
+          in the batch. 0 generates a compleatly black image, 1 does not modify
+          the input image while any other non-negative number modify the 
+          brightness by this factor.
 
     Returns:
         torch.Tensor: Adjusted image.
@@ -35,9 +36,10 @@ def adjust_brightness(image: torch.Tensor,
 
     Args:
         image (torch.Tensor): Image to be adjusted.
-        brightness_factor (torch.Tensor): Brightness adjust factor. 0 generates
-          a compleatly black image, 1 does not modify the input image while any
-          other non-negative number modify the brightness by this factor.
+        brightness_factor (torch.Tensor): Brightness adjust factor per element
+          in the batch. 0 generates a compleatly black image, 1 does not modify
+          the input image while any other non-negative number modify the 
+          brightness by this factor.
 
     Returns:
         torch.Tensor: Adjusted image.
@@ -51,9 +53,14 @@ def adjust_brightness(image: torch.Tensor,
         raise ValueError("Input size must have a shape of (*, H, W). Got {}"
                          .format(image.shape))
 
-    if brightness_factor < 0.0:
+
+    if (brightness_factor < torch.zeros(1)).any():
         raise ValueError("Brightness factor must be non-negative. Got {}"
                          .format(brightness_factor))
+
+    if torch.is_tensor(brightness_factor):
+        for _ in image.shape[1:]:
+            brightness_factor = brightness_factor.unsqueeze(-1)
 
     # Apply brightness factor to each channel
     adjust_image: torch.Tensor = image * brightness_factor
