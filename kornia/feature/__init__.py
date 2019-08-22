9bf681c4e5984662d69dc83470607b29517ded77
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-08-22 17:34:35 +0200

    clean LAF commit

--- kornia/feature/__init__.py
+++ kornia/feature/__init__.py
@@ -1,10 +1,30 @@
 from .harris import CornerHarris, corner_harris
 from .nms import NonMaximaSuppression2d, non_maxima_suppression2d
+from .laf import (extract_patches_from_pyramid,
+                  extract_patches_simple,
+                  visualize_LAF,
+                  normalize_LAF,
+                  denormalize_LAF,
+                  LAF2pts,
+                  ell2LAF,
+                  make_upright,
+                  get_laf_scale,
+                  angle_to_rotation_matrix)
 from .siftdesc import SIFTDescriptor
 __all__ = [
     "non_maxima_suppression2d",
     "corner_harris",
     "NonMaximaSuppression2d",
     "CornerHarris",
+    "extract_patches_from_pyramid",
+    "extract_patches_simple",
+    "visualize_LAF",
+    "normalize_LAF",
+    "denormalize_LAF",
+    "LAF2pts",
+    "ell2LAF",
+    "make_upright",
+    "get_laf_scale",
+    "angle_to_rotation_matrix"
     "SIFTDescriptor"
 ]
