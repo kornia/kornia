099529b4d522afbd92df32387008f0f3bb299a6e
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-08-30 15:05:28 +0200

    review fix

--- kornia/feature/__init__.py
+++ kornia/feature/__init__.py
@@ -1,15 +1,14 @@
+from kornia.geometry import angle_to_rotation_matrix
 from .harris import CornerHarris, corner_harris
 from .nms import NonMaximaSuppression2d, non_maxima_suppression2d
 from .laf import (extract_patches_from_pyramid,
                   extract_patches_simple,
-                  visualize_laf,
-                  normalize_LAF,
-                  denormalize_LAF,
+                  normalize_laf,
+                  denormalize_laf,
                   laf_to_boundary_points,
                   ellipse_to_laf,
                   make_upright,
                   get_laf_scale)
-from kornia.geometry.transform.imgwarp import angle_to_rotation_matrix
 from .siftdesc import SIFTDescriptor
 __all__ = [
     "non_maxima_suppression2d",
@@ -18,13 +17,11 @@ __all__ = [
     "CornerHarris",
     "extract_patches_from_pyramid",
     "extract_patches_simple",
-    "visualize_laf",
-    "normalize_LAF",
-    "denormalize_LAF",
+    "normalize_laf",
+    "denormalize_laf",
     "laf_to_boundary_points",
     "ellipse_to_laf",
     "make_upright",
     "get_laf_scale",
-    "angle_to_rotation_matrix"
     "SIFTDescriptor"
 ]
