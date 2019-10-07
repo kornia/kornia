dac6839364405ff932bc33a5ca7845cba4f8b4ce
edgarriba <edgar.riba@gmail.com>
2019-10-07 02:07:10 +0200

    refactor adjust_brightness and implement adjust_constrast

--- kornia/color/adjust.py
+++ kornia/color/adjust.py
@@ -1,70 +1,122 @@
+from typing import Union
+
 import torch
 import torch.nn as nn
 
 
-class AdjustBrightness(nn.Module):
-    r"""Adjust Brightness of an Image
+def adjust_contrast(input: torch.Tensor,
+                    contrast_factor: Union[float, torch.Tensor]) -> torch.Tensor:
+    r"""Adjust Contrast of an image.
 
-    See :class:`~kornia.color.AdjustBrightness` for details.
-
-    Args:
-        image (torch.Tensor): Image to be adjusted.
-        brightness_factor (torch.Tensor): Brightness adjust factor per element
-          in the batch. 0 generates a compleatly black image, 1 does not modify
-          the input image while any other non-negative number modify the
-          brightness by this factor.
-
-    Returns:
-        torch.Tensor: Adjusted image.
+    See :class:`~kornia.color.AdjustContrast` for details.
     """
 
-    def __init__(self) -> None:
-        super(AdjustBrightness, self).__init__()
+    if not torch.is_tensor(input):
+        raise TypeError(f"Input type is not a torch.Tensor. Got {type(input)}")
 
-    def forward(self,  # type: ignore
-                image: torch.Tensor,  # type: ignore
-                brightness_factor: torch.Tensor  # type: ignore
-                ) -> torch.Tensor:  # type: ignore
-        return adjust_brightness(image, brightness_factor)
+    if not isinstance(contrast_factor, (float, torch.Tensor,)):
+        raise TypeError(f"The factor should be either a float or torch.Tensor. "
+                        f"Got {type(contrast_factor)}")
 
+    if isinstance(contrast_factor, float):
+        contrast_factor = torch.tensor([contrast_factor])
 
-def adjust_brightness(image: torch.Tensor,
-                      brightness_factor: torch.Tensor) -> torch.Tensor:
-    r"""Adjust Brightness of an Image
+    contrast_factor = contrast_factor.to(input.device).to(input.dtype)
 
-    See :class:`~kornia.color.AdjustBrightness` for details.
+    if (contrast_factor < torch.zeros(1)).any():
+        raise ValueError(f"Contrast factor must be non-negative. Got {contrast_factor}")
 
-    Args:
-        image (torch.Tensor): Image to be adjusted.
-        brightness_factor (torch.Tensor): Brightness adjust factor per element
-          in the batch. 0 generates a compleatly black image, 1 does not modify
-          the input image while any other non-negative number modify the
-          brightness by this factor.
-
-    Returns:
-        torch.Tensor: Adjusted image.
-    """
-
-    if not torch.is_tensor(image):
-        raise TypeError("Input type is not a torch.Tensor. Got {}".format(
-            type(image)))
-
-    if len(image.shape) < 3:
-        raise ValueError("Input size must have a shape of (*, H, W). Got {}"
-                         .format(image.shape))
-
-    if (brightness_factor < torch.zeros(1)).any():
-        raise ValueError("Brightness factor must be non-negative. Got {}"
-                         .format(brightness_factor))
-
-    if torch.is_tensor(brightness_factor):
-        for _ in image.shape[1:]:
-            brightness_factor = brightness_factor.unsqueeze(-1)
+    for _ in input.shape[1:]:
+        contrast_factor = torch.unsqueeze(contrast_factor, dim=-1)
 
     # Apply brightness factor to each channel
-    adjust_image: torch.Tensor = image * brightness_factor
+    x_adjust: torch.Tensor = input + contrast_factor
 
     # Truncate between pixel values
-    out: torch.Tensor = torch.clamp(adjust_image, 0.0, 1.0)
+    out: torch.Tensor = torch.clamp(x_adjust, 0.0, 1.0)
 
     return out
+
+
+def adjust_brightness(input: torch.Tensor,
+                      brightness_factor: Union[float, torch.Tensor]) -> torch.Tensor:
+    r"""Adjust Brightness of an image.
+
+    See :class:`~kornia.color.AdjustBrightness` for details.
+    """
+
+    if not torch.is_tensor(input):
+        raise TypeError(f"Input type is not a torch.Tensor. Got {type(input)}")
+
+    if not isinstance(brightness_factor, (float, torch.Tensor,)):
+        raise TypeError(f"The factor should be either a float or torch.Tensor. "
+                        f"Got {type(brightness_factor)}")
+
+    if isinstance(brightness_factor, float):
+        brightness_factor = torch.tensor([brightness_factor])
+
+    brightness_factor = brightness_factor.to(input.device).to(input.dtype)
+
+    if (brightness_factor < torch.zeros(1)).any():
+        raise ValueError(f"Brightness factor must be non-negative. Got {brightness_factor}")
+
+    for _ in input.shape[1:]:
+        brightness_factor = torch.unsqueeze(brightness_factor, dim=-1)
+
+    # Apply brightness factor to each channel
+    x_adjust: torch.Tensor = input * brightness_factor
+
+    # Truncate between pixel values
+    out: torch.Tensor = torch.clamp(x_adjust, 0.0, 1.0)
+
+    return out
+
+
+class AdjustContrast(nn.Module):
+    r"""Adjust Contrast of an image.
+
+    The input image is expected to be in the range of [0, 1].
+
+    Args:
+        image (torch.Tensor): Image to be adjusted in the shape of (*, N).
+        contrast_factor (Union[float, torch.Tensor]): Contrast adjust factor per element
+          in the batch. 0 generates a compleatly black image, 1 does not modify
+          the input image while any other non-negative number modify the
+          brightness by this factor.
+
+    Returns:
+        torch.Tensor: Adjusted image.
+    """
+
+    def __init__(self, contrast_factor: Union[float, torch.Tensor]) -> None:
+        super(AdjustContrast, self).__init__()
+        self.contrast_factor = contrast_factor
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return adjust_contrast(input, self.contrast_factor)
+
+
+class AdjustBrightness(nn.Module):
+    r"""Adjust Brightness of an image.
+
+    The input image is expected to be in the range of [0, 1].
+
+    Args:
+        image (torch.Tensor): Image to be adjusted in the shape of (*, N).
+        brightness_factor (Union[float, torch.Tensor]): Brightness adjust factor per element
+          in the batch. 0 generates a compleatly black image, 1 does not modify
+          the input image while any other non-negative number modify the
+          brightness by this factor.
+
+    Returns:
+        torch.Tensor: Adjusted image.
+    """
+
+    def __init__(self, brightness_factor: Union[float, torch.Tensor]) -> None:
+        super(AdjustBrightness, self).__init__()
+        self.brightness_factor = brightness_factor
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:  # type: ignore
+        return adjust_brightness(input, self.brightness_factor)
+
+
