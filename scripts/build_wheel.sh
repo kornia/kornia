5f24d9dc3ebe985d2d9aa700326d74fd01c294f3
edgarriba <edgar.riba@gmail.com>
2019-10-07 12:57:46 +0200

    fix typos in docs

--- scripts/build_wheel.sh
+++ scripts/build_wheel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -ex
 
 ## NOTE: change this each release
-export KORNIA_BUILD_VERSION=0.1.3
-export KORNIA_BUILD_NUMBER=2  # use 1 for final release, otherwise will be post2, post3, postN
+export KORNIA_BUILD_VERSION=0.1.4
+export KORNIA_BUILD_NUMBER=1  # use 1 for final release, otherwise will be post2, post3, postN
 
 # move to project root and create the wheel
 
