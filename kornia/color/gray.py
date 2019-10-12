b90f271d897cd96f3101999587ceeef28c35a856
HassanAlsamahi <hassanalsamahi12@gmail.com>
2019-10-12 11:42:48 +0200

    Fix typo in color files

--- kornia/color/gray.py
+++ kornia/color/gray.py
@@ -8,7 +8,7 @@ class RgbToGrayscale(nn.Module):
     the image data is assumed to be in the range of (0, 1).
 
     args:
-        input (torch.Tensor): image to be converted to grayscale.
+        input (torch.Tensor): RGB image to be converted to grayscale.
 
     returns:
         torch.Tensor: grayscale version of the image.
@@ -37,7 +37,7 @@ def rgb_to_grayscale(input: torch.Tensor) -> torch.Tensor:
     See :class:`~kornia.color.RgbToGrayscale` for details.
 
     Args:
-        input (torch.Tensor): Image to be converted to grayscale.
+        input (torch.Tensor): RGB image to be converted to grayscale.
 
     Returns:
         torch.Tensor: Grayscale version of the image.
@@ -49,6 +49,7 @@ def rgb_to_grayscale(input: torch.Tensor) -> torch.Tensor:
     if len(input.shape) < 3 and input.shape[-3] != 3:
         raise ValueError("Input size must have a shape of (*, 3, H, W). Got {}"
                          .format(input.shape))
+
 
     # https://docs.opencv.org/4.0.1/de/d25/imgproc_color_conversions.html
     r, g, b = torch.chunk(input, chunks=3, dim=-3)
