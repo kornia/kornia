5ab7b0f22e1d12d8f76515a59e322dc60e15f0b0
ananyamukh6 <ananyamukh6@gmail.com>
2019-10-15 22:00:25 -0700

    Remove numpy dependency

--- test/test_losses.py
+++ test/test_losses.py
@@ -280,30 +280,50 @@ class TestTotalVariation:
         assert_allclose(actual, expected)
 
     # Total variation for 3D tensors
-    @pytest.mark.parametrize('input', [
-        torch.rand(3, 4, 5),
-        torch.rand(1, 2, 3),
+    @pytest.mark.parametrize('input,expected', [
+        (torch.tensor([[[0.11747694, 0.5717714, 0.89223915, 0.2929412, 0.63556224],
+                        [0.5371079, 0.13416398, 0.7782737, 0.21392655, 0.1757018],
+                        [0.62360305, 0.8563448, 0.25304103, 0.68539226, 0.6956515],
+                        [0.9350611, 0.01694632, 0.78724295, 0.4760313, 0.73099905]],
+
+                       [[0.4788819, 0.45253807, 0.932798, 0.5721999, 0.7612051],
+                        [0.5455887, 0.8836531, 0.79551977, 0.6677338, 0.74293613],
+                        [0.4830376, 0.16420758, 0.15784949, 0.21445751, 0.34168917],
+                        [0.8675162, 0.5468113, 0.6117004, 0.01305223, 0.17554593]],
+
+                       [[0.6423703, 0.5561105, 0.54304767, 0.20339686, 0.8553698],
+                        [0.98024786, 0.31562763, 0.10122144, 0.17686582, 0.26260805],
+                        [0.20522952, 0.14523649, 0.8601968, 0.02593213, 0.7382898],
+                        [0.71935296, 0.9625162, 0.42287344, 0.07979459, 0.9149871]]]), torch.tensor(33.001236)),
+        (torch.tensor([[[0.09094203, 0.32630223, 0.8066123],
+                        [0.10921168, 0.09534764, 0.48588026]]]), torch.tensor(1.6900232)),
     ])
-    def test_tv_on_3d(self, input):
-        actual = kornia.losses.total_variation(input)
-        input_np = input.numpy()
-        pixel_dif1 = input_np[:, 1:, :] - input_np[:, :-1, :]
-        pixel_dif2 = input_np[:, :, 1:] - input_np[:, :, :-1]
-        expected = torch.tensor(np.sum(np.abs(pixel_dif1)) + np.sum(np.abs(pixel_dif2)))
-        assert_allclose(actual, expected)
+    def test_tv_on_3d(self, input, expected):
+        assert_allclose(kornia.losses.total_variation(input), expected)
 
     # Total variation for 4D tensors
-    @pytest.mark.parametrize('input', [
-        torch.rand(2, 3, 4, 5),
-        torch.rand(3, 1, 2, 3),
+    @pytest.mark.parametrize('input,expected', [
+        (torch.tensor([[[[0.8756, 0.0920],
+                         [0.8034, 0.3107]],
+                        [[0.3069, 0.2981],
+                         [0.9399, 0.7944]],
+                        [[0.6269, 0.1494],
+                         [0.2493, 0.8490]]],
+                       [[[0.3256, 0.9923],
+                         [0.2856, 0.9104]],
+                        [[0.4107, 0.4387],
+                           [0.2742, 0.0095]],
+                        [[0.7064, 0.3674],
+                           [0.6139, 0.2487]]]]), torch.tensor([5.0054283, 3.1870906])),
+        (torch.tensor([[[[0.1104, 0.2284, 0.4371],
+                         [0.4569, 0.1906, 0.8035]]],
+                       [[[0.0552, 0.6831, 0.8310],
+                         [0.3589, 0.5044, 0.0802]]],
+                       [[[0.5078, 0.5703, 0.9110],
+                         [0.4765, 0.8401, 0.2754]]]]), torch.tensor([1.9565653, 2.5786452, 2.2681699])),
     ])
-    def test_tv_on_4d(self, input):
-        actual = kornia.losses.total_variation(input)
-        input_np = input.numpy()
-        pixel_dif1 = input_np[:, :, 1:, :] - input_np[:, :, :-1, :]
-        pixel_dif2 = input_np[:, :, :, 1:] - input_np[:, :, :, :-1]
-        expected = torch.tensor(np.sum(np.abs(pixel_dif1), axis=(1, 2, 3)) + np.sum(np.abs(pixel_dif2), axis=(1, 2, 3)))
-        assert_allclose(actual, expected)
+    def test_tv_on_4d(self, input, expected):
+        assert_allclose(kornia.losses.total_variation(input), expected)
 
     # Expect ValueError to be raised when tensors of ndim != 3 or 4 are passed
     @pytest.mark.parametrize('input', [
