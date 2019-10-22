f0c4187e69b327da6fc6c35849a9b65702176cab
ananyamukh6 <ananyamukh6@gmail.com>
2019-10-21 21:07:35 -0700

    Initial commit. Adding psnr.py

--- kornia/__init__.py
+++ kornia/__init__.py
@@ -58,7 +58,8 @@ from kornia.losses import (
     dice_loss,
     tversky_loss,
     inverse_depth_smoothness_loss,
-    total_variation
+    total_variation,
+    psnr
 )
 from kornia.utils import (
     one_hot,
