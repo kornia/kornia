04c54ccfc4aec86479a032c0b84615da8189de4b
edgarriba <edgar.riba@gmail.com>
2019-11-01 22:58:23 +0100

    implement cuda test for color module

--- test/color/test_adjust.py
+++ test/color/test_adjust.py
@@ -2,7 +2,7 @@ import pytest
 
 import kornia
 import kornia.testing as utils  # test utils
-from test.common import device_type
+from test.common import device
 
 import torch
 from torch.autograd import gradcheck
@@ -10,7 +10,7 @@ from torch.testing import assert_allclose
 
 
 class TestAdjustSaturation:
-    def test_saturation_one(self):
+    def test_saturation_one(self, device):
         data = torch.tensor([[[.5, .5],
                               [.5, .5]],
 
@@ -20,13 +20,15 @@ class TestAdjustSaturation:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
-        expected = data
+        data = data.to(device)
+        expected = data.clone()
+
         f = kornia.color.AdjustSaturation(1.)
         assert_allclose(f(data), expected)
 
 
 class TestAdjustHue:
-    def test_hue_one(self):
+    def test_hue_one(self, device):
         data = torch.tensor([[[.5, .5],
                               [.5, .5]],
 
@@ -36,13 +38,15 @@ class TestAdjustHue:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
-        expected = data
+        data = data.to(device)
+        expected = data.clone()
+
         f = kornia.color.AdjustHue(1.)
         assert_allclose(f(data), expected)
 
 
 class TestAdjustGamma:
-    def test_gamma_zero(self):
+    def test_gamma_zero(self, device):
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
 
@@ -52,11 +56,13 @@ class TestAdjustGamma:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
+        data = data.to(device)
         expected = torch.ones_like(data)
+
         f = kornia.color.AdjustGamma(0.)
         assert_allclose(f(data), expected)
 
-    def test_gamma_one(self):
+    def test_gamma_one(self, device):
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
 
@@ -66,11 +72,13 @@ class TestAdjustGamma:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
-        expected = data
+        data = data.to(device)
+        expected = data.clone()
+
         f = kornia.color.AdjustGamma(1.)
         assert_allclose(f(data), expected)
 
-    def test_gamma_one_gain_two(self):
+    def test_gamma_one_gain_two(self, device):
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
 
@@ -89,10 +97,13 @@ class TestAdjustGamma:
                                  [[.5, .5],
                                   [.5, .5]]])  # 3x2x2
 
+        data = data.to(device)
+        expected = expected.to(device)
+
         f = kornia.color.AdjustGamma(1., 2.)
         assert_allclose(f(data), expected)
 
-    def test_gamma_two(self):
+    def test_gamma_two(self, device):
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
 
@@ -111,19 +122,23 @@ class TestAdjustGamma:
                                  [[.0625, .0625],
                                   [.0625, .0625]]])  # 3x2x2
 
+        data = data.to(device)
+        expected = expected.to(device)
+
         f = kornia.color.AdjustGamma(2.)
         assert_allclose(f(data), expected)
 
-    def test_gradcheck(self):
+    def test_gradcheck(self, device):
         batch_size, channels, height, width = 2, 3, 4, 5
         img = torch.ones(batch_size, channels, height, width)
+        img = img.to(device)
         img = utils.tensor_to_gradcheck_var(img)  # to var
         assert gradcheck(kornia.adjust_gamma, (img, 1., 2.),
                          raise_exception=True)
 
 
 class TestAdjustContrast:
-    def test_factor_zero(self):
+    def test_factor_zero(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -134,13 +149,14 @@ class TestAdjustContrast:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
+        data = data.to(device)
         expected = torch.zeros_like(data)
 
         f = kornia.color.AdjustContrast(0.)
 
         assert_allclose(f(data), expected)
 
-    def test_factor_one(self):
+    def test_factor_one(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -150,14 +166,14 @@ class TestAdjustContrast:
 
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
-
-        expected = data
+        data = data.to(device)
+        expected = data.clone()
 
         f = kornia.color.AdjustContrast(1.)
 
         assert_allclose(f(data), expected)
 
-    def test_factor_two(self):
+    def test_factor_two(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -177,11 +193,14 @@ class TestAdjustContrast:
                                  [[.5, .5],
                                   [.5, .5]]])  # 3x2x2
 
+        data = data.to(device)
+        expected = expected.to(device)
+
         f = kornia.color.AdjustContrast(2.)
 
         assert_allclose(f(data), expected)
 
-    def test_factor_tensor(self):
+    def test_factor_tensor(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -208,10 +227,15 @@ class TestAdjustContrast:
                                   [1., 1.]]])  # 4x2x2
 
         factor = torch.tensor([0, 1, 1.5, 2])
+
+        data = data.to(device)
+        expected = expected.to(device)
+        factor = factor.to(device)
+
         f = kornia.color.AdjustContrast(factor)
         assert_allclose(f(data), expected)
 
-    def test_factor_tensor_color(self):
+    def test_factor_tensor_color(self, device):
         # prepare input data
         data = torch.tensor([[[[1., 1.],
                                [1., 1.]],
@@ -250,10 +274,15 @@ class TestAdjustContrast:
                                    [1., 1.]]]])  # 2x3x2x2
 
         factor = torch.tensor([1, 2])
+
+        data = data.to(device)
+        expected = expected.to(device)
+        factor = factor.to(device)
+
         f = kornia.color.AdjustContrast(factor)
         assert_allclose(f(data), expected)
 
-    def test_factor_tensor_shape(self):
+    def test_factor_tensor_shape(self, device):
         # prepare input data
         data = torch.tensor([[[[1., 1., .5],
                                [1., 1., .5]],
@@ -292,19 +321,25 @@ class TestAdjustContrast:
                                    [.6, .4, .2]]]])  # 2x3x2x3
 
         factor = torch.tensor([1.5, 2.])
+
+        data = data.to(device)
+        expected = expected.to(device)
+        factor = factor.to(device)
+
         f = kornia.color.AdjustContrast(factor)
         assert_allclose(f(data), expected)
 
-    def test_gradcheck(self):
+    def test_gradcheck(self, device):
         batch_size, channels, height, width = 2, 3, 4, 5
         img = torch.ones(batch_size, channels, height, width)
+        img = img.to(device)
         img = utils.tensor_to_gradcheck_var(img)  # to var
         assert gradcheck(kornia.adjust_contrast, (img, 2.),
                          raise_exception=True)
 
 
 class TestAdjustBrightness:
-    def test_factor_zero(self):
+    def test_factor_zero(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -315,12 +350,13 @@ class TestAdjustBrightness:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
-        expected = data
+        data = data.to(device)
+        expected = data.clone()
 
         f = kornia.color.AdjustBrightness(0.)
         assert_allclose(f(data), expected)
 
-    def test_factor_one(self):
+    def test_factor_one(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -331,12 +367,13 @@ class TestAdjustBrightness:
                              [[.25, .25],
                               [.25, .25]]])  # 3x2x2
 
+        data = data.to(device)
         expected = torch.ones_like(data)
 
         f = kornia.color.AdjustBrightness(1.)
         assert_allclose(f(data), expected)
 
-    def test_factor_minus(self):
+    def test_factor_minus(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -356,10 +393,13 @@ class TestAdjustBrightness:
                                  [[0., 0.],
                                   [0., 0.]]])  # 3x2x2
 
-        f = kornia.color.AdjustBrightness(-.5)
+        data = data.to(device)
+        expected = expected.to(device)
+
+        f = kornia.color.AdjustBrightness(-0.5)
         assert_allclose(f(data), expected)
 
-    def test_factor_tensor(self):
+    def test_factor_tensor(self, device):
         # prepare input data
         data = torch.tensor([[[1., 1.],
                               [1., 1.]],
@@ -373,13 +413,16 @@ class TestAdjustBrightness:
                              [[.5, .5],
                               [.5, .5]]])  # 4x2x2
 
-        expected = torch.ones_like(data)
-
         factor = torch.tensor([0, 0.5, 0.75, 2])
+
+        data = data.to(device)
+        expected = torch.ones_like(data)
+        factor = factor.to(device)
+
         f = kornia.color.AdjustBrightness(factor)
         assert_allclose(f(data), expected)
 
-    def test_factor_tensor_color(self):
+    def test_factor_tensor_color(self, device):
         # prepare input data
         data = torch.tensor([[[[1., 1.],
                                [1., 1.]],
@@ -418,12 +461,18 @@ class TestAdjustBrightness:
                                    [.7, .7]]]])  # 2x3x2x2
 
         factor = torch.tensor([0.25, 0.1])
+
+        data = data.to(device)
+        expected = expected.to(device)
+        factor = factor.to(device)
+
         f = kornia.color.AdjustBrightness(factor)
         assert_allclose(f(data), expected)
 
-    def test_gradcheck(self):
+    def test_gradcheck(self, device):
         batch_size, channels, height, width = 2, 3, 4, 5
         img = torch.ones(batch_size, channels, height, width)
+        img = img.to(device)
         img = utils.tensor_to_gradcheck_var(img)  # to var
         assert gradcheck(kornia.adjust_brightness, (img, 2.),
                          raise_exception=True)
--- test/color/test_hsv.py
+++ test/color/test_hsv.py
@@ -2,7 +2,7 @@ import pytest
 
 import kornia
 import kornia.testing as utils  # test utils
-from test.common import device_type
+from test.common import device
 
 import torch
 from torch.autograd import gradcheck
@@ -11,7 +11,7 @@ from torch.testing import assert_allclose
 
 class TestRgbToHsv:
 
-    def test_rgb_to_hsv(self):
+    def test_rgb_to_hsv(self, device):
 
         data = torch.tensor([[[21., 22.],
                               [22., 22.]],
@@ -31,10 +31,13 @@ class TestRgbToHsv:
                                  [[21.0000 / 255, 22.0000 / 255],
                                   [22.0000 / 255, 22.0000 / 255]]])
 
+        data = data.to(device)
+        expected = expected.to(device)
+
         f = kornia.color.RgbToHsv()
         assert_allclose(f(data / 255), expected, atol=1e-4, rtol=1e-5)
 
-    def test_batch_rgb_to_hsv(self):
+    def test_batch_rgb_to_hsv(self, device):
 
         data = torch.tensor([[[21., 22.],
                               [22., 22.]],
@@ -53,12 +56,17 @@ class TestRgbToHsv:
 
                                  [[21.0000 / 255, 22.0000 / 255],
                                   [22.0000 / 255, 22.0000 / 255]]])  # 3x2x2
-        f = kornia.color.RgbToHsv()
+
+        data = data.to(device)
+        expected = expected.to(device)
+
         data = data.repeat(2, 1, 1, 1)  # 2x3x2x2
         expected = expected.repeat(2, 1, 1, 1)  # 2x3x2x2
+
+        f = kornia.color.RgbToHsv()
         assert_allclose(f(data / 255), expected, atol=1e-4, rtol=1e-5)
 
-    def test_gradcheck(self):
+    def test_gradcheck(self, device):
 
         data = torch.tensor([[[[21., 22.],
                                [22., 22.]],
@@ -69,13 +77,14 @@ class TestRgbToHsv:
                               [[8., 8.],
                                [8., 8.]]]])  # 3x2x2
 
+        data = data.to(device)
         data = utils.tensor_to_gradcheck_var(data)  # to var
 
         assert gradcheck(kornia.color.RgbToHsv(), (data,),
                          raise_exception=True)
 
     @pytest.mark.skip(reason="turn off all jit for a while")
-    def test_jit(self):
+    def test_jit(self, device):
         @torch.jit.script
         def op_script(data: torch.Tensor) -> torch.Tensor:
 
@@ -89,6 +98,7 @@ class TestRgbToHsv:
                                   [[8., 8.],
                                    [8., 8.]]]])  # 3x2x2
 
+            data = data.to(device)
             actual = op_script(data)
             expected = kornia.rgb_to_hsv(data)
             assert_allclose(actual, expected)
@@ -96,7 +106,7 @@ class TestRgbToHsv:
 
 class TestHsvToRgb:
 
-    def test_hsv_to_rgb(self):
+    def test_hsv_to_rgb(self, device):
 
         expected = torch.tensor([[[21., 22.],
                                   [22., 22.]],
@@ -116,10 +126,13 @@ class TestHsvToRgb:
                              [[21.0000 / 255, 22.0000 / 255],
                               [22.0000 / 255, 22.0000 / 255]]])
 
+        data = data.to(device)
+        expected = expected.to(device)
+
         f = kornia.color.HsvToRgb()
         assert_allclose(f(data), expected / 255, atol=1e-3, rtol=1e-3)
 
-    def test_batch_hsv_to_rgb(self):
+    def test_batch_hsv_to_rgb(self, device):
 
         expected = torch.tensor([[[21., 22.],
                                   [22., 22.]],
@@ -139,13 +152,17 @@ class TestHsvToRgb:
                              [[21.0000 / 255, 22.0000 / 255],
                               [22.0000 / 255, 22.0000 / 255]]])  # 3x2x2
 
-        f = kornia.color.HsvToRgb()
+        data = data.to(device)
+        expected = expected.to(device)
+
         data = data.repeat(2, 1, 1, 1)  # 2x3x2x2
         expected = expected.repeat(2, 1, 1, 1)  # 2x3x2x2
+
+        f = kornia.color.HsvToRgb()
         assert_allclose(f(data), expected / 255, atol=1e-3, rtol=1e-3)
 
     @pytest.mark.skip(reason="turn off all jit for a while")
-    def test_jit(self):
+    def test_jit(self, device):
         @torch.jit.script
         def op_script(data: torch.Tensor) -> torch.Tensor:
             return kornia.hsv_to_rgb(data)
@@ -159,6 +176,7 @@ class TestHsvToRgb:
                                   [[8., 8.],
                                    [8., 8.]]]])  # 3x2x2
 
+            data = data.to(device)
             actual = op_script(data)
             expected = kornia.hsv_to_rgb(data)
             assert_allclose(actual, expected)
