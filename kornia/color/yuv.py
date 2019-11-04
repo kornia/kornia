be2d82583147cfc2d074ff88440f42e5dd23ae66
ananyamukh6 <ananyamukh6@gmail.com>
2019-11-03 21:51:54 -0800

    Fix bug in concat axis for ndim=3

--- kornia/color/yuv.py
+++ kornia/color/yuv.py
@@ -50,7 +50,7 @@ def rgb_to_yuv(input: torch.Tensor):
     y = 0.299*r + 0.587*g + 0.114*b
     u = -0.14713*r -0.28886*g + 0.436*b
     v = 0.615*r -0.51499*g - 0.10001*b
-    return torch.cat((y,u,v), 1)
+    return torch.cat((y,u,v), -3)
 
 
 
@@ -98,4 +98,4 @@ def yuv_to_rgb(input: torch.Tensor):
     r = r + 1.14*b # coefficient for g is 0
     g = r + -0.396*g - 0.581*b
     b = r + 2.029*g # coefficient for b is 0
-    return torch.cat((r,g,b), 1)
+    return torch.cat((r,g,b), -3)
