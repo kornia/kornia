a5815529bec8f4e631242513fab2e80c06bc45c7
ananyamukh6 <ananyamukh6@gmail.com>
2019-11-10 10:00:42 -0800

    Add grad test

--- test/color/test_yuv.py
+++ test/color/test_yuv.py
@@ -20,3 +20,23 @@ class TestRgbYuvConversion:
 
         assert_allclose(input, yuv_to_rgb_converter(rgb_to_yuv_converter(input)), rtol=0.005, atol=0.005)
         assert_allclose(input, rgb_to_yuv_converter(yuv_to_rgb_converter(input)), rtol=0.005, atol=0.005)
+
+
+    def test_gradcheck(self):
+
+        # prepare input data
+        data = torch.tensor([[[0.1, 0.2],
+                              [0.1, 0.1]],
+
+                             [[0.2, 0.5],
+                              [0.4, 0.2]],
+
+                             [[0.3, 0.3],
+                              [0.5, 0.5]]])  # 3x2x2
+
+        data = utils.tensor_to_gradcheck_var(data)  # to var
+
+        assert gradcheck(kornia.color.YuvToRgb(), (data,),
+                         raise_exception=True)
+        assert gradcheck(kornia.color.RgbToYuv(), (data,),
+                         raise_exception=True)
