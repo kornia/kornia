8dd8dfdae4e84f533531e82cd67e41aed527bc1d
Matteo <matteo.rontauroli@outlook.it>
2019-11-12 20:46:30 +0100

    Fixed most of the raised issues

--- kornia/filters/motion.py
+++ kornia/filters/motion.py
@@ -3,15 +3,15 @@ from typing import Tuple
 import torch
 import torch.nn as nn
 
-from ..filters.kernels import get_motion_kernel2d
-from ..filters.filter import filter2D
+from kornia.filters.kernels import get_motion_kernel2d
+from kornia.filters.filter import filter2D
 
 
 class MotionBlur(nn.Module):
     r"""Blurs a tensor using the motion filter.
 
     Args:
-        kernel_size (Tuple[int, int]): the motion kernel size.
+        ksize (int): motion kernel width and height. It should be odd and positive.
         angle (float): angle of the motion blur in degrees (anti-clockwise rotation).
         direction (float): forward/backward direction of the motion blur.
             Lower values towards -1.0 will point the motion blur towards the back (with angle provided via angle),
@@ -28,26 +28,28 @@ class MotionBlur(nn.Module):
         - Input: :math:`(B, C, H, W)`
         - Output: :math:`(B, C, H, W)`
 
-    Example:
+    Examples::
         >>> input = torch.rand(2, 4, 5, 7)
-        >>> motion_blur = kornia.filters.MotionBlur((3, 3), 35., 0.5)
+        >>> motion_blur = kornia.filters.MotionBlur(3, 35., 0.5)
         >>> output = motion_blur(input)  # 2x4x5x7
     """
 
     def __init__(
-            self, kernel_size: Tuple[int, int], angle: float,
-            direction: float, border_type: str = 'constant'
+            self, ksize: int, angle: float, direction: float, border_type: str = 'constant'
     ) -> None:
         super(MotionBlur, self).__init__()
-        self.kernel_size: Tuple[int, int] = kernel_size
+        self.ksize = ksize
         self.angle: float = angle
         self.direction: float = direction
         assert border_type in ["constant", "reflect", "replicate", "circular"]
-        self.border_type = border_type
-        self.kernel = torch.unsqueeze(get_motion_kernel2d(kernel_size, angle, direction), dim=0)
+        self.border_type: str = border_type
+        self.kernel: torch.Tensor = torch.unsqueeze(
+            get_motion_kernel2d(self.ksize, self.angle, self.direction),
+            dim=0
+        )
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__} (kernel_size={self.kernel_size}, ' \
+        return f'{self.__class__.__name__} (ksize={self.ksize}, ' \
                f'angle={self.angle}, direction={self.direction})'
 
     def forward(self, x: torch.Tensor):  # type: ignore
@@ -60,7 +62,7 @@ class MotionBlur(nn.Module):
 
 def motion_blur(
     input: torch.Tensor,
-    kernel_size: Tuple[int, int],
+    ksize: int,
     angle: float,
     direction: float,
     border_type: str = 'constant'
@@ -70,4 +72,4 @@ def motion_blur(
 
     See :class:`~kornia.filters.MotionBlur` for details.
     """
-    return MotionBlur(kernel_size, angle, direction, border_type)(input)
+    return MotionBlur(ksize, angle, direction, border_type)(input)
--- test/filters/test_motion.py
+++ test/filters/test_motion.py
@@ -14,21 +14,20 @@ from torch.testing import assert_allclose
 @pytest.mark.parametrize("angle", [0., 360.])
 @pytest.mark.parametrize("direction", [-1., 1.])
 def test_get_motion_kernel2d(ksize, angle, direction):
-    kernel = kornia.get_motion_kernel2d((ksize, ksize), angle, direction)
+    kernel = kornia.get_motion_kernel2d(ksize, angle, direction)
     assert kernel.shape == (ksize, ksize)
-    assert kernel.sum().item() == pytest.approx(1.0)
+    assert_allclose(kernel.sum(), 1.)
 
 
 class TestMotionBlur:
     @pytest.mark.parametrize("batch_shape", [(1, 4, 8, 15), (2, 3, 11, 7)])
-    @pytest.mark.parametrize("device_type", ("cpu", "cuda"))
     def test_motion_blur(self, batch_shape, device_type):
         ksize = 5
         angle = 200.
         direction = 0.3
 
         input = torch.rand(batch_shape).to(torch.device(device_type))
-        motion = kornia.filters.MotionBlur((ksize, ksize), angle, direction)
+        motion = kornia.filters.MotionBlur(ksize, angle, direction)
         assert motion(input).shape == batch_shape
 
     def test_gradcheck(self):
@@ -41,7 +40,7 @@ class TestMotionBlur:
         input = utils.tensor_to_gradcheck_var(input)
         assert gradcheck(
             kornia.motion_blur,
-            (input, (ksize, ksize), angle, direction, "replicate"),
+            (input, ksize, angle, direction, "replicate"),
             raise_exception=True,
         )
 
@@ -51,16 +50,16 @@ class TestMotionBlur:
         @torch.jit.script
         def op_script(
             input: torch.Tensor,
-            kernel_size: Tuple[int, int],
+            ksize: int,
             angle: float,
             direction: float
         ) -> torch.Tensor:
-            return kornia.filters.motion_blur(input, kernel_size, angle, direction)
+            return kornia.filters.motion_blur(input, ksize, angle, direction)
 
         img = torch.rand(2, 3, 4, 5)
-        kernel_size = (5, 5)
+        ksize = 5
         angle = 65.
         direction = .1
-        actual = op_script(img, kernel_size, angle, direction)
-        expected = kornia.filters.motion_blur(img, kernel_size, angle, direction)
+        actual = op_script(img, ksize, angle, direction)
+        expected = kornia.filters.motion_blur(img, ksize, angle, direction)
         assert_allclose(actual, expected)
