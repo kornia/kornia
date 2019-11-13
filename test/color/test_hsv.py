5e2f713a0601c85bb68723cd9e52a0884f517879
Pau Riba <priba@cvc.uab.cat>
2019-11-13 09:20:49 +0100

    Test color transforms against opencv

--- test/color/test_hsv.py
+++ test/color/test_hsv.py
@@ -3,8 +3,11 @@ import pytest
 import kornia
 import kornia.testing as utils  # test utils
 from test.common import device_type
+from kornia.geometry import pi
 
 import cv2
+import numpy as np
+import math
 
 import torch
 from torch.autograd import gradcheck
@@ -21,7 +24,7 @@ class TestRgbToHsv:
         data_cv = data.numpy().transpose(1, 2, 0)
         expected = cv2.cvtColor(data_cv, cv2.COLOR_RGB2HSV)
 
-        h_expected = expected[:,:,0]/360.
+        h_expected = 2*math.pi*expected[:,:,0]/360.
         s_expected = expected[:,:,1]
         v_expected = expected[:,:,2]
 
@@ -37,29 +40,26 @@ class TestRgbToHsv:
         assert_allclose(s, s_expected)
         assert_allclose(v, v_expected)
 
-    def test_batch_rgb_to_hsv(self):
+    def test_batch_rgb_to_hls(self):
 
-        data = torch.tensor([[[21., 22.],
-                              [22., 22.]],
+        data = torch.rand(3,5,5)
 
-                             [[13., 14.],
-                              [14., 14.]],
+        # OpenCV
+        data_cv = data.numpy().transpose(1, 2, 0)
+        expected = cv2.cvtColor(data_cv, cv2.COLOR_RGB2HSV)
 
-                             [[8., 8.],
-                              [8., 8.]]])  # 3x2x2
+        expected[:,:,0] = 2*math.pi*expected[:,:,0]/360.
+        expected = expected.transpose(2,0,1)
 
-        expected = torch.tensor([[[0.0641, 0.0714],
-                                  [0.0714, 0.0714]],
-
-                                 [[0.6190, 0.6364],
-                                  [0.6364, 0.6364]],
-
-                                 [[21.0000 / 255, 22.0000 / 255],
-                                  [22.0000 / 255, 22.0000 / 255]]])  # 3x2x2
+        # Kornia
         f = kornia.color.RgbToHsv()
+
         data = data.repeat(2, 1, 1, 1)  # 2x3x2x2
-        expected = expected.repeat(2, 1, 1, 1)  # 2x3x2x2
-        assert_allclose(f(data / 255), expected, atol=1e-4, rtol=1e-5)
+
+        expected = np.expand_dims(expected,0)
+        expected = expected.repeat(2, 0)  # 2x3x2x2
+
+        assert_allclose(f(data), expected)
 
     def test_gradcheck(self):
 
@@ -100,27 +100,51 @@ class TestRgbToHsv:
 class TestHsvToRgb:
 
     def test_hsv_to_rgb(self):
+        np.random.seed(0)
+        image = np.random.random([1000, 1000, 3]).astype(np.float32)
+        image[..., 0] *= 360
 
-        expected = torch.tensor([[[21., 22.],
-                                  [22., 22.]],
+        cv_img = cv2.cvtColor(image, cv2.COLOR_HLS2RGB)
 
-                                 [[13., 14.],
-                                  [14., 14.]],
-
-                                 [[8., 8.],
-                                  [8., 8.]]])
-
-        data = torch.tensor([[[0.0641, 0.0714],
-                              [0.0714, 0.0714]],
-
-                             [[0.6190, 0.6364],
-                              [0.6364, 0.6364]],
-
-                             [[21.0000 / 255, 22.0000 / 255],
-                              [22.0000 / 255, 22.0000 / 255]]])
-
+        k_img = image.copy()
+        k_img[..., 0] /= 360
+        k_img = torch.from_numpy(image.transpose([2, 0, 1]))
         f = kornia.color.HsvToRgb()
+        k_img = f(k_img)
+        k_img = k_img.numpy().transpose([1, 2, 0])
+        import pdb; pdb.set_trace()
+
+        print(np.abs(cv_img - k_img).max())  # 0.9972929
+        assert np.allclose(cv_img, k_img)
+
         assert_allclose(f(data), expected / 255, atol=1e-3, rtol=1e-3)
+
+    def test_hsv_to_rgb(self):
+
+        data = torch.rand(3,5,5)
+        #data[2] = 2*pi*data[2]
+
+        # OpenCV
+        data_cv = data.numpy().transpose(1, 2, 0)
+        data_cv[2] = 360*data_cv[2] #/(2*math.pi)
+        expected = cv2.cvtColor(data_cv, cv2.COLOR_HSV2RGB)
+
+        r_expected = expected[:,:,0]
+        g_expected = expected[:,:,1]
+        b_expected = expected[:,:,2]
+
+        # Kornia
+        f = kornia.color.HsvToRgb()
+        result = f(data)
+
+        r = result[0,:,:]
+        g = result[1,:,:]
+        b = result[2,:,:]
+
+        import pdb; pdb.set_trace()
+        assert_allclose(r, r_expected)
+        assert_allclose(g, g_expected)
+        assert_allclose(b, b_expected)
 
     def test_batch_hsv_to_rgb(self):
 
