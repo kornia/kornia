c7a99e820421aee61b8ca73dc8e7a0d4694fe424
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-12-13 17:59:46 +0100

    added hardnms3d and tests for hardnms 2d

--- kornia/feature/nms.py
+++ kornia/feature/nms.py
@@ -40,6 +40,35 @@ class NonMaximaSuppression2d(nn.Module):
         return x * x_mask  # return original masked by local max
 
 
+class NonMaximaSuppression3d(nn.Module):
+    r"""Applies non maxima suppression to filter.
+    """
+    def __init__(self, kernel_size: Tuple[int, int, int]):
+        super(NonMaximaSuppression3d, self).__init__()
+        self.kernel_size: Tuple[int, int, int] = kernel_size
+        self.padding: Tuple[int,
+                            int,
+                            int] = self._compute_zero_padding3d(kernel_size)
+        self.max_pool3d = nn.MaxPool3d(kernel_size, stride=1,
+                                       padding=self.padding)
+
+    @staticmethod
+    def _compute_zero_padding3d(
+            kernel_size: Tuple[int, int, int]) -> Tuple[int, int, int]:
+        assert isinstance(kernel_size, tuple), type(kernel_size)
+        assert len(kernel_size) == 3, kernel_size
+
+        def pad(x):
+            return (x - 1) // 2  # zero padding function
+
+        kd, ky, kx = kernel_size     # we assume a cubic kernel
+        return pad(kd), pad(ky), pad(kx)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:  # type: ignore
+        assert len(x.shape) == 5, x.shape
+        # find local maximum values
+        return x * (x == self.max_pool3d(x)).to(x.dtype)
+
 # functiona api
 
 
@@ -50,3 +79,12 @@ def non_maxima_suppression2d(
     See :class:`~kornia.feature.NonMaximaSuppression2d` for details.
     """
     return NonMaximaSuppression2d(kernel_size)(input)
+
+
+def non_maxima_suppression3d(
+        input: torch.Tensor, kernel_size: Tuple[int, int, int]) -> torch.Tensor:
+    r"""Applies non maxima suppression to filter.
+
+    See :class:`~kornia.feature.NonMaximaSuppression3d` for details.
+    """
+    return NonMaximaSuppression3d(kernel_size)(input)
