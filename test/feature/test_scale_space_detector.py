94e912653af474e74492c40e20bd5f19771a8b93
Dmytro Mishkin <ducha.aiki@gmail.com>
2019-12-14 01:11:22 +0100

    fix1

--- test/feature/test_scale_space_detector.py
+++ test/feature/test_scale_space_detector.py
@@ -43,32 +43,6 @@ class TestScaleSpaceDetector:
         assert_allclose(expected_laf, lafs, rtol=0.0001, atol=1e-04)
         assert_allclose(expected_resp, resps, rtol=0.0001, atol=1e-04)
 
-    def test_toy_strict_maxima(self, device):
-        inp = torch.zeros(1, 1, 32, 32, device=device)
-        inp[:, :, 8:-8, 8:-8] = 1.0
-        n_feats = 4
-        nms = kornia.geometry.ConvSoftArgmax3d(kernel_size=(3, 3, 3),  # nms windows size (scale, height, width)
-                                               stride=(1, 1, 1),  # stride (scale, height, width)
-                                               padding=(1, 1, 1),
-                                               temperature=0.1,
-                                               strict_maxima_bonus=1.)
-        det = ScaleSpaceDetector(n_feats,
-                                 resp_module=kornia.feature.CornerHarris(0.04),
-                                 nms_module=nms,
-                                 mr_size=1.0).to(device)
-        lafs, resps = det(inp)
-        expected_laf = torch.tensor([[[[3.4559, 0.0000, 9.9992],
-                                      [-0.0000, 3.4559, 20.0008]],
-                                     [[3.4559, 0.0000, 9.9992],
-                                      [-0.0000, 3.4559, 9.9992]],
-                                     [[3.4559, 0.0000, 20.0008],
-                                      [-0.0000, 3.4559, 9.9992]],
-                                     [[3.4559, 0.0000, 20.0008],
-                                      [-0.0000, 3.4559, 20.0008]]]], device=device)
-        expected_resp = torch.tensor([[0.0007, 0.0007, 0.0007, 0.0007]], device=device)
-        assert_allclose(expected_laf, lafs)
-        assert_allclose(expected_resp, resps, atol=1e-04, rtol=1e-4)
-
     def test_gradcheck(self, device):
         batch_size, channels, height, width = 1, 1, 31, 21
         patches = torch.rand(batch_size, channels, height, width, device=device)
