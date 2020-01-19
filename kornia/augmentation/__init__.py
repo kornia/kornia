d0a81f9a9eee5698b9b7ea37476f604841272191
edgarriba <edgar.riba@gmail.com>
2020-01-19 16:05:33 +0100

    implement random_affine

--- kornia/augmentation/__init__.py
+++ kornia/augmentation/__init__.py
@@ -8,6 +8,7 @@ __all__ = [
     "random_grayscale",
     "random_rectangle_erase",
     "random_perspective",
+    "random_affine",
     "perspective",
     "color_jitter",
     "RandomHorizontalFlip",
--- kornia/augmentation/perspective.py
+++ kornia/augmentation/perspective.py
@@ -1,19 +1,68 @@
-from typing import Union, Tuple, cast
+from typing import Union, Tuple, cast, Optional, List
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 import kornia
 
 
 UnionType = Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]
 
+TupleInt = Tuple[int, int]
+TupleFloat = Tuple[float, float]
+UnionFloat = Union[float, TupleFloat]
 
-def _get_perspective_params(
-    batch_size: int,
-    width: int, height: int,
-    distortion_scale: float
-) -> Tuple[torch.Tensor, torch.Tensor]:
+
+def _get_affine_params(degrees: TupleFloat, translate: Optional[TupleFloat],
+                       scales: Optional[TupleFloat], shears: Optional[TupleFloat],
+                       img_size: TupleInt, batch_size: int) -> torch.Tensor:
+    r"""Get parameters for affine transformation. The returned matrix is Bx3x3.
+
+    Returns:
+        torch.Tensor: params to be passed to the affine transformation.
+    """
+    angle = torch.empty(batch_size).uniform_(degrees[0], degrees[1])
+
+    # compute tensor ranges
+    if scales is not None:
+        scale = torch.empty(batch_size).uniform_(scales[0], scales[1])
+    else:
+        scale = torch.ones(batch_size)
+
+    if shears is not None:
+        shear = torch.empty(batch_size).uniform_(shears[0], shears[1])
+    else:
+        shear = torch.zeros(batch_size)
+
+    height, width = img_size
+    if translate is not None:
+        max_dx: float = translate[0] * width
+        max_dy: float = translate[1] * height
+        translations = torch.cat([
+            torch.empty(batch_size).uniform_(-max_dx, max_dx),
+            torch.empty(batch_size).uniform_(-max_dy, max_dy),
+        ], dim=-1)
+    else:
+        translations = torch.zeros(batch_size, 2)
+
+    center: torch.Tensor = torch.tensor(
+        [width, height], dtype=torch.float32).view(1, 2) / 2
+
+    # concatenate transforms
+    transform: torch.Tensor = kornia.get_rotation_matrix2d(center, angle, scale)
+    transform[..., 2] += translations  # tx/ty
+    transform[..., 0, 1] += shear
+    transform[..., 1, 0] += shear
+
+    # pad transform to get Bx3x3
+    transform_h = F.pad(transform, [0, 0, 0, 1], value=0.)
+    transform_h[..., -1, -1] += 1.0
+    return transform_h
+
+
+def _get_perspective_params(batch_size: int, width: int, height: int,
+                            distortion_scale: float) -> Tuple[torch.Tensor, torch.Tensor]:
     r"""Get parameters for ``perspective`` for a random perspective transform.
 
     Args:
@@ -147,6 +196,100 @@ def random_perspective(input: torch.Tensor,
     return out_data
 
 
+def random_affine(input: torch.Tensor,
+                  degrees: UnionFloat,
+                  translate: Optional[TupleFloat] = None,
+                  scale: Optional[TupleFloat] = None,
+                  shear: Optional[UnionFloat] = None,
+                  return_transform: bool = False,
+                  mode: str = 'bilinear',
+                  padding_mode: str = 'zeros') -> UnionType:
+    r"""Random affine transformation of the image keeping center invariant
+
+        Args:
+            input (torch.Tensor): Tensor to be transformed with shape (*, C, H, W).
+            degrees (float or tuple): Range of degrees to select from.
+                If degrees is a number instead of sequence like (min, max), the range of degrees
+                will be (-degrees, +degrees). Set to 0 to deactivate rotations.
+            translate (tuple, optional): tuple of maximum absolute fraction for horizontal
+                and vertical translations. For example translate=(a, b), then horizontal shift
+                is randomly sampled in the range -img_width * a < dx < img_width * a and vertical shift is
+                randomly sampled in the range -img_height * b < dy < img_height * b. Will not translate by default.
+            scale (tuple, optional): scaling factor interval, e.g (a, b), then scale is
+                randomly sampled from the range a <= scale <= b. Will keep original scale by default.
+            shear (sequence or float, optional): Range of degrees to select from.
+                If shear is a number, a shear parallel to the x axis in the range (-shear, +shear)
+                will be apllied. Else if shear is a tuple or list of 2 values a shear parallel to the x axis in the
+                range (shear[0], shear[1]) will be applied. Else if shear is a tuple or list of 4 values,
+                a x-axis shear in (shear[0], shear[1]) and y-axis shear in (shear[2], shear[3]) will be applied.
+                Will not apply shear by default
+            return_transform (bool): if ``True`` return the matrix describing the transformation
+                applied to each. Default: False.
+            mode (str): interpolation mode to calculate output values
+                'bilinear' | 'nearest'. Default: 'bilinear'.
+            padding_mode (str): padding mode for outside grid values
+                'zeros' | 'border' | 'reflection'. Default: 'zeros'.
+    """
+    if not torch.is_tensor(input):
+        raise TypeError(f"Input type is not a torch.Tensor. Got {type(input)}")
+
+    # check angle ranges
+    degrees_tmp: TupleFloat
+    if isinstance(degrees, float):
+        if degrees < 0.:
+            raise ValueError("If degrees is a single number, it must be positive.")
+        degrees_tmp = (-degrees, degrees)
+    else:
+        assert isinstance(degrees, (tuple, list)) and len(degrees) == 2, \
+            "degrees should be a list or tuple and it must be of length 2."
+        degrees_tmp = degrees
+
+    # check translation range
+    if translate is not None:
+        assert isinstance(translate, (tuple, list)) and len(translate) == 2, \
+            "translate should be a list or tuple and it must be of length 2."
+        for t in translate:
+            if not (0.0 <= t <= 1.0):
+                raise ValueError("translation values should be between 0 and 1")
+
+    # check scale range
+    if scale is not None:
+        assert isinstance(scale, (tuple, list)) and len(scale) == 2, \
+            "scale should be a list or tuple and it must be of length 2."
+        for s in scale:
+            if s <= 0:
+                raise ValueError("scale values should be positive")
+
+    # check shear range
+    shear_tmp: Optional[TupleFloat]
+    if shear is not None:
+        if isinstance(shear, float):
+            if shear < 0:
+                raise ValueError("If shear is a single number, it must be positive.")
+            shear_tmp = (-shear, shear)
+        else:
+            assert isinstance(shear, (tuple, list)) and len(shear) == 2, \
+                "shear should be a list or tuple and it must be of length 2."
+            shear_tmp = shear
+    else:
+        shear_tmp = shear
+
+    # compute affine parameters and apply transform
+    batch_size, _, height, width = input.shape
+
+    transform: torch.Tensor = _get_affine_params(
+        degrees_tmp, translate, scale, shear_tmp, (height, width), batch_size)
+    transform = transform.to(input.device, input.dtype)
+
+    out_affine: torch.Tensor = kornia.warp_affine(
+        input, transform[:, :2], (height, width), mode, padding_mode)
+
+    if return_transform:
+        return out_affine, transform
+
+    return out_affine
+
+
 class RandomPerspective(nn.Module):
     r"""Performs Perspective transformation of the given torch.Tensor randomly with a given probability.
 
@@ -157,6 +300,7 @@ class RandomPerspective(nn.Module):
         applied to each. Default: False.
         input tensor.
     """
+
     def __init__(self, distortion_scale: float = 0.5, p: float = 0.5, return_transform: bool = False) -> None:
         super(RandomPerspective, self).__init__()
         self.p: float = p
@@ -171,6 +315,58 @@ class RandomPerspective(nn.Module):
 
         if isinstance(input, tuple):
             raise NotImplementedError("wait for AugmentationBase class")
-        return random_perspective(
-            input, distortion_scale=self.distortion_scale, p=self.p,
-            return_transform=self.return_transform)
+
+        return random_perspective(input, self.distortion_scale, self.p, self.return_transform)
+
+
+class RandomAffine(nn.Module):
+    r"""Random affine transformation of the image keeping center invariant.
+
+        Args:
+            degrees (float or tuple): Range of degrees to select from.
+                If degrees is a number instead of sequence like (min, max), the range of degrees
+                will be (-degrees, +degrees). Set to 0 to deactivate rotations.
+            translate (tuple, optional): tuple of maximum absolute fraction for horizontal
+                and vertical translations. For example translate=(a, b), then horizontal shift
+                is randomly sampled in the range -img_width * a < dx < img_width * a and vertical shift is
+                randomly sampled in the range -img_height * b < dy < img_height * b. Will not translate by default.
+            scale (tuple, optional): scaling factor interval, e.g (a, b), then scale is
+                randomly sampled from the range a <= scale <= b. Will keep original scale by default.
+            shear (sequence or float, optional): Range of degrees to select from.
+                If shear is a number, a shear parallel to the x axis in the range (-shear, +shear)
+                will be apllied. Else if shear is a tuple or list of 2 values a shear parallel to the x axis in the
+                range (shear[0], shear[1]) will be applied. Else if shear is a tuple or list of 4 values,
+                a x-axis shear in (shear[0], shear[1]) and y-axis shear in (shear[2], shear[3]) will be applied.
+                Will not apply shear by default
+            return_transform (bool): if ``True`` return the matrix describing the transformation
+                applied to each. Default: False.
+            mode (str): interpolation mode to calculate output values
+                'bilinear' | 'nearest'. Default: 'bilinear'.
+            padding_mode (str): padding mode for outside grid values
+                'zeros' | 'border' | 'reflection'. Default: 'zeros'.
+    """
+
+    def __init__(self,
+                 degrees: UnionFloat,
+                 translate: Optional[TupleFloat] = None,
+                 scale: Optional[TupleFloat] = None,
+                 shear: Optional[UnionFloat] = None,
+                 return_transform: bool = False,
+                 mode: str = 'bilinear',
+                 padding_mode: str = 'zeros') -> None:
+        super(RandomAffine, self).__init__()
+        self.degrees = degrees
+        self.translate = translate
+        self.scale = scale
+        self.shear = shear
+        self.return_transform = return_transform
+        self.mode = mode
+        self.padding_mode = padding_mode
+
+    def forward(self, input: UnionType) -> UnionType:  # type: ignore
+
+        if isinstance(input, tuple):
+            raise NotImplementedError("wait for AugmentationBase class")
+
+        return random_affine(input, self.degrees, self.translate, self.scale, self.shear,
+                             self.return_transform, self.mode, self.padding_mode)
