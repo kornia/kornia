50f1e34fe708c7a4cba8da0122a9c334f221af5c
Jian S <sj8716643@126.com>
2020-01-19 22:36:40 +0800

    Added tests for functional.py

--- test/augmentation/test_param_gen.py
+++ test/augmentation/test_param_gen.py
@@ -10,10 +10,10 @@ class TestRandomProbGen:
         batch_size = 8
 
         halfs = _random_prob_gen(batch_size=batch_size, p=.5)
-        expected_halfs = [False, False,  True, False,  True,  False,  True, False]
+        expected_halfs = [False, False, True, False, True, False, True, False]
         zeros = _random_prob_gen(batch_size=batch_size, p=0.)['batch_prob']
         ones = _random_prob_gen(batch_size=batch_size, p=1.)['batch_prob']
-        
+
         assert list(halfs.keys()) == ['batch_prob'], "Redundant keys found apart from `batch_prob`"
         assert (halfs['batch_prob'] == torch.tensor(expected_halfs)).long().sum() == batch_size
         assert (zeros == torch.tensor([False] * batch_size)).long().sum() == batch_size
@@ -24,7 +24,7 @@ class TestRandomProbGen:
         batch_size = 8
         jitter_params = _random_color_jitter_gen(batch_size, brightness=0.2, contrast=0.3, saturation=0.4, hue=0.1)
         jitter_params_tuple = _random_color_jitter_gen(batch_size, brightness=(-0.2, 0.2),
-            contrast=(0.7, 1.3), saturation=(0.6, 1.4), hue=(-0.1, 0.1))
+                                                       contrast=(0.7, 1.3), saturation=(0.6, 1.4), hue=(-0.1, 0.1))
         expected_jitter_params = {
             'brightness_factor': torch.tensor([
                 0.15290771424770355, 0.1660015732049942, -0.046854496002197266, 0.18372227251529694,
@@ -54,13 +54,22 @@ class TestRandomProbGen:
                 1.025185465812683, 0.7269839644432068, 1.1233408451080322, 0.862247109413147])
         }
 
-        assert set(list(jitter_params.keys())) == set(['brightness_factor', 'contrast_factor', 'hue_factor', 'saturation_factor']), \
+        assert set(list(jitter_params.keys())) == set(list(jitter_params_tuple.keys())) == \
+            set(['brightness_factor', 'contrast_factor', 'hue_factor', 'saturation_factor']), \
             "Redundant keys found apart from 'brightness_factor', 'contrast_factor', 'hue_factor', 'saturation_factor'"
-        assert (jitter_params['brightness_factor'] == expected_jitter_params['brightness_factor']).long().sum() == batch_size
-        assert (jitter_params['contrast_factor'] == expected_jitter_params['contrast_factor']).long().sum() == batch_size
-        assert (jitter_params['hue_factor'] == expected_jitter_params['hue_factor']).long().sum() == batch_size
-        assert (jitter_params['saturation_factor'] == expected_jitter_params['saturation_factor']).long().sum() == batch_size
-        assert (jitter_params_tuple['brightness_factor'] == expected_jitter_params_tuple['brightness_factor']).long().sum() == batch_size
-        assert (jitter_params_tuple['contrast_factor'] == expected_jitter_params_tuple['contrast_factor']).long().sum() == batch_size
-        assert (jitter_params_tuple['hue_factor'] == expected_jitter_params_tuple['hue_factor']).long().sum() == batch_size
-        assert (jitter_params_tuple['saturation_factor'] == expected_jitter_params_tuple['saturation_factor']).long().sum() == batch_size
+        assert (jitter_params['brightness_factor'] == expected_jitter_params['brightness_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params['contrast_factor'] == expected_jitter_params['contrast_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params['hue_factor'] == expected_jitter_params['hue_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params['saturation_factor'] == expected_jitter_params['saturation_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params_tuple['brightness_factor'] == expected_jitter_params_tuple['brightness_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params_tuple['contrast_factor'] == expected_jitter_params_tuple['contrast_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params_tuple['hue_factor'] == expected_jitter_params_tuple['hue_factor']) \
+            .long().sum() == batch_size
+        assert (jitter_params_tuple['saturation_factor'] == expected_jitter_params_tuple['saturation_factor']) \
+            .long().sum() == batch_size
