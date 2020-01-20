89485e6e95233f9b5ac815d79acd445a59f525c5
Jian S <sj8716643@126.com>
2020-01-20 09:38:05 +0800

    Updated typings & code cleaning

--- kornia/augmentation/__init__.py
+++ kornia/augmentation/__init__.py
@@ -4,17 +4,13 @@ from kornia.augmentation.random_erasing import *
 
 __all__ = [
     "random_hflip",
-    # "apply_hflip",
     "RandomHorizontalFlip",
     "random_vflip",
-    # "apply_vflip",
     "RandomVerticalFlip",
     "color_jitter",
-    # "apply_color_jitter",
     "ColorJitter",
     "random_rectangle_erase",
     "RandomRectangleErasing",
     "random_grayscale",
-    # "apply_grayscale",
     "RandomGrayscale",
 ]
