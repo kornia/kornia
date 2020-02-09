a5e10a96f1d842e1afe2beb1435b691d375fcbec
Diego <dmarez8ece@gmail.com>
2020-02-08 16:39:43 -0800

    add gamma nonlinearity in luv

--- kornia/color/luv.py
+++ kornia/color/luv.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import torch
 import torch.nn as nn
 
@@ -7,7 +8,8 @@ from .xyz import rgb_to_xyz, xyz_to_rgb
 class RgbToLuv(nn.Module):
     r"""Converts an image from RGB to Luv
 
-    The image data is assumed to be in the range of :math:`[0, 1]`.
+    The image data is assumed to be in the range of :math:`[0, 1]`. Luv
+    color is computed using the D65 white point.
 
     args:
         image (torch.Tensor): RGB image to be converted to Luv.
@@ -22,6 +24,7 @@ class RgbToLuv(nn.Module):
         >>> output = luv(input)  # 2x3x4x5
     Reference:
         [1] https://docs.opencv.org/4.0.1/de/d25/imgproc_color_conversions.html
+        [2] https://www.easyrgb.com/en/math.php
     """
 
     def __init__(self) -> None:
@@ -80,23 +83,38 @@ def rgb_to_luv(image: torch.Tensor) -> torch.Tensor:
         raise ValueError("Input size must have a shape of (*, 3, H, W). Got {}"
                          .format(image.shape))
 
-    xyz_im: torch.Tensor = rgb_to_xyz(image)
+    r: torch.Tensor = image[...,0,:,:]
+    g: torch.Tensor = image[...,1,:,:]
+    b: torch.Tensor = image[...,2,:,:]
+
+    rs: torch.Tensor = torch.where(r > 0.04045, torch.pow(( ( r + 0.055 ) / 1.055 ), 2.4),  r / 12.92)
+    gs: torch.Tensor = torch.where(g > 0.04045, torch.pow(( ( g + 0.055 ) / 1.055 ), 2.4),  g / 12.92)
+    bs: torch.Tensor = torch.where(b > 0.04045, torch.pow(( ( b + 0.055 ) / 1.055 ), 2.4),  b / 12.92)
+
+    image_s = torch.stack((rs, gs, bs), dim = -3)
+
+
+    xyz_im: torch.Tensor = rgb_to_xyz(image_s)
 
     x: torch.Tensor = xyz_im[..., 0, :, :]
-    y: torch.Tensor = xyz_im[..., 1, :, :]
+    y: torch.Tensor = xyz_im[..., 1, :, :] 
     z: torch.Tensor = xyz_im[..., 2, :, :]
 
-    L: torch.Tensor = torch.where(y > 0.008856,
-                                  116 * torch.pow(y, 1 / 3) - 16,
+    L: torch.Tensor = torch.where(torch.gt(y,0.008856),
+                                  116. * torch.pow(y, 1. / 3.) - 16.,
                                   903.3 * y)
 
-    eps: float = torch.finfo(torch.float32).eps
 
+    eps: float = torch.finfo(torch.float64).eps 
+
+    xyz_ref_white: Tuple[float,float,float] = (.95047, 1., 1.08883)
+    u_w: float = (4 * xyz_ref_white[0])/(xyz_ref_white[0] + 15 * xyz_ref_white[1] + 3 * xyz_ref_white[2])
+    v_w: float = (9 * xyz_ref_white[1])/(xyz_ref_white[0] + 15 * xyz_ref_white[1] + 3 * xyz_ref_white[2])
     u_p: torch.Tensor = (4 * x) / (x + 15 * y + 3 * z + eps)
     v_p: torch.Tensor = (9 * y) / (x + 15 * y + 3 * z + eps)
 
-    u: torch.Tensor = 13 * L * (u_p - 0.19793943)
-    v: torch.Tensor = 13 * L * (v_p - 0.46831096)
+    u: torch.Tensor = 13 * L * (u_p - u_w)
+    v: torch.Tensor = 13 * L * (v_p - v_w)
 
     out = torch.stack((L, u, v), dim=-3)
 
@@ -118,20 +136,35 @@ def luv_to_rgb(image: torch.Tensor) -> torch.Tensor:
     u: torch.Tensor = image[..., 1, :, :]
     v: torch.Tensor = image[..., 2, :, :]
 
-    y: torch.Tensor = torch.where(L > 7.99961,
+    y: torch.Tensor = torch.where(L >  7.999608396531903,
                                   torch.pow((L + 16) / 116, 3.0),
                                   L / 903.3)
 
-    eps = torch.finfo(torch.float32).eps
+    xyz_ref_white: Tuple[float,float,float] = (0.95047, 1., 1.08883)
+    u_w: float = (4 * xyz_ref_white[0])/(xyz_ref_white[0] + 15 * xyz_ref_white[1] + 3 * xyz_ref_white[2])
+    v_w: float = (9 * xyz_ref_white[1])/(xyz_ref_white[0] + 15 * xyz_ref_white[1] + 3 * xyz_ref_white[2])
 
-    u_p: torch.Tensor = (u / (13. * L + eps)) + 0.19793943
-    v_p: torch.Tensor = (v / (13. * L + eps)) + 0.46831096
+    eps: float = torch.finfo(torch.float64).eps 
 
-    x: torch.Tensor = y * (9. * u_p) / (4. * v_p + eps)
-    z: torch.Tensor = y * ((-3. * u_p - 20. * v_p + 12) / (4. * v_p + eps))
+    a: torch.Tensor = u_w + u/(13 * L + eps)
+    b: torch.Tensor = v_w + v/(13 * L + eps)
+    c: torch.Tensor = 3 * y * (5 * b - 3)
+
+    z: torch.Tensor = ((a - 4) * c - 15 * a * b * y) / (12 * b)
+    x: torch.Tensor = -(c / b + 3. * z)
 
     xyz_im: torch.Tensor = torch.stack((x, y, z), -3)
 
-    rgb_im: torch.Tensor = rgb_to_xyz(xyz_im)
+    rgbs_im: torch.Tensor = xyz_to_rgb(xyz_im)
+
+    rs: torch.Tensor = rgbs_im[...,0,:,:]
+    gs: torch.Tensor = rgbs_im[...,1,:,:]
+    bs: torch.Tensor = rgbs_im[...,2,:,:]
+
+    r: torch.Tensor = torch.where(rs > 0.0031308, 1.055 * torch.pow(rs, 1/2.4) - 0.055,  12.92 * rs)
+    g: torch.Tensor = torch.where(gs > 0.0031308, 1.055 * torch.pow(gs, 1/2.4) - 0.055,  12.92 * gs)
+    b: torch.Tensor = torch.where(bs > 0.0031308, 1.055 * torch.pow(bs, 1/2.4) - 0.055,  12.92 * bs)
+
+    rgb_im: torch.Tensor = torch.stack((r,g,b), dim = -3)
 
     return rgb_im
