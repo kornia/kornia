dcf20398c98f1382dd0b2ed9c86504114be11b73
Xenofon Karagiannis <akis.karagian@gmail.com>
2020-04-01 23:58:43 +0300

    fixing float, tensor compatibility

--- kornia/losses/focal.py
+++ kornia/losses/focal.py
@@ -152,8 +152,9 @@ def binary_focal_loss_with_logits(
                 inp.device, target.device))
 
     probs = torch.sigmoid(inp)
-    loss_tmp = -alpha * torch.pow((1. - probs), gamma) * target * torch.log(probs + torch.tensor(eps)) \
-               - (1 - alpha) * torch.pow(probs, gamma) * (1. - target) * torch.log(1. - (probs + torch.tensor(eps)))
+    loss_tmp = -alpha * torch.pow((torch.tensor(1.) - probs), gamma) * target * torch.log(probs + torch.tensor(eps)) \
+               - (1 - alpha) * torch.pow(probs, gamma) * (torch.tensor(1.) - target) * \
+               torch.log(torch.tensor(1.) - (probs + torch.tensor(eps)))
 
     if reduction == 'none':
         loss = loss_tmp
