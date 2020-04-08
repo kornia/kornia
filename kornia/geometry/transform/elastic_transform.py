04120b1147b25de15abd4ecdb3f601c29be35224
IssamLaradji <issam.laradji@gmail.com>
2020-04-08 13:28:35 +0000

    updated elastic transform

--- kornia/geometry/transform/elastic_transform.py
+++ kornia/geometry/transform/elastic_transform.py
@@ -1,7 +1,4 @@
-"""Elastic Transform
-"""
-
-# Authors: Issam H. Laradji <issam.laradji@gmail.com>
+from typing import Tuple, Optional
 
 import torch 
 import math
@@ -10,7 +7,11 @@ import kornia
 import torch.nn.functional as F
 
 
-def elastic_transform_2d(img, kernel_size=(3,3), sigma=(4,4), alpha=(32,32), random_seed=None):
+def elastic_transform_2d(tensor: torch.Tensor, 
+                         kernel_size: Tuple[int, int] = (3,3), 
+                         sigma: Tuple[float, float] = (4.,4.), 
+                         alpha: Tuple[float, float] = (32.,32.), 
+                         random_seed: Optional = None) -> torch.Tensor:
     r"""Applies elastic transform of images as described in [Simard2003]_.
 
     Args:
@@ -20,6 +21,7 @@ def elastic_transform_2d(img, kernel_size=(3,3), sigma=(4,4), alpha=(32,32), ran
                                      Larger sigma results in smaller pixel displacements. Default:(4,4).
         alpha (Tuple[float, float]):  the scaling factor that controls the intensity of the deformation
                                   in the y and x directions, respectively. Default:32.
+        random_seed (Optional): random seed for generating the displacement vector. Default:None.
         
 
     Returns:
@@ -31,26 +33,27 @@ def elastic_transform_2d(img, kernel_size=(3,3), sigma=(4,4), alpha=(32,32), ran
                       Proc. of the International Conference on Document Analysis and
                       Recognition, 2003.
     """
-    generator = torch.Generator(device=img.device)
+    generator = torch.Generator(device='cpu')
     if random_seed is not None:
         generator.manual_seed(random_seed)
 
-    H, W = img.shape[-2:]
+    n, c, h, w = tensor.shape
     
     # Get Gaussian kernel for 'y' and 'x' displacement
-    kernel_y = kornia.filters.get_gaussian_kernel2d(kernel_size, (sigma[0], sigma[0]))[None, None]
-    kernel_x = kornia.filters.get_gaussian_kernel2d(kernel_size, (sigma[1], sigma[1]))[None, None]
+    kernel_y = kornia.filters.get_gaussian_kernel2d(kernel_size, (sigma[0], sigma[0]))[None].to(device=tensor.device)
+    kernel_x = kornia.filters.get_gaussian_kernel2d(kernel_size, (sigma[1], sigma[1]))[None].to(device=tensor.device)
 
     # Convolve over a random displacement matrix and scale them with 'alpha'
-    displacement = torch.rand(2, H, W, generator=generator)*0.1
-    disp_y = F.conv2d(displacement[0][None, None], weight=kernel_y, padding=kernel_size[0]//2) * alpha[0]
-    disp_x = F.conv2d(displacement[1][None, None], weight=kernel_x, padding=kernel_size[0]//2) * alpha[1]
+    displacement = torch.rand(n, 2, h, w, generator=generator).to(device=tensor.device) * 0.1
+    disp_y = kornia.filters.filter2D(displacement[:,[0]], kernel=kernel_y, border_type='reflect') * alpha[0]
+    disp_x = kornia.filters.filter2D(displacement[:,[1]], kernel=kernel_x, border_type='reflect') * alpha[1]
 
     # stack and normalize displacement
-    disp = torch.stack([disp_y, disp_x], dim=4).squeeze(0) 
+    disp = torch.cat([disp_y, disp_x], dim=1).squeeze(0).permute(0,2,3,1)
     
     # Warp image based on displacement matrix
-    grid = kornia.utils.create_meshgrid(H, W)
-    warped =  F.grid_sample(img, (grid + disp).clamp(-1,1), align_corners=True)
+    grid = kornia.utils.create_meshgrid(h, w).to(device=tensor.device)
+    warped =  F.grid_sample(tensor, (grid + disp).clamp(-1,1), align_corners=True)
     
     return warped
+
