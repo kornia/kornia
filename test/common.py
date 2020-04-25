dd5e1ce74e5be1520c3f48176d3400bcb933bcaf
edgarriba <edgar.riba@gmail.com>
2020-04-25 13:53:10 +0200

    add dtype test type

--- test/common.py
+++ test/common.py
@@ -18,12 +18,31 @@ def get_test_devices() -> Dict[str, torch.device]:
     return devices
 
 
+def get_test_dtypes() -> Dict[str, torch.device]:
+    """Creates a dictionary with the dtypes the source code.
+
+    Return:
+        dict(str, torch.dtype): list with dtype names.
+    """
+    dtypes: Dict[str, torch.dtype] = {}
+    dtypes["float32"] = torch.float32
+    dtypes["float64"] = torch.float64
+    return dtypes
+
+
 # setup the devices to test the source code
 
 TEST_DEVICES: Dict[str, torch.device] = get_test_devices()
+TEST_DTYPES: Dict[str, torch.dtype] = get_test_dtypes()
 
 
 @pytest.fixture()
 def device(request) -> torch.device:
     _device_type: str = request.config.getoption('--typetest')
     return TEST_DEVICES[_device_type]
+
+
+@pytest.fixture()
+def dtype(request) -> torch.device:
+    _dtype_name: str = request.config.getoption('--dtypetest')
+    return TEST_DTYPES[_dtype_name]
