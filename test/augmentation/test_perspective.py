11e31209e7712249ccb079a0fb975921d2db2bec
Edgar Riba <edgar.riba@gmail.com>
2020-05-01 18:58:57 +0200

    update to python 3.8 (#550)
    * update to python 3.8
    
    * move tutorials to separated directory

--- test/augmentation/test_perspective.py
+++ test/augmentation/test_perspective.py
@@ -21,7 +21,7 @@ class TestPerspective:
         params = dict(
             batch_prob=batch_prob, start_points=start_points,
             end_points=end_points, interpolation=torch.tensor(1),
-            align_corners=False)
+            align_corners=torch.tensor(False))
         out_data = F.apply_perspective(x_data, params)
 
         assert out_data.shape == x_data.shape
@@ -43,7 +43,7 @@ class TestPerspective:
             start_points=start_points,
             end_points=end_points,
             interpolation=torch.tensor(1),
-            align_corners=False
+            align_corners=torch.tensor(False)
         )
         assert gradcheck(F.apply_perspective, (input, params,), raise_exception=True)
 
