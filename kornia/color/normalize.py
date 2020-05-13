227d073482b91f09b6b519194a6ed30ac6c2e3f8
Diego <dmarez8ece@gmail.com>
2020-05-12 23:30:12 -0700

    undo changes to normalize

--- kornia/color/normalize.py
+++ kornia/color/normalize.py
@@ -20,7 +20,6 @@ class Normalize(nn.Module):
         std (torch.Tensor or float): Standard deviations for each channel.
 
     """
-
     def __init__(self, mean: Union[torch.Tensor, float], std: Union[torch.Tensor, float]) -> None:
 
         super(Normalize, self).__init__()
@@ -110,7 +109,6 @@ class Denormalize(nn.Module):
         std (torch.Tensor or float): Standard deviations for each channel.
 
     """
-
     def __init__(self, mean: Union[torch.Tensor, float], std: Union[torch.Tensor, float]) -> None:
 
         super(Denormalize, self).__init__()
