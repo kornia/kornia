df93618db8bb393d2eedcdee2c39b8a80eba8cc7
Edgar Riba <edgar.riba@gmail.com>
2020-05-17 11:56:55 +0200

    [Feat] add epipolar geometry package (#569)
    * add epipolar geometry package
    
    * add docs and projection matrices 4x4 -> 3x4
    
    * fix imports for python 3.6
    
    * add epipolar geometry image
    
    * add minor missing details in docs

---
+++ test/geometry/epipolar/test_triangulation.py
@@ -0,0 +1,54 @@
+import pytest
+
+import torch
+from torch.autograd import gradcheck
+from torch.testing import assert_allclose
+
+import kornia
+import kornia.geometry.epipolar as epi
+
+
+class TestTriangulation:
+    def test_smoke(self, device, dtype):
+        P1 = torch.rand(1, 3, 4, device=device, dtype=dtype)
+        P2 = torch.rand(1, 3, 4, device=device, dtype=dtype)
+        points1 = torch.rand(1, 1, 2, device=device, dtype=dtype)
+        points2 = torch.rand(1, 1, 2, device=device, dtype=dtype)
+        points3d = epi.triangulate_points(P1, P2, points1, points2)
+        assert points3d.shape == (1, 1, 3)
+
+    @pytest.mark.parametrize("batch_size, num_points", [(1, 3), (2, 4), (3, 5)])
+    def test_shape(self, batch_size, num_points, device, dtype):
+        B, N = batch_size, num_points
+        P1 = torch.rand(B, 3, 4, device=device, dtype=dtype)
+        P2 = torch.rand(1, 3, 4, device=device, dtype=dtype)
+        points1 = torch.rand(1, N, 2, device=device, dtype=dtype)
+        points2 = torch.rand(B, N, 2, device=device, dtype=dtype)
+        points3d = epi.triangulate_points(P1, P2, points1, points2)
+        assert points3d.shape == (B, N, 3)
+
+    def test_two_view(self, device, dtype):
+        num_views: int = 2
+        num_points: int = 10
+        scene: Dict[str, torch.Tensor] = epi.generate_scene(num_views, num_points)
+
+        P1 = scene['P'][0:1]
+        P2 = scene['P'][1:2]
+        x1 = scene['points2d'][0:1]
+        x2 = scene['points2d'][1:2]
+
+        X = epi.triangulate_points(P1, P2, x1, x2)
+        x_reprojected = kornia.transform_points(scene['P'], X.expand(num_views, -1, -1))
+
+        assert_allclose(scene['points3d'], X, rtol=1e-4, atol=1e-4)
+        assert_allclose(scene['points2d'], x_reprojected)
+
+    def test_gradcheck(self, device):
+        points1 = torch.rand(1, 8, 2, device=device, dtype=torch.float64, requires_grad=True)
+        points2 = torch.rand(1, 8, 2, device=device, dtype=torch.float64)
+        P1 = epi.eye_like(3, points1)
+        P1 = torch.nn.functional.pad(P1, [0, 1])
+        P2 = epi.eye_like(3, points2)
+        P2 = torch.nn.functional.pad(P2, [0, 1])
+        assert gradcheck(epi.triangulate_points,
+                         (P1, P2, points1, points2,), raise_exception=True)
