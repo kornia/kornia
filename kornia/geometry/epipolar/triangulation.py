39b43c50798916c9d0ee9028940fde285fa2b14d
Edgar Riba <edgar.riba@gmail.com>
2020-05-17 11:56:55 +0200

    [Feat] add epipolar geometry package (#569)
    * add epipolar geometry package
    
    * add docs and projection matrices 4x4 -> 3x4
    
    * fix imports for python 3.6
    
    * add epipolar geometry image
    
    * add minor missing details in docs

---
+++ kornia/geometry/epipolar/triangulation.py
@@ -0,0 +1,56 @@
+"""Module with the functionalites for triangulation."""
+
+import torch
+import kornia
+
+
+# https://github.com/opencv/opencv_contrib/blob/master/modules/sfm/src/triangulation.cpp#L68
+
+def triangulate_points(P1: torch.Tensor, P2: torch.Tensor, points1: torch.Tensor,
+                       points2: torch.Tensor) -> torch.Tensor:
+    r"""Reconstructs a bunch of points by triangulation.
+
+    Triangulates the 3d position of 2d correspondences between several images.
+    Reference: Internally it uses DLT method from Hartley/Zisserman 12.2 pag.312
+
+    The input points are assumend to be in homogeneous coordinate system and being inliers
+    correspondences. The method does not perform any robust estimation.
+
+    Args:
+        P1 (torch.Tensor): The projection matrix for the first camera with shape :math:`(*, 3, 4)`.
+        P2 (torch.Tensor): The projection matrix for the second camera with shape :math:`(*, 3, 4)`.
+        points1 (torch.Tensor): The set of points seen from the first camera frame in the camera plane
+          coordinates with shape :math:`(*, N, 2)`.
+        points2 (torch.Tensor): The set of points seen from the second camera frame in the camera plane
+          coordinates with shape :math:`(*, N, 2)`.
+
+    Returns:
+        torch.Tensor: The reconstructed 3d points in the world frame with shape :math:`(*, N, 3)`.
+
+    """
+    assert len(P1.shape) >= 2 and P1.shape[-2:] == (3, 4), P1.shape
+    assert len(P2.shape) >= 2 and P2.shape[-2:] == (3, 4), P2.shape
+    assert len(P1.shape[:-2]) == len(P2.shape[:-2]), (P1.shape, P2.shape)
+    assert len(points1.shape) >= 2 and points1.shape[-1] == 2, points1.shape
+    assert len(points2.shape) >= 2 and points2.shape[-1] == 2, points2.shape
+    assert len(points1.shape[:-2]) == len(points2.shape[:-2]), (points1.shape, points2.shape)
+    assert len(P1.shape[:-2]) == len(points1.shape[:-2]), (P1.shape, points1.shape)
+
+    # allocate and construct the equations matrix with shape (*, 4, 4)
+    points_shape = max(points1.shape, points2.shape)  # this allows broadcasting
+    X = torch.zeros(points_shape[:-1] + (4, 4)).type_as(points1)
+
+    for i in range(4):
+        X[..., 0, i] = points1[..., 0] * P1[..., 2:3, i] - P1[..., 0:1, i]
+        X[..., 1, i] = points1[..., 1] * P1[..., 2:3, i] - P1[..., 1:2, i]
+        X[..., 2, i] = points2[..., 0] * P2[..., 2:3, i] - P2[..., 0:1, i]
+        X[..., 3, i] = points2[..., 1] * P2[..., 2:3, i] - P2[..., 1:2, i]
+
+    # 1. Solve the system Ax=0 with smallest eigenvalue
+    # 2. Return homogeneous coordinates
+
+    U, S, V = torch.svd(X)
+
+    points3d_h = V[..., -1]
+    points3d: torch.Tensor = kornia.convert_points_from_homogeneous(points3d_h)
+    return points3d
---
+++ test/geometry/epipolar/test_common.py
@@ -0,0 +1,46 @@
+from typing import Dict
+
+import torch
+import kornia.geometry.epipolar as epi
+
+
+def generate_two_view_random_scene(
+    device: torch.device = torch.device("cpu"),
+    dtype: torch.dtype = torch.float32
+) -> Dict[str, torch.Tensor]:
+
+    num_views: int = 2
+    num_points: int = 30
+
+    scene: Dict[str, torch.Tensor] = epi.generate_scene(num_views, num_points)
+
+    # internal parameters (same K)
+    K1 = scene['K'].to(device, dtype)
+    K2 = K1.clone()
+
+    # rotation
+    R1 = scene['R'][0:1].to(device, dtype)
+    R2 = scene['R'][1:2].to(device, dtype)
+
+    # translation
+    t1 = scene['t'][0:1].to(device, dtype)
+    t2 = scene['t'][1:2].to(device, dtype)
+
+    # projection matrix, P = K(R|t)
+    P1 = scene['P'][0:1].to(device, dtype)
+    P2 = scene['P'][1:2].to(device, dtype)
+
+    # fundamental matrix
+    F_mat = epi.fundamental_from_projections(
+        P1[..., :3, :], P2[..., :3, :])
+
+    F_mat = epi.normalize_transformation(F_mat)
+
+    # points 3d
+    X = scene['points3d'].to(device, dtype)
+
+    # projected points
+    x1 = scene['points2d'][0:1].to(device, dtype)
+    x2 = scene['points2d'][1:2].to(device, dtype)
+
+    return dict(K1=K1, K2=K2, R1=R1, R2=R2, t1=t1, t2=t2, P1=P1, P2=P2, F=F_mat, X=X, x1=x1, x2=x2)
