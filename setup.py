d6ce13115e7d6118b5859b3eef0c9e9215ebca6e
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2020-07-29 19:10:13 +0200

    fixed pytorch dependencies

--- setup.py
+++ setup.py
@@ -16,6 +16,14 @@ import distutils.command.clean
 
 # NOTE(maintainers): modify this variable each time you do a release
 
+kornia_pt_dependencies = {
+'0.3.1': '1.5.0',
+'0.2.2': '1.4.0',
+'0.1.4': '1.3.1',
+'0.1.4': '1.3.0',
+'0.1.4': '1.2.0',
+}
+
 version = '0.3.2'
 
 #################################
@@ -75,7 +83,7 @@ class clean(distutils.command.clean.clean):
 
 requirements = [
     'numpy',
-    'torch>=1.5.0',
+    'torch>=' + kornia_pt_dependencies[version],
 ]
 
 
