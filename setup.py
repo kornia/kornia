d9a5784601153eb85cff09a4f0d7a874506b2b04
edgarriba <edgar.riba@gmail.com>
2020-07-31 10:07:24 +0200

    upgrade to 0.4.0/1.6.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import distutils.command.clean
 
 # NOTE(maintainers): modify this variable each time you do a release
 
-version = '0.3.2'
+version = '0.4.0'
 
 
 # NOTE(maintainers): update this dictionary each time you do a release
@@ -25,7 +25,8 @@ version = '0.3.2'
 # Once a pytorch version (in the future) breaks a kornia version, we could just
 # add a maximal version.
 kornia_pt_dependencies = {
-    '0.3.2': '>=1.5.0,<1.6.0',  # torch==1.6.0 broke test/geometry/transform/test_crop.py
+    '0.4.0': '>=1.6.0,<1.7.0',
+    '0.3.2': '>=1.5.0,<1.6.0',
     '0.3.1': '>=1.5.0',
     '0.2.2': '>=1.4.0',
     '0.1.4': '>=1.2.0',
