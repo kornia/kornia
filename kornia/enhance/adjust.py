7f65905cd699f33cc4fd27afc99d3aec4f4a96fd
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2020-09-02 23:46:19 -0300

    feat/equalize3d (#639)
    * Create  to equalize a sequence of images based on the volume's histogram
    
    * Extract  from equalize function for better reuse
    
    * Change name to conform with other functionalities
    
    * Add unit tests for equalize and equalize3d
    
    * Add value-based tests to equalize and equalize3d fuctions
    
    * Update test/enhance/test_adjust.py
    
    Co-authored-by: shijianjian <sj8716643@126.com>
    
    * Update test/enhance/test_adjust.py
    
    Co-authored-by: shijianjian <sj8716643@126.com>
    
    * Solve code review minor changes:
    
    - Missing return type on equalize3d
    - Proper rendering of docstring
    - Move tensor scaling to subfunction
    - Initialize tensor with device and dtype
    
    * Update kornia/enhance/adjust.py to fix lint
    
    Co-authored-by: shijianjian <sj8716643@126.com>

--- kornia/enhance/adjust.py
+++ kornia/enhance/adjust.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from kornia.augmentation.utils import _transform_input3d
 from kornia.color.hsv import rgb_to_hsv, hsv_to_rgb
 from kornia.utils.image import _to_bchw
 from kornia.constants import pi
@@ -428,57 +429,82 @@ def sharpness(input: torch.Tensor, factor: Union[float, torch.Tensor]) -> torch.
     return torch.stack([_blend_one(input[i], result[i], factor[i]) for i in range(len(factor))])
 
 
-def equalize(input: torch.Tensor) -> torch.Tensor:
-    """ Apply equalize on the input tensor.
+# Code taken from: https://github.com/pytorch/vision/pull/796
+def _scale_channel(im):
+    """Scale the data in the channel to implement equalize."""
+    im = im * 255
 
+    # Compute the histogram of the image channel.
+    histo = torch.histc(im, bins=256, min=0, max=255)
+    # For the purposes of computing the step, filter out the nonzeros.
+    nonzero_histo = torch.reshape(histo[histo != 0], [-1])
+    step = (torch.sum(nonzero_histo) - nonzero_histo[-1]) // 255
+
+    def build_lut(histo, step):
+        # Compute the cumulative sum, shifting by step // 2
+        # and then normalization by step.
+        lut = (torch.cumsum(histo, 0) + (step // 2)) // step
+        # Shift lut, prepending with 0.
+        lut = torch.cat([torch.zeros(1), lut[:-1]])
+        # Clip the counts to be in range.  This is done
+        # in the C code for image.point.
+        return torch.clamp(lut, 0, 255)
+
+    # If step is zero, return the original image.  Otherwise, build
+    # lut from the full histogram and step and then index from it.
+    if step == 0:
+        result = im
+    else:
+        # can't index using 2d index. Have to flatten and then reshape
+        result = torch.gather(build_lut(histo, step), 0, im.flatten().long())
+        result = result.reshape_as(im)
+
+    return result / 255.
+
+
+def equalize(input: torch.Tensor) -> torch.Tensor:
+    r"""Apply equalize on the input tensor.
     Implements Equalize function from PIL using PyTorch ops based on uint8 format:
     https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/autoaugment.py#L352
 
     Args:
-        input (torch.Tensor): image tensor with shapes like (C, H, W) or (B, C, H, W) to equalize.
+        input (torch.Tensor): image tensor with shapes like :math:(C, H, W) or :math:(B, C, H, W) to equalize.
 
     Returns:
         torch.Tensor: Sharpened image or images.
     """
-    input = _to_bchw(input) * 255
-
-    # Code taken from: https://github.com/pytorch/vision/pull/796
-    def scale_channel(im, c):
-        """Scale the data in the channel to implement equalize."""
-        im = im[c, :, :]
-        # Compute the histogram of the image channel.
-        histo = torch.histc(im, bins=256, min=0, max=255)
-        # For the purposes of computing the step, filter out the nonzeros.
-        nonzero_histo = torch.reshape(histo[histo != 0], [-1])
-        step = (torch.sum(nonzero_histo) - nonzero_histo[-1]) // 255
-
-        def build_lut(histo, step):
-            # Compute the cumulative sum, shifting by step // 2
-            # and then normalization by step.
-            lut = (torch.cumsum(histo, 0) + (step // 2)) // step
-            # Shift lut, prepending with 0.
-            lut = torch.cat([torch.zeros(1), lut[:-1]])
-            # Clip the counts to be in range.  This is done
-            # in the C code for image.point.
-            return torch.clamp(lut, 0, 255)
-
-        # If step is zero, return the original image.  Otherwise, build
-        # lut from the full histogram and step and then index from it.
-        if step == 0:
-            result = im
-        else:
-            # can't index using 2d index. Have to flatten and then reshape
-            result = torch.gather(build_lut(histo, step), 0, im.flatten().long())
-            result = result.reshape_as(im)
-
-        return result / 255.
+    input = _to_bchw(input)
 
     res = []
     for image in input:
         # Assumes RGB for now.  Scales each channel independently
         # and then stacks the result.
-        scaled_image = torch.stack([scale_channel(image, i) for i in range(len(image))])
+        scaled_image = torch.stack([_scale_channel(image[i, :, :]) for i in range(len(image))])
         res.append(scaled_image)
+    return torch.stack(res)
+
+
+def equalize3d(input: torch.Tensor) -> torch.Tensor:
+    r"""Equalizes the values for a 3D volumetric tensor.
+
+    Implements Equalize function for a sequence of images using PyTorch ops based on uint8 format:
+    https://github.com/tensorflow/tpu/blob/master/models/official/efficientnet/autoaugment.py#L352
+
+    Args:
+    input (torch.Tensor): image tensor with shapes like :math:(C, D, H, W) or :math:(B, C, D, H, W) to equalize.
+
+    Returns:
+    torch.Tensor: Sharpened image or images with same shape as the input.
+    """
+    input = _transform_input3d(input)
+
+    res = []
+    for volume in input:
+        # Assumes RGB for now.  Scales each channel independently
+        # and then stacks the result.
+        scaled_input = torch.stack([_scale_channel(volume[i, :, :, :]) for i in range(len(volume))])
+        res.append(scaled_input)
+
     return torch.stack(res)
 
 
