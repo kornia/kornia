f53ced556e62db8a41d4b4f9c0b489547a1cd00d
Edgar Riba <edgar.riba@gmail.com>
2020-09-21 20:29:43 +0200

    [Feat] Adds normalize_min_max functionality (#684)
    * reorganize enhance module imports
    
    * implement normalize_min_max
    
    * more clear names and add numerical test
    
    * improve test exception

--- kornia/enhance/core.py
+++ kornia/enhance/core.py
@@ -2,6 +2,12 @@ import torch
 import torch.nn as nn
 
 
+__all__ = [
+    "add_weighted",
+    "AddWeighted",
+]
+
+
 def add_weighted(src1: torch.Tensor, alpha: float,
                  src2: torch.Tensor, beta: float,
                  gamma: float) -> torch.Tensor:
