af72d8443dc7ca956f4e7242c1324c63d65a8b68
Jian S <sj8716643@126.com>
2020-10-14 17:02:39 +0800

    Moved crop module under transform

--- kornia/geometry/__init__.py
+++ kornia/geometry/__init__.py
@@ -1,15 +1,7 @@
 from kornia.geometry.camera import *
 from kornia.geometry.conversions import *
 from kornia.geometry.linalg import *
-# TODO: Uncomment this line after removed the kornia.geometry.transform.crop
-# from kornia.geometry.transform import *
-# TODO: Remove the following 5 line after removed the kornia.geometry.transform.crop
-from kornia.geometry.transform.imgwarp import *
-from kornia.geometry.transform.pyramid import *
-from kornia.geometry.transform.affwarp import *
-from kornia.geometry.transform.flips import *
-from kornia.geometry.transform.projwarp import *
-from kornia.geometry.crop import *
+from kornia.geometry.transform import *
 from kornia.geometry.warp import *
 from kornia.geometry.depth import *
 from kornia.geometry.subpix import *
--- kornia/geometry/crop/__init__.py
+++
@@ -1,2 +0,0 @@
-from kornia.geometry.crop.crop2d import *
-from kornia.geometry.crop.crop3d import *
