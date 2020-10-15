a72003a5de1fa6a0a7ef7bdf1f3a3a79d6c535c0
fthorde <manyids2@gmail.com>
2020-10-15 14:22:53 +0200

    Added MKDGradients.

--- test/feature/test_mkd.py
+++ test/feature/test_mkd.py
@@ -21,3 +21,42 @@ def test_get_kron_order(d1,d2):
     out = get_kron_order(d1, d2)
     assert out.shape == (d1 * d2, 2)
 
+class TestMKDGradients:
+    @pytest.mark.parametrize("ps", [5, 13, 25])
+    def test_shape(self, ps, device):
+        inp = torch.ones(1, 1, ps, ps, device=device)
+        gradients = MKDGradients().to(device)
+        out = gradients(inp)
+        assert out.shape == (1, 2, ps, ps)
+
+    @pytest.mark.parametrize("bs", [1, 5, 13])
+    def test_batch_shape(self, bs, device):
+        inp = torch.ones(bs, 1, 15, 15, device=device)
+        gradients = MKDGradients().to(device)
+        out = gradients(inp)
+        assert out.shape == (bs, 2, 15, 15)
+
+    def test_print(self, device):
+        gradients = MKDGradients().to(device)
+        gradients.__repr__()
+
+    def test_toy(self, device):
+        patch = torch.ones(1, 1, 6, 6, device=device).float()
+        patch[0, 0, :, 3:] = 0
+        gradients = MKDGradients().to(device)
+        out = gradients(patch)
+        expected = torch.Tensor([0, 0, 1., 1., 0, 0], device=device)
+        expected_mags = expected.unsqueeze(0).repeat(6,1)
+        expected_oris = expected_mags * 0
+        assert_allclose(out[0,0,:,:], expected_mags, atol=1e-3, rtol=1e-3)
+        assert_allclose(out[0,1,:,:], expected_oris, atol=1e-3, rtol=1e-3)
+
+    def test_gradcheck(self, device):
+        batch_size, channels, height, width = 1, 1, 13, 13
+        patches = torch.rand(batch_size, channels, height, width, device=device)
+        patches = utils.tensor_to_gradcheck_var(patches)  # to var
+
+        def grad_describe(patches):
+            return MKDGradients()(patches)
+        assert gradcheck(grad_describe, (patches),
+                         raise_exception=True, nondet_tol=1e-4)
