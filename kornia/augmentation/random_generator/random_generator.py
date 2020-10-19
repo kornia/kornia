49fc81b9e2d1242b1f61f4e576854ceb504f1324
Jian S <sj8716643@126.com>
2020-10-19 23:43:01 +0800

    Fixed inconsistant merging

--- kornia/augmentation/random_generator/random_generator.py
+++ kornia/augmentation/random_generator/random_generator.py
@@ -170,8 +170,8 @@ def random_affine_generator(
     # compute tensor ranges
     if scale is not None:
         _joint_range_check(cast(torch.Tensor, scale[:2]), "scale")
-        scale = _adapted_uniform((batch_size,), scale[0], scale[1], same_on_batch).unsqueeze(1).repeat(1, 2)
-        if len(scale) == 4:
+        _scale = _adapted_uniform((batch_size,), scale[0], scale[1], same_on_batch).unsqueeze(1).repeat(1, 2)
+        if len(_scale) == 4:
             _joint_range_check(cast(torch.Tensor, scale[2:]), "scale_y")
             _scale[:, 1] = _adapted_uniform((batch_size,), scale[2], scale[3], same_on_batch)
     else:
