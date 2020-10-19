f73f845ef921ba50124fe4e6b8f80922b2a6dbff
shijianjian <sj8716643@126.com>
2020-10-19 23:16:46 +0800

    [Bug] Affine scale bug fix (#714)
    * Affine scale bug fix
    
    * Updated tests

--- kornia/augmentation/random_generator/random_generator.py
+++ kornia/augmentation/random_generator/random_generator.py
@@ -170,7 +170,7 @@ def random_affine_generator(
     # compute tensor ranges
     if scale is not None:
         _joint_range_check(cast(torch.Tensor, scale[:2]), "scale")
-        scale = _adapted_uniform((batch_size,), scale[0], scale[1], same_on_batch).repeat(1, 2)
+        scale = _adapted_uniform((batch_size,), scale[0], scale[1], same_on_batch).unsqueeze(1).repeat(1, 2)
         if len(scale) == 4:
             _joint_range_check(cast(torch.Tensor, scale[2:]), "scale_y")
             scale[:, 1] = _adapted_uniform((batch_size,), scale[2], scale[3], same_on_batch)
