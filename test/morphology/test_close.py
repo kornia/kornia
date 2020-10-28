2891026dda66c8bd48cb8896c57b89654a1b7d9a
juclique <juclique@gmail.com>
2020-10-28 16:11:02 +0100

    Morphology test: added dtype

--- test/morphology/test_close.py
+++ test/morphology/test_close.py
@@ -6,20 +6,20 @@ from torch.autograd import gradcheck
 
 
 class TestClose:
-    def test_shape_channels(self, device):
-        input = torch.rand(1, 3, 4, 6).to(device)
-        kernel = torch.rand(3, 3).to(device)
+    def test_shape_channels(self, device, dtype):
+        input = torch.rand(1, 3, 4, 6, device=device, dtype=dtype)
+        kernel = torch.rand(3, 3, device=device, dtype=dtype)
         test = close(input, kernel)
         assert test.shape == (1, 3, 4, 6)
 
-    def test_shape_batch(self, device):
-        input = torch.rand(3, 2, 6, 10).to(device)
-        kernel = torch.rand(3, 3).to(device)
+    def test_shape_batch(self, device, dtype):
+        input = torch.rand(3, 2, 6, 10, device=device, dtype=dtype)
+        kernel = torch.rand(3, 3, device=device, dtype=dtype)
         test = close(input, kernel)
         assert test.shape == (3, 2, 6, 10)
 
-    def test_gradcheck(self, device):
-        input = torch.rand(2, 3, 4, 4, requires_grad=True).to(device)
-        kernel = torch.rand(3, 3, requires_grad=True).to(device).double()
+    def test_gradcheck(self, device, dtype):
+        input = torch.rand(2, 3, 4, 4, requires_grad=True, device=device, dtype=dtype)
+        kernel = torch.rand(3, 3, requires_grad=True, device=device, dtype=dtype)
         input = utils.tensor_to_gradcheck_var(input)  # to var
         assert gradcheck(close, (input, kernel), raise_exception=True)
