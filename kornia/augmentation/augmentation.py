7c8d80a2dfbacbbae26b54169a714e71a29f9310
FGeri <14985132+FGeri@users.noreply.github.com>
2020-10-30 09:46:39 +0100

    RandomREsizedCrop fixed (#748)
    Co-authored-by: Feher Gergo (CC-AD/EPE-Bp) <bosch.feher@extaccount.com>

--- kornia/augmentation/augmentation.py
+++ kornia/augmentation/augmentation.py
@@ -795,7 +795,7 @@ class RandomResizedCrop(AugmentationBase2D):
     ) -> None:
         # Since PyTorch does not support ragged tensor. So cropping function happens all the time.
         super(RandomResizedCrop, self).__init__(
-            p=1., return_transform=return_transform, same_on_batch=same_on_batch, p_batch=1.)
+            p=1., return_transform=return_transform, same_on_batch=same_on_batch, p_batch=p)
         self.size = size
         self.scale = cast(torch.Tensor, scale) if isinstance(scale, torch.Tensor) else torch.tensor(scale)
         self.ratio = cast(torch.Tensor, ratio) if isinstance(ratio, torch.Tensor) else torch.tensor(ratio)
