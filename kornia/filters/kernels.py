9e844e338a4da222e8ed4f53a2a52de356977b5b
shijianjian <sj8716643@126.com>
2020-11-14 18:02:00 +0800

    Added tests for _extract_device_dtype

--- kornia/filters/kernels.py
+++ kornia/filters/kernels.py
@@ -686,8 +686,9 @@ def get_motion_kernel3d(kernel_size: int, angle: Union[torch.Tensor, Tuple[float
 
     Args:
         kernel_size (int): motion kernel width, height and depth. It should be odd and positive.
-        angle (tensor or float): Range of yaw (x-axis), pitch (y-axis), roll (z-axis) to select from.
+        angle (tensor or tuple): Range of yaw (x-axis), pitch (y-axis), roll (z-axis) to select from.
             If tensor, it must be :math:`(B, 3)`.
+            If tuple, it must be (yaw, pitch, raw).
         direction (float): forward/backward direction of the motion blur.
             Lower values towards -1.0 will point the motion blur towards the back (with angle provided via angle),
             while higher values towards 1.0 will point the motion blur forward. A value of 0.0 leads to a
@@ -697,9 +698,9 @@ def get_motion_kernel3d(kernel_size: int, angle: Union[torch.Tensor, Tuple[float
         torch.Tensor: the motion blur kernel.
 
     Shape:
-        - Output: :math:`(B, ksize, ksize, ksize)`
+        - Output: :math:`(B, kernel_size, kernel_size, kernel_size)`
 
-    Examples::
+    Examples:
         >>> get_motion_kernel3d(3, (0., 0., 0.), 0.)
         tensor([[[[0.0000, 0.0000, 0.0000],
                   [0.0000, 0.0000, 0.0000],
@@ -712,24 +713,29 @@ def get_motion_kernel3d(kernel_size: int, angle: Union[torch.Tensor, Tuple[float
                  [[0.0000, 0.0000, 0.0000],
                   [0.0000, 0.0000, 0.0000],
                   [0.0000, 0.0000, 0.0000]]]])
-        >>> get_motion_kernel3d(3, (215., 215., 215.), -0.5)
+        >>> get_motion_kernel3d(3, (90., 90., 0.), -0.5)
         tensor([[[[0.0000, 0.0000, 0.0000],
                   [0.0000, 0.0000, 0.0000],
-                  [0.0000, 0.0000, 0.0000]],
+                  [0.0000, 0.5000, 0.0000]],
         <BLANKLINE>
                  [[0.0000, 0.0000, 0.0000],
-                  [0.1667, 0.3333, 0.5000],
+                  [0.0000, 0.3333, 0.0000],
                   [0.0000, 0.0000, 0.0000]],
         <BLANKLINE>
-                 [[0.0000, 0.0000, 0.0000],
+                 [[0.0000, 0.1667, 0.0000],
                   [0.0000, 0.0000, 0.0000],
                   [0.0000, 0.0000, 0.0000]]]])
     """
     if not isinstance(kernel_size, int) or kernel_size % 2 == 0 or kernel_size < 3:
         raise TypeError(f"ksize must be an odd integer >= than 3. Got {kernel_size}.")
 
+    device, dtype = _extract_device_dtype([
+        angle if isinstance(angle, torch.Tensor) else None,
+        direction if isinstance(direction, torch.Tensor) else None,
+    ])
+
     if not isinstance(angle, torch.Tensor):
-        angle = torch.tensor([angle])
+        angle = torch.tensor([angle], device=device, dtype=dtype)
 
     angle = cast(torch.Tensor, angle)
     if angle.dim() == 1:
@@ -737,7 +743,7 @@ def get_motion_kernel3d(kernel_size: int, angle: Union[torch.Tensor, Tuple[float
     assert len(angle.shape) == 2 and angle.size(1) == 3, f"angle must be (B, 3). Got {angle}."
 
     if not isinstance(direction, torch.Tensor):
-        direction = torch.tensor([direction])
+        direction = torch.tensor([direction], device=device, dtype=dtype)
 
     direction = cast(torch.Tensor, direction)
     if direction.dim() == 0:
@@ -750,10 +756,10 @@ def get_motion_kernel3d(kernel_size: int, angle: Union[torch.Tensor, Tuple[float
     kernel_tuple: Tuple[int, int, int] = (kernel_size, kernel_size, kernel_size)
     # direction from [-1, 1] to [0, 1] range
     direction = (torch.clamp(direction, -1., 1.) + 1.) / 2.
-    kernel = torch.zeros((direction.size(0), *kernel_tuple), dtype=torch.float)
+    kernel = torch.zeros((direction.size(0), *kernel_tuple), device=device, dtype=dtype)
 
     # Element-wise linspace
-    kernel[:, kernel_size // 2, kernel_tuple[0] // 2, :] = torch.stack(
+    kernel[:, kernel_size // 2, kernel_size // 2, :] = torch.stack(
         [(direction + ((1 - 2 * direction) / (kernel_size - 1)) * i) for i in range(kernel_size)], dim=-1)
     kernel = kernel.unsqueeze(1)
     # rotate (counterclockwise) kernel by given angle
