ab154796d93b5464ee1d1e16dc6f0af531fbd7c4
shijianjian <sj8716643@126.com>
2020-11-18 01:34:46 +0800

    Fixed #785.

--- kornia/augmentation/random_generator/random_generator.py
+++ kornia/augmentation/random_generator/random_generator.py
@@ -187,7 +187,7 @@ def random_affine_generator(
     if scale is not None:
         _joint_range_check(cast(torch.Tensor, scale[:2]), "scale")
         _scale = _adapted_uniform((batch_size,), scale[0], scale[1], same_on_batch).unsqueeze(1).repeat(1, 2)
-        if len(_scale) == 4:
+        if len(scale) == 4:
             _joint_range_check(cast(torch.Tensor, scale[2:]), "scale_y")
             _scale[:, 1] = _adapted_uniform((batch_size,), scale[2], scale[3], same_on_batch)
     else:
