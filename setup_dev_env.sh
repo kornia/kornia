225332472faacff5bd750933032698baeae3908b
edgarriba <edgar.riba@gmail.com>
2020-11-20 23:57:31 +0100

    adapt script for pytorch nghtlies

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -38,6 +38,12 @@ fi
 python_version=${PYTHON_VERSION:-"3.7"}
 pytorch_version=${PYTORCH_VERSION:-"1.6.0"}
 pytorch_mode=${PYTORCH_MODE:-""}  # use `cpuonly` for CPU or leave it in blank for GPU
+pytorch_channel="pytorch"
+# configure for nightly builds
+if [ "$pytorch_version" == "nightly" ]; then
+    pytorch_version=""
+    pytorch_channel="pytorch-nightly"
+fi
 
 # create an environment with the specific python version
 $conda_bin config --append channels conda-forge
@@ -49,7 +55,7 @@ $conda_bin clean -ya
 source $conda_bin_dir/activate $dev_env_dir/envs/venv
 
 # install pytorch and torchvision
-conda install pytorch=$pytorch_version torchvision $pytorch_mode -c pytorch
+conda install pytorch=$pytorch_version torchvision $pytorch_mode -c $pytorch_channel
 
 # install testing dependencies
 pip install -r requirements-dev.txt
