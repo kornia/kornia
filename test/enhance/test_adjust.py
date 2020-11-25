add some fixes for tpu precision

test/enhance/test_adjust.py
@@ -2,6 +2,7 @@ import pytest
 
 import kornia
 from kornia.testing import tensor_to_gradcheck_var, BaseTester
+import kornia.testing as utils
 from kornia.constants import pi
 
 import torch
@@ -794,10 +795,11 @@ class TestSharpness(BaseTester):
               [0.5185, 0.6977, 0.8000]]]], device=device, dtype=dtype)
 
         # If factor == 1, shall return original
-        assert_allclose(TestSharpness.f(inputs, 1), inputs, rtol=1e-4, atol=1e-4)
-        assert_allclose(TestSharpness.f(inputs, torch.tensor([1., 1.])), inputs, rtol=1e-4, atol=1e-4)
-        assert_allclose(TestSharpness.f(inputs, 0.8), expected_08, rtol=1e-4, atol=1e-4)
-        assert_allclose(TestSharpness.f(inputs, torch.tensor([0.8, 1.3])), expected_08_13, rtol=1e-4, atol=1e-4)
+        tol_val: float = utils._get_precision(device, dtype)
+        assert_allclose(TestSharpness.f(inputs, 1), inputs, rtol=tol_val, atol=tol_val)
+        assert_allclose(TestSharpness.f(inputs, torch.tensor([1., 1.])), inputs, rtol=tol_val, atol=tol_val)
+        assert_allclose(TestSharpness.f(inputs, 0.8), expected_08, rtol=tol_val, atol=tol_val)
+        assert_allclose(TestSharpness.f(inputs, torch.tensor([0.8, 1.3])), expected_08_13, rtol=tol_val, atol=tol_val)
 
     @pytest.mark.grad
     def test_gradcheck(self, device, dtype):

test/enhance/test_normalize.py
@@ -267,8 +267,8 @@ class TestNormalizeMinMax(BaseTester):
     def test_range(self, device, dtype, min_val, max_val):
         x = torch.rand(1, 2, 4, 5, device=device, dtype=dtype)
         out = kornia.normalize_min_max(x, min_val=min_val, max_val=max_val)
-        assert_allclose(out.min(), min_val)
-        assert_allclose(out.max(), max_val)
+        assert_allclose(out.min().item(), min_val)
+        assert_allclose(out.max().item(), max_val)
 
     def test_values(self, device, dtype):
         x = torch.tensor([[[

test/enhance/test_zca.py
@@ -4,7 +4,6 @@ import pytest
 import kornia
 import kornia.testing as utils  # test utils
 
-
 import torch
 from torch.autograd import gradcheck
 from torch.testing import assert_allclose
@@ -31,7 +30,8 @@ class TestZCA:
 
         actual = zca(data)
 
-        assert_allclose(actual, expected)
+        tol_val: float = utils._get_precision(device, dtype)
+        assert_allclose(actual, expected, rtol=tol_val, atol=tol_val)
 
     @pytest.mark.parametrize("dim", [0, 1])
     def test_dim_args(self, dim, device, dtype):
@@ -59,7 +59,8 @@ class TestZCA:
         zca = kornia.enhance.ZCAWhitening(dim=dim)
         actual = zca(data, True)
 
-        assert_allclose(actual, expected, rtol=1e-4, atol=1e-4)
+        tol_val: float = utils._get_precision(device, dtype)
+        assert_allclose(actual, expected, rtol=tol_val, atol=tol_val)
 
     @pytest.mark.parametrize("input_shape,eps", [((15, 2, 2, 2), 1e-6), ((10, 4), .1), ((20, 3, 2, 2), 1e-3)])
     def test_identity(self, input_shape, eps, device, dtype):
@@ -77,7 +78,8 @@ class TestZCA:
 
         data_hat = zca.inverse_transform(data_w)
 
-        assert_allclose(data, data_hat)
+        tol_val: float = utils._get_precision(device, dtype)
+        assert_allclose(data, data_hat, rtol=tol_val, atol=tol_val)
 
     def test_grad_zca_individual_transforms(self, device, dtype):
         """
@@ -180,4 +182,6 @@ class TestZCA:
 
         actual = kornia.zca_whiten(data, unbiased=unbiased)
 
-        assert_allclose(actual, expected)
+       
+        tol_val: float = utils._get_precision(device, dtype)
+        assert_allclose(actual, expected, atol=tol_val, rtol=tol_val)