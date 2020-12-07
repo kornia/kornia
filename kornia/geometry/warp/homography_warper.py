04aa8ecae3abe690edb65fc41f85ca5208b6e5d9
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2020-12-07 13:06:34 -0500

    added epsilon in case dimensions == 1

--- kornia/geometry/warp/homography_warper.py
+++ kornia/geometry/warp/homography_warper.py
@@ -288,16 +288,17 @@ def normal_transform_pixel(height: int, width: int) -> torch.Tensor:
     Returns:
         torch.Tensor: normalized transform with shape :math:`(1, 3, 3)`.
     """
+    # prevent divide by zero bugs
+    width = 1e-14 if width == 1 else width - 1.0
+    height = 1e-14 if height == 1 else height - 1.0
+
     tr_mat = torch.tensor([[1.0, 0.0, -1.0],
                            [0.0, 1.0, -1.0],
                            [0.0, 0.0, 1.0]])  # 3x3
 
-    tr_mat[0, 0] = tr_mat[0, 0] * 2.0 / (width - 1.0)
-    tr_mat[1, 1] = tr_mat[1, 1] * 2.0 / (height - 1.0)
+    tr_mat[0, 0] = tr_mat[0, 0] * 2.0 / width
+    tr_mat[1, 1] = tr_mat[1, 1] * 2.0 / height
 
-    # in the case that height or width is 1, prevent divide by zero bug
-    # torch.finfo(torch.float32).max
-    tr_mat[torch.isinf(tr_mat)] = 3.40282e+38
     return tr_mat.unsqueeze(0)  # 1x3x3
 
 
@@ -312,18 +313,20 @@ def normal_transform_pixel3d(depth: int, height: int, width: int) -> torch.Tenso
     Returns:
         Tensor: normalized transform with shape :math:`(1, 4, 4)`.
     """
+    # prevent divide by zero bugs
+    width = 1e-14 if width == 1 else width - 1.0
+    height = 1e-14 if height == 1 else height - 1.0
+    depth = 1e-14 if depth == 1 else depth - 1.0
+
     tr_mat = torch.tensor([[1.0, 0.0, 0.0, -1.0],
                            [0.0, 1.0, 0.0, -1.0],
                            [0.0, 0.0, 1.0, -1.0],
                            [0.0, 0.0, 0.0, 1.0]])  # 4x4
 
-    tr_mat[0, 0] = tr_mat[0, 0] * 2.0 / (width - 1.0)
-    tr_mat[1, 1] = tr_mat[1, 1] * 2.0 / (height - 1.0)
-    tr_mat[2, 2] = tr_mat[2, 2] * 2.0 / (depth - 1.0)
+    tr_mat[0, 0] = tr_mat[0, 0] * 2.0 / width
+    tr_mat[1, 1] = tr_mat[1, 1] * 2.0 / height
+    tr_mat[2, 2] = tr_mat[2, 2] * 2.0 / depth
 
-    # in the case that height, width, or depth is 1, prevent divide by zero bug
-    # torch.finfo(torch.float32).max
-    tr_mat[torch.isinf(tr_mat)] = 3.40282e+38
     return tr_mat.unsqueeze(0)  # 1x4x4
 
 
