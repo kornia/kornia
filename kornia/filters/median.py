90aa1857515b5508eaec61ff9c4cf60cddbb5a53
edgarriba <edgar.riba@gmail.com>
2020-12-18 17:46:13 +0100

    refactor median blur

--- kornia/filters/median.py
+++ kornia/filters/median.py
@@ -1,15 +1,58 @@
-from typing import Tuple
+from typing import List, Tuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from kornia.filters.kernels import get_binary_kernel2d
+
+import kornia.filters.kernels as kernels
 
 
 def _compute_zero_padding(kernel_size: Tuple[int, int]) -> Tuple[int, int]:
     r"""Utility function that computes zero padding tuple."""
-    computed: Tuple[int, ...] = tuple([(k - 1) // 2 for k in kernel_size])
+    computed: List[int] = [(k - 1) // 2 for k in kernel_size]
     return computed[0], computed[1]
+
+
+def median_blur(input: torch.Tensor,
+                kernel_size: Tuple[int, int]) -> torch.Tensor:
+    r"""Blurs an image using the median filter.
+
+    Args:
+        input (torch.Tensor): the input image with shape :math:`(B,C,H,W)`.
+        kernel_size (Tuple[int, int]): the blurring kernel size.
+
+    Returns:
+        torch.Tensor: the blurred input tensor with shape :math:`(B,C,H,W)`.
+
+    Example:
+        >>> input = torch.rand(2, 4, 5, 7)
+        >>> output = median_blur(input, (3, 3))
+        >>> output.shape
+        torch.Size([2, 4, 5, 7])
+    """
+    if not isinstance(input, torch.Tensor):
+        raise TypeError("Input type is not a torch.Tensor. Got {}"
+                        .format(type(input)))
+
+    if not len(input.shape) == 4:
+        raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}"
+                         .format(input.shape))
+
+    padding: Tuple[int, int] = _compute_zero_padding(kernel_size)
+
+    # prepare kernel
+    kernel: torch.Tensor = kernels.get_binary_kernel2d(kernel_size).to(input)
+    b, c, h, w = input.shape
+
+    # map the local window to single vector
+    features: torch.Tensor = F.conv2d(
+        input.reshape(b * c, 1, h, w), kernel, padding=padding, stride=1)
+    features = features.view(b, c, -1, h, w)  # BxCx(K_h * K_w)xHxW
+
+    # compute the median along the feature axis
+    median: torch.Tensor = torch.median(features, dim=2)[0]
+
+    return median
 
 
 class MedianBlur(nn.Module):
@@ -28,41 +71,14 @@ class MedianBlur(nn.Module):
     Example:
         >>> input = torch.rand(2, 4, 5, 7)
         >>> blur = MedianBlur((3, 3))
-        >>> output = blur(input)  # 2x4x5x7
+        >>> output = blur(input)
+        >>> output.shape
+        torch.Size([2, 4, 5, 7])
     """
 
     def __init__(self, kernel_size: Tuple[int, int]) -> None:
         super(MedianBlur, self).__init__()
-        self.kernel: torch.Tensor = get_binary_kernel2d(kernel_size)
-        self.padding: Tuple[int, int] = _compute_zero_padding(kernel_size)
+        self.kernel_size: Tuple[int, int] = kernel_size
 
-    def forward(self, input: torch.Tensor):  # type: ignore
-        if not torch.is_tensor(input):
-            raise TypeError("Input type is not a torch.Tensor. Got {}"
-                            .format(type(input)))
-        if not len(input.shape) == 4:
-            raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}"
-                             .format(input.shape))
-        # prepare kernel
-        b, c, h, w = input.shape
-        kernel: torch.Tensor = self.kernel.to(input.device).to(input.dtype)
-        # map the local window to single vector
-        features: torch.Tensor = F.conv2d(
-            input.reshape(b * c, 1, h, w), kernel, padding=self.padding, stride=1)
-        features = features.view(b, c, -1, h, w)  # BxCx(K_h * K_w)xHxW
-
-        # compute the median along the feature axis
-        median: torch.Tensor = torch.median(features, dim=2)[0]
-        return median
-
-
-# functiona api
-
-
-def median_blur(input: torch.Tensor,
-                kernel_size: Tuple[int, int]) -> torch.Tensor:
-    r"""Blurs an image using the median filter.
-
-    See :class:`~kornia.filters.MedianBlur` for details.
-    """
-    return MedianBlur(kernel_size)(input)
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return median_blur(input, self.kernel_size)
--- test/filters/test_median.py
+++ test/filters/test_median.py
@@ -11,17 +11,15 @@ from torch.autograd import gradcheck
 
 
 class TestMedianBlur:
-    def test_shape(self, device):
-        inp = torch.zeros(1, 3, 4, 4).to(device)
-        median = kornia.filters.MedianBlur((3, 3))
-        assert median(inp).shape == (1, 3, 4, 4)
+    def test_shape(self, device, dtype):
+        inp = torch.zeros(1, 3, 4, 4, device=device, dtype=dtype)
+        assert kornia.filters.median_blur(inp, (3, 3)).shape == (1, 3, 4, 4)
 
-    def test_shape_batch(self, device):
-        inp = torch.zeros(2, 6, 4, 4).to(device)
-        blur = kornia.filters.BoxBlur((3, 3))
-        assert blur(inp).shape == (2, 6, 4, 4)
+    def test_shape_batch(self, device, dtype):
+        inp = torch.zeros(2, 6, 4, 4, device=device, dtype=dtype)
+        assert kornia.filters.median_blur(inp, (3, 3)).shape == (2, 6, 4, 4)
 
-    def test_kernel_3x3(self, device):
+    def test_kernel_3x3(self, device, dtype):
         inp = torch.tensor([[
             [0., 0., 0., 0., 0.],
             [0., 3., 7., 5., 0.],
@@ -34,38 +32,43 @@ class TestMedianBlur:
             [65., 59., 2.0, 0., 0.],
             [0.0, 0.0, 0.0, 0., 0.],
             [0.0, 0.0, 0.0, 0., 0.]
-        ]]).repeat(2, 1, 1, 1).to(device)
+        ]], device=device, dtype=dtype).repeat(2, 1, 1, 1)
 
         kernel_size = (3, 3)
         actual = kornia.filters.median_blur(inp, kernel_size)
-        assert_allclose(actual[0, 0, 2, 2], torch.tensor(3.).to(device))
-        assert_allclose(actual[0, 1, 1, 1], torch.tensor(14.).to(device))
+        assert_allclose(actual[0, 0, 2, 2], torch.tensor(3.).to(actual))
+        assert_allclose(actual[0, 1, 1, 1], torch.tensor(14.).to(actual))
 
-    def test_noncontiguous(self, device):
+    def test_noncontiguous(self, device, dtype):
         batch_size = 3
-        inp = torch.rand(3, 5, 5).expand(batch_size, -1, -1, -1).to(device)
+        inp = torch.rand(3, 5, 5, device=device, dtype=dtype).expand(batch_size, -1, -1, -1)
 
         kernel_size = (3, 3)
         actual = kornia.filters.median_blur(inp, kernel_size)
         expected = actual
         assert_allclose(actual, actual)
 
-    def test_gradcheck(self, device):
+    def test_gradcheck(self, device, dtype):
         batch_size, channels, height, width = 1, 2, 5, 4
-        img = torch.rand(batch_size, channels, height, width).to(device)
+        img = torch.rand(batch_size, channels, height, width, device=device, dtype=dtype)
         img = utils.tensor_to_gradcheck_var(img)  # to var
         assert gradcheck(kornia.filters.median_blur, (img, (5, 3),),
                          raise_exception=True)
 
-    @pytest.mark.skip("")
-    @pytest.mark.skip(reason="turn off all jit for a while")
-    def test_jit(self, device):
-        @torch.jit.script
-        def op_script(input: torch.Tensor,
-                      kernel_size: Tuple[int, int]) -> torch.Tensor:
-            return kornia.filters.median_blur(input, kernel_size)
+    def test_jit(self, device, dtype):
         kernel_size = (3, 5)
-        img = torch.rand(2, 3, 4, 5).to(device)
+        img = torch.rand(2, 3, 4, 5, device=device, dtype=dtype)
+        op = kornia.filters.median_blur
+        op_script = torch.jit.script(op)
         actual = op_script(img, kernel_size)
-        expected = kornia.filters.median_blur(img, kernel_size)
+        expected = op(img, kernel_size)
+        assert_allclose(actual, expected)
+
+    def test_module(self, device, dtype):
+        kernel_size = (3, 5)
+        img = torch.rand(2, 3, 4, 5, device=device, dtype=dtype)
+        op = kornia.filters.median_blur
+        op_module = kornia.filters.MedianBlur((3, 5))
+        actual = op_module(img)
+        expected = op(img, kernel_size)
         assert_allclose(actual, expected)
