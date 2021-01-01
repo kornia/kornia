47633e3efe1f408f49560193a8306859185f2cad
xen0f0n <akis.karagian@gmail.com>
2021-01-01 19:53:58 +0200

    [Feat] Binary focal loss (#830)
    * Binary focal loss - fixing weighting factor (alpha, 1-alpha)
    
    * Fixed examples in binary_focal_loss
    
    * Added binary focal loss in init file
    
    * fix output shape doc for get_affine_matrix2d (#828)
    
    * Binary focal loss - fixing weighting factor (alpha, 1-alpha)
    
    * Fixed examples in binary_focal_loss
    
    * Added binary focal loss in init file
    
    * Added doctest for binary_focal_loss_with_logits
    
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- kornia/losses/focal.py
+++ kornia/losses/focal.py
@@ -122,3 +122,117 @@ class FocalLoss(nn.Module):
             input: torch.Tensor,
             target: torch.Tensor) -> torch.Tensor:
         return focal_loss(input, target, self.alpha, self.gamma, self.reduction, self.eps)
+
+
+def binary_focal_loss_with_logits(
+        inp: torch.Tensor,
+        target: torch.Tensor,
+        alpha: float = .25,
+        gamma: float = 2.0,
+        reduction: str = 'none',
+        eps: float = 1e-8) -> torch.Tensor:
+    r"""Function that computes Binary Focal loss.
+
+    args:
+        inp (torch.Tensor) : input data tensor
+        target (torch.Tensor): the target tensor
+        alpha (float): Weighting factor for the rare class :math:`\alpha \in [0, 1]`.  Default: 0.25.
+        gamma (float): Focusing parameter :math:`\gamma >= 0`.  Default: 2.0.
+        reduction (str, optional): Specifies the reduction to apply to the.  Default: 'none'.
+        eps (float): for numerically stability when dividing. Default: 1e-8.
+
+    returns:
+        torch.tensor: loss
+
+    Examples:
+    >>> num_classes = 1
+    >>> kwargs = {"alpha": 0.25, "gamma": 2.0, "reduction": 'mean'}
+    >>> logits = torch.tensor([[[[6.325]]],[[[5.26]]],[[[87.49]]]])
+    >>> labels = torch.tensor([[[1.]],[[1.]],[[0.]]])
+    >>> binary_focal_loss_with_logits(logits, labels, **kwargs)
+    tensor(4.6052)
+    """
+
+    if not isinstance(inp, torch.Tensor):
+        raise TypeError("Input type is not a torch.Tensor. Got {}"
+                        .format(type(inp)))
+
+    if not len(inp.shape) >= 2:
+        raise ValueError("Invalid input shape, we expect BxCx*. Got: {}"
+                         .format(inp.shape))
+
+    if inp.size(0) != target.size(0):
+        raise ValueError('Expected input batch_size ({}) to match target batch_size ({}).'
+                         .format(inp.size(0), target.size(0)))
+
+    if not inp.device == target.device:
+        raise ValueError(
+            "input and target must be in the same device. Got: {} and {}".format(
+                inp.device, target.device))
+
+    probs = torch.sigmoid(inp)
+    target = target.unsqueeze(dim=1)
+    loss_tmp = -alpha * torch.pow((1. - probs), gamma) * target * torch.log(probs + eps) \
+               - (1 - alpha) * torch.pow(probs, gamma) * (1. - target) * torch.log(1. - probs + eps)
+    loss_tmp = loss_tmp.squeeze(dim=1)
+
+    if reduction == 'none':
+        loss = loss_tmp
+    elif reduction == 'mean':
+        loss = torch.mean(loss_tmp)
+    elif reduction == 'sum':
+        loss = torch.sum(loss_tmp)
+    else:
+        raise NotImplementedError("Invalid reduction mode: {}"
+                                  .format(reduction))
+    return loss
+
+
+class BinaryFocalLossWithLogits(nn.Module):
+    r"""Criterion that computes Focal loss.
+
+    According to :cite:`lin2017focal`, the Focal loss is computed as follows:
+
+    .. math::
+
+        \text{FL}(p_t) = -\alpha_t (1 - p_t)^{\gamma} \, \text{log}(p_t)
+
+    where:
+       - :math:`p_t` is the model's estimated probability for each class.
+
+
+    Arguments:
+        alpha (float): Weighting factor for the rare class :math:`\alpha \in [0, 1]`.
+        gamma (float): Focusing parameter :math:`\gamma >= 0`.
+        reduction (str, optional): Specifies the reduction to apply to the
+         output: ‘none’ | ‘mean’ | ‘sum’. ‘none’: no reduction will be applied,
+         ‘mean’: the sum of the output will be divided by the number of elements
+         in the output, ‘sum’: the output will be summed. Default: ‘none’.
+
+    Shape:
+        - Input: :math:`(N, 1, *)`
+        - Target: :math:`(N, 1, *)`
+
+    Examples:
+        >>> N = 1  # num_classes
+        >>> kwargs = {"alpha": 0.25, "gamma": 2.0, "reduction": 'mean'}
+        >>> loss = BinaryFocalLossWithLogits(**kwargs)
+        >>> input = torch.randn(1, N, 3, 5, requires_grad=True)
+        >>> target = torch.empty(1, 3, 5, dtype=torch.long).random_(N)
+        >>> output = loss(input, target)
+        >>> output.backward()
+    """
+
+    def __init__(self, alpha: float, gamma: float = 2.0,
+                 reduction: str = 'none') -> None:
+        super(BinaryFocalLossWithLogits, self).__init__()
+        self.alpha: float = alpha
+        self.gamma: float = gamma
+        self.reduction: str = reduction
+        self.eps: float = 1e-8
+
+    def forward(
+            self,
+            input: torch.Tensor,
+            target: torch.Tensor) -> torch.Tensor:
+        return binary_focal_loss_with_logits(input, target, self.alpha, self.gamma, self.reduction, self.eps)
