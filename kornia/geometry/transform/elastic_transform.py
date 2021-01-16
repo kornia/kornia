0da760011c6ce120c78750dec1fb2b08e2bb32f9
edgarriba <edgar.riba@gmail.com>
2021-01-16 17:00:23 +0100

    implement functional elastic transform

--- kornia/geometry/transform/elastic_transform.py
+++ kornia/geometry/transform/elastic_transform.py
@@ -1,67 +1,90 @@
 from typing import Tuple, Optional
 
-import torch 
-import math
-import kornia
-
+import torch
 import torch.nn.functional as F
 
+import kornia
+from kornia.filters.kernels import get_gaussian_kernel2d
 
-def elastic_transform_2d(tensor: torch.Tensor, 
-                         kernel_size: Tuple[int, int] = (3,3), 
-                         sigma: Tuple[float, float] = (4.,4.), 
-                         alpha: Tuple[float, float] = (32.,32.), 
-                         random_seed: Optional = None) -> torch.Tensor:
-    r"""Applies elastic transform of images as described in [Simard2003]_.
+
+__all__ = ["elastic_transform2d"]
+
+
+def elastic_transform2d(image: torch.Tensor,
+                        noise: torch.Tensor,
+                        kernel_size: Tuple[int, int] = (3, 3),
+                        sigma: Tuple[float, float] = (4., 4.),
+                        alpha: Tuple[float, float] = (32., 32.),
+                        align_corners: bool = False) -> torch.Tensor:
+    r"""Applies elastic transform of images as described in :cite:`Simard2003BestPF`.
 
     Args:
-        img (torch.Tensor): input image.
-        kernel_size (Tuple[int, int]): the size of the Gaussian kernel. Default:(3,3).
-        sigma (Tuple[float, float]): the standard deviation of the Gaussian in the y and x directions, respecitvely. 
-                                     Larger sigma results in smaller pixel displacements. Default:(4,4).
-        alpha (Tuple[float, float]):  the scaling factor that controls the intensity of the deformation
-                                  in the y and x directions, respectively. Default:32.
-        random_seed (Optional): random seed for generating the displacement vector. Default:None.
-        
+        image (torch.Tensor): Input image to be transformed with shape :math:`(B, C, H, W)`.
+        noise (torch.Tensor): Noise image used to spatially transform the input image. Same
+          resolution as the input image with shape :math:`(B, 2, H, W)`. The coordinates order
+          it is expected to be in x-y.
+        kernel_size (Tuple[int, int]): the size of the Gaussian kernel. Default: (3, 3).
+        sigma (Tuple[float, float]): The standard deviation of the Gaussian in the y and x directions,
+          respecitvely. Larger sigma results in smaller pixel displacements. Default: (4, 4).
+        alpha (Tuple[float, float]): The scaling factor that controls the intensity of the deformation
+          in the y and x directions, respectively. Default: 32.
+        align_corners (bool): Interpolation flag used by `grid_sample`. Default: False.
+
+    .. note:
+        `sigma` and `alpha` can also be a `torch.Tensor`. However, you could not torchscript
+         this function with tensor until PyTorch 1.8 is released.
 
     Returns:
-        img (torch.Tensor): the elastically transformed input image.
+        torch.Tensor: the elastically transformed input image with shape :math:`(B,C,H,W)`.
 
-    References:
-        [Simard2003]: Simard, Steinkraus and Platt, "Best Practices for
-                      Convolutional Neural Networks applied to Visual Document Analysis", in
-                      Proc. of the International Conference on Document Analysis and
-                      Recognition, 2003.
+    Example:
+        >>> image = torch.rand(1, 3, 5, 5)
+        >>> noise = torch.rand(1, 2, 5, 5, requires_grad=True)
+        >>> image_hat = elastic_transform2d(image, noise, (3, 3))
+        >>> image_hat.mean().backward()
+
+        >>> image = torch.rand(1, 3, 5, 5)
+        >>> noise = torch.rand(1, 2, 5, 5)
+        >>> sigma = torch.tensor([4., 4.], requires_grad=True)
+        >>> image_hat = elastic_transform2d(image, noise, (3, 3), sigma)
+        >>> image_hat.mean().backward()
+
+        >>> image = torch.rand(1, 3, 5, 5)
+        >>> noise = torch.rand(1, 2, 5, 5)
+        >>> alpha = torch.tensor([16., 32.], requires_grad=True)
+        >>> image_hat = elastic_transform2d(image, noise, (3, 3), alpha=alpha)
+        >>> image_hat.mean().backward()
     """
-    generator = torch.Generator(device='cpu')
-    if random_seed is not None:
-        generator.manual_seed(random_seed)
+    if not isinstance(image, torch.Tensor):
+        raise TypeError(f"Input image is not torch.Tensor. Got {type(image)}")
 
-    n, c, h, w = tensor.shape
-    
+    if not isinstance(noise, torch.Tensor):
+        raise TypeError(f"Input noise is not torch.Tensor. Got {type(noise)}")
+
+    if not len(image.shape) == 4:
+        raise ValueError(f"Invalid image shape, we expect BxCxHxW. Got: {image.shape}")
+
+    if not len(noise.shape) == 4 or noise.shape[1] != 2:
+        raise ValueError(f"Invalid noise shape, we expect Bx2xHxW. Got: {noise.shape}")
+
     # Get Gaussian kernel for 'y' and 'x' displacement
-    kernel_y = get_gaussian_kernel2d(kernel_size, (sigma[0], sigma[0]))[None]
-    kernel_x = get_gaussian_kernel2d(kernel_size, (sigma[1], sigma[1]))[None]
+    kernel_x: torch.Tensor = get_gaussian_kernel2d(kernel_size, (sigma[0], sigma[0]))[None]
+    kernel_y: torch.Tensor = get_gaussian_kernel2d(kernel_size, (sigma[1], sigma[1]))[None]
 
     # Convolve over a random displacement matrix and scale them with 'alpha'
-    disp = torch.rand(n, 2, h, w, generator=generator).to(device=tensor.device)
-    disp[:,0] *= disp_scale[0]
-    disp[:,1] *= disp_scale[1]
+    disp_x: torch.Tensor = noise[:, :1]
+    disp_y: torch.Tensor = noise[:, 1:]
 
-    disp_y = kornia.filters.filter2D(disp[:,[0]], kernel=kernel_y, border_type='reflect') * alpha[0]
-    disp_x = kornia.filters.filter2D(disp[:,[1]], kernel=kernel_x, border_type='reflect') * alpha[1]
-
-    # scale displacements
-    disp_y *= alpha[0]
-    disp_x *= alpha[1]
+    disp_x = kornia.filters.filter2D(disp_x, kernel=kernel_y, border_type='constant') * alpha[0]
+    disp_y = kornia.filters.filter2D(disp_y, kernel=kernel_x, border_type='constant') * alpha[1]
 
     # stack and normalize displacement
-    disp = torch.cat([disp_y, disp_x], dim=1).squeeze(0).permute(0,2,3,1)
-    
+    disp = torch.cat([disp_x, disp_y], dim=1).permute(0, 2, 3, 1)
+
     # Warp image based on displacement matrix
-    grid = kornia.utils.create_meshgrid(h, w).to(device=tensor.device)
-    warped =  F.grid_sample(tensor, (grid + disp).clamp(-1,1), align_corners=True)
-    
+    b, c, h, w = image.shape
+    grid = kornia.utils.create_meshgrid(h, w, device=image.device).to(image.dtype)
+    warped = F.grid_sample(
+        image, (grid + disp).clamp(-1, 1), align_corners=align_corners)
+
     return warped
-
-
--- test/geometry/transform/test_elastic_transform.py
+++ test/geometry/transform/test_elastic_transform.py
@@ -1,31 +1,84 @@
+import pytest
+
+import torch
+from torch.autograd import gradcheck
+from torch.testing import assert_allclose
+
 import kornia
+from kornia.geometry.transform import elastic_transform2d
 
-import numpy as np
-from scipy.ndimage.interpolation import map_coordinates
-from scipy.ndimage.filters import gaussian_filter
-
-
-def elastic_transform_numpy(image, alpha, sigma, random_state=None):
-    """
-    obtained from: https://gist.github.com/chsasank/4d8f68caf01f041a6453e67fb30f8f5a
-    """
-    assert len(image.shape)==2
-
-    if random_state is None:
-        random_state = np.random.RandomState(None)
-
-    shape = image.shape
-
-    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-
-    x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), indexing='ij')
-    indices = np.reshape(x+dx, (-1, 1)), np.reshape(y+dy, (-1, 1))
-    
-    return map_coordinates(image, indices, order=1).reshape(shape)
 
 class TestElasticTransform:
-    def test_equivalence(self):
-        image = np.random.rand(100,100,3)
-        print(elastic_transform_numpy(image, alpha=30, sigma=10, random_state=42))
-        assert(3==3)
+
+    def test_smoke(self, device, dtype):
+        image = torch.rand(1, 4, 5, 5, device=device, dtype=dtype)
+        noise = torch.rand(1, 2, 5, 5, device=device, dtype=dtype)
+        assert elastic_transform2d(image, noise) is not None
+
+    @pytest.mark.parametrize(
+        "batch, channels, height, width", [(1, 3, 3, 4), (2, 2, 2, 4), (1, 5, 4, 1)])
+    def test_cardinality(self, device, dtype, batch, channels, height, width):
+        shape = batch, channels, height, width
+        img = torch.ones(shape, device=device, dtype=dtype)
+        noise = torch.ones((batch, 2, height, width), device=device, dtype=dtype)
+        assert elastic_transform2d(img, noise).shape == shape
+
+    def test_exception(self, device, dtype):
+        with pytest.raises(TypeError):
+            assert elastic_transform2d([0.])
+
+        with pytest.raises(TypeError):
+            assert elastic_transform2d(torch.tensor(), 1)
+
+        with pytest.raises(ValueError):
+            img = torch.ones(1, 1, 1, device=device, dtype=dtype)
+            noise = torch.ones(1, 2, 1, 1, device=device, dtype=dtype)
+            assert elastic_transform2d(img, noise)
+
+        with pytest.raises(ValueError):
+            img = torch.ones(1, 1, 1, 1, device=device, dtype=dtype)
+            noise = torch.ones(1, 3, 1, 1, device=device, dtype=dtype)
+            assert elastic_transform2d(img, noise)
+
+    @pytest.mark.parametrize("kernel_size, sigma, alpha", [
+        [(3, 3), (4., 4.), (32., 32.)],
+        [(5, 3), (4., 8.), (16., 32.)],
+        [(5, 5), torch.tensor([2., 8.]), torch.tensor([16., 64.])],
+    ])
+    def test_valid_paramters(self, device, dtype, kernel_size, sigma, alpha):
+        image = torch.rand(1, 4, 5, 5, device=device, dtype=dtype)
+        noise = torch.rand(1, 2, 5, 5, device=device, dtype=dtype)
+        assert elastic_transform2d(image, noise, kernel_size, sigma, alpha) is not None
+
+    def test_values(self, device, dtype):
+        image = torch.tensor([[
+            [[0.0018, 0.7521, 0.7550],
+             [0.2053, 0.4249, 0.1369],
+             [0.1027, 0.3992, 0.8773]]
+        ]], device=device, dtype=dtype)
+
+        noise = torch.ones(1, 2, 3, 3, device=device, dtype=dtype)
+
+        expected = torch.tensor([[
+            [[0.2193, 0.2193, 0.2193],
+             [0.2193, 0.2193, 0.2193],
+             [0.2193, 0.2193, 0.2193]]
+        ]], device=device, dtype=dtype)
+
+        actual = elastic_transform2d(image, noise)
+        assert_allclose(actual, expected, atol=1e-3, rtol=1e-3)
+
+    @pytest.mark.parametrize("requires_grad", [True, False])
+    def test_gradcheck(self, device, dtype, requires_grad):
+        image = torch.rand(1, 1, 3, 3, device=device, dtype=torch.float64, requires_grad=requires_grad)
+        noise = torch.rand(1, 2, 3, 3, device=device, dtype=torch.float64, requires_grad=not requires_grad)
+        assert gradcheck(elastic_transform2d, (image, noise,), raise_exception=True)
+
+    def test_jit(self, device, dtype):
+        image = torch.rand(1, 4, 5, 5, device=device, dtype=dtype)
+        noise = torch.rand(1, 2, 5, 5, device=device, dtype=dtype)
+
+        op = kornia.geometry.transform.elastic_transform2d
+        op_jit = torch.jit.script(op)
+
+        assert_allclose(op(image, noise), op_jit(image, noise))
