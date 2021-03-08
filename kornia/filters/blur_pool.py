119696ca2a14c2a479a4157590dc277939b0af37
shijianjian <sj8716643@126.com>
2021-03-08 11:47:57 +0800

    Moved kernel computing to buffer

--- kornia/filters/blur_pool.py
+++ kornia/filters/blur_pool.py
@@ -7,11 +7,18 @@ import torch.nn.functional as F
 
 import kornia
 from .kernels import get_pascal_kernel_2d
-from .median import _compute_zero_padding
+from .median import _compute_zero_padding  # TODO: Move to proper place
+
+__all__ = [
+    "BlurPool2D",
+    "MaxBlurPool2D",
+    "blur_pool2d",
+    "max_blur_pool2d",
+]
 
 
 class BlurPool2D(nn.Module):
-    r"""Compute blurs and downsample a given feature map.
+    r"""Compute blur (anti-aliasing) and downsample a given feature map.
 
     See :cite:`zhang2019shiftinvar` for more details.
 
@@ -31,24 +38,23 @@ class BlurPool2D(nn.Module):
               W_{out} = \left\lfloor\frac{W_{in}  + 2 \times \text{kernel\_size//2}[1] -
                 \text{kernel\_size}[1]}{\text{stride}[1]} + 1\right\rfloor
 
-    Returns:
-        torch.Tensor: the transformed tensor.
-
     Examples:
-    >>> input = torch.eye(5)[None, None]
-    >>> bp = BlurPool2D(kernel_size=3, stride=2)
-    >>> bp(input)
-    tensor([[[[0.3125, 0.0625, 0.0000],
-              [0.0625, 0.3750, 0.0625],
-              [0.0000, 0.0625, 0.3125]]]])
+        >>> input = torch.eye(5)[None, None]
+        >>> bp = BlurPool2D(kernel_size=3, stride=2)
+        >>> bp(input)
+        tensor([[[[0.3125, 0.0625, 0.0000],
+                  [0.0625, 0.3750, 0.0625],
+                  [0.0000, 0.0625, 0.3125]]]])
     """
     def __init__(self, kernel_size: int, stride: int = 2):
         super(BlurPool2D, self).__init__()
         self.kernel_size = kernel_size
         self.stride = stride
+        self.register_buffer('kernel', get_pascal_kernel_2d(kernel_size, norm=True))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return blur_pool2d(input, self.kernel_size, self.stride)
+        return _blur_pool_by_kernel2d(
+            input, self.kernel.repeat((input.size(1), 1, 1, 1)), self.stride)
 
 
 class MaxBlurPool2D(nn.Module):
@@ -71,15 +77,15 @@ class MaxBlurPool2D(nn.Module):
         torch.Tensor: the transformed tensor.
 
     Examples:
-    >>> input = torch.eye(5)[None, None]
-    >>> mbp = MaxBlurPool2D(kernel_size=3, stride=2, max_pool_size=2, ceil_mode=False)
-    >>> mbp(input)
-    tensor([[[[0.5625, 0.3125],
-              [0.3125, 0.8750]]]])
-    >>> seq = nn.Sequential(nn.MaxPool2d(kernel_size=2, stride=1), BlurPool2D(kernel_size=3, stride=2))
-    >>> seq(input)
-    tensor([[[[0.5625, 0.3125],
-              [0.3125, 0.8750]]]])
+        >>> input = torch.eye(5)[None, None]
+        >>> mbp = MaxBlurPool2D(kernel_size=3, stride=2, max_pool_size=2, ceil_mode=False)
+        >>> mbp(input)
+        tensor([[[[0.5625, 0.3125],
+                  [0.3125, 0.8750]]]])
+        >>> seq = nn.Sequential(nn.MaxPool2d(kernel_size=2, stride=1), BlurPool2D(kernel_size=3, stride=2))
+        >>> seq(input)
+        tensor([[[[0.5625, 0.3125],
+                  [0.3125, 0.8750]]]])
     """
     def __init__(self, kernel_size: int, stride: int = 2, max_pool_size: int = 2, ceil_mode: bool = False):
         super(MaxBlurPool2D, self).__init__()
@@ -87,9 +93,11 @@ class MaxBlurPool2D(nn.Module):
         self.stride = stride
         self.max_pool_size = max_pool_size
         self.ceil_mode = ceil_mode
+        self.register_buffer('kernel', get_pascal_kernel_2d(kernel_size, norm=True))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return max_blur_pool2d(input, self.kernel_size, self.stride, self.max_pool_size, self.ceil_mode)
+        return _max_blur_pool_by_kernel2d(
+            input, self.kernel.repeat((input.size(1), 1, 1, 1)), self.stride, self.max_pool_size, self.ceil_mode)
 
 
 def blur_pool2d(input: torch.Tensor, kernel_size: int, stride: int = 2):
@@ -120,15 +128,14 @@ def blur_pool2d(input: torch.Tensor, kernel_size: int, stride: int = 2):
         This function is tested against https://github.com/adobe/antialiased-cnns.
 
     Examples:
-    >>> input = torch.eye(5)[None, None]
-    >>> blur_pool2d(input, 3)
-    tensor([[[[0.3125, 0.0625, 0.0000],
-              [0.0625, 0.3750, 0.0625],
-              [0.0000, 0.0625, 0.3125]]]])
+        >>> input = torch.eye(5)[None, None]
+        >>> blur_pool2d(input, 3)
+        tensor([[[[0.3125, 0.0625, 0.0000],
+                  [0.0625, 0.3750, 0.0625],
+                  [0.0000, 0.0625, 0.3125]]]])
     """
-    padding: Tuple[int, int] = _compute_zero_padding((kernel_size, kernel_size))
     kernel = get_pascal_kernel_2d(kernel_size, norm=True).repeat((input.size(1), 1, 1, 1)).to(input)
-    return F.conv2d(input, kernel, padding=padding, stride=stride, groups=input.size(1))
+    return _blur_pool_by_kernel2d(input, kernel, stride)
 
 
 def max_blur_pool2d(
@@ -138,24 +145,44 @@ def max_blur_pool2d(
 
     See :class:`~kornia.filters.MaxBlurPool2d` for details.
 
+    Args:
+        kernel_size (int): the kernel size for max pooling.
+        stride (int): stride for pooling.
+        max_pool_size (int): the kernel size for max pooling.
+        ceil_mode (bool): should be true to match output size of conv2d with same kernel size.
+
     Note:
         This function is tested against https://github.com/adobe/antialiased-cnns.
 
     Examples:
-    >>> input = torch.eye(5)[None, None]
-    >>> max_blur_pool2d(input, 3)
-    tensor([[[[0.5625, 0.3125],
-              [0.3125, 0.8750]]]])
+        >>> input = torch.eye(5)[None, None]
+        >>> max_blur_pool2d(input, 3)
+        tensor([[[[0.5625, 0.3125],
+                  [0.3125, 0.8750]]]])
     """
     if not len(input.shape) == 4:
         raise ValueError(f"Invalid input shape, we expect BxCxHxW. Got: {input.shape}")
-    padding: Tuple[int, int] = _compute_zero_padding((kernel_size, kernel_size))
-    # compute local maxima
-    x_max: torch.Tensor = F.max_pool2d(
-        input, kernel_size=max_pool_size,
-        padding=0, stride=1, ceil_mode=ceil_mode)
-
-    # blur and downsample
     kernel = get_pascal_kernel_2d(kernel_size, norm=True).repeat((input.size(1), 1, 1, 1)).to(input)
-    x_down = F.conv2d(x_max, kernel, padding=padding, stride=stride, groups=x_max.size(1))
-    return x_down
+    return _max_blur_pool_by_kernel2d(input, kernel, stride, max_pool_size, ceil_mode)
+
+
+def _blur_pool_by_kernel2d(input: torch.Tensor, kernel: torch.Tensor, stride: int):
+    """Compute blur_pool by a given :math:`CxC_{out}xNxN` kernel."""
+    assert len(kernel.shape) == 4 and kernel.size(-1) == kernel.size(-2), \
+        f"Invalid kernel shape. Expect CxC_outxNxN, Got {kernel.shape}"
+    padding: Tuple[int, int] = _compute_zero_padding((kernel.shape[-2], kernel.shape[-1]))
+    return F.conv2d(input, kernel, padding=padding, stride=stride, groups=input.size(1))
+
+
+def _max_blur_pool_by_kernel2d(
+    input: torch.Tensor, kernel: torch.Tensor, stride: int, max_pool_size: int, ceil_mode: bool
+):
+    """Compute max_blur_pool by a given :math:`CxC_{out}xNxN` kernel."""
+    assert len(kernel.shape) == 4 and kernel.size(-1) == kernel.size(-2), \
+        f"Invalid kernel shape. Expect CxC_outxNxN, Got {kernel.shape}"
+    # compute local maxima
+    input = F.max_pool2d(
+        input, kernel_size=max_pool_size, padding=0, stride=1, ceil_mode=ceil_mode)
+    # blur and downsample
+    padding: Tuple[int, int] = _compute_zero_padding((kernel.shape[-2], kernel.shape[-1]))
+    return F.conv2d(input, kernel, padding=padding, stride=stride, groups=input.size(1))
