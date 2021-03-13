2c8ae53a8ad3efe33f3e417f34cbd19fb214a854
Edgar Riba <edgar.riba@gmail.com>
2021-03-13 13:42:10 +0100

    fix linter error

--- kornia/utils/draw.py
+++ kornia/utils/draw.py
@@ -61,9 +61,13 @@ def draw_rectangle(
                 image[b, :, int(rectangle[b, n, 1]):int(rectangle[b, n, 3] + 1),
                       int(rectangle[b, n, 0]):int(rectangle[b, n, 2] + 1)] = color[b, n, :, None, None]
             else:
-                image[b, :, int(rectangle[b, n, 1]):int(rectangle[b, n, 3] + 1), rectangle[b, n, 0]] = color[b, n, :, None]
-                image[b, :, int(rectangle[b, n, 1]):int(rectangle[b, n, 3] + 1), rectangle[b, n, 2]] = color[b, n, :, None]
-                image[b, :, rectangle[b, n, 1], int(rectangle[b, n, 0]):int(rectangle[b, n, 2] + 1)] = color[b, n, :, None]
-                image[b, :, rectangle[b, n, 3], int(rectangle[b, n, 0]):int(rectangle[b, n, 2] + 1)] = color[b, n, :, None]
+                image[b, :, int(rectangle[b, n, 1]):int(rectangle[b, n, 3] + 1),
+                      rectangle[b, n, 0]] = color[b, n, :, None]
+                image[b, :, int(rectangle[b, n, 1]):int(rectangle[b, n, 3] + 1),
+                      rectangle[b, n, 2]] = color[b, n, :, None]
+                image[b, :, rectangle[b, n, 1], int(rectangle[b, n, 0]):int(
+                    rectangle[b, n, 2] + 1)] = color[b, n, :, None]
+                image[b, :, rectangle[b, n, 3], int(rectangle[b, n, 0]):int(
+                    rectangle[b, n, 2] + 1)] = color[b, n, :, None]
 
     return image
