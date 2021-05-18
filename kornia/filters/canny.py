647173e5916abb84e552092147b0d037b70637a6
Pau <priba@cvc.uab.cat>
2021-05-19 01:15:54 +0200

    First Canny working version. Different results than openCV and scikit-image

--- kornia/filters/canny.py
+++ kornia/filters/canny.py
@@ -4,12 +4,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from kornia.geometry.conversions import rad2deg
+
 from kornia.filters import (
     gaussian_blur2d, spatial_gradient
 )
 
+from .kernels import (
+    get_canny_nms_kernel, get_hysteresis_kernel
+)
 
-def canny(input: torch.Tensor, kernel_size: Tuple[int, int] = (5,5), sigma: Tuple[float, float] = (1,1), normalized: bool = True, eps: float = 1e-6) -> torch.Tensor:
+# https://towardsdatascience.com/implement-canny-edge-detection-from-scratch-with-pytorch-a1cccfa58bed
+# https://github.com/DCurro/CannyEdgePytorch/blob/master/net_canny.py
+
+def canny(input: torch.Tensor, low_threshold: int = None, high_threshold: int = None, kernel_size: Tuple[int, int] = (5,5), sigma: Tuple[float, float] = (1,1), hysteresis: bool = False, eps: float = 1e-6) -> torch.Tensor:
     r"""Computes the Canny operator and returns the magnitude per channel.
 
     Args:
@@ -34,11 +42,14 @@ def canny(input: torch.Tensor, kernel_size: Tuple[int, int] = (5,5), sigma: Tupl
         raise ValueError("Invalid input shape, we expect BxCxHxW. Got: {}"
                          .format(input.shape))
 
+    device: torch.device = input.device
+    dtype: torch.dtype = input.dtype
+
     # Gaussian filter
     blurred: torch.Tensor = gaussian_blur2d(input, kernel_size, sigma)
 
     # comput the x/y gradients
-    edges: torch.Tensor = spatial_gradient(blurred, normalized=normalized)
+    edges: torch.Tensor = spatial_gradient(blurred, normalized=False)
 
     # unpack the edges
     gx: torch.Tensor = edges[:, :, 0]
@@ -48,11 +59,59 @@ def canny(input: torch.Tensor, kernel_size: Tuple[int, int] = (5,5), sigma: Tupl
     magnitude: torch.Tensor = torch.sqrt(gx * gx + gy * gy + eps)
     angle: torch.Tensor = torch.atan2(gy, gx)
 
+    # Radians to Degrees
+    angle = rad2deg(angle)
+
+    # Round angle to the nearest 45 degree
+    angle = torch.round(angle / 45) * 45
+
     # Non-maximal suppression
+    nms_kernels: torch.Tensor = get_canny_nms_kernel(device, dtype)
+    nms_magnitude: torch.Tensor = F.conv2d(magnitude, nms_kernels, padding=nms_kernels.shape[-1]//2)
+
+    positive_idx: torch.Tensor = (angle / 45) % 8
+    positive_idx = positive_idx.long()
+
+    negative_idx: torch.Tensor = ((angle / 45) + 4) % 8
+    negative_idx = negative_idx.long()
+
+    channel_select_filtered_positive: torch.Tensor = torch.gather(nms_magnitude, 1, positive_idx)
+    channel_select_filtered_negative: torch.Tensor = torch.gather(nms_magnitude, 1, negative_idx)
+
+    channel_select_filtered: torch.Tensor = torch.stack([channel_select_filtered_positive,channel_select_filtered_negative], 1)
+
+    is_max: torch.Tensor = channel_select_filtered.min(dim=1)[0] > 0.0
+
+    magnitude[is_max==0] = 0.0
 
     # Threshold & Hysteresis
+    if low_threshold is not None:
+        magnitude = F.threshold(magnitude, low_threshold, 0.0)
 
-    return magnitude
+        if high_threshold is not None:
+            low: torch.Tensor = magnitude > low_threshold
+            high: torch.Tensor = magnitude > high_threshold
+
+            edges: torch.Tensor = low * 0.5 + high * 0.5
+
+            if hysteresis:
+
+                edges_old: torch.Tensor = torch.zeros(edges.shape, device=edges.device, dtype=edges.dtype)
+                hysteresis_kernels: torch.Tensor = get_hysteresis_kernel(device, dtype)
+
+                while ((edges_old - edges).abs() != 0).any():
+                    weak: torch.Tensor = (edges == 0.5).float()
+                    strong: torch.Tensor = (edges == 1).float()
+
+                    hysteresis_magnitude: torch.Tensor = F.conv2d(edges, hysteresis_kernels, padding=hysteresis_kernels.shape[-1]//2)
+                    hysteresis_magnitude = (hysteresis_magnitude == 1).any(1, keepdim=True).float()
+                    hysteresis_magnitude = hysteresis_magnitude * weak + strong
+
+                    edges_old = edges.clone()
+                    edges = hysteresis_magnitude + (hysteresis_magnitude==0) * weak * 0.5
+
+
+    return magnitude, hysteresis_magnitude
 
 
 class Canny(nn.Module):
@@ -75,15 +134,20 @@ class Canny(nn.Module):
     """
 
     def __init__(self,
-                 kernel_size: Tuple[int, int] = (5,5), sigma: Tuple[float, float] = (1,1), normalized: bool = True, eps: float = 1e-6) -> None:
+                 low_threshold: int = None, high_threshold: int = None,
+                 kernel_size: Tuple[int, int] = (5,5), sigma: Tuple[float, float] = (1,1), normalized: bool = True, hysteresis: bool = False, eps: float = 1e-6) -> None:
         super(Canny, self).__init__()
 
         # Gaussian blur parameters
         self.kernel_size = kernel_size
         self.sigma = sigma
 
-        # Spatial gradients
-        self.normalized = normalized
+        # Double threshold
+        self.low_threshold = low_threshold
+        self.high_threshold = high_threshold
+
+        # Hysteresis
+        self.hysteresis = hysteresis
 
         self.eps: float = eps
 
@@ -93,5 +157,5 @@ class Canny(nn.Module):
             'sigma=' + str(self.sigma) + ')'
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return canny(input, self.kernel_size, self.sigma, self.normalized, self.eps)
+        return canny(input, self.low_threshold, self.high_threshold, self.kernel_size, self.sigma, self.hysteresis, self.eps)
 
