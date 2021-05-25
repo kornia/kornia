de3af335f498ee3b7f73487da3a2c283231685b0
shijianjian <sj8716643@126.com>
2021-05-25 09:10:04 +0800

    [feat] Added inverse for augmentations. (#1013)

--- kornia/augmentation/base.py
+++ kornia/augmentation/base.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch.distributions import Bernoulli
 
 import kornia
+from kornia.utils.helpers import _torch_inverse_cast
 from . import functional as F
 from .utils import (
     _transform_input,
@@ -278,6 +279,97 @@ class AugmentationBase2D(_AugmentationBase):
     def identity_matrix(self, input) -> torch.Tensor:
         """Return 3x3 identity matrix."""
         return kornia.eye_like(3, input)
+
+
+class IntensityAugmentationBase2D(AugmentationBase2D):
+    r"""IntensityAugmentationBase2D base class for customized intensity augmentation implementations.
+
+    For any augmentation, the implementation of "generate_parameters" and "apply_transform" are required while the
+    "compute_transformation" is only required when passing "return_transform" as True.
+
+    Args:
+        p (float): probability for applying an augmentation. This param controls the augmentation probabilities
+                   element-wisely for a batch.
+        p_batch (float): probability for applying an augmentation to a batch. This param controls the augmentation
+                         probabilities batch-wisely.
+        return_transform (bool): if ``True`` return the matrix describing the geometric transformation applied to each
+                                      input tensor. If ``False`` and the input is a tuple the applied transformation
+                                      wont be concatenated.
+        same_on_batch (bool): apply the same transformation across the batch. Default: False.
+        keepdim (bool): whether to keep the output shape the same as input (True) or broadcast it
+                        to the batch form (False). Default: False.
+    """
+
+    def compute_transformation(self, input: torch.Tensor, params: Dict[str, torch.Tensor]) -> torch.Tensor:
+        return self.identity_matrix(input)
+
+
+class GeometricAugmentationBase2D(AugmentationBase2D):
+    r"""GeometricAugmentationBase2D base class for customized geometric augmentation implementations.
+
+    For any augmentation, the implementation of "generate_parameters" and "apply_transform" are required while the
+    "compute_transformation" is only required when passing "return_transform" as True.
+
+    Args:
+        p (float): probability for applying an augmentation. This param controls the augmentation probabilities
+                   element-wisely for a batch.
+        p_batch (float): probability for applying an augmentation to a batch. This param controls the augmentation
+                         probabilities batch-wisely.
+        return_transform (bool): if ``True`` return the matrix describing the geometric transformation applied to each
+                                      input tensor. If ``False`` and the input is a tuple the applied transformation
+                                      wont be concatenated.
+        same_on_batch (bool): apply the same transformation across the batch. Default: False.
+        keepdim (bool): whether to keep the output shape the same as input (True) or broadcast it
+                        to the batch form (False). Default: False.
+    """
+
+    def inverse_transform(
+        self, input: torch.Tensor, transform: Optional[torch.Tensor] = None,
+        size: Optional[Tuple[int, int]] = None, **kwargs
+    ) -> torch.Tensor:
+        """By default, the exact transformation as ``apply_transform`` will be used.
+        """
+        return self.apply_transform(input, params=self._params, transform=transform)
+
+    def compute_inverse_transformation(self, transform: torch.Tensor):
+        """Compute the inverse transform of given transformation matrices.
+        """
+        return _torch_inverse_cast(transform)
+
+    def inverse(
+        self, input: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]],
+        params: Optional[Dict[str, torch.Tensor]] = None, size: Optional[Tuple[int, int]] = None, **kwargs
+    ) -> torch.Tensor:
+        if isinstance(input, (list, tuple,)):
+            input, transform = input
+        else:
+            transform = self._transform_matrix
+        ori_shape = input.shape
+        in_tensor = self.transform_tensor(input)
+        batch_shape = input.shape
+        if params is None:
+            params = self._params
+        if size is None and "input_size" in params:
+            # Majorly for copping functions
+            size = params['input_size'].unique(dim=0).squeeze().numpy().tolist()
+            size = (size[0], size[1])
+        if 'batch_prob' not in params:
+            params['batch_prob'] = torch.tensor([True] * batch_shape[0])
+            warnings.warn("`batch_prob` is not found in params. Will assume applying on all data.")
+        output = input.clone()
+        to_apply = params['batch_prob']
+        # if no augmentation needed
+        if torch.sum(to_apply) == 0:
+            output = in_tensor
+        # if all data needs to be augmented
+        elif torch.sum(to_apply) == len(to_apply):
+            transform = self.compute_inverse_transformation(transform)
+            output = self.inverse_transform(in_tensor, transform, size, **kwargs)
+        else:
+            transform[to_apply] = self.compute_inverse_transformation(transform[to_apply])
+            output[to_apply] = self.inverse_transform(
+                in_tensor[to_apply], transform[to_apply], size, **kwargs)
+        return cast(torch.Tensor, _transform_output_shape(output, ori_shape)) if self.keepdim else output
 
 
 class AugmentationBase3D(_AugmentationBase):
