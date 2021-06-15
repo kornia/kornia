9937293188a6aee011f3daf9140de08f846eb1de
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-06-15 18:57:28 +0200

    Update morphology.py
    no flip for erosion

--- kornia/morphology/morphology.py
+++ kornia/morphology/morphology.py
@@ -165,7 +165,7 @@ def erosion(
     Hpad, Wpad = output.shape[-2:]
     reshape_kernel = _neight2channels_like_kernel(kernel)
     output, _ = F.conv2d(
-        output.view(B * C, 1, Hpad, Wpad), reshape_kernel, padding=0, bias=-neighborhood.view(-1).flip(0)
+        output.view(B * C, 1, Hpad, Wpad), reshape_kernel, padding=0, bias=-neighborhood.view(-1)
     ).min(dim=1)
     output = output.view(B, C, H, W)
     return output
