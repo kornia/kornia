5f9b06b8d64bcd43eebd186ae21d22a1d0165b5e
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-06-17 15:19:44 +0200

    increase test tolerance, so cuda-float16 not failing

--- test/morphology/test_closing.py
+++ test/morphology/test_closing.py
@@ -26,7 +26,7 @@ class TestClosing:
         expected = torch.tensor([[0.7, 1.0, 0.8], [0.7, 0.7, 0.8], [0.7, 0.9, 0.8]], device=device, dtype=dtype)[
             None, None, :, :
         ]
-        assert_allclose(closing(tensor, kernel), expected)
+        assert_allclose(closing(tensor, kernel), expected, atol=1e-4, rtol=1e-4)
 
     def test_structural_element(self, device, dtype):
         tensor = torch.tensor([[0.5, 1.0, 0.3], [0.7, 0.3, 0.8], [0.4, 0.9, 0.2]], device=device, dtype=dtype)[
@@ -39,7 +39,8 @@ class TestClosing:
             None, None, :, :
         ]
         assert_allclose(
-            closing(tensor, torch.ones_like(structural_element), structuring_element=structural_element), expected
+            closing(tensor, torch.ones_like(structural_element), structuring_element=structural_element), expected,
+            atol=1e-4, rtol=1e-4
         )
 
     def test_exception(self, device, dtype):
