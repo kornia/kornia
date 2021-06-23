cd51772bbe4eec0addd4e149b2f47f999020e7a0
Edgar Riba <edgar.riba@gmail.com>
2021-06-23 08:22:29 +0200

    remove numpy import (#1116)
    * remove numpy import
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * undo and add setup develop in gh actions
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove create_checkerboard
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- test/morphology/test_bottom_hat.py
+++ test/morphology/test_bottom_hat.py
@@ -39,8 +39,10 @@ class TestBottomHat:
             None, None, :, :
         ]
         assert_allclose(
-            bottom_hat(tensor, torch.ones_like(structural_element), structuring_element=structural_element), expected,
-            atol=1e-3, rtol=1e-3
+            bottom_hat(tensor, torch.ones_like(structural_element), structuring_element=structural_element),
+            expected,
+            atol=1e-3,
+            rtol=1e-3,
         )
 
     def test_exception(self, device, dtype):
