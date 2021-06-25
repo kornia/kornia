c80f21b4d00ae0da14aa0f4c3bf350c03bc361f1
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-06-25 13:16:43 +0200

    Update generate_example_images.py

--- docs/generate_example_images.py
+++ docs/generate_example_images.py
@@ -12,7 +12,8 @@ import kornia as K
 mod = importlib.import_module("kornia.augmentation")
 
 BASE_IMAGE_URL: str = "https://raw.githubusercontent.com/kornia/data/main/panda.jpg"
-OUTPUT_PATH = Path(__file__).absolute().parent / "source/_static"
+OUTPUT_PATH = Path(__file__).absolute().parent / "build/html/_static"
+os.makedirs(OUTPUT_PATH, exist_ok=True)
 
 # perform request
 response = requests.get(BASE_IMAGE_URL).content
