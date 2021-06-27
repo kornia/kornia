a42750f735bc83e29b90dc70140843ed08971a80
Jian Shi <sj8716643@126.com>
2021-06-27 20:35:53 +0800

    [feat] Added random_apply to augmentaton containers (#1125)
    * Added random_apply parameter for containers.
    
    * Added random_apply
    
    * Updated patchwise forward to be image-patch-wise.
    
    * Added tests
    
    * Fixed lint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed lint
    
    * Bug fix
    
    * fixed typing
    
    * Added replacement feature for containers
    
    * Removed redundent imports
    
    * Fixed a bug during the invert
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/augmentation/container/image.py
+++ kornia/augmentation/container/image.py
@@ -1,22 +1,38 @@
-from typing import Dict, Optional, Tuple, Union
+from collections import OrderedDict
+from itertools import zip_longest
+from typing import Any, Iterator, List, NamedTuple, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
 
 from kornia.augmentation.base import _AugmentationBase
 
+__all__ = ["ImageSequential"]
+
+
+class ParamItem(NamedTuple):
+    name: str
+    data: Union[dict, list]
+
 
 class ImageSequential(nn.Sequential):
     r"""Sequential for creating kornia image processing pipeline.
 
     Args:
-        *args (nn.Module): a list of kornia augmentation and image operation modules.
-        same_on_batch (bool, optional): apply the same transformation across the batch.
-            If None, it will not overwrite the function-wise settings. Default: None.
-        return_transform (bool, optional): if ``True`` return the matrix describing the transformation
-            applied to each. If None, it will not overwrite the function-wise settings. Default: None.
-        keepdim (bool, optional): whether to keep the output shape the same as input (True) or broadcast it
-            to the batch form (False). If None, it will not overwrite the function-wise settings. Default: None.
+        *args : a list of kornia augmentation and image operation modules.
+        same_on_batch: apply the same transformation across the batch.
+            If None, it will not overwrite the function-wise settings.
+        return_transform: if ``True`` return the matrix describing the transformation
+            applied to each. If None, it will not overwrite the function-wise settings.
+        keepdim: whether to keep the output shape the same as input (True) or broadcast it
+            to the batch form (False). If None, it will not overwrite the function-wise settings.
+        random_apply: randomly select a sublist (order agnostic) of args to
+            apply transformation.
+            If int, a fixed number of transformations will be selected.
+            If (a,), x number of transformations (a <= x <= len(args)) will be selected.
+            If (a, b), x number of transformations (a <= x <= b) will be selected.
+            If True, the whole list of args will be processed as a sequence in a random order.
+            If False, the whole list of args will be processed as a sequence in original order.
 
     Returns:
         Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]: the tensor (, and the transformation matrix)
@@ -33,6 +49,7 @@ class ImageSequential(nn.Sequential):
         ...     kornia.enhance.Invert(),
         ... return_transform=True,
         ... same_on_batch=True,
+        ... random_apply=10,
         ... )
         >>> out = aug_list(input)
         >>> out[0].shape, out[1].shape
@@ -54,12 +71,14 @@ class ImageSequential(nn.Sequential):
         same_on_batch: Optional[bool] = None,
         return_transform: Optional[bool] = None,
         keepdim: Optional[bool] = None,
+        random_apply: Union[int, bool, Tuple[int, int]] = False,
     ) -> None:
-        super(ImageSequential, self).__init__(*args)
         self.same_on_batch = same_on_batch
         self.return_transform = return_transform
         self.keepdim = keepdim
-        for arg in args:
+        # To name the modules properly
+        _args = OrderedDict()
+        for idx, arg in enumerate(args):
             if not isinstance(arg, nn.Module):
                 raise NotImplementedError(f"Only nn.Module are supported at this moment. Got {arg}.")
             if isinstance(arg, _AugmentationBase):
@@ -69,39 +88,102 @@ class ImageSequential(nn.Sequential):
                     arg.return_transform = return_transform
                 if keepdim is not None:
                     arg.keepdim = keepdim
-        self._params: Dict[str, Dict[str, torch.Tensor]] = {}
+            _args.update({f"{arg.__class__.__name__}_{idx}": arg})
+        super(ImageSequential, self).__init__(_args)
+
+        self._params: List[Any] = []
+        self.random_apply: Union[Tuple[int, int], bool]
+        if random_apply:
+            if isinstance(random_apply, (bool,)) and random_apply is True:
+                self.random_apply = (len(args), len(args) + 1)
+            elif isinstance(random_apply, (int,)):
+                self.random_apply = (random_apply, random_apply + 1)
+            elif (
+                isinstance(random_apply, (tuple,))
+                and len(random_apply) == 2
+                and isinstance(random_apply[0], (int,))
+                and isinstance(random_apply[1], (int,))
+            ):
+                self.random_apply = (random_apply[0], random_apply[1] + 1)
+            elif isinstance(random_apply, (tuple,)) and len(random_apply) == 1 and isinstance(random_apply[0], (int,)):
+                self.random_apply = (random_apply[0], len(args) + 1)
+            else:
+                raise ValueError(f"Non-readable random_apply. Got {random_apply}.")
+            assert (
+                isinstance(self.random_apply, (tuple,))
+                and len(self.random_apply) == 2
+                and isinstance(self.random_apply[0], (int,))
+                and isinstance(self.random_apply[0], (int,))
+            ), f"Expect a tuple of (int, int). Got {self.random_apply}."
+        else:
+            self.random_apply = False
+
+    def _get_child_sequence(self) -> Iterator[Tuple[str, nn.Module]]:
+        if self.random_apply:
+            # random_apply will not be boolean here.
+            num_samples = int(torch.randint(*self.random_apply, (1,)).item())  # type: ignore
+            indices = torch.multinomial(
+                torch.ones((len(self),)), num_samples, replacement=True if num_samples > len(self) else False
+            )
+            return self._get_children_by_indices(indices)
+        return self.named_children()
+
+    def _get_children_by_indices(self, indices: torch.Tensor) -> Iterator[Tuple[str, nn.Module]]:
+        modules = list(self.named_children())
+        for idx in indices:
+            yield modules[idx]
+
+    def _get_children_by_module_names(self, names: List[str]) -> Iterator[Tuple[str, nn.Module]]:
+        modules = list(self.named_children())
+        for name in names:
+            yield modules[list(dict(self.named_children()).keys()).index(name)]
+
+    def get_forward_sequence(self, params: Optional[List[ParamItem]] = None) -> Iterator[Tuple[str, nn.Module]]:
+        if params is None:
+            named_modules = self._get_child_sequence()
+        else:
+            named_modules = self._get_children_by_module_names([p.name for p in params])
+        return named_modules
 
     def apply_to_input(
         self,
         input: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]],
-        module: nn.Module,
-        param: Optional[Dict[str, torch.Tensor]] = None,
+        module_name: str,
+        module: Optional[nn.Module] = None,
+        param: Optional[ParamItem] = None,
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
-        func_name = module.__class__.__name__
-        if isinstance(module, _AugmentationBase) and param is None:
-            input = module(input)
-            self._params.update({func_name: module._params})
-        elif isinstance(module, _AugmentationBase) and param is not None:
-            input = module(input, param)
-            self._params.update({func_name: param})
+        if module is None:
+            module = self.get_submodule(module_name)
+        if param is not None:
+            assert module_name == param.name
+            _param = param.data
         else:
+            _param = None  # type: ignore
+
+        if isinstance(module, (_AugmentationBase, ImageSequential)) and _param is None:
+            input = module(input)
+            self._params.append(ParamItem(module_name, module._params))
+        elif isinstance(module, (_AugmentationBase, ImageSequential)) and _param is not None:
+            input = module(input, params=_param)
+            self._params.append(ParamItem(module_name, _param))
+        else:
+            assert (
+                _param == {} or _param is None
+            ), f"Non-augmentaion operation {module_name} require empty parameters. Got {module}."
             # In case of return_transform = True
             if isinstance(input, (tuple, list)):
                 input = (module(input[0]), input[1])
             else:
                 input = module(input)
+            self._params.append(ParamItem(module_name, {}))
         return input
 
     def forward(
-        self,
-        input: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]],
-        params: Optional[Dict[str, Dict[str, torch.Tensor]]] = None,
+        self, input: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]], params: Optional[List[ParamItem]] = None
     ) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
-        if params is None:
-            params = {}
-        self._params = {}
-        for module in self.children():
-            func_name = module.__class__.__name__
-            param = params[func_name] if func_name in params else None
-            input = self.apply_to_input(input, module, param)  # type: ignore
+        self._params = []
+        named_modules = self.get_forward_sequence(params)
+        params = [] if params is None else params
+        for (name, module), param in zip_longest(named_modules, params):
+            input = self.apply_to_input(input, name, module, param=param)  # type: ignore
         return input
--- test/augmentation/test_container.py
+++ test/augmentation/test_container.py
@@ -8,6 +8,30 @@ from kornia.constants import BorderType
 from kornia.geometry.transform import bbox_to_mask
 
 
+def reproducibility_test(input, seq):
+    if isinstance(input, (tuple, list)):
+        output_1 = seq(*input)
+        output_2 = seq(*input, params=seq._params)
+    else:
+        output_1 = seq(input)
+        output_2 = seq(input, params=seq._params)
+
+    if isinstance(output_1, (tuple, list)) and isinstance(output_2, (tuple, list)):
+        [
+            assert_allclose(o1, o2)
+            for o1, o2 in zip(output_1, output_2)
+            if isinstance(o1, (torch.Tensor,)) and isinstance(o2, (torch.Tensor,))
+        ]
+    elif isinstance(output_1, (tuple, list)) and isinstance(output_2, (torch.Tensor,)):
+        assert_allclose(output_1[0], output_2)
+    elif isinstance(output_2, (tuple, list)) and isinstance(output_1, (torch.Tensor,)):
+        assert_allclose(output_1, output_2[0])
+    elif isinstance(output_2, (torch.Tensor,)) and isinstance(output_1, (torch.Tensor,)):
+        assert_allclose(output_1, output_2, msg=f"{seq._params}")
+    else:
+        assert False, ("cannot compare", type(output_1), type(output_2))
+
+
 class TestVideoSequential:
     @pytest.mark.parametrize('shape', [(3, 4), (2, 3, 4), (2, 3, 5, 6), (2, 3, 4, 5, 6, 7)])
     @pytest.mark.parametrize('data_format', ["BCTHW", "BTCHW"])
@@ -45,24 +69,13 @@ class TestVideoSequential:
         input = torch.randint(255, (1, 3, 3, 5, 6), device=device, dtype=dtype).repeat(2, 1, 1, 1, 1) / 255.0
         torch.manual_seed(21)
         aug_list = K.VideoSequential(augmentation, data_format=data_format, same_on_frame=True)
-        output = aug_list(input)
-
-    @pytest.mark.parametrize('augmentations', [[K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5), K.RandomAffine(360, p=0.5)]])
-    @pytest.mark.parametrize('data_format', ["BCTHW", "BTCHW"])
-    def test_p_half(self, augmentations, data_format, device, dtype):
-        input = torch.randn(1, 3, 3, 5, 6, device=device, dtype=dtype).repeat(2, 1, 1, 1, 1)
-        torch.manual_seed(21)
-        aug_list = K.VideoSequential(*augmentations, data_format=data_format, same_on_frame=True)
-        output = aug_list(input)
-
-        assert not (output[0] == input[0]).all()
-        assert (output[1] == input[1]).all()
+        reproducibility_test(input, aug_list)
 
     @pytest.mark.parametrize(
         'augmentations',
         [
             [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0), K.RandomAffine(360, p=1.0)],
-            [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0)],
+            [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0), K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0)],
             [K.RandomAffine(360, p=1.0), kornia.color.BgrToRgb()],
             [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.0), K.RandomAffine(360, p=0.0)],
             [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.0)],
@@ -70,8 +83,11 @@ class TestVideoSequential:
         ],
     )
     @pytest.mark.parametrize('data_format', ["BCTHW", "BTCHW"])
-    def test_same_on_frame(self, augmentations, data_format, device, dtype):
-        aug_list = K.VideoSequential(*augmentations, data_format=data_format, same_on_frame=True)
+    @pytest.mark.parametrize('random_apply', [1, (1, 1), (1,), 10, True, False])
+    def test_same_on_frame(self, augmentations, data_format, random_apply, device, dtype):
+        aug_list = K.VideoSequential(
+            *augmentations, data_format=data_format, same_on_frame=True, random_apply=random_apply
+        )
 
         if data_format == 'BCTHW':
             input = torch.randn(2, 3, 1, 5, 6, device=device, dtype=dtype).repeat(1, 1, 4, 1, 1)
@@ -85,6 +101,7 @@ class TestVideoSequential:
             assert (output[:, 0] == output[:, 1]).all()
             assert (output[:, 1] == output[:, 2]).all()
             assert (output[:, 2] == output[:, 3]).all()
+        reproducibility_test(input, aug_list)
 
     @pytest.mark.parametrize(
         'augmentations', [[K.RandomAffine(360, p=1.0)], [K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0)]]
@@ -101,17 +118,15 @@ class TestVideoSequential:
 
         torch.manual_seed(0)
         output_1 = aug_list_1(input)
-        param_1 = list(aug_list_1.children())[0]._params
 
         torch.manual_seed(0)
         if data_format == 'BCTHW':
             input = input.transpose(1, 2)
         output_2 = aug_list_2(input.reshape(-1, 3, 5, 6))
-        param_2 = list(aug_list_2.children())[0]._params
         output_2 = output_2.view(2, 4, 3, 5, 6)
         if data_format == 'BCTHW':
             output_2 = output_2.transpose(1, 2)
-        assert (output_1 == output_2).all()
+        assert (output_1 == output_2).all(), dict(aug_list_1._params)
 
     @pytest.mark.jit
     @pytest.mark.skip(reason="turn off due to Union Type")
@@ -127,17 +142,20 @@ class TestSequential:
     @pytest.mark.parametrize('same_on_batch', [True, False, None])
     @pytest.mark.parametrize("return_transform", [True, False, None])
     @pytest.mark.parametrize("keepdim", [True, False, None])
-    def test_construction(self, same_on_batch, return_transform, keepdim):
+    @pytest.mark.parametrize('random_apply', [1, (2, 2), (1, 2), (2,), 10, True, False])
+    def test_construction(self, same_on_batch, return_transform, keepdim, random_apply):
         K.ImageSequential(
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0),
             K.RandomAffine(360, p=1.0),
             same_on_batch=same_on_batch,
             return_transform=return_transform,
             keepdim=keepdim,
+            random_apply=random_apply,
         )
 
     @pytest.mark.parametrize("return_transform", [True, False, None])
-    def test_forward(self, return_transform, device, dtype):
+    @pytest.mark.parametrize('random_apply', [1, (2, 2), (1, 2), (2,), 10, True, False])
+    def test_forward(self, return_transform, random_apply, device, dtype):
         inp = torch.randn(1, 3, 30, 30, device=device, dtype=dtype)
         aug = K.ImageSequential(
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0),
@@ -145,12 +163,14 @@ class TestSequential:
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0, return_transform=True),
             K.RandomAffine(360, p=1.0),
             return_transform=return_transform,
+            random_apply=random_apply,
         )
         out = aug(inp)
         if isinstance(out, (tuple,)):
             assert out[0].shape == inp.shape
         else:
             assert out.shape == inp.shape
+        reproducibility_test(inp, aug)
 
 
 class TestAugmentationSequential:
@@ -162,7 +182,8 @@ class TestAugmentationSequential:
         with pytest.raises(Exception):  # AssertError and NotImplementedError
             K.AugmentationSequential(augmentation_list, data_keys=data_keys)
 
-    def test_forward_and_inverse(self, device, dtype):
+    @pytest.mark.parametrize('random_apply', [1, (2, 2), (1, 2), (2,), 10, True, False])
+    def test_forward_and_inverse(self, random_apply, device, dtype):
         inp = torch.randn(1, 3, 1000, 500, device=device, dtype=dtype)
         bbox = torch.tensor([[[355, 10], [660, 10], [660, 250], [355, 250]]], device=device, dtype=dtype)
         keypoints = torch.tensor([[[465, 115], [545, 116]]], device=device, dtype=dtype)
@@ -173,12 +194,14 @@ class TestAugmentationSequential:
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0),
             K.RandomAffine(360, p=1.0),
             data_keys=["input", "mask", "bbox", "keypoints"],
+            random_apply=random_apply,
         )
         out = aug(inp, mask, bbox, keypoints)
         assert out[0].shape == inp.shape
         assert out[1].shape == mask.shape
         assert out[2].shape == bbox.shape
         assert out[3].shape == keypoints.shape
+        reproducibility_test((inp, mask, bbox, keypoints), aug)
 
         out_inv = aug.inverse(*out)
         assert out_inv[0].shape == inp.shape
@@ -194,13 +217,16 @@ class TestAugmentationSequential:
             torch.tensor([[[155, 0], [900, 0], [900, 400], [155, 400]]], device=device, dtype=dtype), 1000, 500
         )[:, None].float()
 
+        aug = K.AugmentationSequential(
+            K.RandomAffine(360, p=1.0, return_transform=False), data_keys=['input', 'mask', 'bbox', 'keypoints']
+        )
+        reproducibility_test((inp, mask, bbox, keypoints), aug)
+
         aug = K.AugmentationSequential(K.RandomAffine(360, p=1.0, return_transform=True))
         assert aug(inp, data_keys=['input'])[0].shape == inp.shape
         aug = K.AugmentationSequential(K.RandomAffine(360, p=1.0, return_transform=False))
         assert aug(inp, data_keys=['input']).shape == inp.shape
         assert aug(mask, data_keys=['mask']).shape == mask.shape
-        assert aug(bbox, data_keys=['bbox']).shape == bbox.shape
-        assert aug(keypoints, data_keys=['keypoints']).shape == keypoints.shape
 
         aug = K.AugmentationSequential(K.RandomAffine(360, p=1.0, return_transform=True))
         assert aug.inverse(inp, data_keys=['input']).shape == inp.shape
@@ -211,7 +237,8 @@ class TestAugmentationSequential:
         aug = K.AugmentationSequential(K.RandomAffine(360, p=1.0, return_transform=True))
         assert aug.inverse(mask, data_keys=['mask']).shape == mask.shape
 
-    def test_forward_and_inverse_return_transform(self, device, dtype):
+    @pytest.mark.parametrize('random_apply', [2, (1, 1), (2,), 10, True, False])
+    def test_forward_and_inverse_return_transform(self, random_apply, device, dtype):
         inp = torch.randn(1, 3, 1000, 500, device=device, dtype=dtype)
         bbox = torch.tensor([[[355, 10], [660, 10], [660, 250], [355, 250]]], device=device, dtype=dtype)
         keypoints = torch.tensor([[[465, 115], [545, 116]]], device=device, dtype=dtype)
@@ -222,6 +249,7 @@ class TestAugmentationSequential:
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0, return_transform=True),
             K.RandomAffine(360, p=1.0, return_transform=True),
             data_keys=["input", "mask", "bbox", "keypoints"],
+            random_apply=random_apply,
         )
         out = aug(inp, mask, bbox, keypoints)
         assert out[0][0].shape == inp.shape
@@ -229,13 +257,16 @@ class TestAugmentationSequential:
         assert out[2].shape == bbox.shape
         assert out[3].shape == keypoints.shape
 
+        reproducibility_test((inp, mask, bbox, keypoints), aug)
+
         out_inv = aug.inverse(*out)
         assert out_inv[0].shape == inp.shape
         assert out_inv[1].shape == mask.shape
         assert out_inv[2].shape == bbox.shape
         assert out_inv[3].shape == keypoints.shape
 
-    def test_inverse_and_forward_return_transform(self, device, dtype):
+    @pytest.mark.parametrize('random_apply', [1, (2, 2), (1, 2), (2,), 10, True, False])
+    def test_inverse_and_forward_return_transform(self, random_apply, device, dtype):
         inp = torch.randn(1, 3, 1000, 500, device=device, dtype=dtype)
         bbox = torch.tensor([[[355, 10], [660, 10], [660, 250], [355, 250]]], device=device, dtype=dtype)
         keypoints = torch.tensor([[[465, 115], [545, 116]]], device=device, dtype=dtype)
@@ -246,6 +277,7 @@ class TestAugmentationSequential:
             K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0, return_transform=True),
             K.RandomAffine(360, p=1.0, return_transform=True),
             data_keys=["input", "mask", "bbox", "keypoints"],
+            random_apply=random_apply,
         )
 
         out_inv = aug.inverse(inp, mask, bbox, keypoints)
@@ -260,6 +292,8 @@ class TestAugmentationSequential:
         assert out[2].shape == bbox.shape
         assert out[3].shape == keypoints.shape
 
+        reproducibility_test((inp, mask, bbox, keypoints), aug)
+
     @pytest.mark.jit
     @pytest.mark.skip(reason="turn off due to Union Type")
     def test_jit(self, device, dtype):
@@ -273,31 +307,63 @@ class TestAugmentationSequential:
 
 
 class TestPatchSequential:
+    @pytest.mark.parametrize(
+        'error_param',
+        [
+            {"random_apply": False, "patchwise_apply": True, "grid_size": (2, 3)},
+            {"random_apply": 2, "patchwise_apply": True},
+            {"random_apply": (2, 3), "patchwise_apply": True},
+        ],
+    )
+    def test_exception(self, error_param):
+        with pytest.raises(Exception):  # AssertError and NotImplementedError
+            K.PatchSequential(
+                K.ImageSequential(
+                    K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
+                    K.RandomPerspective(0.2, p=0.5),
+                    K.RandomSolarize(0.1, 0.1, p=0.5),
+                ),
+                K.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                K.ImageSequential(
+                    K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
+                    K.RandomPerspective(0.2, p=0.5),
+                    K.RandomSolarize(0.1, 0.1, p=0.5),
+                ),
+                K.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                **error_param,
+            )
+
     @pytest.mark.parametrize('shape', [(2, 3, 24, 24)])
     @pytest.mark.parametrize('padding', ["same", "valid"])
     @pytest.mark.parametrize('patchwise_apply', [True, False])
     @pytest.mark.parametrize('same_on_batch', [True, False, None])
     @pytest.mark.parametrize('keepdim', [True, False, None])
-    def test_forward(self, shape, padding, patchwise_apply, same_on_batch, keepdim, device, dtype):
-        seq = K.PatchSequential(
-            K.ImageSequential(
-                K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
-                K.RandomPerspective(0.2, p=0.5),
-                K.RandomSolarize(0.1, 0.1, p=0.5),
-            ),
-            K.ColorJitter(0.1, 0.1, 0.1, 0.1),
-            K.ImageSequential(
-                K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
-                K.RandomPerspective(0.2, p=0.5),
-                K.RandomSolarize(0.1, 0.1, p=0.5),
-            ),
-            K.ColorJitter(0.1, 0.1, 0.1, 0.1),
-            grid_size=(2, 2),
-            padding=padding,
-            patchwise_apply=patchwise_apply,
-            same_on_batch=same_on_batch,
-            keepdim=keepdim,
-        )
+    @pytest.mark.parametrize('random_apply', [1, (2, 2), (1, 2), (2,), 10, True, False])
+    def test_forward(self, shape, padding, patchwise_apply, same_on_batch, keepdim, random_apply, device, dtype):
+        try:  # skip wrong param settings.
+            seq = K.PatchSequential(
+                K.ImageSequential(
+                    K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
+                    K.RandomPerspective(0.2, p=0.5),
+                    K.RandomSolarize(0.1, 0.1, p=0.5),
+                ),
+                K.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                K.ImageSequential(
+                    K.ColorJitter(0.1, 0.1, 0.1, 0.1, p=0.5),
+                    K.RandomPerspective(0.2, p=0.5),
+                    K.RandomSolarize(0.1, 0.1, p=0.5),
+                ),
+                K.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                grid_size=(2, 2),
+                padding=padding,
+                patchwise_apply=patchwise_apply,
+                same_on_batch=same_on_batch,
+                keepdim=keepdim,
+                random_apply=random_apply,
+            )
+        except:
+            return
+
         input = torch.randn(*shape, device=device, dtype=dtype)
         trans = torch.randn(shape[0], 3, 3, device=device, dtype=dtype)
         out = seq(input)
@@ -306,6 +372,8 @@ class TestPatchSequential:
         out = seq((input, trans))
         assert out[0].shape[-3:] == input.shape[-3:]
         assert out[1].shape == trans.shape
+
+        reproducibility_test(input, seq)
 
     def test_intensity_only(self):
         seq = K.PatchSequential(
