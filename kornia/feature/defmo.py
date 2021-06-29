22185b46dc1b704c032726cbf0a75fd76c1bcab7
Denys Rozumnyi <rozumden@gmail.com>
2021-06-29 15:40:46 +0200

    Update defmo.py
    No torchvision dependency.

--- kornia/feature/defmo.py
+++ kornia/feature/defmo.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional, Callable, List, Type
 
 import torch
 import torch.nn as nn
@@ -8,10 +8,16 @@ urls["defmo_encoder"] = "http://ptak.felk.cvut.cz/personal/rozumden/defmo_saved_
 urls["defmo_rendering"] = "http://ptak.felk.cvut.cz/personal/rozumden/defmo_saved_models/rendering_best.pt"
 
 
+# conv1x1, conv3x3, Bottleneck, ResNet taken from https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
 
 class Bottleneck(nn.Module):
     # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
@@ -48,7 +54,7 @@ class Bottleneck(nn.Module):
         self.downsample = downsample
         self.stride = stride
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
 
         out = self.conv1(x)
@@ -74,7 +80,7 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(
         self,
-        block: Type[Union[BasicBlock, Bottleneck]],
+        block: Type[Bottleneck],
         layers: List[int],
         num_classes: int = 1000,
         zero_init_residual: bool = False,
@@ -126,12 +132,10 @@ class ResNet(nn.Module):
             for m in self.modules():
                 if isinstance(m, Bottleneck):
                     nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
 
     def _make_layer(
         self,
-        block: Type[Union[BasicBlock, Bottleneck]],
+        block: Type[Bottleneck],
         planes: int,
         blocks: int,
         stride: int = 1,
@@ -169,7 +173,7 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
         # See note [TorchScript super()]
         x = self.conv1(x)
         x = self.bn1(x)
@@ -187,7 +191,7 @@ class ResNet(nn.Module):
 
         return x
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self._forward_impl(x)
 
 
@@ -233,7 +237,7 @@ class RenderingDeFMO(nn.Module):
         shuffled_times = []
         for ki in range(times.shape[0]):
             shuffled_times.append(torch.randperm(times.shape[1]))
-        shuffled_times = torch.stack(shuffled_times, 1).contiguous().transpose()
+        shuffled_times = torch.stack(shuffled_times, 1).contiguous().transpose(1,0)
         for ki in range(times.shape[1]):
             t_tensor = (
                 times[range(times.shape[0]), shuffled_times[:, ki]]
