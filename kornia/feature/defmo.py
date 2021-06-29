0d909bd1b9ceef3f66dec61a5ff98832adf33ebc
Denys Rozumnyi <rozumden@gmail.com>
2021-06-29 15:30:02 +0200

    Update defmo.py

--- kornia/feature/defmo.py
+++ kornia/feature/defmo.py
@@ -7,6 +7,9 @@ urls: Dict[str, str] = {}
 urls["defmo_encoder"] = "http://ptak.felk.cvut.cz/personal/rozumden/defmo_saved_models/encoder_best.pt"
 urls["defmo_rendering"] = "http://ptak.felk.cvut.cz/personal/rozumden/defmo_saved_models/rendering_best.pt"
 
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 class Bottleneck(nn.Module):
     # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
