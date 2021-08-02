d4a263aaab40e881a07a5a8b7a49e928f1201875
Bulat Suleymanov <75295501+bsuleymanov@users.noreply.github.com>
2021-08-02 19:00:05 +0500

    Update kornia/enhance/histogram.py
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- kornia/enhance/histogram.py
+++ kornia/enhance/histogram.py
@@ -235,7 +235,7 @@ def image_histogram2d(
     if centers.numel() == 0:
         centers = min + bandwidth * (torch.arange(n_bins, device=image.device, dtype=image.dtype).float() + 0.5)
     centers = centers.reshape(-1, 1, 1, 1, 1)
-    u = abs(image.unsqueeze(0) - centers) / bandwidth
+    u = torch.abs(image.unsqueeze(0) - centers) / bandwidth
     if kernel == "triangular":
         mask = (u <= 1).float()
         kernel_values = (1 - u) * mask
