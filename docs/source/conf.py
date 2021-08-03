aa31f8d730864c71948cef32f9d3ed9138401755
Edgar Riba <edgar.riba@gmail.com>
2021-08-03 19:53:29 +0200

    add analytics html file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -134,6 +134,7 @@ html_favicon = '_static/img/kornia_logo_mini.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['_extra']
 
 # html_style_path = 'css/pytorch_theme.css'
 html_context = {'css_files': ['https://fonts.googleapis.com/css?family=Lato', '_static/css/pytorch_theme.css']}
