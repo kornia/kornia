0a7b6cd92f298660b6a91855a07edaf354a70f8a
Edgar Riba <edgar.riba@gmail.com>
2021-08-06 14:14:50 +0200

    Update connected_components.py

--- kornia/contrib/connected_components.py
+++ kornia/contrib/connected_components.py
@@ -11,6 +11,9 @@ def connected_components(input: torch.Tensor, num_iterations: int = 100) -> torc
 
     https://gist.github.com/efirdc/5d8bd66859e574c683a504a4690ae8bc
 
+    .. note::
+        This is an experimental API subject to changes and optimization improvements.
+
     Args:
         input: the binarized input image with shape :math:`(B, 1, H, W)`.
           The image must be in floating point with range [0, 1].
@@ -22,9 +25,6 @@ def connected_components(input: torch.Tensor, num_iterations: int = 100) -> torc
     Example:
         >>> img = torch.rand(2, 1, 4, 5)
         >>> img_labels = connected_components(img, num_iterations=100)
-
-    .. note::
-        This is an experimental API subject to changes and optimization improvements.
     """
     if not isinstance(input, torch.Tensor):
         raise TypeError(f"Input input type is not a torch.Tensor. Got: {type(input)}")
