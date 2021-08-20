5f3b42aa9384288e3ce58fe09cdaba18039ebf4e
Denys Rozumnyi <rozumden@gmail.com>
2021-08-20 16:16:22 +0200

    Update test_defmo.py

--- test/feature/test_defmo.py
+++ test/feature/test_defmo.py
@@ -8,26 +8,31 @@ from kornia.testing import assert_close
 
 
 class TestDeFMO:
+    
+    @staticmethod
     def test_shape(self, device, dtype):
         inp = torch.ones(1, 6, 240, 320, device=device, dtype=dtype)
         defmo = DeFMO().to(device, dtype)
         defmo.eval()  # batchnorm with size 1 is not allowed in train mode
         out = defmo(inp)
         assert out.shape == (1, 24, 4, 240, 320)
-
+    
+    @staticmethod
     def test_shape_batch(self, device):
         inp = torch.ones(16, 6, 240, 320, device=device)
         defmo = DeFMO().to(device)
         out = defmo(inp)
         assert out.shape == (16, 24, 4, 240, 320)
 
+    @staticmethod
     @pytest.mark.skip("jacobian not well computed")
     def test_gradcheck(self, device):
         patches = torch.rand(2, 6, 240, 320, device=device)
         patches = utils.tensor_to_gradcheck_var(patches)  # to var
         defmo = DeFMO().to(patches.device, patches.dtype)
         assert gradcheck(defmo, (patches,), eps=1e-4, atol=1e-4, raise_exception=True)
-
+    
+    @staticmethod
     @pytest.mark.jit
     def test_jit(self, device, dtype):
         B, C, H, W = 2, 6, 240, 320
