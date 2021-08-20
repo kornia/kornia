7084104c6bf5c5ac09d1926bd91172e8d5430b61
Denys Rozumnyi <rozumden@gmail.com>
2021-08-20 15:41:27 +0200

    Update test/feature/test_defmo.py
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- test/feature/test_defmo.py
+++ test/feature/test_defmo.py
@@ -9,7 +9,7 @@ from kornia.testing import assert_close
 
 class TestDeFMO:
     def test_shape(self, device, dtype):
-        inp = torch.ones(1, 6, 240, 320, device=device)
+        inp = torch.ones(1, 6, 240, 320, device=device, dtype=dtype)
         defmo = DeFMO().to(device)
         defmo.eval()  # batchnorm with size 1 is not allowed in train mode
         out = defmo(inp)
