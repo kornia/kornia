99f3b49cb9c889fe5237efe7ebb7433dd3262082
shijianjian <sj8716643@126.com>
2021-08-27 09:32:21 +0800

    code init

--- kornia/augmentation/container/augment.py
+++ kornia/augmentation/container/augment.py
@@ -18,6 +18,7 @@ from kornia.geometry.linalg import transform_points
 
 from .image import ImageSequential, ParamItem
 from .patch import PatchSequential
+from .video import VideoSequential
 
 __all__ = ["AugmentationSequential"]
 
@@ -366,3 +367,161 @@ class AugmentationSequential(ImageSequential):
             outputs[idx] = input
 
         return self.__packup_output__(outputs, label)
+
+
+class VideoAugmentationSequential(AugmentationSequential, VideoSequential):  # type: ignore
+    r"""VideoAugmentationSequential for handling multiple 5-dim input types like inputs, masks, keypoints at once.
+
+    Args:
+        *args: a list of kornia augmentation modules.
+        data_format: only BCTHW and BTCHW are supported.
+        data_keys: the input type sequential for applying augmentations.
+            Accepts "input", "mask", "bbox", "bbox_xyxy", "bbox_xywh", "keypoints".
+        same_on_frame: apply the same transformation across the channel per frame.
+        same_on_batch: apply the same transformation across the batch.
+            If None, it will not overwrite the function-wise settings.
+        return_transform: if ``True`` return the matrix describing the transformation
+            applied to each. If None, it will not overwrite the function-wise settings.
+        keepdim: whether to keep the output shape the same as input (True) or broadcast it
+            to the batch form (False). If None, it will not overwrite the function-wise settings.
+        random_apply: randomly select a sublist (order agnostic) of args to
+            apply transformation.
+            If int, a fixed number of transformations will be selected.
+            If (a,), x number of transformations (a <= x <= len(args)) will be selected.
+            If (a, b), x number of transformations (a <= x <= b) will be selected.
+            If True, the whole list of args will be processed as a sequence in a random order.
+            If False, the whole list of args will be processed as a sequence in original order.
+
+    .. note::
+        Mix augmentations (e.g. RandomMixUp, RandomCutMix) can only be working with "input" data key.
+        It is not clear how to deal with the conversions of masks, bounding boxes and keypoints.
+
+    .. note::
+        See a working example `here <https://kornia-tutorials.readthedocs.io/en/
+        latest/data_augmentation_sequential.html>`__.
+
+    Examples:
+        >>> import kornia
+        >>> input = torch.randn(2, 3, 5, 6)[None]
+        >>> bbox = torch.tensor([[
+        ...     [1., 1.],
+        ...     [2., 1.],
+        ...     [2., 2.],
+        ...     [1., 2.],
+        ... ]]).expand(2, -1, -1)[None]
+        >>> points = torch.tensor([[[1., 1.]]]).expand(2, -1, -1)[None]
+        >>> aug_list = VideoAugmentationSequential(
+        ...     kornia.augmentation.ColorJitter(0.1, 0.1, 0.1, 0.1, p=1.0),
+        ...     kornia.augmentation.RandomAffine(360, p=1.0),
+        ...     data_keys=["input", "mask", "bbox", "keypoints"]
+        ... )
+        >>> out = aug_list(input, input, bbox, points)
+        >>> [o.shape for o in out]
+        [torch.Size([2, 3, 5, 6]), torch.Size([2, 3, 5, 6]), torch.Size([2, 4, 2]), torch.Size([2, 1, 2])]
+    """
+
+    def __init__(
+        self,
+        *args: Union[_AugmentationBase, ImageSequential],
+        data_format: str = "BTCHW",
+        same_on_frame: bool = True,
+        data_keys: List[Union[str, int, DataKey]] = [DataKey.INPUT],
+        return_transform: Optional[bool] = None,
+        random_apply: Union[int, bool, Tuple[int, int]] = False,
+    ) -> None:
+        # NOTE: One downside of this is the torch.nn.Module will be initilized twice.
+        VideoSequential.__init__(
+            self,
+            *args,
+            data_format=data_format,
+            same_on_frame=same_on_frame,
+            random_apply=random_apply
+        )
+        AugmentationSequential.__init__(
+            self,
+            *args,
+            data_keys=data_keys,
+            same_on_batch=None,
+            return_transform=return_transform,
+            keepdim=None,
+            random_apply=random_apply,
+        )
+
+    def forward(  # type: ignore
+        self,
+        *args: TensorWithTransformMat,
+        label: Optional[torch.Tensor] = None,
+        params: Optional[List[ParamItem]] = None,
+        data_keys: Optional[List[Union[str, int, DataKey]]] = None,
+    ) -> Union[
+        TensorWithTransformMat,
+        Tuple[TensorWithTransformMat, Optional[torch.Tensor]],
+        List[TensorWithTransformMat],
+        Tuple[List[TensorWithTransformMat], Optional[torch.Tensor]],
+    ]:
+        """Define the video computation performed."""
+        if data_keys is None:
+            data_keys = cast(List[Union[str, int, DataKey]], self.data_keys)
+        else:
+            data_keys = [DataKey.get(inp) for inp in data_keys]
+
+        in_args: List[TensorWithTransformMat] = []
+        frame_num: int
+        for idx, _input in enumerate(args):
+            if isinstance(_input, (tuple, list)):
+                inp = _input[0]
+            else:
+                inp = _input
+            if idx == 0:
+                frame_num = inp.size(self._temporal_channel)
+                inp, label = self._input_shape_convert_in(inp, label, frame_num)
+            else:
+                assert inp.size(self._temporal_channel) == frame_num, \
+                    f"Expect same frame size. Got {inp.size(self._temporal_channel)} and {frame_num}."
+                inp, _ = self._input_shape_convert_in(inp, None, frame_num)
+            if isinstance(_input, (tuple, list)):
+                in_args.append((inp, _input[1]))
+            else:
+                in_args.append(inp)
+
+        out = super().forward(*in_args, label=label, params=params, data_keys=data_keys)
+
+        output: Union[TensorWithTransformMat, List[TensorWithTransformMat]]
+        if self.return_label:
+            output, label = out  # type: ignore
+        else:
+            output = out  # type: ignore
+
+        # TODO: This is an ugly implementation
+        if isinstance(output, (tuple, list)):
+            _output = []
+            for i, outp in enumerate(output):
+                if isinstance(outp, (tuple, list)):
+                    _output2 = []
+                    for j, outp2 in enumerate(outp):
+                        if isinstance(outp2, (tuple, list)):
+                            if i == 0 and j == 0:
+                                outp2, label = self._input_shape_convert_back(outp2[0], label, frame_num)
+                                outp21, _ = self._input_shape_convert_back(outp2[1], None, frame_num)
+                            else:
+                                outp2, _ = self._input_shape_convert_back(outp2[0], None, frame_num)
+                                outp21, _ = self._input_shape_convert_back(outp2[1], None, frame_num)
+                            _output2.append((outp2, outp21))
+                        else:
+                            if i == 0 and j == 0:
+                                outp2, label = self._input_shape_convert_back(outp2[0], label, frame_num)
+                            else:
+                                outp2, _ = self._input_shape_convert_back(outp2[0], None, frame_num)
+                            _output2.append(outp2)
+                else:
+                    if i == 0:
+                        outp, label = self._input_shape_convert_back(outp, label, frame_num)
+                    else:
+                        outp, _ = self._input_shape_convert_back(outp, None, frame_num)
+                    _output.append(outp)
+            output = _output  # type: ignore
+        else:
+            output, label = self._input_shape_convert_back(output, label, frame_num)
+            output = [output]
+
+        return self.__packup_output__(output, label)  # type: ignore
