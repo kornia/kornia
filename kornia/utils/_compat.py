737c2a519b2a48516893db3e588df17f77f3b049
Edgar Riba <edgar.riba@gmail.com>
2021-08-30 10:34:41 +0200

    Fix mypy issue with torch.linalg (#1236)
    * add a compatibiliy stub for torch solve
    
    * change missing _torch_solve_cast calls
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * bump pytorch version to 1.7.1
    
    * Fix mypy issue with torch.linalg
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/utils/_compat.py
+++ kornia/utils/_compat.py
@@ -3,7 +3,10 @@ import torch
 from packaging import version
 
 if version.parse(torch.__version__) > version.parse("1.7.1"):
-    from torch.linalg import solve
+    # TODO: remove the type: ignore once Python 3.6 is deprecated.
+    # It turns out that Pytorch has no attribute `torch.linalg` for
+    # Python 3.6 / PyTorch 1.7.0, 1.7.1
+    from torch.linalg import solve  # type: ignore
 else:
     from torch import solve as _solve
 
