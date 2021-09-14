0119e76ce1f42e9e35a7a7fb53a33528511cb0de
shijianjian <sj8716643@126.com>
2021-09-14 23:29:16 +0800

    Update

--- kornia/contrib/__init__.py
+++ kornia/contrib/__init__.py
@@ -1,5 +1,4 @@
 from kornia.contrib.connected_components import connected_components
 from kornia.contrib.extract_patches import extract_tensor_patches, ExtractTensorPatches
-from kornia.contrib.max_blur_pool import max_blur_pool2d, MaxBlurPool2d
 
-__all__ = ["connected_components", "extract_tensor_patches", "max_blur_pool2d", "ExtractTensorPatches", "MaxBlurPool2d"]
+__all__ = ["connected_components", "extract_tensor_patches", "ExtractTensorPatches"]
