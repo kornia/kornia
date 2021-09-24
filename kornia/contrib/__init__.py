807e7178bca18578ba8bebb7da81c8e09fbea59a
Jian Shi <sj8716643@126.com>
2021-09-24 09:28:38 +0800

    [feat] Added combine patches (#1309)

--- kornia/contrib/__init__.py
+++ kornia/contrib/__init__.py
@@ -1,6 +1,19 @@
 from kornia.contrib.classification import ClassificationHead
 from kornia.contrib.connected_components import connected_components
-from kornia.contrib.extract_patches import extract_tensor_patches, ExtractTensorPatches
+from kornia.contrib.extract_patches import (
+    combine_tensor_patches,
+    CombineTensorPatches,
+    extract_tensor_patches,
+    ExtractTensorPatches,
+)
 from kornia.contrib.vit import VisionTransformer
 
-__all__ = ["connected_components", "extract_tensor_patches", "ExtractTensorPatches"]
+__all__ = [
+    "connected_components",
+    "extract_tensor_patches",
+    "ExtractTensorPatches",
+    "combine_tensor_patches",
+    "CombineTensorPatches",
+    "VisionTransformer",
+    "ClassificationHead"
+]
