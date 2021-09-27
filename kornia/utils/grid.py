bfdf1eb63b3ce1d5cafd00285f2c797629d62872
shiyangc-intusurg <42385339+shiyangc-intusurg@users.noreply.github.com>
2021-09-27 15:49:16 -0700

    bugfix in create_meshgrid3d along depth
    a copypaste error in depth axis.

--- kornia/utils/grid.py
+++ kornia/utils/grid.py
@@ -95,7 +95,7 @@ def create_meshgrid3d(
     if normalized_coordinates:
         xs = (xs / (width - 1) - 0.5) * 2
         ys = (ys / (height - 1) - 0.5) * 2
-        zs = (ys / (height - 1) - 0.5) * 2
+        zs = (zs / (depth - 1) - 0.5) * 2
     # generate grid by stacking coordinates
     base_grid: torch.Tensor = torch.stack(torch.meshgrid([zs, xs, ys])).transpose(1, 2)  # 3xHxW
     return base_grid.unsqueeze(0).permute(0, 3, 4, 2, 1)  # 1xHxWx3
