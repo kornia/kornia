822f56f2e566bec8893ac1e7b5acc1a1cc23e53e
thatbrguy <bharathrajn98@gmail.com>
2021-10-06 00:31:56 +0530

    added support for batched input, addressed suggestions, changed tolerances, also did minor fixes

--- kornia/geometry/calibration/pnp.py
+++ kornia/geometry/calibration/pnp.py
@@ -1,35 +1,38 @@
 import torch
+import kornia
 
 from kornia.geometry.conversions import convert_points_to_homogeneous
 
 
 def solve_pnp_dlt(
     world_points: torch.Tensor, img_points: torch.Tensor,
-    intrinsic: torch.Tensor, eps: float = 1e-8,
+    intrinsics: torch.Tensor, eps: float = 1e-8,
 ) -> torch.Tensor:
     r"""This function attempts to solve the Perspective-n-Point (PnP)
     problem using Direct Linear Transform (DLT).
 
-    Given :math:`N` 3D points (where :math:`N \geq 6`) in the world
-    space, :math:`N` corresponding 2D points in the image space and
-    an intrinsic matrix, this function tries to estimate the world
-    to camera transformation matrix.
+    Given a batch (where batch size is :math:`B`) of :math:`N` 3D points
+    (where :math:`N \geq 6`) in the world space, a batch of :math:`N`
+    corresponding 2D points in the image space and a batch of
+    intrinsic matrices, this function tries to estimate a batch of
+    world to camera transformation matrices.
 
-    This function needs atleast 6 points (i.e. :math:`N \geq 6`) to
-    provide a solution. This function cannot be used if all the 3D world
-    points lie on a line or if all the 3D world points lie on a plane.
+    This implementation needs atleast 6 points (i.e. :math:`N \geq 6`) to
+    provide solutions. This function cannot be used if all the 3D world
+    points (of any element of the batch) lie on a line or if all the
+    3D world points (of any element of the batch) lie on a plane.
 
     Args:
-        world_points        :   A tensor with shape :math:`(N, 3)` representing
+        world_points        :   A tensor with shape :math:`(B, N, 3)` representing
                                 the points in the world space.
-        img_points          :   A tensor with shape :math:`(N, 2)` representing
+        img_points          :   A tensor with shape :math:`(B, N, 2)` representing
                                 the points in the image space.
-        intrinsic           :   A tensor with shape :math:`(3, 3)` representing
-                                the intrinsic matrix.
+        intrinsics          :   A tensor with shape :math:`(B, 3, 3)` representing
+                                the intrinsic matrices.
 
     Returns:
-        A tensor with shape :math:`(3, 4)` representing the estimated world to
-        camera transformation matrix.
+        A tensor with shape :math:`(B, 3, 4)` representing the estimated world to
+        camera transformation matrices.
     """
     # This function was implemented based on ideas inspired from multiple references.
     # ============
@@ -46,86 +49,91 @@ def solve_pnp_dlt(
     if type(img_points) is not torch.Tensor:
         raise TypeError(f"Type of img_points is not torch.Tensor. Got {type(img_points)}")
 
-    if type(intrinsic) is not torch.Tensor:
-        raise TypeError(f"Type of intrinsic is not torch.Tensor. Got {type(intrinsic)}")
+    if type(intrinsics) is not torch.Tensor:
+        raise TypeError(f"Type of intrinsics is not torch.Tensor. Got {type(intrinsics)}")
 
     if type(eps) is not float:
         raise TypeError(f"Type of eps is not float. Got {type(world_points)}")
 
-    if (len(world_points.shape) == 2) and (world_points.shape[1] != 3):
+    if (len(world_points.shape) != 3) or (world_points.shape[2] != 3):
         raise AssertionError(
-            f"world_points must be of shape (N, 3). Got shape {world_points.shape}."
+            f"world_points must be of shape (B, N, 3). Got shape {world_points.shape}."
         )
 
-    if (len(img_points.shape) == 2) and (img_points.shape[1] != 2):
+    if (len(img_points.shape) != 3) or (img_points.shape[2] != 2):
         raise AssertionError(
-            f"img_points must be of shape (N, 2). Got shape {img_points.shape}."
+            f"img_points must be of shape (B, N, 2). Got shape {img_points.shape}."
         )
 
-    if intrinsic.shape != (3, 3):
+    if (len(intrinsics.shape) != 3) or (intrinsics.shape[1:] != (3, 3)):
         raise AssertionError(
-            f"intrinsic must be of shape (3, 3). Got shape {intrinsic.shape}."
+            f"intrinsics must be of shape (B, 3, 3). Got shape {intrinsics.shape}."
         )
 
-    if world_points.shape[0] != img_points.shape[0]:
+    if world_points.shape[1] != img_points.shape[1]:
         raise AssertionError("world_points and img_points must have equal number of points.")
 
-    if world_points.shape[0] < 6:
+    if (world_points.shape[0] != img_points.shape[0]) or (world_points.shape[0] != intrinsics.shape[0]):
+        raise AssertionError("world_points, img_points and intrinsics must have the same batch size.")
+
+    if world_points.shape[1] < 6:
         raise AssertionError(
             f"Atleast 6 points are required to use this function. "
-            f"Got {world_points.shape[0]} points."
+            f"Got {world_points.shape[1]} points."
         )
 
-    # Checking if world_points has rank = 3. This function cannot be used
-    # if all world_points lie on a line or if all world points lie on a plane.
-    s = torch.linalg.svdvals(world_points)
-    if s[-1] < eps:
+    # Checking if world_points (of any element of the batch) has rank = 3. This
+    # function cannot be used if all world_points (of any element of the batch) lie
+    # on a line or if all world points lie on a plane.
+    _, s, _ = torch.svd(world_points)
+    if torch.any(s[:, -1] < eps):
         raise AssertionError(
             f"The last singular value is smaller than {eps}. This function "
-            f"cannot be used if all world_points lie on a line "
-            f"or if all world_points lie on a plane."
+            f"cannot be used if all world_points (of any element of the batch) "
+            f"lie on a line or if all world_points (of any element of the batch) "
+            f"lie on a plane."
         )
 
-    N = world_points.shape[0]
-    intrinsic_inv = torch.linalg.inv(intrinsic)
-
-    img_points_h = convert_points_to_homogeneous(img_points)
+    B, N = world_points.shape[:2]
+    intrinsics_inv = torch.inverse(intrinsics)
     world_points_h = convert_points_to_homogeneous(world_points)
 
     # Getting normalized image points.
-    norm_points = torch.matmul(intrinsic_inv, img_points_h.T).T
+    norm_points = kornia.geometry.transform_points(intrinsics_inv, img_points)
 
     # Setting up the system (the matrix A in Ax=0)
-    system = torch.zeros((2 * N, 12), dtype=world_points.dtype, device=world_points.device)
-    system[0::2, 0:4] = world_points_h
-    system[1::2, 4:8] = world_points_h
-    system[0::2, 8:12] = world_points_h * (-1) * norm_points[:, 0:1]
-    system[1::2, 8:12] = world_points_h * (-1) * norm_points[:, 1:2]
+    system = torch.zeros((B, 2 * N, 12), dtype=world_points.dtype, device=world_points.device)
+    system[:, 0::2, 0:4] = world_points_h
+    system[:, 1::2, 4:8] = world_points_h
+    system[:, 0::2, 8:12] = world_points_h * (-1) * norm_points[..., 0:1]
+    system[:, 1::2, 8:12] = world_points_h * (-1) * norm_points[..., 1:2]
 
-    # Getting the solution vector.
-    u, s, v = torch.svd(system)
-    solution = v[:, -1]
+    # Getting the solution vectors.
+    _, _, v = torch.svd(system)
+    solution = v[..., -1]
 
-    # Reshaping the solution vector to the correct shape.
-    pred_world_to_cam = solution.reshape(3, 4)
+    # Reshaping the solution vectors to the correct shape.
+    pred_world_to_cam = solution.reshape(B, 3, 4)
 
-    # We may need to multiply pred_world_to_cam with a scalar. This is
-    # because if x is a solution to Ax=0, then cx is also a solution.
-    # We can find this scalar by using the properties of the
-    # rotation matrix. We do this in two parts:
+    # We obtained one solution for each element of the batch. We may
+    # need to multiply each solution with a scalar. This is because
+    # if x is a solution to Ax=0, then cx is also a solution. We can
+    # find the required scalars by using the properties of
+    # rotation matrices. We do this in two parts:
 
     # First, we fix the sign by making sure that the determinant of
-    # the rotation matrix is non negative (since determinant of a rotation
-    # matrix should be 1).
-    det = torch.linalg.det(pred_world_to_cam[:3, :3])
-    if det < 0:
-        pred_world_to_cam = pred_world_to_cam * -1
+    # the all the rotation matrices are non negative (since determinant
+    # of a rotation matrix should be 1).
+    det = torch.det(pred_world_to_cam[:, :3, :3])
+    sign_fix = torch.where(det < 0, -1, 1)
+    pred_world_to_cam = pred_world_to_cam * sign_fix[:, None, None]
 
-    # Then, we make sure that norm of a column of the rotation matrix
-    # is 1. Do note that the norm of any column of a rotation matrix
-    # should be 1. Here we use the 0th column to calculate norm_col.
-    # We then multiply all elements of pred_world_to_cam with (1/norm_col)
-    norm_col = torch.linalg.norm(pred_world_to_cam[:3, 0])
-    pred_world_to_cam = pred_world_to_cam * (1 / norm_col)
+    # Then, we make sure that norm of the 0th columns of the rotation
+    # matrices are 1. Do note that the norm of any column of a rotation
+    # matrix should be 1. Here we use the 0th column to calculate norm_col.
+    # We then multiply pred_world_to_cam with mul_factor.
+    norm_col = torch.sqrt(torch.sum(pred_world_to_cam[:, :3, 0] ** 2, axis=1))
+    mul_factor = (1 / norm_col)[:, None, None]
+    pred_world_to_cam = pred_world_to_cam * mul_factor
 
     return pred_world_to_cam
--- test/geometry/calibration/test_pnp.py
+++ test/geometry/calibration/test_pnp.py
@@ -1,100 +1,128 @@
 import pytest
-import torch
-from torch.autograd import gradcheck
-
 import kornia
-from kornia.geometry.conversions import convert_points_to_homogeneous
+import torch
+
+from torch.autograd import gradcheck
 from kornia.testing import assert_close, tensor_to_gradcheck_var
 
 
 class TestSolvePnpDlt:
 
-    def _get_samples(self, shape, low, high, device, dtype):
+    @staticmethod
+    def _get_samples(shape, low, high, device, dtype):
         """Return a tensor having the given shape and whose values are in the range [low, high)"""
         return ((high - low) * torch.rand(shape, device=device, dtype=dtype)) + low
 
-    def _project_to_image(self, world_points, world_to_cam_4x4, intrinsic):
-        world_points_h = convert_points_to_homogeneous(world_points)
-        cam_points = (torch.matmul(world_to_cam_4x4, world_points_h.T).T)[:, :3]
+    @staticmethod
+    def _project_to_image(world_points, world_to_cam_4x4, repeated_intrinsics):
+        r"""Projects points in the world coordinate system to the image coordinate system.
 
-        temp = torch.matmul(intrinsic, cam_points.T).T
-        img_points = temp[:, :2] / temp[:, 2:3]
+        Since cam_points will have shape (B, N, 3), repeated_intrinsics should have
+        shape (B, N, 3, 3) so that kornia.geometry.project_points can be used.
+        """
+        cam_points = kornia.geometry.transform_points(world_to_cam_4x4, world_points)
+        img_points = kornia.geometry.project_points(cam_points, repeated_intrinsics)
 
         return img_points
 
-    def _get_test_data(self, num_points, device, dtype):
-        intrinsic = torch.tensor([
-            [500.0, 0.0, 250.0],
-            [0.0, 500.0, 250.0],
-            [0.0, 0.0, 1.0],
-        ], dtype=dtype, device=device)
-
+    def _create_world_to_cam(self, device, dtype):
+        """Creates a ground truth world to cam matrix that can be used for the test data."""
         tau = 2 * 3.141592653589793
-        torch.manual_seed(84)
-
         angle_axis = self._get_samples(
             shape=(1, 3), low=-tau, high=tau, dtype=dtype, device=device,
         )
         translation = self._get_samples(
             shape=(3,), low=-100, high=100, dtype=dtype, device=device,
         )
-        world_points = self._get_samples(
-            shape=(num_points, 3), low=-100, high=100, dtype=dtype, device=device,
-        )
 
-        rotation = kornia.angle_axis_to_rotation_matrix(angle_axis)
-
+        rotation = kornia.geometry.angle_axis_to_rotation_matrix(angle_axis)
         world_to_cam_4x4 = torch.eye(4, dtype=dtype, device=device)
         world_to_cam_4x4[:3, :3] = rotation
         world_to_cam_4x4[:3, 3] = translation
 
-        img_points = self._project_to_image(world_points, world_to_cam_4x4, intrinsic)
-        world_to_cam_3x4 = world_to_cam_4x4[:3, :]
+        return world_to_cam_4x4
 
-        return intrinsic, world_to_cam_3x4, world_points, img_points
+    def _get_test_data(self, num_points, device, dtype):
+        """Creates some test data. Batch size is fixed to 2 for all tests."""
+        batch_size = 2
+        torch.manual_seed(84)
+        arr_1 = self._create_world_to_cam(device, dtype)
+        arr_2 = self._create_world_to_cam(device, dtype)
+        world_to_cam_mats = torch.stack([arr_1, arr_2], axis=0)
+
+        intrinsic_1 = torch.tensor([
+            [500.0, 0.0, 250.0],
+            [0.0, 500.0, 250.0],
+            [0.0, 0.0, 1.0],
+        ], dtype=dtype, device=device)
+
+        intrinsic_2 = torch.tensor([
+            [1000.0, 0.0, 550.0],
+            [0.0, 750.0, 200.0],
+            [0.0, 0.0, 1.0],
+        ], dtype=dtype, device=device)
+
+        intrinsics = torch.stack([intrinsic_1, intrinsic_2], dim=0)
+
+        world_points = self._get_samples(
+            shape=(batch_size, num_points, 3),
+            low=-100, high=100, dtype=dtype, device=device,
+        )
+
+        repeated_intrinsics = intrinsics.unsqueeze(1).repeat(1, num_points, 1, 1)
+        img_points = self._project_to_image(world_points, world_to_cam_mats, repeated_intrinsics)
+        world_to_cam_3x4 = world_to_cam_mats[:, :3, :]
+
+        return intrinsics, world_to_cam_3x4, world_points, img_points
 
     @pytest.mark.parametrize("num_points", (6, 20, 200))
     def test_smoke(self, num_points, device, dtype):
-        intrinsic, gt_world_to_cam, world_points, img_points = \
-            self._get_test_data(num_points, device, dtype)
 
-        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsic)
-        assert pred_world_to_cam.shape == (3, 4)
+        intrinsics, _, world_points, img_points = \
+            self._get_test_data(num_points, device, dtype)
+        batch_size = world_points.shape[0]
+
+        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsics)
+        assert pred_world_to_cam.shape == (batch_size, 3, 4)
 
     @pytest.mark.parametrize("num_points", (6, 20, 200))
     def test_gradcheck(self, num_points, device, dtype):
-        intrinsic, gt_world_to_cam, world_points, img_points = \
+
+        intrinsics, _, world_points, img_points = \
             self._get_test_data(num_points, device, dtype)
 
         world_points = tensor_to_gradcheck_var(world_points)
         img_points = tensor_to_gradcheck_var(img_points)
-        intrinsic = tensor_to_gradcheck_var(intrinsic)
+        intrinsics = tensor_to_gradcheck_var(intrinsics)
 
         assert gradcheck(
-            kornia.solve_pnp_dlt, (world_points, img_points, intrinsic),
-            raise_exception=True, atol=1e-4
+            kornia.solve_pnp_dlt, (world_points, img_points, intrinsics),
+            raise_exception=True, atol=1e-3
         )
 
     @pytest.mark.parametrize("num_points", (6, 20, 200))
     def test_pred_world_to_cam(self, num_points, device, dtype):
-        intrinsic, gt_world_to_cam, world_points, img_points = \
+
+        intrinsics, gt_world_to_cam, world_points, img_points = \
             self._get_test_data(num_points, device, dtype)
 
-        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsic)
-        assert_close(pred_world_to_cam, gt_world_to_cam, atol=1e-4, rtol=1e-4)
+        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsics)
+        assert_close(pred_world_to_cam, gt_world_to_cam, atol=1e-3, rtol=1e-3)
 
     @pytest.mark.parametrize("num_points", (6, 20, 200))
     def test_project(self, num_points, device, dtype):
-        intrinsic, gt_world_to_cam, world_points, img_points = \
+
+        intrinsics, _, world_points, img_points = \
             self._get_test_data(num_points, device, dtype)
 
-        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsic)
+        pred_world_to_cam = kornia.solve_pnp_dlt(world_points, img_points, intrinsics)
 
-        pred_world_to_cam_4x4 = torch.eye(4, dtype=dtype, device=device)
-        pred_world_to_cam_4x4[:3, :] = pred_world_to_cam
+        pred_world_to_cam_4x4 = kornia.eye_like(4, pred_world_to_cam)
+        pred_world_to_cam_4x4[:, :3, :] = pred_world_to_cam
 
+        repeated_intrinsics = intrinsics.unsqueeze(1).repeat(1, num_points, 1, 1)
         pred_img_points = self._project_to_image(
-            world_points, pred_world_to_cam_4x4, intrinsic
+            world_points, pred_world_to_cam_4x4, repeated_intrinsics
         )
 
         # Different tolerances for dtype torch.float32
