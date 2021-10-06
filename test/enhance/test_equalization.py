add numerical diff test and fix cuda

test/enhance/test_equalization.py
@@ -75,7 +75,9 @@ class TestEqualization(BaseTester):
         inputs = tensor_to_gradcheck_var(inputs)
 
         def grad_rot(input, a, b, c):
-            rot = rotate(input, torch.tensor(30., dtype=input.dtype))
+            rot = rotate(input, torch.tensor(30.,
+                                             dtype=input.dtype,
+                                             device=device))
             return enhance.equalize_clahe(rot, a, b, c)
         assert gradcheck(grad_rot, (inputs, 40.0, (2, 2), True), raise_exception=True)
 
@@ -213,9 +215,35 @@ class TestEqualization(BaseTester):
                                    0.8118,
                                    0.8745,
                                    1.0000]]], dtype=res.dtype, device=res.device)
+        exp_diff = torch.tensor([[[0.1250,
+                                   0.8752,
+                                   0.9042,
+                                   0.9167,
+                                   0.8401,
+                                   0.8852,
+                                   0.9302,
+                                   0.9120,
+                                   0.8750,
+                                   0.8370,
+                                   0.9620,
+                                   0.9077,
+                                   0.8750,
+                                   0.8754,
+                                   0.9204,
+                                   0.9167,
+                                   0.8370,
+                                   0.8806,
+                                   0.9096,
+                                   1.0000]]], dtype=res.dtype, device=res.device)
         assert torch.allclose(
             res[..., 0, :],
             expected,
+            atol=1e-04,
+            rtol=1e-04,
+        )
+        assert torch.allclose(
+            res_diff[..., 0, :],
+            exp_diff,
             atol=1e-04,
             rtol=1e-04,
         )