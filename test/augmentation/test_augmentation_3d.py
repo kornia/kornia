591b7295331dc8618669db9ecb62f1fc0c464fb7
Anirudh Buvanesh <anirudhb1102@gmail.com>
2021-10-17 22:11:02 +0530

    Minor fixes

--- test/augmentation/test_augmentation_3d.py
+++ test/augmentation/test_augmentation_3d.py
@@ -965,18 +965,18 @@ class TestRandomEqualize3D:
 
 class TestRandomAffine3D:
 
-    def test_batch_random_affine_3d(self, device):
+    def test_batch_random_affine_3d(self, device, dtype):
 
         f = RandomAffine3D((0, 0, 0), p=1., return_transform=True)  # No rotation
-        tensor = torch.tensor([[[[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]]]])  # 1 x 1 x 1 x 3 x 3
+        tensor = torch.tensor([[[[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]]]],
+                              device=device, dtype=dtype)  # 1 x 1 x 1 x 3 x 3
 
-        expected = torch.tensor([[[[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]]]])  # 1 x 1 x 1 x 3 x 3
-        expected = expected.to(device)
+        expected = torch.tensor([[[[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]]]],
+                                device=device, dtype=dtype)  # 1 x 1 x 1 x 3 x 3
 
         expected_transform = torch.tensor(
-            [[[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]]
-        )  # 1 x 4 x 4
-        expected_transform = expected_transform.to(device)
+            [[[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]],
+            device=device, dtype=dtype)  # 1 x 4 x 4
 
         tensor = tensor.repeat(5, 3, 1, 1, 1)  # 5 x 3 x 3 x 3 x 3
         expected = expected.repeat(5, 3, 1, 1, 1)  # 5 x 3 x 3 x 3 x 3
