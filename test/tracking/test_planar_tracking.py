f8da336b6f5c1581f81a78d1caf67944e57cf79a
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-10-18 17:40:19 +0200

    stupid CI OOM

--- test/tracking/test_planar_tracking.py
+++ test/tracking/test_planar_tracking.py
@@ -6,7 +6,7 @@ from torch.autograd import gradcheck
 import kornia
 import kornia.testing as utils  # test utils
 from kornia.feature import *
-from kornia.geometry import transform_points
+from kornia.geometry import transform_points, resize
 from kornia.testing import assert_close
 from kornia.tracking import *
 
@@ -32,13 +32,18 @@ class TestHomographyTracker:
                         reason='1.6.0 not supporting the pretrained weights as they are packed.')
     def test_real(self, device, dtype):
         # This is not unit test, but that is quite good integration test
-        tracker = HomographyTracker().to(device, dtype)
+        matcher = LocalFeatureMatcher(GFTTAffNetHardNet(1000),
+                                      DescriptorMatcher('snn', 0.8)).to(device)
+        tracker = HomographyTracker(matcher).to(device, dtype)
         data = torch.load("data/test/loftr_outdoor_and_homography_data.pt")
+        h0, w0 = data["image0"].shape[2:]
+        data["image0"] = resize(data["image0"], (int(h0 // 2), int(w0 // 2)))
+        data["image1"] = resize(data["image1"], (int(h0 // 2), int(w0 // 2)))
         tracker.set_target(data["image0"])
         homography, success = tracker(data["image1"])
         assert success
-        pts_src = data['pts0'].to(device, dtype)
-        pts_dst = data['pts1'].to(device, dtype)
+        pts_src = data['pts0'].to(device, dtype) / 2.0
+        pts_dst = data['pts1'].to(device, dtype) / 2.0
         # Reprojection error of 5px is OK
         assert_close(
             transform_points(homography[None], pts_src[None]),
