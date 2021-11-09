c7bafec6e302d6a4bf1a7b04d9c0260cd5d08425
Yaniv Hollander <yaniv.hollander@gmail.com>
2021-11-08 21:19:39 -0500

    Create nerf.py

---
+++ kornia/geometry/nerf/nerf.py
@@ -0,0 +1,45 @@
+import unittest
+import torch
+from typing import Tuple
+
+from kornia.geometry.nerf.from_nerfmm import train_model
+import torch.nn as nn
+
+
+class CameraCalibration:
+    def __init__(self, nerf_model: torch.nn.Module, n_epoch: int = 1):
+        self._nerf_model = nerf_model
+        self._images: torch.Tensor = None
+        self._nerf_model: nn.Module = None
+        self._focal_net: nn.Module = None
+        self._pose_param_net: nn.Module = None
+
+        self._n_epoch = n_epoch
+
+    def add_images(self, images: torch.Tensor):
+        self._images = images
+
+    def run(self):
+        self._nerf_model, self._focal_net, self._pose_param_net = \
+            train_model(self._images, n_epoch = self._n_epoch)
+
+    def model(self) -> nn.Module:
+        return self._nerf_model
+
+    def focals(self) -> Tuple[float, float]:
+        fxfy = self._focal_net()
+        return fxfy[0], fxfy[1]
+
+    def pose(self, camera_id: int):
+        return self._pose_param_net(camera_id)
+
+    def n_epoch(self) -> int:
+        return self._n_epoch
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
