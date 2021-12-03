ee093033d0ab43bcce4d5f77fd817fb6cdd039ea
Yaniv Hollander <yaniv.hollander@gmail.com>
2021-12-02 22:09:14 -0500

    Update nerf.py

--- kornia/geometry/nerf/nerf.py
+++ kornia/geometry/nerf/nerf.py
@@ -25,19 +25,38 @@ class RayParameters:
 
 
 class LearnFocals(nn.Module):
-    def __init__(self, h: int, w: int, req_grad: bool):
+    def __init__(self, h: int, w: int, num_cams: int, req_grad: bool, multi_focal: bool):
         super().__init__()
-        self.fxfy = nn.Parameter(torch.ones(size=(num_cams, 2), dtype=torch.float32), requires_grad=req_grad)
+        self.__h = h
+        self.__w = w
+        self.__multi_focal = multi_focal
+        if multi_focal:
+            self.__fxfy = \
+                nn.Parameter(torch.tensor(torch.ones(size=(num_cams, 2)), dtype=torch.float32), requires_grad=req_grad)
+        else:
+            self.__fxfy = \
+                nn.Parameter(torch.tensor(torch.ones(size=(1, 2)), dtype=torch.float32), requires_grad=req_grad)
 
     def forward(self, cam_id):
-        pass
+        if self.__multi_focal:
+            fx = self.__fxfy[cam_id][0]
+            fy = self.__fxfy[cam_id][1]
+        else:
+            fx = self.__fxfy[0][0]
+            fy = self.__fxfy[0][1]
+        fxfy = torch.stack([fx**2 * self.__w, fy**2 * self.__h])
+        return fxfy
+
+    def is_multi_focal(self):
+        return self.__multi_focal
 
 
 class CameraCalibration:
     def __init__(self,
                  nerf_model: nn.Module = TinyNerf(pos_in_dims=63, dir_in_dims=27, D=128),
                  device: str = 'cpu', checkpoint_path: str = None, checkpoint_save_frequency: int = 1,
-                 n_selected_rays: int = 32):
+                 n_selected_rays: int = 32,
+                 lr: float = 0.001):
         self._nerf_model: nn.Module = nerf_model
         self._focal_net: nn.Module = None
         self._pose_param_net: nn.Module = None
@@ -58,6 +77,10 @@ class CameraCalibration:
         self._checkpoint_save_frequency = checkpoint_save_frequency
 
         self._n_selected_rays = n_selected_rays
+
+        self._lr = lr
+
+        self._multi_focal: bool = None
 
         self._images: torch.Tensor = None
         self._image_filenames: List[str] = None
@@ -88,25 +111,41 @@ class CameraCalibration:
     def get_image_sizes(self) -> Tuple[int, int, int, int]:
         return self._images.shape[0], self._images.shape[1], self._images.shape[2], self._images.shape[3]
 
-    def __init_optimizers(self):
-        self._opt_nerf: torch.optim.Optimizer = torch.optim.Adam(self._nerf_model.parameters(), lr=0.001)
-        self._opt_focal: torch.optim.Optimizer = torch.optim.Adam(self._focal_net.parameters(), lr=0.001)
-        self._opt_pose: torch.optim.Optimizer = torch.optim.Adam(self._pose_param_net.parameters(), lr=0.001)
-
-    def init_training(self, load_checkpoint_path=None):
+    def __init_camera_models(self, multi_focal: bool):
         n_imgs = self._images.shape[0]
         h = self._images.shape[1]
         w = self._images.shape[2]
-
-        self._epoch0 = 0
-        self._focal_net = LearnFocal(h, w, req_grad=True)
+        self._focal_net = LearnFocals(h, w, n_imgs, req_grad=True, multi_focal=multi_focal)
         self._pose_param_net = LearnPose(num_cams=n_imgs, learn_R=True, learn_t=True)
+
+    def __init_optimizers(self):     # FIXME: handle learning rate parameterization properly
+        self._opt_nerf: torch.optim.Optimizer = torch.optim.Adam(self._nerf_model.parameters(), lr=self._lr)
+        self._opt_focal: torch.optim.Optimizer = torch.optim.Adam(self._focal_net.parameters(), lr=self._lr)
+        self._opt_pose: torch.optim.Optimizer = torch.optim.Adam(self._pose_param_net.parameters(), lr=self._lr)
+
+    def __reset_nerf(self):
+        for layer in self._nerf_model.children():
+            if hasattr(layer, 'reset_parameters'):
+                layer.reset_parameters()
+
+    def init_training(self, load_checkpoint_path=None, reset_only_nerf=False, multi_focal=True):
+        if reset_only_nerf and (not self._focal_net or not self._pose_param_net):
+            raise RuntimeError('Cannot reset only Nerf model parameters if camera models were not instantiated')
+        self._epoch0 = 0
+
         if load_checkpoint_path is not None:
             self.__load_model_checkpoint(load_checkpoint_path)
 
         else:
+            if not reset_only_nerf:
+                self.__init_camera_models(multi_focal)
+                self._multi_focal = multi_focal
+            self.__reset_nerf()
             self.__to_device()
             self.__init_optimizers()
+
+        if reset_only_nerf:
+            self.__reset_nerf()
 
         # FIXME: Verify how these schedulers work when models and optimizers are loaded from a checkpoint
         self._scheduler_nerf = MultiStepLR(self._opt_nerf, milestones=list(range(0, 10000, 10)), gamma=0.9954)
@@ -131,9 +170,14 @@ class CameraCalibration:
                                       self._device)
             train_psnr = mse2psnr(l2_loss)
 
-            fxfy = self._focal_net()
-            print('epoch {:4d} Training PSNR {:.3f}, estimated fx {:.1f} fy {:.1f}'.format(epoch_i + 1, train_psnr, fxfy[0],
-                                                                                           fxfy[1]))
+            fxfy = self._focal_net(0)
+            if not self._focal_net.is_multi_focal():
+                print('epoch {:4d} Training PSNR {:.3f}, estimated fx {:.1f} fy {:.1f}'
+                      .format(epoch_i + 1, train_psnr, fxfy[0], fxfy[1]))
+            else:
+                fxfy2 = self._focal_net(1)
+                print('epoch {:4d} Training PSNR {:.3f}, estimated fx {:.1f} fy {:.1f}; fx {:.1f} fy {:.1f}'
+                      .format(epoch_i + 1, train_psnr, fxfy[0], fxfy[1], fxfy2[0], fxfy2[1]))
 
             self._scheduler_nerf.step()
             self._scheduler_focal.step()
@@ -147,6 +191,7 @@ class CameraCalibration:
         torch.save({
             'epoch': epoch,
             'nerf_model_state_dict': self._nerf_model.state_dict(),
+            'multi_focal': self._multi_focal,
             'focal_net_state_dict': self._focal_net.state_dict(),
             'pose_param_net_state_dict': self._pose_param_net.state_dict(),
             'nerf_optimizer_state_dict': self._opt_nerf.state_dict(),
@@ -161,9 +206,10 @@ class CameraCalibration:
         self._pose_param_net.to(device)
 
     def __load_model_checkpoint(self, load_checkpoint_path: str):
-        device = torch.device(self._device)
         checkpoint = torch.load(load_checkpoint_path)
         self._nerf_model.load_state_dict(checkpoint['nerf_model_state_dict'])
+        self._multi_focal = checkpoint['multi_focal']
+        self.__init_camera_models(self._multi_focal)
         self._focal_net.load_state_dict(checkpoint['focal_net_state_dict'])
         self._pose_param_net.load_state_dict(checkpoint['pose_param_net_state_dict'])
         self.__to_device()
@@ -176,8 +222,8 @@ class CameraCalibration:
     def model(self) -> nn.Module:
         return self._nerf_model
 
-    def focals(self) -> Tuple[float, float]:
-        fxfy = self._focal_net()
+    def focals(self, camera_id: int) -> Tuple[float, float]:
+        fxfy = self._focal_net(camera_id)
         return fxfy[0], fxfy[1]
 
     def pose(self, camera_id: int):
@@ -187,7 +233,7 @@ class CameraCalibration:
         device = torch.device('cpu')
         rt = self.pose(camera_id).to(device)
         rt = rt[0:3]
-        fxfy = self._focal_net().to(device)
+        fxfy = self._focal_net(camera_id).to(device)
         h = self._images.shape[1]
         w = self._images.shape[2]
         k = torch.tensor([[fxfy[0], 0,       h / 2],
@@ -199,7 +245,7 @@ class CameraCalibration:
         return self._n_epoch
 
     def render_image_from_training_set(self, camera_id: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        fxfy = self._focal_net()
+        fxfy = self._focal_net(camera_id)
         h = self._images.shape[1]
         w = self._images.shape[2]
         c2w = self._pose_param_net(camera_id)
@@ -210,17 +256,86 @@ class CameraCalibration:
         return rendered_img, rendered_depth
 
 
+def initialize_nerf_object_for_testing(checkpoint_path: str = None) -> CameraCalibration:
+    x = CameraCalibration(checkpoint_path = checkpoint_path)
+    images = torch.rand(3, 40, 50, 3)
+    x.set_images(images)
+    return x
+
+def equal_models(model1: nn.Module, model2: nn.Module) -> bool:
+    models_equal = True
+    for p1, p2 in zip(model1.parameters(), model2.parameters()):
+        if p1.data.ne(p2.data).sum() > 0:
+            models_equal = False
+            break
+    return models_equal
+
+import copy
+
+
 class MyTestCase(unittest.TestCase):
     def test_nerf(self):
         x = CameraCalibration()
-        images = torch.rand(2, 40, 50, 3)
+        images = torch.rand(3, 40, 50, 3)
         x.set_images(images)
         x.init_training()
-        x.run(n_epoch=2)
+        x.run(n_epoch=5)
 
         img, depth = x.render_image_from_training_set(1)
         print(img.shape, depth.shape)
 
+    def test_initialize_and_run(self):
+        x = initialize_nerf_object_for_testing()
+        x.init_training()
+        x.run(n_epoch=2)
+
+    def test_initialize_from_checkpoint(self):
+        checkpoint_path = os.path.join(os.getcwd(), 'checkpoint')
+        x = initialize_nerf_object_for_testing(checkpoint_path=checkpoint_path)
+        x.init_training()
+        x.run(n_epoch=2)
+        fxfy = x.focals(0)
+        pose = x.pose(0)
+
+        x = initialize_nerf_object_for_testing()
+        x.init_training(load_checkpoint_path=checkpoint_path)
+        fxfy_after_load = x.focals(0)
+        self.assertEqual(fxfy, fxfy_after_load)
+        pose_after_load = x.pose(0)
+        self.assertTrue(torch.Tensor.equal(pose, pose_after_load))
+
+    def test_initialize_only_nerf_and_run(self):
+        x = initialize_nerf_object_for_testing()
+        with self.assertRaises(RuntimeError):
+            x.init_training(reset_only_nerf=True)
+        x.init_training()
+        x.run(n_epoch=2)
+        fxfy = x.focals(0)
+        pose = x.pose(0)
+        model = copy.deepcopy(x.model())
+
+        x.init_training(reset_only_nerf=True)
+
+        fxfy_after_reset = x.focals(0)
+        self.assertEqual(fxfy, fxfy_after_reset)
+        pose_after_reset = x.pose(0)
+        self.assertTrue(torch.Tensor.equal(pose, pose_after_reset))
+        model_after_reset = x.model()
+
+        self.assertFalse(equal_models(model, model_after_reset))
+
+    def test_initialize_run_and_initialize(self):
+        x = initialize_nerf_object_for_testing()
+        x.init_training()
+        x.run(n_epoch=2)
+        model = copy.deepcopy(x.model())
+
+        x.init_training()
+        model_after_init = x.model()
+
+        self.assertFalse(equal_models(model, model_after_init))
+
 
 if __name__ == '__main__':
     unittest.main()
+    # MyTestCase().test_initialize_run_and_initialize()
