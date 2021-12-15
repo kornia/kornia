Fix padded input shape in parameter computation and fix for VideoSequential

kornia/augmentation/augmentation.py
@@ -1074,13 +1074,13 @@ class RandomCrop(GeometricAugmentationBase2D):
 
     def forward_parameters_precrop(self, batch_shape) -> Dict[str, torch.Tensor]:
         input_pad = self.compute_padding(batch_shape)
-        batch_shape = (
+        batch_shape_new = (
             *batch_shape[:2],
-            batch_shape[2] + input_pad[1] + input_pad[3],  # original height + top + bottom padding
-            batch_shape[3] + input_pad[0] + input_pad[2]  # original width + left + right padding
+            batch_shape[2] + input_pad[2] + input_pad[3],  # original height + top + bottom padding
+            batch_shape[3] + input_pad[0] + input_pad[1]  # original width + left + right padding
         )
         padding_size = torch.tensor(tuple(input_pad), dtype=torch.long).expand(batch_shape[0], -1)
-        _params = super().forward_parameters(batch_shape)
+        _params = super().forward_parameters(batch_shape_new)
         _params.update({"padding_size": padding_size})
         return _params
 

kornia/augmentation/container/video.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 
 import kornia
+from kornia.augmentation import RandomCrop
 from kornia.augmentation.base import _AugmentationBase, MixAugmentationBase, TensorWithTransformMat
 from kornia.augmentation.container.base import SequentialBase
 from kornia.augmentation.container.utils import InputApplyInverse, MaskApplyInverse
@@ -180,7 +181,14 @@ class VideoSequential(ImageSequential):
 
         params = []
         for name, module in named_modules:
-            if isinstance(module, (SequentialBase,)):
+            if isinstance(module, RandomCrop):
+                param = module.forward_parameters_precrop(batch_shape)
+                if self.same_on_frame:
+                # param.update({'src': self.__repeat_param_across_channels__(param['src'], frame_num)})
+                    param["src"] = param["src"].repeat(frame_num, 1, 1)
+                    param["dst"] = param["dst"].repeat(frame_num, 1, 1)
+                param = ParamItem(name, param)
+            elif isinstance(module, (SequentialBase,)):
                 seq_param = module.forward_parameters(batch_shape)
                 if self.same_on_frame:
                     raise ValueError("Sequential is currently unsupported for ``same_on_frame``.")