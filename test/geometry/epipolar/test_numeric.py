815627ac2843f6542f0e648b22ca8bb2216f1376
Nitai Fingerhut <nitai@Nitais-MacBook-Pro-3.local>
2022-01-18 17:39:52 +0200

    fixing tests

--- test/geometry/epipolar/test_numeric.py
+++ test/geometry/epipolar/test_numeric.py
@@ -21,30 +21,28 @@ class TestSkewSymmetric:
         assert cross_product_matrix.shape == (B, 3, 3)
 
     @pytest.mark.parametrize("shapes", [(1, 1), (1, 5), (2, 1), (2, 5), (4, 1), (4, 5)])
-    def test_shapes(self, shapes, device, dtype):
-        for shape in shapes:
-            input_shape = shape + (3, )
-            output_shape = shape + (3, 3)
-            t = torch.rand(*input_shape, device=device, dtype=dtype)
-            cross_product_matrix = epi.cross_product_matrix(t)
-            assert cross_product_matrix.shape == output_shape
+    def test_shapes(self, device, dtype, shapes):
+        input_shape = shapes + (3, )
+        output_shape = shapes + (3, 3)
+        t = torch.rand(*input_shape, device=device, dtype=dtype)
+        cross_product_matrix = epi.cross_product_matrix(t)
+        assert cross_product_matrix.shape == output_shape
 
     @pytest.mark.parametrize("shapes", [(1, 1), (1, 5), (2, 1), (2, 5), (4, 1), (4, 5)])
-    def test_funcional_shapes(self, shapes, device, dtype):
-        for shape in shapes:
-            input_shape = shape + (3, )
-            t = torch.rand(*input_shape, device=device, dtype=dtype)
+    def test_funcional_shapes(self, device, dtype, shapes):
+        input_shape = shapes + (3, )
+        t = torch.rand(*input_shape, device=device, dtype=dtype)
 
-            # Feed batches
-            cross_product_matrices = []
-            for i in range(t.shape[1]):
-                cross_product_matrices.append(epi.cross_product_matrix(t[:, i, ...]))
-            cross_product_matrix_v1 = torch.stack(cross_product_matrices, dim=1)
+        # Feed batches
+        cross_product_matrices = []
+        for i in range(t.shape[1]):
+            cross_product_matrices.append(epi.cross_product_matrix(t[:, i, ...]))
+        cross_product_matrix_parts = torch.stack(cross_product_matrices, dim=1)
 
-            # Feed one-shot
-            cross_product_matrix_v2 = epi.cross_product_matrix(t)
+        # Feed one-shot
+        cross_product_matrix_whole = epi.cross_product_matrix(t)
 
-            assert_close(cross_product_matrix_v1, cross_product_matrix_v2)
+        assert_close(cross_product_matrix_parts, cross_product_matrix_whole)
 
     def test_mean_std(self, device, dtype):
         vec = torch.tensor([[1.0, 2.0, 3.0]], device=device, dtype=dtype)
