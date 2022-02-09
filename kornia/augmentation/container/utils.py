17ede25e85221d2e1817aa955cf7e29841b090fd
shijianjian <sj8716643@126.com>
2022-02-09 22:57:45 +0800

    bug fix

--- kornia/augmentation/container/utils.py
+++ kornia/augmentation/container/utils.py
@@ -80,7 +80,10 @@ class ApplyInverseImpl(ApplyInverseInterface):
             param: the corresponding parameters to the module.
         """
 
-        mat: Optional[torch.Tensor] = cls._get_transformation(input, module, param)
+        if hasattr(module, "_transform_matrix") and module._transform_matrix is not None:
+            mat = module._transform_matrix
+        else:
+            mat: Optional[torch.Tensor] = cls._get_transformation(input, module, param)
         to_apply = None
         if isinstance(module, _AugmentationBase):
             to_apply = param.data['batch_prob']  # type: ignore
@@ -103,7 +106,10 @@ class ApplyInverseImpl(ApplyInverseInterface):
                 to apply transformations.
             param: the corresponding parameters to the module.
         """
-        mat: Optional[torch.Tensor] = cls._get_transformation(input, module, param)
+        if hasattr(module, "_transform_matrix") and module._transform_matrix is not None:
+            mat = module._transform_matrix
+        else:
+            mat: Optional[torch.Tensor] = cls._get_transformation(input, module, param)
 
         if mat is not None:
             transform: torch.Tensor = cls._get_inverse_transformation(mat)
