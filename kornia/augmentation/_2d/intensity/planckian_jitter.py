aa1189c194a1fe2045c68f70e3af5af314e3c381
nrupatunga <nrupatunga.tunga@gmail.com>
2022-03-11 12:17:46 +0530

    [feat-planckian]: update docs and examples

--- kornia/augmentation/_2d/intensity/planckian_jitter.py
+++ kornia/augmentation/_2d/intensity/planckian_jitter.py
@@ -75,6 +75,9 @@ class PlanckianJitter(IntensityAugmentationBase2D):
 
     Args:
         mode: 'blackbody' or 'CIED'.
+        select_from: choose a list of jitters to apply from. For
+        `blackbody` choose in the range [0, 24], and for `CIED` choose
+        in the range [0, 22]
         same_on_batch: apply the same transformation across the batch.
         p: probability that the random erasing operation will be performed.
         keepdim: whether to keep the output shape the same as input (True) or broadcast it
@@ -87,16 +90,43 @@ class PlanckianJitter(IntensityAugmentationBase2D):
         Input tensor must be float and normalized into [0, 1]
 
     Examples:
+    To apply planckian jitter based on mode
         >>> rng = torch.manual_seed(0)
         >>> input = torch.randn(1, 3, 2, 2)
-        >>> aug = PlanckianJitter(mode='CIED', p=1.0)
+        >>> aug = PlanckianJitter(mode='CIED')
         >>> aug(input)
-            tensor([[[[ 1.0000, -0.2574],
-              [-1.9111,  0.4986]],
-             [[-1.0845, -1.3986],
-              [ 0.4033,  0.8380]],
-             [[-0.8552, -0.4796],
-              [-0.7094,  0.2164]]]])
+        tensor([[[[ 1.0000, -0.2389],
+                  [-1.7740,  0.4628]],
+        <BLANKLINE>
+                 [[-1.0845, -1.3986],
+                  [ 0.4033,  0.8380]],
+        <BLANKLINE>
+                 [[-0.9228, -0.5175],
+                  [-0.7654,  0.2335]]]])
+
+    To apply planckian jitter on image(s) from list of interested jitters
+        >>> rng = torch.manual_seed(0)
+        >>> input = torch.randn(2, 3, 2, 2)
+        >>> aug = PlanckianJitter(mode='blackbody', select_from=[23, 24, 1, 2])
+        >>> aug(input)
+        tensor([[[[-1.1258, -1.1524],
+                  [-0.2506, -0.4339]],
+        <BLANKLINE>
+                 [[ 0.8487,  0.6920],
+                  [-0.3160, -2.1152]],
+        <BLANKLINE>
+                 [[ 0.4681, -0.1577],
+                  [ 1.4437,  0.2660]]],
+        <BLANKLINE>
+        <BLANKLINE>
+                [[[ 0.2465,  1.0000],
+                  [-0.2125, -0.1653]],
+        <BLANKLINE>
+                 [[ 0.9318,  1.0000],
+                  [ 1.0000,  0.0537]],
+        <BLANKLINE>
+                 [[ 0.2426, -0.1621],
+                  [-0.3302, -0.9093]]]])
     """
 
     def __init__(self,
@@ -108,12 +138,16 @@ class PlanckianJitter(IntensityAugmentationBase2D):
                  return_transform: Optional[bool] = None) -> None:
         super().__init__(p=p, return_transform=return_transform, same_on_batch=same_on_batch, keepdim=keepdim)
 
+        if isinstance(select_from, int):
+            select_from = [select_from]
+
         self.idx = select_from
-        if select_from:
+        if select_from is not None:
             self.register_buffer('pl',
                                  _planckian_coeffs_ratio[mode][select_from])
         else:
             self.register_buffer('pl', _planckian_coeffs_ratio[mode])
+
         self._param_generator = cast(rg.PlanckianJitterGenerator,
                                      rg.PlanckianJitterGenerator([0, self.pl.shape[0]]))
 
@@ -122,6 +156,7 @@ class PlanckianJitter(IntensityAugmentationBase2D):
     ) -> Tensor:
 
         list_idx = params['idx'].tolist()
+        __import__('pdb').set_trace()
 
         mult_ch_zero = self.pl[list_idx][:, 0]
         mult_ch_two = self.pl[list_idx][:, 1]
