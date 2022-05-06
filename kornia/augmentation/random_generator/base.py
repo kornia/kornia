9cb150587b5449843f04d3bc3533e8933f3d6c47
Jian Shi <sj8716643@126.com>
2022-05-06 18:32:06 +0300

    [feat] Added distribution mapper (#1667)
    * Added distribution mapper
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixed mypy
    
    * Updated naming
    
    * Updated naming
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/augmentation/random_generator/base.py
+++ kornia/augmentation/random_generator/base.py
@@ -1,7 +1,8 @@
-from typing import Dict
+from typing import Callable, Dict, Optional
 
 import torch
 import torch.nn as nn
+from torch.distributions import Distribution
 
 
 class _PostInitInjectionMetaClass(type):
@@ -37,3 +38,57 @@ class RandomGeneratorBase(nn.Module, metaclass=_PostInitInjectionMetaClass):
 
     def forward(self, batch_shape: torch.Size, same_on_batch: bool = False) -> Dict[str, torch.Tensor]:  # type:ignore
         raise NotImplementedError
+
+
+class DistributionWithMapper(Distribution):
+    """Wraps a distribution with a value mapper function.
+
+    This is used to restrict the output values of a given distribution by a value mapper function.
+    The value mapper function can be functions like sigmoid, tanh, etc.
+
+    Args:
+        dist: the target distribution.
+        map_fn: the callable function to adjust the output from distributions.
+
+    Example:
+        >>> from torch.distributions import Normal
+        >>> import torch.nn as nn
+        >>> # without mapper
+        >>> dist = DistributionWithMapper(Normal(0., 1.,), map_fn=None)
+        >>> _ = torch.manual_seed(0)
+        >>> dist.rsample((8,))
+        tensor([ 1.5410, -0.2934, -2.1788,  0.5684, -1.0845, -1.3986,  0.4033,  0.8380])
+        >>> # with sigmoid mapper
+        >>> dist = DistributionWithMapper(Normal(0., 1.,), map_fn=nn.Sigmoid())
+        >>> _ = torch.manual_seed(0)
+        >>> dist.rsample((8,))
+        tensor([0.8236, 0.4272, 0.1017, 0.6384, 0.2527, 0.1980, 0.5995, 0.6980])
+    """
+
+    def __init__(self, dist: Distribution, map_fn: Optional[Callable] = None) -> None:
+        self.dist = dist
+        self.map_fn = map_fn
+
+    def rsample(self, sample_shape: torch.Size) -> torch.Tensor:  # type:ignore
+        out = self.dist.rsample(sample_shape)
+        if self.map_fn is not None:
+            out = self.map_fn(out)
+        return out
+
+    def sample(self, sample_shape: torch.Size) -> torch.Tensor:  # type:ignore
+        out = self.dist.sample(sample_shape)
+        if self.map_fn is not None:
+            out = self.map_fn(out)
+        return out
+
+    def sample_n(self, n) -> torch.Tensor:
+        out = self.dist.sample_n(n)
+        if self.map_fn is not None:
+            out = self.map_fn(out)
+        return out
+
+    def __getattr__(self, attr):
+        try:
+            return getattr(self, attr)
+        except AttributeError:
+            return getattr(self.dist, attr)
