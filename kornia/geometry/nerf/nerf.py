ac54e4faaff5b00bf3983ea24f0957cc40ef7b12
Yaniv Hollander <yhollander@american-robotics.com>
2022-05-20 02:14:48 +0000

    Sync

---
+++ kornia/geometry/nerf/nerf.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+
+class PositionalEncoder(nn.Module):
+    r"""
+    Sine-cosine positional encoder for input points.
+    """
+
+    def __init__(self, d_input: int, n_freqs: int, log_space: bool = False):
+        super().__init__()
+        self.d_input = d_input
+        self.n_freqs = n_freqs
+        self.log_space = log_space
+        self.d_output = d_input * (1 + 2 * self.n_freqs)
+        self.embed_fns = [lambda x: x]
+
+        # Define frequencies in either linear or log scale
+        if self.log_space:
+            freq_bands = 2.0 ** torch.linspace(0.0, self.n_freqs - 1, self.n_freqs)
+        else:
+            freq_bands = torch.linspace(2.0**0.0, 2.0 ** (self.n_freqs - 1), self.n_freqs)
+
+        # Alternate sin and cos
+        for freq in freq_bands:
+            self.embed_fns.append(lambda x, freq=freq: torch.sin(x * freq))
+            self.embed_fns.append(lambda x, freq=freq: torch.cos(x * freq))
+
+    def forward(self, x) -> torch.Tensor:
+        r"""
+        Apply positional encoding to input.
+        """
+        return torch.concat([fn(x) for fn in self.embed_fns], dim=-1)
