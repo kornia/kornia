6e7d174d46c8c4fca25fc823c62e55abc19c07a5
Yaniv Hollander <yhollander@american-robotics.com>
2022-05-23 03:09:34 +0000

    Sync

--- kornia/geometry/nerf/nerf.py
+++ kornia/geometry/nerf/nerf.py
@@ -9,25 +9,27 @@ class PositionalEncoder(nn.Module):
 
     def __init__(self, d_input: int, n_freqs: int, log_space: bool = False):
         super().__init__()
-        self.d_input = d_input
-        self.n_freqs = n_freqs
-        self.log_space = log_space
-        self.d_output = d_input * (1 + 2 * self.n_freqs)
-        self.embed_fns = [lambda x: x]
+        self._d_input = d_input
+        self._n_freqs = n_freqs
+        self._log_space = log_space
+        self._d_output = d_input * (1 + 2 * self._n_freqs)
+        self._embed_fns = [lambda x: x]
 
         # Define frequencies in either linear or log scale
-        if self.log_space:
-            freq_bands = 2.0 ** torch.linspace(0.0, self.n_freqs - 1, self.n_freqs)
+        if self._log_space:
+            freq_bands = 2.0 ** torch.linspace(0.0, self._n_freqs - 1, self._n_freqs)
         else:
-            freq_bands = torch.linspace(2.0**0.0, 2.0 ** (self.n_freqs - 1), self.n_freqs)
+            freq_bands = torch.linspace(2.0**0.0, 2.0 ** (self._n_freqs - 1), self._n_freqs)
 
         # Alternate sin and cos
         for freq in freq_bands:
-            self.embed_fns.append(lambda x, freq=freq: torch.sin(x * freq))
-            self.embed_fns.append(lambda x, freq=freq: torch.cos(x * freq))
+            self._embed_fns.append(lambda x, freq=freq: torch.sin(x * freq))
+            self._embed_fns.append(lambda x, freq=freq: torch.cos(x * freq))
 
-    def forward(self, x) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         r"""
         Apply positional encoding to input.
         """
-        return torch.concat([fn(x) for fn in self.embed_fns], dim=-1)
+        if x.ndim < 2 or x.shape[1] != self._d_input:
+            raise RuntimeError('Invalid input geometry to encode')
+        return torch.concat([fn(x) for fn in self._embed_fns], dim=-1)
