13622d1001945b495421ac80ab4411405f0de9e6
daniel <daniel.koguciuk@gmail.com>
2022-05-29 21:41:39 +0200

    With bihome implementation

--- kornia/losses/perceptual.py
+++ kornia/losses/perceptual.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 import torchvision
@@ -7,8 +9,8 @@ from kornia.geometry.transform.imgwarp import get_perspective_transform, warp_pe
 
 class LossNetwork(nn.Module):
 
-    def __init__(self, model_name: str = 'resnet34', output_layer: int = 2, freeze: bool = True) -> None:
-        super(LossNetwork, self).__init__()
+    def __init__(self, model_name: str = 'resnet34', output_layer: int = 1, freeze: bool = True) -> None:
+        super().__init__()
         assert 'resnet' in model_name, 'Only ResNets are supported ATM'
 
         # Define resnet model
@@ -54,7 +56,7 @@ class LossNetwork(nn.Module):
 
 
 def _image_shape_to_corners(image: torch.Tensor) -> torch.Tensor:
-    """ Convert image size to 4 corners representation in clockwise order.
+    """Convert image size to 4 corners representation in clockwise order.
 
     Args:
         image: image tensor with shape :math:`(B, C, H, W)` where B = batch size, C = number of channels
@@ -74,7 +76,7 @@ def _image_shape_to_corners(image: torch.Tensor) -> torch.Tensor:
 
 
 def _four_point_to_homography(corners: torch.Tensor, deltas: torch.Tensor) -> torch.Tensor:
-    """ Convert 4-point representation introduced in :cite:`detone2016deep` to homography.
+    """Convert 4-point representation introduced in :cite:`detone2016deep` to homography.
 
     Args:
         corners: corners tensor with shape :math:`(B, 4, 2)` where B = batch size
@@ -106,8 +108,8 @@ def _four_point_to_homography(corners: torch.Tensor, deltas: torch.Tensor) -> to
     return homography
 
 
-def _warp(image: torch.Tensor, delta_hat: torch.Tensor) -> torch.Tensor:
-    """ Convert 4-point representation introduced in :cite:`detone2016deep` to homography.
+def _warp(image: torch.Tensor, delta_hat: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    """Convert 4-point representation introduced in :cite:`detone2016deep` to homography.
 
     Args:
         image: image tensor with shape :math:`(B, C, H, W)` where B = batch size, C = number of channels
@@ -118,14 +120,16 @@ def _warp(image: torch.Tensor, delta_hat: torch.Tensor) -> torch.Tensor:
     """
     corners = _image_shape_to_corners(image=image)
     homography = _four_point_to_homography(corners=corners, deltas=delta_hat)
-    image_warped = warp_perspective(image, homography, tuple((image.shape[-2], image.shape[-1])))
-    return image_warped
+    image_warped = warp_perspective(image, homography, (image.shape[-2], image.shape[-1]))
+    return image_warped, torch.inverse(homography)
 
 
-def ihome_loss(
+def bihome_loss(
     patch_1: torch.Tensor,
     patch_2: torch.Tensor,
-    delta_hat: torch.Tensor,
+    delta_hat_12: torch.Tensor,
+    delta_hat_21: torch.Tensor,
+    triplet_mu: float,
     loss_network: nn.Module,
 ) -> torch.Tensor:
     r"""iHomE loss implementation.
@@ -170,14 +174,24 @@ def ihome_loss(
     if patch_1.shape != patch_2.shape:
         raise ValueError(f'Expected patch_1 shape ({patch_1.shape}) to match patch_2 shape ({patch_2.shape}).')
 
-    if not isinstance(delta_hat, torch.Tensor):
-        raise TypeError(f"delta_hat type is not a torch.Tensor. Got {type(delta_hat)}")
+    if not isinstance(delta_hat_12, torch.Tensor):
+        raise TypeError(f"delta_hat_12 type is not a torch.Tensor. Got {type(delta_hat_12)}")
 
-    if not len(delta_hat.shape) == 3 or not delta_hat.shape[1] == 4 or not delta_hat.shape[2] == 2:
-        raise ValueError(f"Invalid input shape of delta_hat, we expect Bx4x2. Got: {delta_hat.shape}")
+    if not len(delta_hat_12.shape) == 3 or not delta_hat_12.shape[1] == 4 or not delta_hat_12.shape[2] == 2:
+        raise ValueError(f"Invalid input shape of delta_hat_12, we expect Bx4x2. Got: {delta_hat_12.shape}")
 
-    if not delta_hat.size(0) == patch_1.size(0):
-        raise ValueError(f'Expected delta_hat batch_size ({delta_hat.size(0)}) to match patch_1 batch size '
+    if not delta_hat_12.size(0) == patch_1.size(0):
+        raise ValueError(f'Expected delta_hat_12 batch_size ({delta_hat_12.size(0)}) to match patch_1 batch size '
+                         f'({patch_1.size(0)}).')
+
+    if not isinstance(delta_hat_21, torch.Tensor):
+        raise TypeError(f"delta_hat_21 type is not a torch.Tensor. Got {type(delta_hat_21)}")
+
+    if not len(delta_hat_21.shape) == 3 or not delta_hat_21.shape[1] == 4 or not delta_hat_21.shape[2] == 2:
+        raise ValueError(f"Invalid input shape of delta_hat_21, we expect Bx4x2. Got: {delta_hat_21.shape}")
+
+    if not delta_hat_21.size(0) == patch_1.size(0):
+        raise ValueError(f'Expected delta_hat_21 batch_size ({delta_hat_21.size(0)}) to match patch_1 batch size '
                          f'({patch_1.size(0)}).')
 
     if not isinstance(loss_network, nn.Module):
@@ -187,33 +201,52 @@ def ihome_loss(
     patch_1_f = loss_network(patch_1)
     patch_2_f = loss_network(patch_2)
 
-    # Warp patch 1 with delta hat
-    patch_1_prime = _warp(patch_1, delta_hat=delta_hat)
+    # Warp patch 1 with delta hat_12
+    patch_1_prime, h1 = _warp(patch_1, delta_hat=delta_hat_12)
     patch_1_prime_f = loss_network(patch_1_prime)
 
-    # Create masks and wark mask 1
+    # Warp patch 2 with delta hat_21
+    patch_2_prime, h2 = _warp(patch_2, delta_hat=delta_hat_21)
+    patch_2_prime_f = loss_network(patch_2_prime)
+
+    # Create and warp masks
     patch_1_m = torch.ones_like(patch_1)
     patch_2_m = torch.ones_like(patch_2)
-    patch_1_m_prime = _warp(patch_1_m, delta_hat=delta_hat)
+    patch_1_m_prime, _ = _warp(patch_1_m, delta_hat=delta_hat_12)
+    patch_2_m_prime, _ = _warp(patch_2_m, delta_hat=delta_hat_21)
 
     # Mask size mismatch downsampling
-    _, f_c, f_h, f_w = patch_1_prime_f.shape
+    _, _, f_h, _ = patch_1_prime_f.shape
     downsample_factor = patch_1_m.shape[-1] // f_h
     downsample_layer = torch.nn.AvgPool2d(kernel_size=downsample_factor, stride=downsample_factor, padding=0)
+    patch_1_m = torch.squeeze(downsample_layer(patch_1_m), dim=1)
     patch_2_m = torch.squeeze(downsample_layer(patch_2_m), dim=1)
     patch_1_m_prime = torch.squeeze(downsample_layer(patch_1_m_prime), dim=1)
+    patch_2_m_prime = torch.squeeze(downsample_layer(patch_2_m_prime), dim=1)
 
     # Triplet Margin Loss
-    l1 = torch.sum(torch.abs(patch_1_prime_f - patch_2_f), axis=1)
-    l3 = torch.sum(torch.abs(patch_1_f - patch_2_f), axis=1)
+    l1 = torch.sum(torch.abs(patch_1_prime_f - patch_2_f), dim=1)
+    l2 = torch.sum(torch.abs(patch_1_f - patch_2_prime_f), dim=1)
+    l3 = torch.sum(torch.abs(patch_1_f - patch_2_f), dim=1)
     ln1_nom = torch.sum(torch.sum(patch_1_m_prime * patch_2_m * (l1 - l3), dim=-1), dim=-1)
     ln1_den = torch.sum(torch.sum(patch_1_m_prime * patch_2_m, dim=-1), dim=-1)
     ln1_den = torch.max(ln1_den, torch.ones_like(ln1_den))
-    loss = torch.sum(ln1_nom / ln1_den)
+    ln2_nom = torch.sum(torch.sum(patch_1_m * patch_2_m_prime * (l2 - l3), dim=-1), dim=-1)
+    ln2_den = torch.sum(torch.sum(patch_1_m * patch_2_m_prime, dim=-1), dim=-1)
+    ln2_den = torch.max(ln2_den, torch.ones_like(ln2_den))
+    ln1 = torch.sum(ln1_nom / ln1_den)
+    ln2 = torch.sum(ln2_nom / ln2_den)
+
+    # Regularization
+    batch_size = patch_1.size(0)
+    eye = torch.eye(3, dtype=h1.dtype, device=h1.device).unsqueeze(dim=0).repeat(batch_size, 1, 1)
+    ln3 = torch.sum((torch.matmul(h1, h2) - eye) ** 2) * triplet_mu
+
+    loss = ln1 + ln2 + ln3
     return loss
 
 
-class iHomELoss(nn.Module):
+class biHomELoss(nn.Module):
     r"""Criterion that computes Focal loss. @TODO: fix description
 
     According to :cite:`lin2018focal`, the Focal loss is computed as follows:
@@ -250,20 +283,41 @@ class iHomELoss(nn.Module):
         >>> output.backward()
     """
 
-    def __init__(self, loss_network_name: str = 'resnet34', loss_network=None) -> None:
+    def __init__(
+        self,
+        loss_network_name: str = 'resnet34',
+        loss_network=None,
+        triplet_mu: float = 0.01,
+    ) -> None:
         super().__init__()
 
         if loss_network is None and loss_network_name is None:
-            raise RuntimeError(f"At least one should be defined out of: loss_network and loss_network_name")
+            raise RuntimeError("At least one should be defined out of: loss_network and loss_network_name")
 
         if loss_network is not None and loss_network_name is not None:
-            raise RuntimeWarning(f"Both are defined out of: loss_network and loss_network_name - I will use "
-                                 f"loss_network_name")
+            raise RuntimeWarning("Both are defined out of: loss_network and loss_network_name - I will use "
+                                 "loss_network_name")
 
         if loss_network_name is not None:
             self.loss_network = LossNetwork(loss_network_name)
         else:
             self.loss_network = loss_network
 
-    def forward(self, patch_1: torch.Tensor, patch_2: torch.Tensor, delta_hat: torch.Tensor) -> torch.Tensor:
-        return ihome_loss(patch_1, patch_2, delta_hat, loss_network=self.loss_network)
+        self.triplet_mu = triplet_mu
+
+    def forward(
+        self,
+        patch_1: torch.Tensor,
+        patch_2: torch.Tensor,
+        delta_hat_12: torch.Tensor,
+        delta_hat_21: torch.Tensor,
+    ) -> torch.Tensor:
+        loss = bihome_loss(
+            patch_1,
+            patch_2,
+            delta_hat_12,
+            delta_hat_21,
+            self.triplet_mu,
+            loss_network=self.loss_network
+        )
+        return loss
