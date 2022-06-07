e14c2726c29a33443f51490087abc2937e7fecf8
Yaniv Hollander <yhollander@american-robotics.com>
2022-06-07 02:17:35 +0000

    Sync

--- kornia/geometry/nerf/rays.py
+++ kornia/geometry/nerf/rays.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List  # , Tuple
 
 import torch
 
@@ -6,48 +6,63 @@ from kornia.geometry.camera import PinholeCamera
 
 
 class RaySampler:
-    class Points2DBase:
-        _camera_ids: List[int] = []
+    class Points2D:
+        def __init__(self, points_2d: torch.Tensor, camera_ids: List[int]) -> None:
+            self._points_2d: torch.Tensor = points_2d
+            self._camera_ids: torch.Tensor = camera_ids
 
-    class Points2D(Points2DBase):
-        _points_2d: torch.Tensor  # (N, 2)
+        @property
+        def points_2d(self) -> torch.Tensor:
+            return self._points_2d
 
-    class Points2DAsList(Points2DBase):  # FIXME: Remove this class
-        _points_2d: List[Tuple[float, float]] = []
+        @property
+        def camera_ids(self) -> torch.Tensor:
+            return self._camera_ids
 
-    _points_2d_camera: Dict[int, Points2D] = {}
+    def __init__(self) -> None:
+        self._points_2d_camera: Dict[int, RaySampler.Points2D] = {}
 
-    def __init__(self, heights: torch.Tensor, widths: torch.Tensor) -> None:
-        self._heights: torch.Tensor = heights
-        self._widths: torch.Tensor = widths
+    @property
+    def points_2d_camera(self) -> Dict[int, Points2D]:
+        return self._points_2d_camera
 
-    def sample_points() -> None:
+    def sample_points(self, heights: torch.Tensor, widths: torch.Tensor, num_rays: torch.Tensor) -> None:
         raise NotImplementedError
 
 
 class RandomRaySampler(RaySampler):
     class CameraDims:
-        _heights: List[int] = []
-        _widths: List[int] = []
-        _camera_ids: List[int] = []
+        def __init__(self) -> None:
+            self._heights: List[int] = []
+            self._widths: List[int] = []
+            self._camera_ids: List[int] = []
 
-    def __init__(self, heights: torch.Tensor, widths: torch.Tensor, num_rays: torch.Tensor) -> None:
-        super().__init__(heights, widths)
-        self._num_rays: torch.Tensor = num_rays.int()
+        @property
+        def heights(self) -> List[int]:
+            return self._heights
 
-    def sample_points(self) -> None:
-        # points_2d_camera: Dict[int, RaySampler.Points2DAsList] = {}
+        @property
+        def widths(self) -> List[int]:
+            return self._widths
 
+        @property
+        def camera_ids(self) -> List[int]:
+            return self._camera_ids
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def sample_points(self, heights: torch.Tensor, widths: torch.Tensor, num_rays: torch.Tensor) -> None:
+        num_rays: torch.Tensor = num_rays.int()
         cameras_dims: Dict[int, RandomRaySampler.CameraDims] = {}
-        for camera_id, (height, width, n) in enumerate(
-            zip(self._heights.numpy(), self._widths.numpy(), self._num_rays.numpy())
-        ):
+        for camera_id, (height, width, n) in enumerate(zip(heights.numpy(), widths.numpy(), num_rays.numpy())):
             if n not in cameras_dims:
                 cameras_dims[n] = RandomRaySampler.CameraDims()
-            cameras_dims[n]._heights.extend([height] * n)
-            cameras_dims[n]._widths.extend([width] * n)
-            cameras_dims[n]._camera_ids.append(camera_id)
+            cameras_dims[n].heights.extend([height] * n)
+            cameras_dims[n].widths.extend([width] * n)
+            cameras_dims[n].camera_ids.append(camera_id)
 
+        self._points_2d_camera = {}
         for n, camera_dims in cameras_dims.items():
             dims = torch.tensor([camera_dims._heights, camera_dims._widths], dtype=torch.float32).T
             points_2d = torch.trunc(torch.rand_like(dims, dtype=torch.float32) * dims).reshape(-1, n, 2)
@@ -67,8 +82,18 @@ class Rays:
 
     # FIXME: Not division to cameras - just big tensors for each ray parameters
 
-    def __init__(self, cameras: PinholeCamera) -> None:
-        pass
+    def __init__(self, cameras: PinholeCamera, ray_sampler: RaySampler, num_rays: torch.Tensor) -> None:
+        if len(cameras.height != num_rays.shape[0]):
+            raise ValueError(
+                'Number of cameras does not match size of tensor to define number of rays to march from each camera'
+            )
+        ray_sampler.sample_points(cameras.height, cameras.width, num_rays)
+
+        # Unproject 2d points in image plane to 3d world for two depths
+        # depths = torch.tensor([[[[1.0]]], [[[2.0]]]])
+        for obj in ray_sampler.points_2d_camera.values():
+            # points_3d = cameras.unproject(obj.points_2d, depths)
+            pass
 
     def _calc_ray_params(self, cameras: PinholeCamera):
         pass
