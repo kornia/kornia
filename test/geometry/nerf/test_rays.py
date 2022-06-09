23652bdb0ad1465dfd90370e9c7953210b4bc7b8
Yaniv Hollander <yhollander@american-robotics.com>
2022-06-09 02:09:05 +0000

    Sync

--- test/geometry/nerf/test_rays.py
+++ test/geometry/nerf/test_rays.py
@@ -1,44 +1,117 @@
+import math
+
 import torch
 
 import kornia
 from kornia.geometry.nerf.rays import RandomRaySampler, Rays  # , RaySampler, UniformRaySampler
 
 
-def test_ray_sampler(self, device, dtype):
-    n_cams1 = 3
-    n_cams2 = 2
-    heights: torch.Tensor = torch.cat((torch.tensor([200] * n_cams1), torch.tensor([100] * n_cams2)))
-    widths: torch.Tensor = torch.cat((torch.tensor([300] * n_cams1), torch.tensor([400] * n_cams2)))
-    num_rays: torch.Tensor = torch.cat((torch.tensor([10] * n_cams1), torch.tensor([15] * n_cams2)))
-    sampler = RandomRaySampler()
-    sampler.sample_points(heights, widths, num_rays)
-    assert len(sampler.points_2d_camera) == 2
-    assert sampler.points_2d_camera[10].points_2d.shape == (3, 10, 2)
-    assert sampler.points_2d_camera[15].points_2d.shape == (2, 15, 2)
+class TestRaySampler:
+    def test_dimensions(self, device, dtype):
+        n_cams1 = 3
+        n_cams2 = 2
+        heights: torch.Tensor = torch.cat(
+            (
+                torch.tensor([200] * n_cams1, device=device, dtype=dtype),
+                torch.tensor([100] * n_cams2, device=device, dtype=dtype),
+            )
+        )
+        widths: torch.Tensor = torch.cat(
+            (
+                torch.tensor([300] * n_cams1, device=device, dtype=dtype),
+                torch.tensor([400] * n_cams2, device=device, dtype=dtype),
+            )
+        )
+        num_rays: torch.Tensor = torch.cat(
+            (
+                torch.tensor([10] * n_cams1, device=device, dtype=dtype),
+                torch.tensor([15] * n_cams2, device=device, dtype=dtype),
+            )
+        )
+        sampler = RandomRaySampler()
+        sampler.sample_points(heights, widths, num_rays)
+        assert len(sampler.points_2d_camera) == 2
+        assert sampler.points_2d_camera[10].points_2d.shape == (3, 10, 2)
+        assert sampler.points_2d_camera[15].points_2d.shape == (2, 15, 2)
 
 
-def test_rays(self, device, dtype):
-    batch_size = 5
-    n = 2  # Points per batch
-    height, width = 4, 6
-    fx, fy, cx, cy = 1, 2, width / 2, height / 2
-    alpha, beta, gamma = 0.0, 0.0, 0.4
-    tx, ty, tz = 0, 0, 3
+class TestRays:
+    def _create_intrinsics(self, fxs, fys, cxs, cys, device, dtype):
+        intrinsics_batch = []
+        for fx, fy, cx, cy in zip(fxs, fys, cxs, cys):
+            intrinsics = torch.eye(4, device=device, dtype=dtype)
+            intrinsics[0, 0] = fx
+            intrinsics[1, 1] = fy
+            intrinsics[0, 2] = cx
+            intrinsics[1, 2] = cy
+            intrinsics_batch.append(intrinsics)
+        return torch.stack(intrinsics_batch)
 
-    intrinsics = self._create_intrinsics(batch_size, fx, fy, cx, cy, device=device, dtype=dtype)
-    extrinsics = self._create_extrinsics_with_rotation(
-        batch_size, alpha, beta, gamma, tx, ty, tz, device=device, dtype=dtype
-    )
+    def _create_extrinsics_with_rotation(self, alphas, betas, gammas, txs, tys, tzs, device, dtype):
+        extrinsics_batch = []
+        for alpha, beta, gamma, tx, ty, tz in zip(alphas, betas, gammas, txs, tys, tzs):
+            Rx = torch.eye(3, device=device, dtype=dtype)
+            Rx[1, 1] = math.cos(alpha)
+            Rx[1, 2] = math.sin(alpha)
+            Rx[2, 1] = -Rx[1, 2]
+            Rx[2, 2] = Rx[1, 1]
 
-    heights = torch.ones(batch_size, device=device, dtype=dtype) * height
-    widths = torch.ones(batch_size, device=device, dtype=dtype) * width
-    num_rays: torch.Tensor = torch.ones(batch_size, device=device, dtype=torch.int) * n
+            Ry = torch.eye(3, device=device, dtype=dtype)
+            Ry[0, 0] = math.cos(beta)
+            Ry[0, 2] = -math.sin(beta)
+            Ry[2, 0] = -Ry[0, 2]
+            Ry[2, 2] = Ry[0, 0]
 
-    cameras = kornia.geometry.camera.PinholeCamera(intrinsics, extrinsics, heights, widths)
+            Rz = torch.eye(3, device=device, dtype=dtype)
+            Rz[0, 0] = math.cos(gamma)
+            Rz[0, 1] = math.sin(gamma)
+            Rz[1, 0] = -Rz[0, 1]
+            Rz[1, 1] = Rz[0, 0]
 
-    ray_sampler = RandomRaySampler()
+            Ryz = torch.matmul(Ry, Rz)
+            R = torch.matmul(Rx, Ryz)
 
-    rays = Rays(
-        cameras, ray_sampler, num_rays
-    )  # FIXME: num_rays should be a property of (random) sampler, and not sent here to Rays
-    print(rays)
+            extrinsics = torch.eye(4, device=device, dtype=dtype)
+            extrinsics[..., 0, -1] = tx
+            extrinsics[..., 1, -1] = ty
+            extrinsics[..., 2, -1] = tz
+            extrinsics[:3, :3] = R
+
+            extrinsics_batch.append(extrinsics)
+        return torch.stack(extrinsics_batch)
+
+    def _create_four_cameras(self, device, dtype) -> kornia.geometry.camera.PinholeCamera:
+        height = torch.tensor([4, 4, 4, 4])
+        width = torch.tensor([6, 6, 6, 6])
+
+        fx = [1.0, 1.0, 1.0, 1.0]
+        fy = [1.0, 1.0, 1.0, 1.0]
+        cx = [width[0] / 2.0, width[1] / 2.0, width[2] / 2.0, width[3] / 2.0]
+        cy = [height[0] / 2.0, height[1] / 2.0, height[2] / 2.0, height[3] / 2.0]
+
+        tx = [1.0, 1.0, 1.0, 1.0]
+        ty = [0.0, 0.0, 0.0, 0.0]
+        tz = [0.0, 0.0, 0.0, 0.0]
+        pi = torch.pi
+        alpha = [0.0, 0.0, 0.0, 0.0]
+        beta = [0.0, 0.0, 0.0, pi / 2.0]
+        gamma = [pi, -pi / 2.0, 0.0, 0.0]
+
+        intrinsics = self._create_intrinsics(fx, fy, cx, cy, device=device, dtype=dtype)
+        extrinsics = self._create_extrinsics_with_rotation(alpha, beta, gamma, tx, ty, tz, device=device, dtype=dtype)
+
+        cameras = kornia.geometry.camera.PinholeCamera(intrinsics, extrinsics, height, width)
+        return cameras
+
+    def test_rays(self, device, dtype):
+
+        num_rays: torch.Tensor = torch.ones(4, device=device, dtype=torch.int) * 2
+
+        cameras = self._create_four_cameras(device=device, dtype=dtype)
+
+        ray_sampler = RandomRaySampler()
+
+        rays = Rays(
+            cameras, ray_sampler, num_rays
+        )  # FIXME: num_rays should be a property of (random) sampler, and not sent here to Rays
+        print(rays)
