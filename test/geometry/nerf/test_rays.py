7b16211ca4007bcc296bcca950b07a6c60c75cb4
Yaniv Hollander <yhollander@american-robotics.com>
2022-06-21 02:32:02 +0000

    Sync

--- test/geometry/nerf/test_rays.py
+++ test/geometry/nerf/test_rays.py
@@ -3,36 +3,36 @@ import math
 import torch
 
 import kornia
-from kornia.geometry.nerf.rays import RandomRaySampler, UniformRaySampler  # , UniformRaySampler
+from kornia.geometry.nerf.rays import RandomRaySampler, UniformRaySampler
+
+
+def create_camera_dimensions(device, dtype):
+    n_cams1 = 3
+    n_cams2 = 2
+    heights: torch.Tensor = torch.cat(
+        (
+            torch.tensor([200] * n_cams1, device=device, dtype=dtype),
+            torch.tensor([100] * n_cams2, device=device, dtype=dtype),
+        )
+    )
+    widths: torch.Tensor = torch.cat(
+        (
+            torch.tensor([300] * n_cams1, device=device, dtype=dtype),
+            torch.tensor([400] * n_cams2, device=device, dtype=dtype),
+        )
+    )
+    num_rays: torch.Tensor = torch.cat(
+        (
+            torch.tensor([10] * n_cams1, device=device, dtype=dtype),
+            torch.tensor([15] * n_cams2, device=device, dtype=dtype),
+        )
+    )
+    return heights, widths, num_rays
 
 
 class TestRaySampler_2DPoints:
-    @staticmethod
-    def __create_camera_dimensions(device, dtype):
-        n_cams1 = 3
-        n_cams2 = 2
-        heights: torch.Tensor = torch.cat(
-            (
-                torch.tensor([200] * n_cams1, device=device, dtype=dtype),
-                torch.tensor([100] * n_cams2, device=device, dtype=dtype),
-            )
-        )
-        widths: torch.Tensor = torch.cat(
-            (
-                torch.tensor([300] * n_cams1, device=device, dtype=dtype),
-                torch.tensor([400] * n_cams2, device=device, dtype=dtype),
-            )
-        )
-        num_rays: torch.Tensor = torch.cat(
-            (
-                torch.tensor([10] * n_cams1, device=device, dtype=dtype),
-                torch.tensor([15] * n_cams2, device=device, dtype=dtype),
-            )
-        )
-        return heights, widths, num_rays
-
     def test_dimensions_random_sampler(self, device, dtype):
-        heights, widths, num_rays = TestRaySampler_2DPoints.__create_camera_dimensions(device, dtype)
+        heights, widths, num_rays = create_camera_dimensions(device, dtype)
         sampler = RandomRaySampler(1, 1, 1)
         points_2d_camera = sampler.sample_points_2d(heights, widths, num_rays)
         assert len(points_2d_camera) == 2
@@ -40,16 +40,12 @@ class TestRaySampler_2DPoints:
         assert points_2d_camera[15].points_2d.shape == (2, 15, 2)
 
     def test_dimensions_uniform_sampler(self, device, dtype):
-        heights, widths, _ = TestRaySampler_2DPoints.__create_camera_dimensions(device, dtype)
+        heights, widths, _ = create_camera_dimensions(device, dtype)
         sampler = UniformRaySampler(1, 1, 1)
         points_2d_camera = sampler.sample_points_2d(heights, widths)
         assert len(points_2d_camera) == 2
         assert points_2d_camera[60000].points_2d.shape == (3, 60000, 2)
         assert points_2d_camera[40000].points_2d.shape == (2, 40000, 2)
-
-
-class TestUniform2DSampler:
-    pass
 
 
 class TestRays:
