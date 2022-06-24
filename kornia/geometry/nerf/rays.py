de15b70c7cc697927e421c97b21fcec1f62cdb7b
Yaniv Hollander <yhollander@american-robotics.com>
2022-06-24 20:59:30 +0000

    Sync

--- kornia/geometry/nerf/rays.py
+++ kornia/geometry/nerf/rays.py
@@ -71,10 +71,8 @@ class RaySampler:
             points_3d = cameras_id.unproject(obj._points_2d.repeat(1, 2, 1), depths).reshape(2 * num_points_group, -1)
             origins.append(points_3d[:num_points_group])
             directions.append(points_3d[num_points_group:] - points_3d[:num_points_group])
-            torch.nn.functional.normalize(directions[-1], out=directions[-1])  # Normalizing direction vectors
-            lengths.append(
-                torch.linspace(self._min_depth, self._max_depth, self._num_ray_points).repeat(num_points_group, 1)
-            )
+            # torch.nn.functional.normalize(directions[-1], out=directions[-1], dim=-1)  # Normalizing direction vectors
+            lengths.append(torch.linspace(0.0, 1.0, self._num_ray_points).repeat(num_points_group, 1))
         self._origins = torch.cat(origins)
         self._directions = torch.cat(directions)
         self._lengths = torch.cat(lengths)
