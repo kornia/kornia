fec7c5fc8ec2c8d823e0606ed6f14daa2482608e
edgar <edgar.riba@gmail.com>
2022-07-01 15:42:27 +0200

    fix typing callable in load storage

--- kornia/contrib/face_detection.py
+++ kornia/contrib/face_detection.py
@@ -1,4 +1,5 @@
 # based on: https://github.com/ShiqiYu/libfacedetection.train/blob/74f3aa77c63234dd954d21286e9a60703b8d0868/tasks/task1/yufacedetectnet.py  # noqa
+from ast import Call
 import math
 from enum import Enum
 from typing import Callable, Dict, List, Optional, Tuple
@@ -281,8 +282,9 @@ class YuFaceDetectNet(nn.Module):
 
         # use torch.hub to load pretrained model
         if pretrained:
+            storage_fcn: Callable = lambda storage, loc: storage
             pretrained_dict = torch.hub.load_state_dict_from_url(
-                url, map_location=lambda storage, loc: storage
+                url, map_location=storage_fcn
             )
             self.load_state_dict(pretrained_dict, strict=True)
         self.eval()
