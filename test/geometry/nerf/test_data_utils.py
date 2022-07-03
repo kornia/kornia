5991c33e8b48ef2018f462cdc73b12f2a7a30e40
Yaniv Hollander <yhollander@american-robotics.com>
2022-07-03 20:56:38 +0000

    Sync

--- test/geometry/nerf/test_data_utils.py
+++ test/geometry/nerf/test_data_utils.py
@@ -1,5 +1,15 @@
-# from kornia.geometry.nerf.data_utils import RayDataset
+from test.geometry.nerf.test_rays import create_four_cameras
 
-# class TestDataset:
-#     def test_uniform_ray_dataset(self, device, dtype):
-#         dataset = RayDataset(cameras, 1, 2)
+import torch
+
+from kornia.geometry.nerf.data_utils import ImageTensors, RayDataset
+
+
+class TestDataset:
+    def test_uniform_ray_dataset(self, device, dtype):
+        cameras = create_four_cameras('cpu', torch.float32)
+        imgs: ImageTensors = []
+        imgs.append(torch.randint(0, 255, (3, cameras.height[0], cameras.width[0]), dtype=torch.uint8))
+        imgs.extend([torch.randint(0, 255, (3, 3, 4, 7), dtype=torch.uint8)[i] for i in range(3)])
+        dataset = RayDataset(cameras, 1, 2)
+        dataset.init_ray_dataset(imgs)
