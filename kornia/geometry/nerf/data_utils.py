d11e1accf22696ac0c211af0f641502d068bf854
Yaniv Hollander <yhollander@american-robotics.com>
2022-07-05 01:52:58 +0000

    Sync

--- kornia/geometry/nerf/data_utils.py
+++ kornia/geometry/nerf/data_utils.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Optional, Union
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import BatchSampler, DataLoader, Dataset, RandomSampler
 from torchvision.io import read_image
 
 from kornia.geometry.camera import PinholeCamera
@@ -10,6 +10,13 @@ from kornia.geometry.nerf.rays import RandomRaySampler, RaySampler, UniformRaySa
 ImagePaths = List[str]
 ImageTensors = List[torch.Tensor]
 Images = Union[ImagePaths, ImageTensors]
+
+
+class DatasetItem:
+    def __init__(self, origin: torch.Tensor, direction: torch.Tensor, rgb: torch.Tensor) -> None:
+        self._origin = origin
+        self._direction = direction
+        self._rgb = rgb
 
 
 class RayDataset(Dataset):  # FIXME: Add device
@@ -67,7 +74,30 @@ class RayDataset(Dataset):  # FIXME: Add device
         return imgs
 
     def __len__(self):
-        self._ray_sampler.origins().shape[0]
+        return self._ray_sampler.origins.shape[0]
 
-    def __getitem__(self, idx: int) -> Any:  # FIXME: idx as a list for batch processing?
-        return self._ray_sampler.origins()[idx], self._ray_sampler.directions()[idx]
+    def __getitem__(self, idx: Union[int, List[int]]) -> Any:
+        origin = self._ray_sampler.origins[idx]
+        direction = self._ray_sampler.directions[idx]
+        camerd_id = self._ray_sampler.camera_ids[idx]
+        point_2d = self._ray_sampler.points_2d[idx]
+        x = point_2d[0].item()
+        y = point_2d[1].item()
+        rgb = self._imgs[camerd_id][:, y, x]
+        return DatasetItem(origin, direction, rgb)
+
+
+class RayDataloader(DataLoader):
+    def __init__(self, dataset: RayDataset, batch_size: int = 1):
+        super().__init__(
+            dataset,
+            sampler=BatchSampler(RandomSampler(dataset), batch_size, drop_last=False),
+            collate_fn=self._collate_rays,
+        )
+
+    @staticmethod
+    def _collate_rays(dataset_items: List[DatasetItem]) -> None:
+        origins = [dataset_item._origin for dataset_item in dataset_items]
+        directions = [dataset_item._direction for dataset_item in dataset_items]
+        rgbs = [dataset_item._rgb for dataset_item in dataset_items]
+        return torch.stack(origins), torch.stack(directions), torch.stack(rgbs)
--- test/geometry/nerf/test_data_utils.py
+++ test/geometry/nerf/test_data_utils.py
@@ -2,7 +2,7 @@ from test.geometry.nerf.test_rays import create_four_cameras
 
 import torch
 
-from kornia.geometry.nerf.data_utils import ImageTensors, RayDataset
+from kornia.geometry.nerf.data_utils import ImageTensors, RayDataloader, RayDataset
 
 
 class TestDataset:
@@ -13,3 +13,12 @@ class TestDataset:
         imgs.extend([torch.randint(0, 255, (3, 3, 4, 7), dtype=torch.uint8)[i] for i in range(3)])
         dataset = RayDataset(cameras, 1, 2)
         dataset.init_ray_dataset(imgs)
+
+        data_loader = RayDataloader(dataset, batch_size=32)
+
+        # data_loader = DataLoader(
+        #     dataset, sampler=BatchSampler(RandomSampler(dataset), 32, drop_last=False), collate_fn=collate_rays
+        # )
+
+        d = next(iter(data_loader))
+        print(d)
