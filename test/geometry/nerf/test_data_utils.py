7a37e7a4b4fd212aef5efd1a39eab24a08a62cf2
Yaniv Hollander <yhollander@american-robotics.com>
2022-07-05 21:36:54 +0000

    Sync

--- test/geometry/nerf/test_data_utils.py
+++ test/geometry/nerf/test_data_utils.py
@@ -9,16 +9,15 @@ class TestDataset:
     def test_uniform_ray_dataset(self, device, dtype):
         cameras = create_four_cameras('cpu', torch.float32)
         imgs: ImageTensors = []
-        imgs.append(torch.randint(0, 255, (3, cameras.height[0], cameras.width[0]), dtype=torch.uint8))
-        imgs.extend([torch.randint(0, 255, (3, 3, 4, 7), dtype=torch.uint8)[i] for i in range(3)])
+        imgs.append(torch.randint(0, 255, (3, cameras.height[0], cameras.width[0]), dtype=torch.uint8))  # (3, 5, 9)
+        imgs.extend([torch.randint(0, 255, (3, 3, 4, 7), dtype=torch.uint8)[i] for i in range(3)])  # (3, 4, 7)
         dataset = RayDataset(cameras, 1, 2)
         dataset.init_ray_dataset(imgs)
 
-        data_loader = RayDataloader(dataset, batch_size=32)
+        data_loader = RayDataloader(dataset, batch_size=32, shufle=False)
+        d = next(iter(data_loader))  # First batch of 32 labeled rays
 
-        # data_loader = DataLoader(
-        #     dataset, sampler=BatchSampler(RandomSampler(dataset), 32, drop_last=False), collate_fn=collate_rays
-        # )
-
-        d = next(iter(data_loader))
-        print(d)
+        # Comparing RGB values between sampled rays and original images
+        assert torch.equal(d[2][0], imgs[0][:, 0, 0])
+        assert torch.equal(d[2][1], imgs[0][:, 0, 1])  # First row, second column in the image (1 sample point index)
+        assert torch.equal(d[2][9], imgs[0][:, 1, 0])  # Second row, first column in the image (9 sample point index)
