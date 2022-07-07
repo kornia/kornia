a4289f73bb17ed5b62bd358c14f9cdca94485be0
Yaniv Hollander <yhollander@american-robotics.com>
2022-07-07 21:16:42 +0000

    Sync

--- kornia/geometry/nerf/nerf_model.py
+++ kornia/geometry/nerf/nerf_model.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 
+from kornia.geometry.nerf.positional_encoder import PositionalEncoder
+from kornia.geometry.nerf.rays import sample_lengths, sample_ray_points
+
 
 class MLP(nn.Module):
     def __init__(self, num_dims, num_units: int = 2, num_unit_layers: int = 4, num_hidden: int = 128):
@@ -27,15 +30,39 @@ class MLP(nn.Module):
 
 
 class NerfModel(nn.Module):
-    def __init__(self, n_pos_dims, n_dir_dims):
+    def __init__(
+        self,
+        num_ray_points: int,
+        num_pos_freqs: int,  # FIXME: Decide on defaults for arguments
+        num_dir_freqs: int,
+        num_units: int,
+        num_nuit_layers: int,
+        num_hidden: int,
+    ):
         super().__init__()
+        self._num_ray_points = num_ray_points
+        self._pos_encoder = PositionalEncoder(num_pos_freqs)
+        self._dir_encoder = PositionalEncoder(num_dir_freqs)
+        self._mlp = MLP(self._pos_encoder.num_encoded_dims, num_units, num_nuit_layers, num_hidden)
 
-    def forward(self):
-        pass
+    def forward(self, origins: torch.Tensor, directions: torch.Tensor):
+
         # Sample xyz for ray parameters
+        batch_size = origins.shape[0]
+        lengths = sample_lengths(
+            batch_size, self._num_ray_points, irregular=False
+        )  # FIXME: handle the case of irregular smapling along rays, and hierarchical sampling
+        points_3d = sample_ray_points(
+            origins, directions, lengths
+        )  # FIXME: Normalize points to [-1, 1] before encoding?
 
-        # Encode positions
+        # Encode positions & directions
+        points_3d_encoded = self._pos_encoder(points_3d)
+        directions_encoded = self._dir_encoder(directions)  # FIXME: Normalize directions to [-1, 1] before encoding?
 
         # Map positional encodings to latent features (MLP with skip connections)
+        y = self._mlp(points_3d_encoded)
+
+        print(directions_encoded, y)  # FIXME: Remove this line
 
         # Return sample point color and density
