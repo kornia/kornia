a50b3e7c06f1602b850f338390c095043726dd6f
edgar <edgar.riba@gmail.com>
2022-07-12 15:57:09 +0200

    add sphinxcontrib-gtagjs in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinxcontrib.bibtex',
+    "sphinxcontrib.gtagjs",
     'sphinxcontrib.youtube',
     'sphinx_design',
 ]
@@ -69,6 +70,8 @@ master_doc = 'index'
 project = 'Kornia'
 author = f'{project} developers'
 copyright = f'2019, {author}'
+
+gtagjs_ids = ["UA-203957082-2"]
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
