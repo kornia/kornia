7377e325ee9de109b75e0932bb55513b21c2acc9
edgar <edgar.riba@gmail.com>
2022-08-07 20:36:57 +0200

    adding final fixes

--- test/x/__init__.py
+++ test/x/__init__.py
@@ -4,5 +4,7 @@ import pytest
 
 from kornia.utils._compat import torch_version
 
+# NOTE: remove once we deprecate pytorch 1.10.2 for macos
+
 if sys.platform == "darwin" and torch_version() == "1.10.2":
     pytest.skip("Accelerate is broken for macos and pytorch 1.10.2", allow_module_level=True)
