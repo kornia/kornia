059fa8e36abfb3fcdf917cb9b4787e2cf6edefd9
Jian Shi <shij0c@kaust.edu.sa>
2022-08-30 10:33:21 +0300

    Added deprecation warnings

--- kornia/augmentation/_2d/mix/mixup.py
+++ kornia/augmentation/_2d/mix/mixup.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, List, Optional, Tuple, Union, cast
+import warnings
 
 import torch
 
@@ -86,6 +87,7 @@ class RandomMixUp(MixAugmentationBase):
     ) -> None:
         super().__init__(p=1.0, p_batch=p, same_on_batch=same_on_batch, keepdim=keepdim)
         self._param_generator = cast(rg.MixupGenerator, rg.MixupGenerator(lambda_val, p=p))
+        warnings.warn("`RandomMixUp` is deprecated. Please use `RandomMixUpV2` instead.")
 
     def apply_transform(  # type: ignore
         self, input: Tensor, label: Tensor, params: Dict[str, Tensor]
