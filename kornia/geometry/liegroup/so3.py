e23788c60dbf1274c2fd079246326fc87681dc98
cjpurackal <christeejacobs@gmail.com>
2022-09-19 11:45:17 +0530

    add vee, inverse, update hat, matrix, identity

--- kornia/geometry/liegroup/so3.py
+++ kornia/geometry/liegroup/so3.py
@@ -1,9 +1,9 @@
 from kornia.core import Tensor, concatenate, stack, zeros_like
 
 from kornia.geometry.quaternion import Quaternion
-from kornia.geometry.liegroup._utils import squared_norm
+from ._utils import squared_norm
+from kornia.testing import KORNIA_CHECK_TYPE, KORNIA_CHECK
 
-from kornia.testing import KORNIA_CHECK_TYPE
 
 class So3:
     def __init__(self, q: Quaternion) -> None:
@@ -28,30 +28,38 @@ class So3:
         return 2 * (n / self.q.real).atan() / n * self.q.vec
 
     @staticmethod
-    def hat(o):
-        ox, oy, oz = o[..., 0], o[..., 1], o[..., 2]
-        zeros = zeros_like(o)[..., 0]
-        row0 = concatenate([zeros, -oz, oy], -1)
-        row1 = concatenate([oz, zeros, -ox], -1)
-        row2 = concatenate([-oy, ox, zeros], -1)
-        return stack([row0, row1, row2], -2)
+    def hat(oo):
+        aa, bb, cc = oo[None, None, ..., 0], oo[None, None, ..., 1], oo[None, None, ..., 2]
+        zeros = zeros_like(oo)[None, None, ..., 0]
+        row0 = concatenate([zeros, -cc, bb], 1)
+        row1 = concatenate([cc, zeros, -aa], 1)
+        row2 = concatenate([-bb, aa, zeros], 1)
+        return concatenate([row0, row1, row2], 0)
+
+    @staticmethod
+    def vee(omega):
+        a, b, c = omega[2, 1, ...], omega[0, 2, ...], omega[1, 0, ...]
+        return stack([a, b, c], 1)
 
     def matrix(self):
-        w, x, y, z = self.q[..., 0], self.q.vec[..., 0], self.q.vec[..., 1], self.q.vec[..., 2]
-        q1 = (1 - 2 * y ** 2 - 2 * z ** 2).reshape(1, 1,-1)
-        q2 = (2 * x * y - 2 * z * w).reshape(1,1,-1)
-        q3 = (2 * x * z + 2 * y * w).reshape(1,1,-1)
-        col1 = concatenate([q1,q2,q3], 1)
-        q1 = (2 * x * y + 2 * z * w).reshape(1,1,-1)
-        q2 = (1 - 2 * x ** 2 - 2 * z ** 2).reshape(1,1,-1)
-        q3 = (2 * y * z - 2 * x * w).reshape(1,1,-1)
-        col2 = concatenate([q1,q2,q3], 1)
-        q1 = (2 * x * z - 2 * y * w).reshape(1,1,-1)
-        q2 = (2 * y * z + 2 * x * w).reshape(1,1,-1)
-        q3 = (1 - 2 * x ** 2 - 2 * y ** 2).reshape(1,1,-1)
-        col3 = concatenate([q1,q2,q3], 1)
-        return concatenate([col1,col2,col3], 0)
+        w, x, y, z = self.q[None, None, ..., 0], self.q.vec[None, None, ..., 0], self.q.vec[None, None, ..., 1], self.q.vec[None, None, ..., 2]
+        q0 = (1 - 2 * y ** 2 - 2 * z ** 2)
+        q1 = (2 * x * y - 2 * z * w)
+        q2 = (2 * x * z + 2 * y * w)
+        row0 = concatenate([q0, q1, q2], 1)
+        q0 = (2 * x * y + 2 * z * w)
+        q1 = (1 - 2 * x ** 2 - 2 * z ** 2)
+        q2 = (2 * y * z - 2 * x * w)
+        row1 = concatenate([q0, q1, q2], 1)
+        q0 = (2 * x * z - 2 * y * w)
+        q1 = (2 * y * z + 2 * x * w)
+        q2 = (1 - 2 * x ** 2 - 2 * y ** 2)
+        row2 = concatenate([q0, q1, q2], 1)
+        return concatenate([row0, row1, row2], 0)
 
     @classmethod
-    def identity(cls):
-        return cls(Quaternion.identity())
+    def identity(cls, batch_size: int) -> 'So3':
+        return cls(Quaternion.identity(batch_size))
+
+    def inverse(self) -> 'So3':
+        return So3(self.q.conj())
