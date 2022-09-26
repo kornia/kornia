6a00ea80991aa15e7c5156a3f5c3959876ddbc47
cjpurackal <christeejacobs@gmail.com>
2022-09-26 18:18:29 +0530

    backend exp for concate and zeros_like

--- kornia/core/_backend.py
+++ kornia/core/_backend.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import numpy as np
 
 # classes
 Tensor = torch.Tensor
@@ -7,10 +8,10 @@ Module = torch.nn.Module
 Parameter = torch.nn.Parameter
 
 # functions
-concatenate = torch.cat
+# concatenate = torch.cat
 stack = torch.stack
 normalize = F.normalize
-zeros_like = torch.zeros_like
+# zeros_like = torch.zeros_like
 zeros = torch.zeros
 where = torch.where
 
@@ -19,3 +20,26 @@ as_tensor = torch.as_tensor
 
 # random
 rand = torch.rand
+
+def concatenate(tensors, dim=0, out=None):
+    """Concatenates the given sequence of seq tensors or ndarrays alongside the specified axis.
+
+    Args:
+        tensors: any python sequence of torch.tensors or np.ndarrays.
+    """
+    if all(isinstance(x, np.ndarray) for x in tensors):
+        return np.concatenate(tensors, axis=dim, out=out)
+    else:
+        return torch.cat(tensors, dim=dim, out=out)
+
+
+def zeros_like(input, dtype=None, device=None):
+    """Returns a tensor filled with the scalar value 0, with the same size as input.
+
+    Args:
+        input: torch.Tensor or np.ndarray
+    """
+    if (isinstance(input, np.ndarray)):
+        return np.zeros_like(input, dtype=dtype)
+    else:
+        return torch.zeros_like(input, dtype=dtype, device=device)
