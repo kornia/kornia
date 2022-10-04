45dee6b18d2c24f15f84dc697db92d7aacccaed3
Dmytro Mishkin <ducha.aiki@gmail.com>
2022-10-04 11:40:43 +0200

    update svd

--- kornia/utils/helpers.py
+++ kornia/utils/helpers.py
@@ -124,7 +124,7 @@ def _torch_svd_cast(input: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, to
     if dtype not in (torch.float32, torch.float64):
         dtype = torch.float32
 
-    out1, out2, out3 = torch.svd(input.to(dtype))
+    out1, out2, out3 = torch.linalg.svd(input.to(dtype))
 
     return (out1.to(input.dtype), out2.to(input.dtype), out3.to(input.dtype))
 
