21d27e1574f9ba6828a7020f43675b0dd10bf66f
Christie Jacob <christeejacobs@gmail.com>
2022-10-12 16:43:57 +0530

    Quaternion.norm bug fix (#1935)
    * norm bug fix
    
    * add norm_shape test
    
    * docstring error fix
    
    * Update quaternion.py
    
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- kornia/geometry/quaternion.py
+++ kornia/geometry/quaternion.py
@@ -309,7 +309,8 @@ class Quaternion(Module):
         Example:
             >>> q = Quaternion.random(batch_size=2)
             >>> q.norm()
-            tensor([1.0000, 1.0000], grad_fn=<NormBackward1>)
+            tensor([[1.0000],
+                    [1.0000]], grad_fn=<NormBackward1>)
         """
         r1, r2, r3 = rand(3, batch_size)
         q1 = (1.0 - r1).sqrt() * ((2 * pi * r2).sin())
@@ -319,7 +320,7 @@ class Quaternion(Module):
         return cls(stack((q1, q2, q3, q4), -1))
 
     def norm(self) -> Tensor:
-        return self.data.norm(p=2, dim=-1)
+        return self.data.norm(p=2, dim=-1, keepdim=True)
 
     def normalize(self) -> 'Quaternion':
         return Quaternion(normalize_quaternion(self.data))
