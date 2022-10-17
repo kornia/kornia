d29371051fec78784b5b39791f20969e88c488d3
Dmytro Mishkin <dmytro.mishkin@hover.to>
2022-10-17 10:08:57 +0200

    lint

--- kornia/core/__init__.py
+++ kornia/core/__init__.py
@@ -1,3 +1,27 @@
-from ._backend import Device, Module, Parameter, Tensor, as_tensor, concatenate, normalize, rand, stack, tensor, where
+from ._backend import (
+    Device,
+    Module,
+    Parameter,
+    Tensor,
+    as_tensor,
+    concatenate,
+    normalize,
+    rand,
+    stack,
+    tensor,
+    where
+)
 
-__all__ = ["concatenate", "Device", "Module", "Tensor", "tensor", "Parameter", "normalize", "stack", "as_tensor", "rand", "where"]
+__all__ = [
+    "concatenate",
+    "Device",
+    "Module",
+    "Tensor",
+    "tensor",
+    "Parameter",
+    "normalize",
+    "stack",
+    "as_tensor",
+    "rand",
+    "where"
+]
