5f7e076aab5aa7788ad057efe989f17026517557
Dmytro Mishkin <ducha.aiki@gmail.com>
2022-10-25 11:19:34 +0200

    Normalize with intrinsics (#1727)
    * Refactor-out normalize-with-intrinsics into separate function
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jian Shi <sj8716643@126.com>
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- kornia/core/__init__.py
+++ kornia/core/__init__.py
@@ -1,3 +1,15 @@
-from ._backend import Device, Module, Parameter, Tensor, as_tensor, concatenate, normalize, rand, stack, where
+from ._backend import Device, Module, Parameter, Tensor, as_tensor, concatenate, normalize, rand, stack, tensor, where
 
-__all__ = ["concatenate", "Device", "Module", "Tensor", "Parameter", "normalize", "stack", "as_tensor", "rand", "where"]
+__all__ = [
+    "concatenate",
+    "Device",
+    "Module",
+    "Tensor",
+    "tensor",
+    "Parameter",
+    "normalize",
+    "stack",
+    "as_tensor",
+    "rand",
+    "where",
+]
