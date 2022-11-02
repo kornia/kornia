e040f6aac3b8c5715306078e70d64c651d53603b
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2022-11-02 06:40:00 -0300

    drop python 3.6 (#1971)

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # Welcome to the Kornia setup.py.
 import sys
 
-# Make sure that kornia is running on Python 3.6.0 or later
+# Make sure that kornia is running on Python 3.7.0 or later
 # (to avoid running into this bug: https://bugs.python.org/issue29246)
 
-if sys.version_info < (3, 6, 0):
-    raise RuntimeError("Kornia requires Python 3.6.0 or later.")
+if sys.version_info < (3, 7, 0):
+    raise RuntimeError("Kornia requires Python 3.7.0 or later.")
 
 
 from setuptools import setup
