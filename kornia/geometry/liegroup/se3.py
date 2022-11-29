2994d530efec417b2aba215f8b52591946831fdd
cjpurackal <christeejacobs@gmail.com>
2022-11-29 16:40:28 +0530

    typing fix?

--- kornia/geometry/liegroup/se3.py
+++ kornia/geometry/liegroup/se3.py
@@ -261,11 +261,11 @@ class Se3(Module):
         """
         r = So3.random(batch_size, device, dtype)
         if batch_size is None:
-            shape = ()
+            shape = (3,)
         else:
             KORNIA_CHECK(batch_size >= 1, msg="batch_size must be positive")
-            shape = (batch_size,)
-        return cls(r, rand(shape + (3,), device=device, dtype=dtype))
+            shape = (batch_size, 3)
+        return cls(r, rand(shape, device=device, dtype=dtype))
 
     @classmethod
     def rot_x(cls, x: Tensor) -> "Se3":
