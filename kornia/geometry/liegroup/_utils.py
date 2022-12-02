4e3b09598602e41796539a581e2574be9af2883b
Christie Jacob <christeejacobs@gmail.com>
2022-12-02 13:07:34 +0530

    update mul method so2 (#2051)

--- kornia/geometry/liegroup/_utils.py
+++ kornia/geometry/liegroup/_utils.py
@@ -3,18 +3,14 @@ def check_so2_z_shape(z):
     z_shape = z.shape
     len_z_shape = len(z_shape)
     if (len_z_shape == 2 and z_shape[1] != 1) or (len_z_shape == 0 and not z.numel()) or (len_z_shape > 2):
-        raise ValueError(f"Invalid input size, we expect [B, 1], [B] or []. Got: {z.shape}")
+        raise ValueError(f"Invalid input size, we expect [B]. Got: {z.shape}")
 
 
 def check_so2_t_shape(t):
     t_shape = t.shape
     len_t_shape = len(t_shape)
-    if (
-        ((len_t_shape == 3) and ((t_shape[1] != 2) or (t_shape[2] != 1)))
-        or ((len_t_shape == 2) and (t_shape[1] > 2))
-        or ((len_t_shape == 1) and (t_shape[0] != 2))
-    ):
-        raise ValueError(f"Invalid translation shape, we expect [B, 2], [B, 2, 1] or [2] Got: {t_shape}")
+    if ((len_t_shape == 2) and (t_shape[1] > 2)) or ((len_t_shape == 1) and (t_shape[0] != 2)) or (len_t_shape > 2):
+        raise ValueError(f"Invalid translation shape, we expect [B, 2], or [2] Got: {t_shape}")
 
 
 def check_so2_theta_shape(theta):
@@ -25,7 +21,7 @@ def check_so2_theta_shape(theta):
         or (len_theta_shape == 0 and not theta.numel())
         or (len_theta_shape > 2)
     ):
-        raise ValueError(f"Invalid input size, we expect [B, 1] or [B]. Got: {theta_shape}")
+        raise ValueError(f"Invalid input size, we expect [B]. Got: {theta_shape}")
 
 
 def check_so2_matrix_shape(matrix):
