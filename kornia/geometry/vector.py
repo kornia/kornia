560f2d2817d50503e79a4139904196dc9b260a36
Christie Jacob <christeejacobs@gmail.com>
2023-01-12 12:19:43 +0530

    Add Vector2 (#2134)
    * vec2
    
    * add vector2

--- kornia/geometry/vector.py
+++ kornia/geometry/vector.py
@@ -5,7 +5,7 @@ from kornia.core.tensor_wrapper import TensorWrapper, wrap
 from kornia.geometry.linalg import batched_dot_product, batched_squared_norm
 from kornia.testing import KORNIA_CHECK
 
-__all__ = ["Vector3", "Scalar"]
+__all__ = ["Vector3", "Vector2", "Scalar"]
 
 
 # TODO: implement more functionality to validate
@@ -79,7 +79,47 @@ class Vector3(TensorWrapper):
         return wrap(stack(tensors, -1), Vector3)
 
 
+class Vector2(TensorWrapper):
+    def __init__(self, vector: Tensor) -> None:
+        super().__init__(vector)
+        KORNIA_CHECK(vector.shape[-1] == 2)
+
+    @property
+    def x(self) -> Tensor:
+        return self.data[..., 0]
+
+    @property
+    def y(self) -> Tensor:
+        return self.data[..., 1]
+
+    def normalized(self) -> "Vector2":
+        return Vector2(normalize(self.data, p=2, dim=-1))
+
+    def dot(self, right: "Vector2") -> Scalar:
+        return Scalar(batched_dot_product(self.data, right.data))
+
+    def squared_norm(self) -> Scalar:
+        return Scalar(batched_squared_norm(self.data))
+
+    @classmethod
+    def random(cls, shape: Optional[Tuple[int, ...]] = None, device=None, dtype=None) -> "Vector2":
+        if shape is None:
+            shape = ()
+        return cls(rand(shape + (2,), device=device, dtype=dtype))
+
+    @classmethod
+    def from_coords(cls, x: Union[float, Tensor], y: Union[float, Tensor], device=None, dtype=None) -> "Vector2":
+        KORNIA_CHECK(type(x) == type(y))
+        KORNIA_CHECK(isinstance(x, (Tensor, float)))
+        if isinstance(x, float):
+            return wrap(as_tensor((x, y), device=device, dtype=dtype), Vector2)
+        # TODO: this is totally insane ...
+        tensors: Tuple[Tensor, ...] = (x, cast(Tensor, y))
+        return wrap(stack(tensors, -1), Vector2)
+
+
 Vec3 = Vector3
+Vec2 = Vector2
 
 # TODO: adapt to TensorWrapper
 
