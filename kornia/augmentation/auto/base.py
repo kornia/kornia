d758c001a988443056fe8b1cdd2c88f8dd8606d2
Jian Shi <sj8716643@126.com>
2023-01-29 17:52:15 +0300

    Added missing files

---
+++ kornia/augmentation/auto/base.py
@@ -0,0 +1,44 @@
+from typing import List, Tuple, Union
+
+import torch.nn as nn
+from kornia.augmentation.auto.operations import OperationBase
+from kornia.core import Tensor, Module
+
+NUMBER = Union[float, int]
+OP_CONFIG = Tuple[str, NUMBER, NUMBER]
+SUBPLOLICY_CONFIG = List[OP_CONFIG]
+
+
+class PolicySequential(Module):
+    """Policy tuple for applying multiple operations.
+
+    Args:
+        operations: a list of operations to perform.
+    """
+
+    def __init__(self, operations: List[OperationBase]) -> None:
+        super().__init__()
+        self.operations = operations
+
+    def forward(self, input: Tensor) -> Tensor:
+        for op in self.operations:
+            input = op(input)
+        return input
+
+
+class PolicyAugmentBase(Module):
+    """
+    """
+
+    def __init__(self, policy: List[SUBPLOLICY_CONFIG]) -> None:
+        super().__init__()
+        self.policies = self.compose_policy(policy)
+
+    def compose_policy(self, policy: List[SUBPLOLICY_CONFIG]) -> nn.ModuleList:
+        policies = nn.ModuleList([])
+        for subpolicy in policy:
+            policies.append(self.compose_subpolicy_sequential(subpolicy))
+        return policies
+
+    def compose_subpolicy_sequential(self, subpolicy: SUBPLOLICY_CONFIG) -> PolicySequential:
+        raise NotImplementedError
