d245816780a634c6272acf1c1b0947df65cd06ba
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2023-02-10 12:18:17 -0300

    Replace `assert_allclose` by `assert_close`
    - The `torch.testing.assert_allclose` is deprecated in favor of `torch.testing.assert_close`

--- test/augmentation/test_dist_mapper.py
+++ test/augmentation/test_dist_mapper.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 from torch.distributions import Normal
-from torch.testing import assert_allclose
 
 from kornia.augmentation.random_generator import DistributionWithMapper
+from kornia.testing import assert_close
 
 
 class TestDistMapper:
@@ -12,4 +12,4 @@ class TestDistMapper:
         dist = DistributionWithMapper(Normal(0.0, 1.0), map_fn=nn.Sigmoid())
         out = dist.rsample((8,))
         exp = torch.tensor([0.8236, 0.4272, 0.1017, 0.6384, 0.2527, 0.1980, 0.5995, 0.6980])
-        assert_allclose(out, exp, rtol=1e-4, atol=1e-4)
+        assert_close(out, exp, rtol=1e-4, atol=1e-4)
