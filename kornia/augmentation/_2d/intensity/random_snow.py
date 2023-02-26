a5ab72d415549b45d67f7d8688e24fabec852943
Dmytro Kalitin <dmytro.kalitin@ucu.edu.ua>
2023-02-26 06:59:27 -0500

    Made parameters as tuples and changed tests accordingly.

--- kornia/augmentation/_2d/intensity/random_snow.py
+++ kornia/augmentation/_2d/intensity/random_snow.py
@@ -1,4 +1,6 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple
+
+import torch
 
 from kornia.augmentation import random_generator as rg
 from kornia.augmentation._2d.intensity.base import IntensityAugmentationBase2D
@@ -11,9 +13,10 @@ class RandomSnow(IntensityAugmentationBase2D):
     r"""Generates snow effect on given tensor image or a batch tensor images.
 
     Args:
-        snow_coefficient: A float between 0 and 1 that controls the amount of snow to add to the
-        image, the larger value corresponds to the more snow.
-        brightness: A float greater than 1 that controls the brightness of the snow.
+        snow_coefficient: A tuple of floats (lower and upper bound) between 0 and 1 that control
+        the amount of snow to add to the image, the larger value corresponds to the more snow.
+        brightness: A tuple of floats (lower and upper bound) greater than 1 that controls the
+        brightness of the snow.
         same_on_batch: If True, apply the same transformation to each image in a batch. Default: False.
         p: Probability of applying the transformation. Default: 0.5.
         keepdim: Keep the output tensor with the same shape as input. Default: False.
@@ -24,7 +27,7 @@ class RandomSnow(IntensityAugmentationBase2D):
 
     Examples:
         >>> inputs = torch.rand(2, 3, 4, 4)
-        >>> snow = kornia.augmentation.RandomSnow(p=1.0, snow_coefficient=0.2, brightness=2.0)
+        >>> snow = kornia.augmentation.RandomSnow(p=1.0, snow_coefficient=(0.1, 0.6), brightness=(1, 5))
         >>> output = snow(inputs)
         >>> output.shape
         torch.Size([2, 3, 4, 4])
@@ -32,18 +35,20 @@ class RandomSnow(IntensityAugmentationBase2D):
 
     def __init__(
         self,
-        snow_coefficient: float = 0.5,
-        brightness: float = 2.5,
+        snow_coefficient: Tuple[float, float] = (0.0, 1.0),
+        brightness: Tuple[float, float] = (1.0, 5.0),
         same_on_batch: bool = False,
         p: float = 0.5,
         keepdim: bool = False,
     ) -> None:
         super().__init__(p=p, same_on_batch=same_on_batch, keepdim=keepdim)
-        KORNIA_CHECK(0 <= snow_coefficient <= 1, "Snow coefficient must be between 0 and 1.")
+        KORNIA_CHECK(
+            [0 <= el <= 1 for el in snow_coefficient].all(), "Snow coefficient values must be between 0 and 1."
+        )
+        KORNIA_CHECK([1 <= el for el in brightness].all(), "Brightness values must be greater than 1.")
 
         self._param_generator = rg.PlainUniformGenerator(
-            ((snow_coefficient, snow_coefficient), "snow_coefficient", 0.5, (0.0, 1.0)),
-            ((brightness, brightness), "brightness", None, None),
+            (snow_coefficient, "snow_coefficient", 0.5, (0.0, 1.0)), (brightness, "brightness", None, None)
         )
 
     def apply_transform(
@@ -53,13 +58,12 @@ class RandomSnow(IntensityAugmentationBase2D):
         brightness = (params["brightness"].to(input)).mean()
         input_HLS = rgb_to_hls(input)
 
-        # Increase Light channel of the image by given brightness for areas based on snow coefficient.
-        new_light = input_HLS[:, :, 1][input_HLS[:, :, 1] < snow_coefficient]
-        new_light = new_light * brightness
+        mask = torch.zeros_like(input_HLS)
+        mask[:, :, 1] = torch.where(input_HLS[:, :, 1] < snow_coefficient, 1, 0)
 
-        # Setting value 255 for white pixels
-        new_light = new_light.clamp(min=0, max=1)
-        input_HLS[:, :, 1][input_HLS[:, :, 1] < snow_coefficient] = new_light
+        # Increase Light channel of the image by given brightness for areas based on snow coefficient.
+        new_light = (input_HLS * mask * brightness).clamp(min=0.0, max=1.0)
+        input_HLS = input_HLS * (1 - mask) + new_light
 
         output = hls_to_rgb(input_HLS)
         return output
