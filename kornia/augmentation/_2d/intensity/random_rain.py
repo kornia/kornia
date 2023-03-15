03eb6ca66613b54cc81bdbac7445957bfefe6f43
Bohdan Vey <bogdan.vey2002@gmail.com>
2023-03-15 14:36:24 +0200

    Random rain augmentation

--- kornia/augmentation/_2d/intensity/random_rain.py
+++ kornia/augmentation/_2d/intensity/random_rain.py
@@ -1,4 +1,6 @@
-from typing import Any, Dict, Optional, Tuple
+from __future__ import annotations
+
+from typing import Any
 
 import torch
 
@@ -17,7 +19,7 @@ class RandomRain(IntensityAugmentationBase2D):
         drop_height: height of the drop in image(same for each drops in one image)
         drop_width: width of the drop in image(same for each drops in one image)
     Shape:
-        - Input: :math:`(C, H, W)` or :math:`(B, C, H, W)`, Optional: :math:`(B, 3, 3)`
+        - Input: :math:`(C, H, W)` or :math:`(B, C, H, W)`
         - Output: :math:`(B, C, H, W)`
 
     Examples:
@@ -37,45 +39,46 @@ class RandomRain(IntensityAugmentationBase2D):
         same_on_batch: bool = False,
         p: float = 0.5,
         keepdim: bool = False,
-        number_of_drops: Tuple[int, int] = (1000, 2000),
-        drop_height: Tuple[int, int] = (5, 20),
-        drop_width: Tuple[int, int] = (-5, 5),
+        number_of_drops: tuple[int, int] = (1000, 2000),
+        drop_height: tuple[int, int] = (5, 20),
+        drop_width: tuple[int, int] = (-5, 5),
     ):
         super().__init__(p=p, same_on_batch=same_on_batch, p_batch=1.0, keepdim=keepdim)
         self._param_generator = RainGenerator(number_of_drops, drop_height, drop_width)
 
     def apply_transform(
-        self, image: Tensor, params: Dict[str, Tensor], flags: Dict[str, Any], transform: Optional[Tensor] = None
+        self, image: Tensor, params: dict[str, Tensor], flags: dict[str, Any], transform: Tensor | None = None
     ) -> Tensor:
+        height_of_drop = params['drop_height_factor']
+        width_of_drop = params['drop_width_factor']
+
+        # Check array
         KORNIA_CHECK(image.shape[1] == 3 or image.shape[1] == 1, "Number of color channels should be 1 or 3.")
         KORNIA_CHECK(len(image.shape) in (3, 4), "Wrong input dimension.")
-
         for i in range(image.shape[0]):
-            number_of_drops: int = int(params['number_of_drops_factor'][i])
-            height_of_drop: int = int(params['drop_height_factor'][i])
-            width_of_drop: int = int(params['drop_width_factor'][i])
-            KORNIA_CHECK(height_of_drop <= image[i].shape[1], "Height of drop should be less than image height")
+            KORNIA_CHECK(int(height_of_drop[i]) <= image[i].shape[1], "Height of drop should be less than image height")
 
-            KORNIA_CHECK(width_of_drop <= image[i].shape[2], "Width of drop should be less than image width")
-            KORNIA_CHECK(height_of_drop > 0, "Height should be bigger than 0")
+            KORNIA_CHECK(int(width_of_drop[i]) <= image[i].shape[2], "Width of drop should be less than image width")
+            KORNIA_CHECK(int(height_of_drop[i]) > 0, "Height should be bigger than 0")
             # Generate start coordinates for each drop
 
-            random_y_coords = torch.randint(low=0, high=image[i].shape[1] - height_of_drop, size=[1, number_of_drops])
-            if width_of_drop < 0:
-                random_x_coords = torch.randint(
-                    low=-width_of_drop - 1, high=image[i].shape[2], size=[1, number_of_drops]
-                )
+        for i in range(image.shape[0]):
+            coordinates_of_drops = params['coordinates_factor'][i]
+            height_of_drop = params['drop_height_factor'][i]
+            width_of_drop = params['drop_width_factor'][i]
+
+            random_y_coords = coordinates_of_drops[:, 0] * (image.shape[2] - height_of_drop - 1)
+            if width_of_drop > 0:
+                random_x_coords = coordinates_of_drops[:, 1] * (image.shape[3] - width_of_drop - 1)
             else:
-                random_x_coords = torch.randint(
-                    low=0, high=image[i].shape[2] - width_of_drop, size=[1, number_of_drops]
-                )
-            coords = torch.cat([random_y_coords, random_x_coords], dim=0).to(image.device)
+                random_x_coords = coordinates_of_drops[:, 1] * (image.shape[3] + width_of_drop - 1) - width_of_drop
+
+            coords = torch.cat([random_y_coords[None], random_x_coords[None]], dim=0).to(image.device, dtype=torch.long)
 
             # Generate how our drop will look like into the image
             size_of_line = max(height_of_drop, abs(width_of_drop))
             x = torch.linspace(start=0, end=height_of_drop, steps=size_of_line, dtype=torch.int32).to(image.device)
             y = torch.linspace(start=0, end=width_of_drop, steps=size_of_line, dtype=torch.int32).to(image.device)
-
             # Draw lines
             for k in range(x.shape[0]):
                 image[i, :, coords[0] + x[k], coords[1] + y[k]] = 200 / 255
--- kornia/augmentation/random_generator/_2d/random_rain.py
+++ kornia/augmentation/random_generator/_2d/random_rain.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from __future__ import annotations
 
 import torch
 from torch.distributions import Uniform
@@ -10,7 +10,7 @@ from kornia.core import Tensor
 
 class RainGenerator(RandomGeneratorBase):
     def __init__(
-        self, number_of_drops: Tuple[int, int], drop_height: Tuple[int, int], drop_width: Tuple[int, int]
+        self, number_of_drops: tuple[int, int], drop_height: tuple[int, int], drop_width: tuple[int, int]
     ) -> None:
         super().__init__()
         self.number_of_drops = number_of_drops
@@ -41,20 +41,37 @@ class RainGenerator(RandomGeneratorBase):
             bounds=(self.drop_width[0], self.drop_width[1] + 1),
         ).to(device=device, dtype=dtype)
 
+        drop_coordinates = _range_bound((0, 1), 'drops_coordinate', center=0.5, bounds=(0, 1)).to(device=device)
+
         self.number_of_drops_sampler = Uniform(number_of_drops[0], number_of_drops[1], validate_args=False)
         self.drop_height_sampler = Uniform(drop_height[0], drop_height[1], validate_args=False)
         self.drop_width_sampler = Uniform(drop_width[0], drop_width[1], validate_args=False)
+        self.coordinates_sampler = Uniform(drop_coordinates[0], drop_coordinates[1], validate_args=False)
 
-    def forward(self, batch_shape: Tuple[int, ...], same_on_batch: bool = False) -> Dict[str, Tensor]:
+    def forward(self, batch_shape: tuple[int, ...], same_on_batch: bool = False) -> dict[str, Tensor]:
         batch_size = batch_shape[0]
         _common_param_check(batch_size, same_on_batch)
         # self.ksize_factor.expand((batch_size, -1))
-        number_of_drops_factor = _adapted_rsampling((batch_size,), self.number_of_drops_sampler, same_on_batch)
-        drop_height_factor = _adapted_rsampling((batch_size,), self.drop_height_sampler, same_on_batch)
-        drop_width_factor = _adapted_rsampling((batch_size,), self.drop_width_sampler, same_on_batch).int()
+        number_of_drops_factor = _adapted_rsampling((batch_size,), self.number_of_drops_sampler, same_on_batch).to(
+            dtype=torch.int32
+        )
+        drop_height_factor = _adapted_rsampling((batch_size,), self.drop_height_sampler, same_on_batch).to(
+            dtype=torch.int32
+        )
+        drop_width_factor = _adapted_rsampling((batch_size,), self.drop_width_sampler, same_on_batch).to(
+            dtype=torch.int32
+        )
+        if not same_on_batch:
+            coordinates_factor = [
+                _adapted_rsampling((number_of_drops, 2), self.coordinates_sampler, False)
+                for number_of_drops in number_of_drops_factor
+            ]
+        else:
+            coordinates_factor = _adapted_rsampling((number_of_drops_factor[0], 2), self.coordinates_sampler, False)
+            coordinates_factor = [coordinates_factor] * batch_size
 
         return dict(
-            number_of_drops_factor=number_of_drops_factor.to(dtype=torch.int32),
-            drop_height_factor=drop_height_factor.to(dtype=torch.int32),
-            drop_width_factor=drop_width_factor.to(dtype=torch.int32),
+            coordinates_factor=coordinates_factor,
+            drop_height_factor=drop_height_factor,
+            drop_width_factor=drop_width_factor,
         )
