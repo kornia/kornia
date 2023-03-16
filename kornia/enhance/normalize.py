3967531cb7c544fe6ec17a2c848674495e5dd8bc
Adam J. Stewart <ajstewart426@gmail.com>
2023-03-15 18:30:09 -0600

    Fix Normalize with integer inputs (#2269)
    * Fix Normalize with integer inputs
    
    * improve typing
    
    ---------
    
    Co-authored-by: edgar <edgar.riba@gmail.com>

--- kornia/enhance/normalize.py
+++ kornia/enhance/normalize.py
@@ -45,10 +45,10 @@ class Normalize(nn.Module):
     ) -> None:
         super().__init__()
 
-        if isinstance(mean, float):
+        if isinstance(mean, (int, float)):
             mean = torch.tensor([mean])
 
-        if isinstance(std, float):
+        if isinstance(std, (int, float)):
             std = torch.tensor([std])
 
         if isinstance(mean, (tuple, list)):
