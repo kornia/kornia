11ba93ac22529d366d8248bbd639247bd70ecf2e
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2023-03-24 15:02:29 +0100

    Reduce the image size used in smoke tests from 256x256 to 64x64

--- test/feature/test_disk.py
+++ test/feature/test_disk.py
@@ -6,7 +6,7 @@ from kornia.feature.disk import DISK, DISKFeatures
 class TestDisk:
     def test_smoke(self, device):
         disk = DISK().to(device)
-        inp = torch.ones(1, 3, 256, 256, device=device)
+        inp = torch.ones(1, 3, 64, 64, device=device)
         output = disk(inp)
         assert isinstance(output, list)
         assert len(output) == 1
@@ -15,7 +15,7 @@ class TestDisk:
 
     def test_smoke_pretrained(self, device):
         disk = DISK.from_pretrained(checkpoint='depth', device=device)
-        inp = torch.ones(1, 3, 256, 256, device=device)
+        inp = torch.ones(1, 3, 64, 64, device=device)
         output = disk(inp)
         assert isinstance(output, list)
         assert len(output) == 1
@@ -24,8 +24,8 @@ class TestDisk:
 
     def test_heatmap_and_dense_descriptors(self, device):
         disk = DISK().to(device)
-        inp = torch.ones(1, 3, 256, 256, device=device)
+        inp = torch.ones(1, 3, 64, 64, device=device)
         heatmaps, descriptors = disk.heatmap_and_dense_descriptors(inp)
 
-        assert heatmaps.shape == (1, 1, 256, 256)
-        assert descriptors.shape == (1, 128, 256, 256)
+        assert heatmaps.shape == (1, 1, 64, 64)
+        assert descriptors.shape == (1, 128, 64, 64)
