a0ba356b88cfb890711b804ec262aeae08881d11
Michał Tyszkiewicz <michal.tyszkiewicz@gmail.com>
2023-03-24 14:55:37 +0100

    Make tests use a non-pretrained variant

--- test/feature/test_disk.py
+++ test/feature/test_disk.py
@@ -1,23 +1,20 @@
 import torch
 
-from kornia.feature.disk import DISK
+from kornia.feature.disk import DISK, DISKFeatures
 
 
 class TestDisk:
-    def test_smoke_depth(self, device):
-        disk: DISK = DISK.from_pretrained(checkpoint='depth', device=device)
+    def test_smoke(self, device):
+        disk = DISK().to(device)
         inp = torch.ones(1, 3, 256, 256, device=device)
         output = disk(inp)
-        assert output is not None
-
-    def test_smoke_epipolar(self, device):
-        disk: DISK = DISK.from_pretrained(checkpoint='epipolar', device=device)
-        inp = torch.ones(1, 3, 256, 256, device=device)
-        output = disk(inp)
-        assert output is not None
+        assert isinstance(output, list)
+        assert len(output) == 1
+        for element in output:
+            assert isinstance(element, DISKFeatures)
 
     def test_heatmap_and_dense_descriptors(self, device):
-        disk: DISK = DISK.from_pretrained(checkpoint='depth', device=device)
+        disk = DISK().to(device)
         inp = torch.ones(1, 3, 256, 256, device=device)
         heatmaps, descriptors = disk.heatmap_and_dense_descriptors(inp)
 
