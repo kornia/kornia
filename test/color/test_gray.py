244bd70aa4c2d5aa14fbae7dc6cd6a8fbebe871c
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2023-04-09 19:27:04 -0300

    skip dynamo test for color gray on torch 2.0.0

--- test/color/test_gray.py
+++ test/color/test_gray.py
@@ -3,8 +3,8 @@ import torch
 from torch.autograd import gradcheck
 
 import kornia
-from kornia.testing import BaseTester  # test utils
-from kornia.testing import assert_close
+from kornia.testing import BaseTester, assert_close
+from kornia.utils._compat import torch_version
 
 
 class TestGrayscaleToRgb(BaseTester):
@@ -201,7 +201,7 @@ class TestRgbToGrayscale(BaseTester):
         img = torch.ones(B, C, H, W, device=device, dtype=torch.float64, requires_grad=True)
         assert gradcheck(kornia.color.rgb_to_grayscale, (img,), raise_exception=True, fast_mode=True)
 
-    @pytest.mark.skip(reason='Is crashing on compile')
+    @pytest.mark.skipif(torch_version() == '2.0.0', reason='Not working on 2.0')
     def test_dynamo(self, device, dtype, torch_optimizer):
         # TODO: investigate the problem with dynamo
         B, C, H, W = 2, 3, 4, 4
@@ -294,7 +294,7 @@ class TestBgrToGrayscale(BaseTester):
         img = torch.ones(B, C, H, W, device=device, dtype=torch.float64, requires_grad=True)
         assert gradcheck(kornia.color.bgr_to_grayscale, (img,), raise_exception=True, fast_mode=True)
 
-    @pytest.mark.skip(reason='Is crashing on compile')
+    @pytest.mark.skipif(torch_version() == '2.0.0', reason='Not working on 2.0')
     def test_dynamo(self, device, dtype, torch_optimizer):
         # TODO: investigate the problem with dynamo
         B, C, H, W = 2, 3, 4, 4
