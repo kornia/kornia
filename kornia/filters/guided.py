ddb190855d38ae2a6c276950b1dd2fd290617cea
Thien Tran <gau.nernst@yahoo.com.sg>
2023-04-13 00:27:01 +0800

    add single channel impl

--- kornia/filters/guided.py
+++ kornia/filters/guided.py
@@ -2,16 +2,50 @@ from __future__ import annotations
 
 from kornia.core import Module, Tensor
 
+from .blur import box_blur
 
-def guided_blur(input: Tensor, guidance: Tensor, kernel_size: tuple[int, int] | int, eps: float | Tensor):
-    raise NotImplementedError
+
+def guided_blur(
+    input: Tensor,
+    guidance: Tensor,
+    kernel_size: tuple[int, int] | int,
+    eps: float | Tensor,
+    border_type: str = 'reflect',
+):
+    if isinstance(eps, Tensor):
+        eps = eps.view(-1, 1, 1, 1)  # N -> NCHW
+
+    mean_i = box_blur(input, kernel_size, border_type)
+    mean_g = box_blur(input, kernel_size, border_type)
+    corr_i = box_blur(input.square(), kernel_size, border_type)
+    corr_ip = box_blur(input * guidance, kernel_size, border_type)
+
+    var_i = corr_i - mean_i.square()
+    cov_ig = corr_ip - mean_i * mean_g
+
+    a = cov_ig / (var_i + eps)
+    b = mean_g - a * mean_i
+
+    mean_a = box_blur(a, kernel_size, border_type)
+    mean_b = box_blur(b, kernel_size, border_type)
+
+    return mean_a * input + mean_b
 
 
 class GuidedBlur(Module):
-    def __init__(self, kernel_size: tuple[int, int] | int, eps: float | Tensor):
+    def __init__(self, kernel_size: tuple[int, int] | int, eps: float | Tensor, border_type: str = 'reflect'):
         super().__init__()
         self.kernel_size = kernel_size
         self.eps = eps
+        self.border_type = border_type
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}"
+            f"(kernel_size={self.kernel_size}, "
+            f"eps={self.eps}, "
+            f"border_type={self.border_type})"
+        )
 
     def forward(self, input: Tensor, guidance: Tensor):
-        return guided_blur(input, guidance, self.kernel_size, self.eps)
+        return guided_blur(input, guidance, self.kernel_size, self.eps, self.border_type)
