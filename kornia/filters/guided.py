0458703002d475fd696a029f0078beff2a41b7b8
Thien Tran <gau.nernst@yahoo.com.sg>
2023-04-13 22:53:44 +0800

    implement multi-channel guided filter

--- kornia/filters/guided.py
+++ kornia/filters/guided.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+import torch
+
 from kornia.core import Module, Tensor
 
 from .blur import box_blur
 
 
-def guided_blur(
+def _guided_blur_grayscale_guidance(
     guidance: Tensor,
     input: Tensor,
     kernel_size: tuple[int, int] | int,
@@ -34,6 +36,59 @@ def guided_blur(
     mean_a = box_blur(a, kernel_size, border_type)
     mean_b = box_blur(b, kernel_size, border_type)
     return mean_a * guidance + mean_b
+
+
+def _guided_blur_multichannel_guidance(
+    guidance: Tensor,
+    input: Tensor,
+    kernel_size: tuple[int, int] | int,
+    eps: float | Tensor,
+    border_type: str = 'reflect',
+):
+    B, C, H, W = guidance.shape
+
+    mean_I = box_blur(guidance, kernel_size, border_type)
+    corr_I = box_blur((guidance.unsqueeze(1) * guidance.unsqueeze(2)).flatten(1, 2), kernel_size, border_type)
+    var_I = corr_I.reshape(B, C, C, H, W) - mean_I.unsqueeze(1) * mean_I.unsqueeze(2)
+
+    if guidance is input:
+        mean_p = mean_I
+        cov_Ip = var_I
+
+    else:
+        mean_p = box_blur(input, kernel_size, border_type)
+        corr_Ip = box_blur((input.unsqueeze(1) * guidance.unsqueeze(2)).flatten(1, 2), kernel_size, border_type)
+        cov_Ip = corr_Ip.reshape(B, C, -1, H, W) - mean_p.unsqueeze(1) * mean_I.unsqueeze(2)
+
+    if isinstance(eps, Tensor):
+        eps = eps.view(-1, 1, 1, 1)  # N -> N, C, C, H, W
+
+    _eps = torch.eye(C, device=guidance.device, dtype=guidance.dtype).view(1, C, C, 1, 1) * eps
+    a1 = (var_I + _eps).permute(0, 3, 4, 1, 2)  # B, H, W, C_guidance, C_guidance
+    a2 = cov_Ip.permute(0, 3, 4, 1, 2)  # B, H, W, C_guidance, C_input
+    a = torch.linalg.solve(a1, a2)  # B, H, W, C_guidance, C_input
+    b = mean_p.permute(0, 2, 3, 1) - (a.transpose(-1, -2) @ mean_I.permute(0, 2, 3, 1).unsqueeze(-1)).squeeze(-1)
+    # B, H, W, C_input
+
+    a = a.flatten(-2).permute(0, 3, 1, 2)
+    b = b.permute(0, 3, 1, 2)
+
+    mean_a = box_blur(a, kernel_size, border_type).permute(0, 2, 3, 1).view(B, H, W, C, -1)
+    mean_b = box_blur(b, kernel_size, border_type)
+    return (guidance.permute(0, 2, 3, 1).unsqueeze(-2) @ mean_a).squeeze(-2).permute(0, 3, 1, 2) + mean_b
+
+
+def guided_blur(
+    guidance: Tensor,
+    input: Tensor,
+    kernel_size: tuple[int, int] | int,
+    eps: float | Tensor,
+    border_type: str = 'reflect',
+) -> Tensor:
+    if guidance.shape[1] == 1:
+        return _guided_blur_grayscale_guidance(guidance, input, kernel_size, eps, border_type)
+    else:
+        return _guided_blur_multichannel_guidance(guidance, input, kernel_size, eps, border_type)
 
 
 def fast_guided_blur(
