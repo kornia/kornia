bd054c1162bb294a1122b3d888cde2ffd802a1bd
Thien Tran <gau.nernst@yahoo.com.sg>
2023-05-14 20:21:12 +0700

    simplify tests

--- test/contrib/models/test_rt_detr.py
+++ test/contrib/models/test_rt_detr.py
@@ -12,40 +12,35 @@ from kornia.contrib.models.structures import DetectionResults
 from kornia.testing import BaseTester
 
 
-@pytest.mark.parametrize('shape', ((1, 3, 224, 224), (2, 3, 256, 256)))
 @pytest.mark.parametrize('backbone_factory', (partial(ResNetD.from_config, 18), partial(PPHGNetV2.from_config, "L")))
-def test_backbone(backbone_factory, shape, device, dtype):
+def test_backbone(backbone_factory, device, dtype):
     backbone = backbone_factory().to(device, dtype)
     assert hasattr(backbone, "out_channels")
     assert len(backbone.out_channels) == 3
 
-    imgs = torch.randn(shape, device=device, dtype=dtype)
+    N, C, H, W = 2, 3, 224, 256
+    imgs = torch.randn(N, C, H, W, device=device, dtype=dtype)
     fmaps = backbone(imgs)
 
     assert len(fmaps) == 3
     downscale = 8
-    N, _, H, W = shape
     for fmap, ch in zip(fmaps, backbone.out_channels):
         assert fmap.shape == (N, ch, H // downscale, W // downscale)
         downscale *= 2
 
 
-@pytest.mark.parametrize("batch_size", (1, 2))
-def test_neck(batch_size, device, dtype):
+def test_neck(device, dtype):
+    N = 2
     in_channels = [64, 128, 256]
-    sizes = [32, 16, 8]
+    sizes = [(32, 24), (16, 12), (8, 6)]
     hidden_dim = 64
     neck = HybridEncoder(in_channels, hidden_dim, 128).to(device, dtype)
-    fmaps = []
-    for ch_in, sz in zip(in_channels, sizes):
-        fmaps.append(torch.randn(batch_size, ch_in, sz, sz, device=device, dtype=dtype))
+    fmaps = [torch.randn(N, ch_in, h, w, device=device, dtype=dtype) for ch_in, (h, w) in zip(in_channels, sizes)]
 
     outs = neck(fmaps)
     assert len(outs) == len(fmaps)
-    for out, fmap in zip(outs, fmaps):
-        assert out.shape[0] == fmap.shape[0]
-        assert out.shape[1] == hidden_dim
-        assert out.shape[2:] == fmap.shape[2:]
+    for out, (h, w) in zip(outs, sizes):
+        assert out.shape == (N, hidden_dim, h, w)
 
 
 def test_rtdetr_head(device, dtype):
