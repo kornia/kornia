f4e0b3117733e533074f0bb2e06fc6d834c5cb27
cjpurackal <christeejacobs@gmail.com>
2023-06-05 18:10:03 +0530

    review changes

--- kornia/utils/draw.py
+++ kornia/utils/draw.py
@@ -23,7 +23,7 @@ def _draw_pixel(image: torch.Tensor, x: int, y: int, color: torch.Tensor) -> Non
     image[:, y, x] = color
 
 
-def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: torch.Tensor, width: torch.Tensor) -> torch.Tensor:
+def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: torch.Tensor) -> torch.Tensor:
     r"""Draw a single line into an image.
 
     Args:
@@ -31,7 +31,6 @@ def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: to
         p1: the start point [x y] of the line with shape (2, ) or (B, 2).
         p2: the end point [x y] of the line with shape (2, ) or (B, 2).
         color: the color of the line with shape :math`(C)` where :math`C` is the number of channels of the image.
-        width: the width of the line with shape (1) or (B, 1).
     Return:
         the image with containing the line.
 
@@ -72,9 +71,6 @@ def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: to
     if color.size(0) != image.size(0):
         raise ValueError("color must have the same number of channels as the image.")
 
-    if width.size(0) != 1 and width.size(0) != image.size(0):
-        raise ValueError("width must have the same batch size as that of image or 1.")
-
     # move p1 and p2 to the same device as the input image
     # move color to the same device and dtype as the input image
     p1 = p1.to(image.device).to(torch.int64)
@@ -105,14 +101,14 @@ def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: to
         dx_zero_y_coords = [
             y
             for y_i, s, dy_ in zip(y1[dx_zero_mask], dy_sign[dx_zero_mask], dy[dx_zero_mask])
-            for y in (y_i + s * torch.arange(0, dy_ + 1, 1).to(image.device))
+            for y in (y_i + s * torch.arange(0, dy_ + 1, 1, device=image.device))
         ]
 
     if dy_zero_mask.any():
         dy_zero_x_coords = [
             x
             for x_i, s, dx_i in zip(x1[dy_zero_mask], dx_sign[dy_zero_mask], dx[dy_zero_mask])
-            for x in (x_i + s * torch.arange(0, dx_i + 1, 1).to(image.device))
+            for x in (x_i + s * torch.arange(0, dx_i + 1, 1, device=image.device))
         ]
         dy_zero_y_coords = [
             y for y_i, dx_i in zip(y1[dy_zero_mask], dx[dy_zero_mask]) for y in y_i.repeat(int(dx_i.item() + 1))
@@ -122,25 +118,29 @@ def draw_line(image: torch.Tensor, p1: torch.Tensor, p2: torch.Tensor, color: to
         dx_gt_dy_x_coords = [
             x
             for x_i, s, dx_i in zip(x1[dx_gt_dy_mask], dx_sign[dx_gt_dy_mask], dx[dx_gt_dy_mask])
-            for x in (x_i + s * torch.arange(0, dx_i + 1, 1).to(image.device))
+            for x in (x_i + s * torch.arange(0, dx_i + 1, 1, device=image.device))
         ]
         dx_gt_dy_y_coords = [
             y
             for y_i, s, dx_i, dy_i in zip(
                 y1[dx_gt_dy_mask], dy_sign[dx_gt_dy_mask], dx[dx_gt_dy_mask], dy[dx_gt_dy_mask]
             )
-            for y in (y_i + s * torch.arange(0, dy_i + 1, dy_i / dx_i)[: int(dx_i.item()) + 1].ceil().to(image.device))
+            for y in (
+                y_i + s * torch.arange(0, dy_i + 1, dy_i / dx_i, device=image.device)[: int(dx_i.item()) + 1].ceil()
+            )
         ]
     if rest_mask.any():
         rest_x_coords = [
             x
             for x_i, s, dx_i, dy_ in zip(x1[rest_mask], dx_sign[rest_mask], dx[rest_mask], dy[rest_mask])
-            for x in (x_i + s * torch.arange(0, dx_i + 1, dx_i / dy_)[: int(dy_.item()) + 1].ceil().to(image.device))
+            for x in (
+                x_i + s * torch.arange(0, dx_i + 1, dx_i / dy_, device=image.device)[: int(dy_.item()) + 1].ceil()
+            )
         ]
         rest_y_coords = [
             y
             for y_i, s, dy_i in zip(y1[rest_mask], dy_sign[rest_mask], dy[rest_mask])
-            for y in (y_i + s * torch.arange(0, dy_i + 1, 1).to(image.device))
+            for y in (y_i + s * torch.arange(0, dy_i + 1, 1, device=image.device))
         ]
     x_coords = torch.tensor(dx_zero_x_coords + dy_zero_x_coords + dx_gt_dy_x_coords + rest_x_coords).long()
     y_coords = torch.tensor(dx_zero_y_coords + dy_zero_y_coords + dx_gt_dy_y_coords + rest_y_coords).long()
