e85d55308e0485f03f31bff3902f8501c48b1980
NripeshN <nn2012@hw.ac.uk>
2023-07-02 13:52:31 +0530

    test fix

--- test/geometry/test_homography.py
+++ test/geometry/test_homography.py
@@ -395,7 +395,7 @@ class TestFindHomographyDLTIter:
     @pytest.mark.parametrize("batch_size, num_points", [(1, 4), (2, 5), (3, 6)])
     def test_shape(self, batch_size, num_points, device, dtype):
         if sys.platform == 'darwin' and torch.__version__ == '1.9.1' and sys.version_info == (3, 8):
-            pytest.skip("test fails on macos")
+            raise NotImplementedError("Implement a stupid routine.")
         else:
             B, N = batch_size, num_points
             points1 = torch.rand(B, N, 2, device=device, dtype=dtype)
