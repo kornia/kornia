d75f4f3f0be9b87c7f3ebd38121ab62950b6daa9
Dmytro Mishkin <ducha.aiki@gmail.com>
2023-07-12 20:41:18 +0200

    Adding LightGlue (#2449)
    * Added LightGlue
    
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/core/_backend.py
+++ kornia/core/_backend.py
@@ -8,21 +8,28 @@ from torch import device
 Tensor = torch.Tensor
 tensor = torch.tensor
 Module = torch.nn.Module
+ModuleList = torch.nn.ModuleList
 Parameter = torch.nn.Parameter
 
 # functions
 # NOTE: ideally we expose what we find in numpy
+arange = torch.arange
 concatenate = torch.cat
 stack = torch.stack
 linspace = torch.linspace
 normalize = F.normalize
 pad = F.pad
 eye = torch.eye
+einsum = torch.einsum
 zeros = torch.zeros
 zeros_like = torch.zeros_like
+ones = torch.ones
+ones_like = torch.ones_like
 where = torch.where
 complex = torch.complex
 diag = torch.diag
+softmax = F.softmax
+
 
 # constructors
 as_tensor = torch.as_tensor
