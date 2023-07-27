ba5c6d4dc7cfcd3d058ac477a13788737bfb1d43
edgar <edgar.riba@gmail.com>
2023-07-27 11:15:51 +0200

    include print image in the Image API

--- kornia/utils/image_print.py
+++ kornia/utils/image_print.py
@@ -372,6 +372,8 @@ def image_to_string(image: Tensor, max_width: int = 256) -> str:
 def print_image(image: Union[str, Tensor], max_width: int = 96) -> None:
     """Print an image to the terminal.
 
+    .. image:: https://github.com/kornia/data/blob/a82d2bdc868f3774a1be91419a2870e54222389e/print_image.png
+
     Args:
         image: path to a valid image file or a tensor.
         max_width: maximum width to print to terminal.
