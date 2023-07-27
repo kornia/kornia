c54f3c9d1c0f35f3680a99fe374d28d363c4981d
Alex Brooks <abrooks9944@email.arizona.edu>
2023-07-27 00:55:27 -0600

    Add draw point2d (#2387)
    * First pass at RGB draw_pixel
    
    * Add test for batch rgb pixels
    
    * Add test for 3rd order grayscale pixel drawing
    
    * Handle pixel drawing for grayscale images with no channel dim
    
    * Add test for drawing pixels on gray images with no channel dim
    
    * Add check for channel dims
    
    * Add docstrings to draw_point2d, fix test names
    
    * Add test for mismatched dtype on draw point 2d
    
    * Fix err message type in draw_point2d
    
    * Expose draw point 2d in utils package
    
    * Use xy coords instead of image coords in point plotter
    
    * Use point tensors in util tests, add test for mat color
    
    * Handle 2nd order color in draw_point2d
    
    * Apply suggestions from code review
    
    Co-authored-by: João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
    
    ---------
    
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>
    Co-authored-by: João Gustavo A. Amorim <joaogustavoamorim@gmail.com>

--- kornia/utils/draw.py
+++ kornia/utils/draw.py
@@ -8,6 +8,33 @@ from kornia.core.check import KORNIA_CHECK, KORNIA_CHECK_SHAPE
 # TODO: implement width of the line
 
 
+def draw_point2d(image: Tensor, points: Tensor, color: Tensor) -> Tensor:
+    r"""Sets one or more coordinates in a Tensor to a color.
+
+    Args:
+        image: the input image on which to draw the points with shape :math`(C,H,W)` or :math`(H,W)`.
+        points: the [x, y] points to be drawn on the image.
+        color: the color of the pixel with :math`(C)` where :math`C` is the number of channels of the image.
+
+    Return:
+        The image with points set to the color.
+    """
+    KORNIA_CHECK(
+        (len(image.shape) == 2 and len(color.shape) == 1) or (image.shape[0] == color.shape[0]),
+        "Color dim must match the channel dims of the provided image",
+    )
+    points = points.to(dtype=torch.int64, device=image.device)
+    x, y = zip(*points)
+    if len(color.shape) == 1:
+        color = torch.unsqueeze(color, dim=1)
+    color = color.to(dtype=image.dtype, device=image.device)
+    if len(image.shape) == 2:
+        image[y, x] = color
+    else:
+        image[:, y, x] = color
+    return image
+
+
 def _draw_pixel(image: torch.Tensor, x: int, y: int, color: torch.Tensor) -> None:
     r"""Draws a pixel into an image.
 
