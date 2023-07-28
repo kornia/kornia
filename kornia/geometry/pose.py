e0719b7b9d162d45d01f2ddfb1faaca4e3ba9db1
cjpurackal <christeejacobs@gmail.com>
2023-07-28 19:35:30 +0530

    review update, add tests

--- kornia/geometry/pose.py
+++ kornia/geometry/pose.py
@@ -7,7 +7,7 @@ from kornia.geometry.liegroup import Se2, Se3, So2, So3
 from kornia.geometry.quaternion import Quaternion
 
 
-def check_R_shape(R: Tensor):
+def check_R_shape(R: Tensor) -> None:
     if len(R.shape) > 3 or len(R.shape) < 2:
         raise ValueError(f"R must be either 2x2 or 3x3, got {R.shape}")
     if list(R.shape[-2:]) not in ([2, 2], [3, 3]):
@@ -18,44 +18,114 @@ class NamedPose:
     r"""Class to represent a named pose between two frames.
 
     Internally represented by either Se2 or Se3.
+
+    Example:
+        >>> b_from_a = NamedPose(Se3.identity(), frame1="frame_a", frame2="frame_b")
+        >>> b_from_a
+        NamedPose(frame2_from_frame1=rotation: Parameter containing:
+        tensor([1., 0., 0., 0.], requires_grad=True)
+        translation: x: 0.0
+        y: 0.0
+        z: 0.0,
+        frame1: frame_a -> frame2: frame_b)
     """
 
-    def __init__(self, t_a_b: Se2 | Se3, frame_a: str | None = None, frame_b: str | None = None) -> None:
-        self.t_a_b = t_a_b
-        self._frame_a = frame_a or uuid.uuid4().hex
-        self._frame_b = frame_b or uuid.uuid4().hex
+    def __init__(self, frame2_from_frame1: Se2 | Se3, frame1: str | None = None, frame2: str | None = None) -> None:
+        """Constructor for NamedPose.
+
+        Args:
+            frame2_from_frame1: Pose from frame 1 to frame 2.
+            frame1: Name of frame a.
+            frame2: Name of frame b.
+        """
+        self._frame2_from_frame1 = frame2_from_frame1
+        self._frame1 = frame1 or uuid.uuid4().hex
+        self._frame2 = frame2 or uuid.uuid4().hex
 
     def __repr__(self) -> str:
-        return f"NamedPose(t_a_b={self.t_a_b}, \n frame: {self._frame_a} -> frame: {self._frame_b})"
+        return f"NamedPose(frame2_from_frame1={self._frame2_from_frame1}, \
+                \nframe1: {self._frame1} -> frame2: {self._frame2})"
 
     def __mul__(self, other: NamedPose) -> NamedPose:
-        t_a_b = self.t_a_b * other.t_a_b
-        return NamedPose(t_a_b, self._frame_a, other.frame_b)
+        """Compose two NamedPoses.
+
+        Args:
+            other: NamedPose to compose with.
+
+        Returns:
+            NamedPose: Composed NamedPose.
+
+        Example:
+            >>> b_from_a = NamedPose(Se3.identity(), frame1="frame_a", frame2="frame_b")
+            >>> c_from_b = NamedPose(Se3.identity(), frame1="frame_b", frame2="frame_c")
+            >>> b_from_a * c_from_b
+            NamedPose(frame2_from_frame1=rotation: Parameter containing:
+            tensor([1., 0., 0., 0.], requires_grad=True)
+            translation: x: 0.0
+            y: 0.0
+            z: 0.0,
+            frame1: frame_a -> frame2: frame_c)
+        """
+        if self.frame2 != other.frame1:
+            raise ValueError(f"Cannot compose {self} with {other}")
+        return NamedPose(self.pose * other.pose, self.frame1, other.frame2)
+
+    @property
+    def pose(self) -> Se2 | Se3:
+        """Pose from frame 1 to frame 2."""
+        return self._frame2_from_frame1
 
     @property
     def rotation(self) -> So3 | So2:
-        return self.t_a_b.rotation
+        """Rotation part of the pose."""
+        return self._frame2_from_frame1.rotation
 
     @property
     def translation(self) -> Tensor:
-        return self.t_a_b.translation
+        """Translation part of the pose."""
+        return self._frame2_from_frame1.translation
 
     @property
-    def frame_a(self) -> str:
-        return self._frame_a
+    def frame1(self) -> str:
+        """Name of frame 1."""
+        return self._frame1
 
     @property
-    def frame_b(self) -> str:
-        return self._frame_b
+    def frame2(self) -> str:
+        """Name of frame 2."""
+        return self._frame2
 
     @classmethod
     def from_RT(
-        cls, R: So3 | So2 | Tensor | Quaternion, T: Tensor, frame_a: str | None = None, frame_b: str | None = None
+        cls, R: So3 | So2 | Tensor | Quaternion, T: Tensor, frame1: str | None = None, frame2: str | None = None
     ) -> NamedPose:
+        """Construct NamedPose from rotation and translation.
+
+        Args:
+            R : Rotation part of the pose.
+            T : Translation part of the pose.
+            frame1 : Name of frame a.
+            frame2 : Name of frame b.
+
+        Returns:
+            NamedPose: NamedPose constructed from rotation and translation.
+
+        Example:
+            >>> b_from_a_rotation = So3.identity()
+            >>> b_from_a_trans = torch.tensor([1., 2., 3.])
+            >>> b_from_a = NamedPose.from_RT(b_from_a_rotation, b_from_a_trans, frame1="frame_a", frame2="frame_b")
+            >>> b_from_a
+            NamedPose(frame2_from_frame1=rotation: Parameter containing:
+            tensor([1., 0., 0., 0.], requires_grad=True)
+            translation: Parameter containing:
+            tensor([1., 2., 3.], requires_grad=True),
+            frame1: frame_a -> frame2: frame_b)
+        """
+        # TODO: Add support for RT matrix
         if isinstance(R, (So3, Quaternion)):
-            return cls(Se3(R, T), frame_a, frame_b)
+            return cls(Se3(R, T), frame1, frame2)
         elif isinstance(R, So2):
-            return cls(Se2(R, T), frame_a, frame_b)
+            return cls(Se2(R, T), frame1, frame2)
         elif isinstance(R, Tensor):
             check_R_shape(R)
             dim = R.shape[-1]
@@ -63,17 +133,46 @@ class NamedPose:
             RT[..., :dim, :dim] = R
             RT[..., :dim, dim] = T
             if dim == 2:
-                return cls(Se2.from_matrix(RT), frame_a, frame_b)
+                return cls(Se2.from_matrix(RT), frame1, frame2)
             elif dim == 3:
-                return cls(Se3.from_matrix(RT), frame_a, frame_b)
+                return cls(Se3.from_matrix(RT), frame1, frame2)
         else:
             raise ValueError(f"R must be either So2, So3, Quaternion, or Tensor, got {type(R)}")
 
-    def from_colmap():
+    @classmethod
+    def from_colmap() -> NamedPose:
         raise NotImplementedError
 
     def inverse(self) -> NamedPose:
-        return NamedPose(self.t_a_b.inverse(), self.frame_b, self.frame_a)
+        """Inverse of the NamedPose.
 
-    def transform(self, points: Tensor) -> Tensor:
-        return self.t_a_b * points
+        Returns:
+            NamedPose: Inverse of the NamedPose.
+
+        Example:
+            >>> b_from_a = NamedPose(Se3.identity(), frame1="frame_a", frame2="frame_b")
+            >>> b_from_a.inverse()
+            NamedPose(frame2_from_frame1=rotation: Parameter containing:
+            tensor([1., -0., -0., -0.], requires_grad=True)
+            translation: x: 0.0
+            y: 0.0
+            z: 0.0,
+            frame1: frame_b -> frame2: frame_a)
+        """
+        return NamedPose(self._frame2_from_frame1.inverse(), self.frame2, self.frame1)
+
+    def transform(self, points_in_frame1: Tensor) -> Tensor:
+        """Transform points from frame 1 to frame 2.
+
+        Args:
+            points_in_frame1: Points in frame 1.
+
+        Returns:
+            Tensor: Points in frame 2.
+
+        Example:
+            >>> b_from_a = NamedPose(Se3.identity(), frame1="frame_a", frame2="frame_b")
+            >>> b_from_a.transform(torch.tensor([1., 2., 3.]))
+            tensor([1., 2., 3.], grad_fn=<AddBackward0>)
+        """
+        return self._frame2_from_frame1 * points_in_frame1
---
+++ test/geometry/test_pose.py
@@ -0,0 +1,90 @@
+import pytest
+import torch
+
+from kornia.geometry import NamedPose
+from kornia.geometry.liegroup import So2, So3, Se2, Se3
+from kornia.testing import BaseTester
+
+
+class TestNamedPose(BaseTester):
+    def test_smoke(self, device, dtype):
+        b_from_a = Se3.identity(device=device, dtype=dtype)
+        pose = NamedPose(b_from_a, frame1="frame_a", frame2="frame_b")
+        assert isinstance(pose, NamedPose)
+        assert isinstance(pose.pose, Se3)
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_cardinality(self, device, dtype):
+        pass
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_jit(self, device, dtype):
+        pass
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_exception(self, device, dtype):
+        pass
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_module(self, device, dtype):
+        pass
+
+    @pytest.mark.skip(reason="not implemented yet")
+    def test_gradcheck(self, device):
+        pass
+
+    def test_mul(self, device, dtype):
+        b_from_a = NamedPose(Se3.trans_x(torch.tensor([1.0], device=device, dtype=dtype)), frame1="frame_a", frame2="frame_b")
+        c_from_b = NamedPose(Se3.trans_y(torch.tensor([1.0], device=device, dtype=dtype)), frame1="frame_b", frame2="frame_c")
+        c_from_a = b_from_a * c_from_b
+        assert isinstance(c_from_a, NamedPose)
+        assert isinstance(c_from_a.pose, Se3)
+        assert c_from_a.frame1 == "frame_a"
+        assert c_from_a.frame2 == "frame_c"
+
+    def test_fromRT(self, device, dtype):
+        b_from_a_rotation = So3.random(device=device, dtype=dtype)
+        b_from_a_translation = torch.tensor([1.0, 2.0, 3.0], device=device, dtype=dtype)
+        b_from_a = NamedPose.from_RT(b_from_a_rotation, b_from_a_translation, frame1="frame_a", frame2="frame_b")
+        assert isinstance(b_from_a, NamedPose)
+        assert isinstance(b_from_a.pose, Se3)
+
+        b_from_a_rotation = So2.random(device=device, dtype=dtype)
+        b_from_a_translation = torch.tensor([1.0, 2.0], device=device, dtype=dtype)
+        b_from_a = NamedPose.from_RT(b_from_a_rotation, b_from_a_translation, frame1="frame_a", frame2="frame_b")
+        assert isinstance(b_from_a, NamedPose)
+        assert isinstance(b_from_a.pose, Se2)
+
+        b_from_a_rotation = torch.eye(3, device=device, dtype=dtype)
+        b_from_a_translation = torch.tensor([1.0, 2.0, 3.0], device=device, dtype=dtype)
+        b_from_a = NamedPose.from_RT(b_from_a_rotation, b_from_a_translation, frame1="frame_a", frame2="frame_b")
+        assert isinstance(b_from_a, NamedPose)
+        assert isinstance(b_from_a.pose, Se3)
+
+        b_from_a_rotation = torch.eye(2, device=device, dtype=dtype)
+        b_from_a_translation = torch.tensor([1.0, 2.0], device=device, dtype=dtype)
+        b_from_a = NamedPose.from_RT(b_from_a_rotation, b_from_a_translation, frame1="frame_a", frame2="frame_b")
+        assert isinstance(b_from_a, NamedPose)
+        assert isinstance(b_from_a.pose, Se2)
+    
+    def test_inverse(self, device, dtype):
+        b_from_a = NamedPose(Se3.trans_x(torch.tensor([1.0], device=device, dtype=dtype)), frame1="frame_a", frame2="frame_b")
+        a_from_b = b_from_a.inverse()
+        assert isinstance(a_from_b, NamedPose)
+        assert isinstance(a_from_b.pose, Se3)
+        assert a_from_b.frame1 == "frame_b"
+        assert a_from_b.frame2 == "frame_a"
+
+    @pytest.mark.parametrize("batch_size", (None, 1, 2, 5))
+    def test_transform(self, device, dtype, batch_size):
+        if batch_size is None:
+            points_in_a = torch.randn(3, device=device, dtype=dtype)
+            b_from_a_se3 = Se3.trans_x(torch.tensor(1.0, device=device, dtype=dtype))
+        else:
+            points_in_a = torch.randn(batch_size, 3, device=device, dtype=dtype)
+            b_from_a_se3 = Se3.trans_x(torch.tensor([1.0], device=device, dtype=dtype))
+        b_from_a = NamedPose(b_from_a_se3, frame1="frame_a", frame2="frame_b")
+        a_from_b = b_from_a.inverse()
+        points_in_b = b_from_a.transform(points_in_a)
+        assert points_in_b.shape == points_in_a.shape
+        self.assert_close(a_from_b.transform(points_in_b), points_in_a)
