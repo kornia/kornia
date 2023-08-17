27043666ef3379a5e32411bbab2244ac501ecbd6
Christie Jacob <christeejacobs@gmail.com>
2023-08-17 13:53:45 +0530

    Update kornia/geometry/pose.py
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>

--- kornia/geometry/pose.py
+++ kornia/geometry/pose.py
@@ -113,7 +113,7 @@ class NamedPose:
 
         Args:
             rotation: Rotation part of the pose.
-            T : Translation part of the pose.
+            translation: Translation part of the pose.
             frame_src : Name of the source frame.
             frame_dst : Name of the destination frame.
 
