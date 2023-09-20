f94a178d6ba0750250e13e780489d7612f5e53f2
NielsPichon <n.pichon@therapanacea.eu>
2023-09-20 15:38:39 +0000

    fix: fix corbertura error

--- kornia/augmentation/_multiprocessing.py
+++ kornia/augmentation/_multiprocessing.py
@@ -6,7 +6,7 @@ class MultiprocessWrapper:
     context."""
 
     def __init__(self, *args, **kwargs) -> None:
-        args = [arg.clone() if isinstance(arg, torch.Tensor) else arg for arg in args]
+        args = (arg.clone() if isinstance(arg, torch.Tensor) else arg for arg in args)
         kwargs = {key: val.clone() if isinstance(val, torch.Tensor) else val for key, val in kwargs.items()}
 
         super().__init__(*args, **kwargs)
