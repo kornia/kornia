8d7308ff75a5dbe35a77eb8366693d6750271afb
Edgar Riba <edgar.riba@gmail.com>
2023-09-26 20:30:18 +0200

    factor out testing code (#2571)

--- tests/color/test_grayscale.py
+++ tests/color/test_grayscale.py
@@ -1,40 +1,14 @@
-import jax
-import keras_core as keras
-import numpy as np
-import pytest
-import tensorflow as tf
-import torch
-
 from kornia.color.gray import grayscale_from_rgb
+from testing.base_test import BaseTester
 
 
-# TODO: make this a fixture
-def genrate_image_data(channel_first: bool = False):
-    if channel_first:
-        shape = (3, 4, 5)
-    else:
-        shape = (4, 5, 3)
+class TestColorGray(BaseTester):
+    def test_channels_first(self):
+        image_rgb = self.generate_random_image_data(shape=(3, 4, 5))
+        image_gray = grayscale_from_rgb(image_rgb, channels_axis=0)
+        assert image_gray.shape == (1, 4, 5)
 
-    backend = keras.backend.backend()
-
-    if backend == "torch":
-        return torch.randint(0, 1, shape, dtype=torch.float32)
-    if backend == "numpy":
-        key = np.random.RandomState(42)
-        return key.uniform(size=shape).astype(np.float32)
-    if backend == "jax":
-        key = jax.random.PRNGKey(42)
-        return jax.random.uniform(key, shape).astype(jax.numpy.float32)
-    if backend == "tensorflow":
-        return tf.random.uniform(shape, dtype=tf.float32)
-
-    return None
-
-
-class TestColorGray:
-    @pytest.mark.parametrize("channel_first", [False, True])
-    def test_channels(self, channel_first):
-        image_rgb = genrate_image_data(channel_first)
-        channels_axis = -3 if channel_first else -1
-        image_gray = grayscale_from_rgb(image_rgb, channels_axis=channels_axis)
-        assert image_gray.shape == (1, 4, 5) if channel_first else (4, 5, 1)
+    def test_channels_last(self):
+        image_rgb = self.generate_random_image_data(shape=(4, 5, 3))
+        image_gray = grayscale_from_rgb(image_rgb, channels_axis=2)
+        assert image_gray.shape == (4, 5, 1)
