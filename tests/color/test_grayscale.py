3975ad1f41c3f023d438e0a8f062f13676528486
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2023-09-26 20:21:15 -0300

    cleanup

--- tests/color/test_grayscale.py
+++ tests/color/test_grayscale.py
@@ -8,8 +8,3 @@ class TestColorGray(BaseTester):
         image_gray = grayscale_from_rgb(image_rgb)
         expected_shape = self.load_shape(None, 4, 5, 1)
         assert image_gray.shape == expected_shape
-
-    # def test_channels_last(self):
-    #     image_rgb = self.generate_random_image(None, 4, 5)
-    #     image_gray = grayscale_from_rgb(image_rgb)
-    #     assert image_gray.shape == (4, 5, 1)
