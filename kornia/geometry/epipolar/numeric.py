2939d8a16932e840f5f7bf68b238fc997578618c
weitong <weitong8591@163.com>
2023-09-28 22:56:02 +0200

    Field list error fixed.

--- kornia/geometry/epipolar/numeric.py
+++ kornia/geometry/epipolar/numeric.py
@@ -27,7 +27,8 @@ def cross_product_matrix(x: torch.Tensor) -> torch.Tensor:
 
 # Reference
 # Nistér, David. An efficient solution to the five-point relative pose problem. 2004.
-# https://github.com/danini/graph-cut-ransac/blob/master/src/pygcransac/include/estimators/solver_essential_matrix_five_point_nister.h#L108
+# https://github.com/danini/graph-cut-ransac/blob/master/src/pygcransac/include/
+# estimators/solver_essential_matrix_five_point_nister.h#L108
 
 def o1(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     r"""Multiply two polynomials of degree one in x, y, z.    
@@ -35,28 +36,33 @@ def o1(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     Args:
         a, b are first order polys :math:`[x,y,z,1]`.
 
-    Returns:      
+    Returns:
         degree 2 poly with the order :math:`[ x^2, x*y, x*z, x, y^2, y*z, y, z^2, z, 1]`.
+    
     """
+
     return torch.stack([a[:, 0] * b[:, 0], a[:, 0] * b[:, 1] + a[:, 1] * b[:, 0], a[:, 0] * b[:, 2] + a[:, 2] * b[:, 0],
-                        a[:, 0] * b[:, 3] + a[:, 3] * b[:, 0], a[:, 1] * b[:, 1], a[:, 1] * b[:, 2] + a[:, 2] * b[:, 1],
-                        a[:, 1] * b[:, 3] + a[:, 3] * b[:, 1], a[:, 2] * b[:, 2], a[:, 2] * b[:, 3] + a[:, 3] * b[:, 2],
-                        a[:, 3] * b[:, 3]], dim=-1)
+        a[:, 0] * b[:, 3] + a[:, 3] * b[:, 0], a[:, 1] * b[:, 1], a[:, 1] * b[:, 2] + a[:, 2] * b[:, 1],
+        a[:, 1] * b[:, 3] + a[:, 3] * b[:, 1], a[:, 2] * b[:, 2], a[:, 2] * b[:, 3] + a[:, 3] * b[:, 2],
+        a[:, 3] * b[:, 3]], dim=-1)
 
 # Reference
 # Nistér, David. An efficient solution to the five-point relative pose problem. 2004.
-# https://github.com/danini/graph-cut-ransac/blob/aae1f40c2e10e31fd2191bac601c53a189673f60/src/pygcransac/include/estimators/solver_essential_matrix_five_point_nister.h#L156
+# https://github.com/danini/graph-cut-ransac/blob/aae1f40c2e10e31fd2191bac601c53a189673f60/src/pygcransac/
+# include/estimators/solver_essential_matrix_five_point_nister.h#L156
 
 def o2(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
     r"""Multiply two polynomials a and b of degrees two and one.
 
     Args:
-      a is second degree poly with order :math:`[ x^2, x*y, x*z, x, y^2, y*z, y, z^2, z, 1]`.
-      b is first degree with order :math:`[x y z 1]`.
+        a is second degree poly with order :math:`[ x^2, x*y, x*z, x, y^2, y*z, y, z^2, z, 1]`.
+        b is first degree with order :math:`[x y z 1]`.
     Returns:
-      a third degree poly with order,
-      :math:`[ x^3, y^3, x^2*y, x*y^2, x^2*z, x^2, y^2*z, y^2, x*y*z, x*y, x*z^2, x*z, x, y*z^2, y*z, y, z^3, z^2, z, 1]`.
+        a third degree poly with order,
+        :math:`[ x^3, y^3, x^2*y, x*y^2, x^2*z, x^2, y^2*z, y^2, x*y*z, x*y, x*z^2, x*z, x, y*z^2, y*z, y, z^3, z^2, z, 1]`.
+    
     """
+    
     return torch.stack([a[:, 0] * b[:, 0], a[:, 4] * b[:, 1], a[:, 0] * b[:, 1] + a[:, 1] * b[:, 0], a[:, 1] * b[:, 1] + a[:, 4] * b[:, 0], a[:, 0] * b[:, 2] + a[:, 2] * b[:, 0],
                         a[:, 0] * b[:, 3] + a[:, 3] * b[:, 0], a[:, 4] * b[:, 2] + a[:, 5] * b[:, 1], a[:, 4] * b[:, 3] + a[:, 6] * b[:, 1],
                         a[:, 1] * b[:, 2] + a[:, 2] * b[:, 1] + a[:, 5] * b[:, 0], a[:, 1] * b[:, 3] + a[:, 3] * b[:, 1] + a[:, 6] * b[:, 0],
@@ -67,14 +73,16 @@ def o2(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
 
 
 # Compute degree 10 poly representing determinant (equation 14 in the paper)
-# https://github.com/danini/graph-cut-ransac/blob/aae1f40c2e10e31fd2191bac601c53a189673f60/src/pygcransac/include/estimators/solver_essential_matrix_five_point_nister.h#L368C5-L368C82
+# https://github.com/danini/graph-cut-ransac/blob/aae1f40c2e10e31fd2191bac601c53a189673f60/src/pygcransac/
+# include/estimators/solver_essential_matrix_five_point_nister.h#L368C5-L368C82
 def det_to_poly(A: torch.Tensor) -> torch.Tensor:
     r"""represent the determinant by the 10th polynomial, used for 5PC solver.
 
     Args:
         A is in the shape of :math:`(*, 3, 13)`.
     Returns:
-        a degree 10 poly, representing determinant (equation 14 in the paper).    
+        a degree 10 poly, representing determinant (equation 14 in the paper).
+ 
     """
 
     cs = torch.zeros(A.shape[0], 11, device=A.device, dtype=A.dtype)
@@ -251,4 +259,3 @@ def det_to_poly(A: torch.Tensor) -> torch.Tensor:
     
 
     return cs
-    
