fix bucket off-by-one-error (#2582)

kornia/color/colormap.py
@@ -159,7 +159,7 @@ def apply_colormap(input_tensor: Tensor, colormap: ColorMap) -> Tensor:
     elif len(input_tensor.shape) == 3 and input_tensor.shape[0] == 1:  # if (1 X H x W)
         input_tensor = input_tensor[0, ...]  # (H x W)
 
-    keys = torch.arange(0, len(colormap), dtype=input_tensor.dtype, device=input_tensor.device)  # (num_colors)
+    keys = torch.arange(0, len(colormap) - 1, dtype=input_tensor.dtype, device=input_tensor.device)  # (num_colors)
 
     index = torch.bucketize(input_tensor, keys)  # shape equals <input_tensor>: (B x H x W) or (H x W)
 