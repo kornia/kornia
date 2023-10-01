970766acca5b810fed4bc818c409aca47e5dc235
yarkoslav <romanus004@gmail.com>
2023-10-01 11:40:33 +0300

    rewritten tests for the multi-head attention setup

--- test/contrib/test_contrib.py
+++ test/contrib/test_contrib.py
@@ -42,7 +42,7 @@ class TestDiamondSquare:
 class TestVisionTransformer:
     @pytest.mark.parametrize("B", [1, 2])
     @pytest.mark.parametrize("H", [1, 3, 8])
-    @pytest.mark.parametrize("D", [128, 768])
+    @pytest.mark.parametrize("D", [240, 768])
     @pytest.mark.parametrize("image_size", [32, 224])
     def test_smoke(self, device, dtype, B, H, D, image_size):
         patch_size = 16
@@ -60,13 +60,25 @@ class TestVisionTransformer:
         assert len(feats) == 12
         for f in feats:
             assert f.shape == (B, T, D)
+        
+    @pytest.mark.parametrize("B", [1, 2])
+    @pytest.mark.parametrize("H", [3, 8])
+    @pytest.mark.parametrize("D", [245, 1001])
+    @pytest.mark.parametrize("image_size", [32, 224])
+    def test_exception(self, device, dtype, B, H, D, image_size):
+        patch_size = 16
+        T = image_size**2 // patch_size**2 + 1  # tokens size
+
+        img = torch.rand(B, 3, image_size, image_size, device=device, dtype=dtype)
+        with pytest.raises(ValueError):
+            vit = kornia.contrib.VisionTransformer(image_size=image_size, num_heads=H, embed_dim=D).to(device, dtype)
 
     def test_backbone(self, device, dtype):
         def backbone_mock(x):
             return torch.ones(1, 128, 14, 14, device=device, dtype=dtype)
 
         img = torch.rand(1, 3, 32, 32, device=device, dtype=dtype)
-        vit = kornia.contrib.VisionTransformer(backbone=backbone_mock).to(device, dtype)
+        vit = kornia.contrib.VisionTransformer(backbone=backbone_mock, num_heads=8).to(device, dtype)
         out = vit(img)
         assert out.shape == (1, 197, 128)
 
