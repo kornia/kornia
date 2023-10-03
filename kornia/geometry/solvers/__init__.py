31690dbf7c4c7923cd1601a4efd01f90af9872c9
weitong <weitong8591@163.com>
2023-10-04 01:19:01 +0200

    fix the bug of doc refer

--- kornia/geometry/solvers/__init__.py
+++ kornia/geometry/solvers/__init__.py
@@ -9,7 +9,7 @@ from .polynomial_solver import (
 __all__ = [
     "solve_quadratic",
     "solve_cubic",
-    'multiply_deg_one_poly',
-    'multiply_deg_two_one_poly',
-    'determinant_to_polynomial',
+    "multiply_deg_one_poly",
+    "multiply_deg_two_one_poly",
+    "determinant_to_polynomial",
 ]
