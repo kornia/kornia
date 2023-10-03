4d89c646885740d4f994e61ade5557edbde95d58
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2023-10-03 10:50:36 +0200

    [feat] Add Average Endpoint Error to Metrics (#2500)
    * Fix support for (*, 3, H, W) tensors when converting from rgb to yuv420 or yuv422
    
    * Implement EPE metric
    
    * Implement class wrapper for EPE
    
    * Manage imports
    
    * Add EPE tests
    
    * Actually we're computing the average endpoint error
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add missing sqrt
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix equations
    
    * Add option for different reductions
    
    * Fix reduction
    
    * Update kornia/metrics/endpoint_error.py
    
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>
    
    * Add AEPE to docs
    
    * Add additional reduction tests and test error cases
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update kornia/metrics/endpoint_error.py
    
    Co-authored-by: João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
    
    * Summarize exception tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add test to check perfect fit case
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix example in doc string
    
    * Fix results in doc string example
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Edgar Riba <edgar.riba@gmail.com>
    Co-authored-by: João Gustavo A. Amorim <joaogustavoamorim@gmail.com>

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -196,6 +196,55 @@ class TestPsnr:
         assert_close(actual, expected)
 
 
+class TestAepe:
+    def test_metric_mean_reduction(self, device, dtype):
+        sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+        expected = torch.tensor(0.565685424, device=device, dtype=dtype)
+        actual = kornia.metrics.aepe(sample, 1.4 * sample, reduction="mean")
+        assert_close(actual, expected)
+
+    def test_metric_sum_reduction(self, device, dtype):
+        sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+        expected = torch.tensor(1.4142, device=device, dtype=dtype) * 4**2
+        actual = kornia.metrics.aepe(sample, 2.0 * sample, reduction="sum")
+        assert_close(actual, expected)
+
+    def test_metric_no_reduction(self, device, dtype):
+        sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+        expected = torch.zeros(4, 4, device=device, dtype=dtype) + 1.4142
+        actual = kornia.metrics.aepe(sample, 2.0 * sample, reduction="none")
+        assert_close(actual, expected)
+
+    def test_perfect_fit(self, device, dtype):
+        sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+        expected = torch.zeros(4, 4, device=device, dtype=dtype)
+        actual = kornia.metrics.aepe(sample, sample, reduction="none")
+        assert_close(actual, expected)
+
+    def test_exception(self, device, dtype):
+        with pytest.raises(TypeError) as errinfo:
+            criterion = kornia.metrics.AEPE()
+            criterion(None, torch.ones(4, 4, 2, device=device, dtype=dtype))
+        assert "Not a Tensor type. Got" in str(errinfo)
+
+        with pytest.raises(NotImplementedError) as errinfo:
+            sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+            _ = kornia.metrics.aepe(sample, 2.0 * sample, reduction="foo")
+        assert "Invalid reduction option." in str(errinfo)
+
+        with pytest.raises(Exception) as errinfo:
+            sample = torch.ones(4, 4, 2, device=device, dtype=dtype)
+            _ = kornia.metrics.aepe(sample, 2.0 * sample[..., 0], reduction="mean")
+        assert "shape must be [['*', '2']]. Got" in str(errinfo)
+
+    def test_smoke(self, device, dtype):
+        input = torch.rand(3, 3, 2, device=device, dtype=dtype)
+        target = torch.rand(3, 3, 2, device=device, dtype=dtype)
+
+        criterion = kornia.metrics.AEPE()
+        assert criterion(input, target) is not None
+
+
 class TestMeanAveragePrecision:
     def test_smoke(self, device, dtype):
         boxes = torch.tensor([[100, 50, 150, 100.0]], device=device, dtype=dtype)
