3d3775f0458a531d9173f27ce9784a7dbfc5cacf
edgar <edgar.riba@gmail.com>
2023-10-11 10:43:51 +0200

    add futures

--- kornia/contrib/models/efficient_vit/backbone.py
+++ kornia/contrib/models/efficient_vit/backbone.py
@@ -1,6 +1,7 @@
 # EfficientViT: Multi-Scale Linear Attention for High-Resolution Dense Prediction
 # Han Cai, Junyan Li, Muyan Hu, Chuang Gan, Song Han
 # International Conference on Computer Vision (ICCV), 2023
+from __future__ import annotations
 
 import torch
 from torch import nn
