d27111ea710bde7d074156f64a4ff66a40516a7b
Anastasiia <anastasiia.petrovych@ucu.edu.ua>
2023-10-13 01:04:54 +0300

    Update random_rain.py

--- kornia/augmentation/random_generator/_2d/random_rain.py
+++ kornia/augmentation/random_generator/_2d/random_rain.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
-
 import math
 import random
-
 import torch
-
 from kornia.augmentation.random_generator.base import RandomGeneratorBase, UniformDistribution
 from kornia.augmentation.utils import _adapted_rsampling, _common_param_check, _range_bound
 from kornia.core import Tensor
@@ -78,7 +75,7 @@ class RainGenerator(RandomGeneratorBase):
             'drop_width_factor': drop_width_factor,
         }
 
-   def random_rain_augmentation(
+    def random_rain_augmentation(
         self,image_val: torch.Tensor,num_raindrops: int = 100,min_length: int = 5,max_length: int = 15, color: torch.Tensor = None)-> torch.Tensor:
         """Apply random rain augmentation to the input image.
         Args:
