97f7cb63baf51235e27b8aad97b2cd185f63c46d
alorthius <alinium3012@gmail.com>
2023-10-31 18:58:01 +0200

    rename input_tensor variables to image

--- kornia/metrics/psnr.py
+++ kornia/metrics/psnr.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn.functional import mse_loss as mse
 
 
-def psnr(input_tensor: torch.Tensor, target: torch.Tensor, max_val: float) -> torch.Tensor:
+def psnr(image: torch.Tensor, target: torch.Tensor, max_val: float) -> torch.Tensor:
     r"""Create a function that calculates the PSNR between 2 images.
 
     PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
@@ -22,7 +22,7 @@ def psnr(input_tensor: torch.Tensor, target: torch.Tensor, max_val: float) -> to
     (e.g for floating point images :math:`\text{MAX}_I=1`).
 
     Args:
-        input_tensor: the input image with arbitrary shape :math:`(*)`.
+        image: the input image with arbitrary shape :math:`(*)`.
         labels: the labels image with arbitrary shape :math:`(*)`.
         max_val: The maximum value in the input tensor.
 
@@ -37,13 +37,13 @@ def psnr(input_tensor: torch.Tensor, target: torch.Tensor, max_val: float) -> to
     Reference:
         https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio#Definition
     """
-    if not isinstance(input_tensor, torch.Tensor):
-        raise TypeError(f"Expected torch.Tensor but got {type(input_tensor)}.")
+    if not isinstance(image, torch.Tensor):
+        raise TypeError(f"Expected torch.Tensor but got {type(image)}.")
 
     if not isinstance(target, torch.Tensor):
         raise TypeError(f"Expected torch.Tensor but got {type(target)}.")
 
-    if input_tensor.shape != target.shape:
-        raise TypeError(f"Expected tensors of equal shapes, but got {input_tensor.shape} and {target.shape}")
+    if image.shape != target.shape:
+        raise TypeError(f"Expected tensors of equal shapes, but got {image.shape} and {target.shape}")
 
-    return 10.0 * torch.log10(max_val**2 / mse(input_tensor, target, reduction='mean'))
+    return 10.0 * torch.log10(max_val**2 / mse(image, target, reduction='mean'))
