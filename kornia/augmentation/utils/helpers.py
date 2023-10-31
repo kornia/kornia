b0dda96beef6c977b8fb1c6213546911324ec5ae
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2023-10-31 07:32:53 -0300

    Make use of composition for augmentation multiprocessing friendliness (#2637)
    Co-authored-by: NielsPichon <n.pichon@therapanacea.eu>
    Co-authored-by: NielsPichon <niels.pichon@outlook.com>

--- kornia/augmentation/utils/helpers.py
+++ kornia/augmentation/utils/helpers.py
@@ -380,3 +380,14 @@ def preprocess_classes(input: Tensor) -> Tensor:
     """Preprocess input class tags."""
     # TODO: We may allow list here.
     return input
+
+
+class MultiprocessWrapper:
+    """Utility class which when used as a base class, makes the class work with the 'spawn' multiprocessing
+    context."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        args = tuple(arg.clone() if isinstance(arg, torch.Tensor) else arg for arg in args)
+        kwargs = {key: val.clone() if isinstance(val, torch.Tensor) else val for key, val in kwargs.items()}
+
+        super().__init__(*args, **kwargs)
