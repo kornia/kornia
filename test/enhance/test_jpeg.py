e2bc77ed3d45181edf16b953b00b06dd8de96243
ChristophReich1996 <christophreich@gmx.net>
2024-01-23 05:03:09 +0100

    Use device and dtype parameters in tests

--- test/enhance/test_jpeg.py
+++ test/enhance/test_jpeg.py
@@ -9,8 +9,8 @@ class TestDiffJPEG(BaseTester):
     def test_smoke(self, device, dtype) -> None:
         """This test standard usage."""
         B, H, W = 2, 32, 32
-        img = torch.rand(B, 3, H, W)
-        jpeg_quality = torch.randint(low=0, high=100, size=(B,))
+        img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+        jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
         img_jpeg = kornia.enhance.diff_jpeg(img, jpeg_quality)
         assert img_jpeg is not None
         assert img_jpeg.shape == img.shape
@@ -18,10 +18,10 @@ class TestDiffJPEG(BaseTester):
     def test_custom_qt(self, device, dtype) -> None:
         """Here we test if we can handle custom quantization tables."""
         B, H, W = 4, 32, 32
-        img = torch.rand(B, 3, H, W)
-        jpeg_quality = torch.randint(low=0, high=100, size=(B,))
-        qt_y = torch.randint(low=1, high=255, size=(B, 8, 8))
-        qt_c = torch.randint(low=1, high=255, size=(B, 8, 8))
+        img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+        jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
+        qt_y = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
+        qt_c = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
         img_jpeg = kornia.enhance.diff_jpeg(img, jpeg_quality, qt_y, qt_c)
         assert img_jpeg is not None
         assert img_jpeg.shape == img.shape
@@ -29,10 +29,10 @@ class TestDiffJPEG(BaseTester):
     def test_non_batch_param(self, device, dtype) -> None:
         """Here we test if we can handle non-batched JPEG parameters (JPEG quality and QT's)."""
         B, H, W = 3, 32, 32
-        img = torch.rand(B, 3, H, W)
-        jpeg_quality = torch.randint(low=0, high=100, size=(1,))
-        qt_y = torch.randint(low=1, high=255, size=(1, 8, 8))
-        qt_c = torch.randint(low=1, high=255, size=(1, 8, 8))
+        img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+        jpeg_quality = torch.randint(low=0, high=100, size=(1,), device=device, dtype=dtype)
+        qt_y = torch.randint(low=1, high=255, size=(1, 8, 8), device=device, dtype=dtype)
+        qt_c = torch.randint(low=1, high=255, size=(1, 8, 8), device=device, dtype=dtype)
         img_jpeg = kornia.enhance.diff_jpeg(img, jpeg_quality, qt_y, qt_c)
         assert img_jpeg is not None
         assert img_jpeg.shape == img.shape
@@ -41,53 +41,53 @@ class TestDiffJPEG(BaseTester):
         """Test exeptions (non-tensor input, wrong JPEG quality shape, wrong img shape, and wrong QT shape.)"""
         with pytest.raises(TypeError) as errinfo:
             B = 2
-            jpeg_quality = torch.randint(low=0, high=100, size=(B,))
+            jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(1904.0, jpeg_quality)
         assert "Not a Tensor type. Got" in str(errinfo)
 
         with pytest.raises(TypeError) as errinfo:
             B, H, W = 2, 32, 32
-            img = torch.rand(B, 3, H, W)
+            img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(img, None)
         assert "Not a Tensor type. Got" in str(errinfo)
 
         with pytest.raises(TypeError) as errinfo:
             B, H, W = 2, 32, 32
-            img = torch.rand(B, 3, H, W)
-            jpeg_quality = torch.randint(low=0, high=100, size=(B, 3, 2, 1))
+            img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+            jpeg_quality = torch.randint(low=0, high=100, size=(B, 3, 2, 1), device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(img, jpeg_quality)
         assert "shape must be [" in str(errinfo)
 
         with pytest.raises(Exception) as errinfo:
             B, H, W = 2, 31, 31
-            img = torch.rand(B, 3, H, W)
-            jpeg_quality = torch.randint(low=0, high=100, size=(B,))
+            img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+            jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(img, jpeg_quality)
         assert "divisible" in str(errinfo)
 
         with pytest.raises(Exception) as errinfo:
             B, H, W = 4, 32, 32
-            img = torch.rand(B, 3, H, W)
-            jpeg_quality = torch.randint(low=0, high=100, size=(B,))
-            qt_y = torch.randint(low=1, high=255, size=(B, 7, 8))
-            qt_c = torch.randint(low=1, high=255, size=(B, 8, 8))
+            img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+            jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
+            qt_y = torch.randint(low=1, high=255, size=(B, 7, 8), device=device, dtype=dtype)
+            qt_c = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(img, jpeg_quality, qt_y, qt_c)
         assert "shape must be [" in str(errinfo)
 
         with pytest.raises(Exception) as errinfo:
             B, H, W = 4, 32, 32
-            img = torch.rand(B, 3, H, W)
-            jpeg_quality = torch.randint(low=0, high=100, size=(B,))
-            qt_y = torch.randint(low=1, high=255, size=(B, 8, 8))
-            qt_c = torch.randint(low=1, high=255, size=(B, 8, 7))
+            img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+            jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
+            qt_y = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
+            qt_c = torch.randint(low=1, high=255, size=(B, 8, 7), device=device, dtype=dtype)
             kornia.enhance.diff_jpeg(img, jpeg_quality, qt_y, qt_c)
         assert "shape must be [" in str(errinfo)
 
     def test_cardinality(self, device, dtype) -> None:
         B, H, W = 1, 16, 16
-        img = torch.zeros(B, 3, H, W)
+        img = torch.zeros(B, 3, H, W, device=device, dtype=dtype)
         img[..., 4:-4, 4:-4] = 1.0
-        jpeg_quality = torch.tensor([2.0])
+        jpeg_quality = torch.tensor([2.0], device=device, dtype=dtype)
         img_jpeg = kornia.enhance.diff_jpeg(img, jpeg_quality)
         # Numbers generated based on reference implementation
         img_jpeg_ref = torch.tensor(
@@ -964,17 +964,19 @@ class TestDiffJPEG(BaseTester):
                         ],
                     ],
                 ]
-            ], dtype=dtype
+            ],
+            device=device,
+            dtype=dtype,
         )
         # We use a slightly higher tolerance since our implementation varies from the reference implementation
         assert_close(img_jpeg[..., :4, :4], img_jpeg_ref[..., :4, :4], rtol=0.01, atol=0.01)
 
     def test_module(self, device, dtype) -> None:
         B, H, W = 4, 16, 16
-        img = torch.rand(B, 3, H, W)
-        jpeg_quality = torch.randint(low=0, high=100, size=(B,))
-        qt_y = torch.randint(low=1, high=255, size=(B, 8, 8))
-        qt_c = torch.randint(low=1, high=255, size=(B, 8, 8))
+        img = torch.rand(B, 3, H, W, device=device, dtype=dtype)
+        jpeg_quality = torch.randint(low=0, high=100, size=(B,), device=device, dtype=dtype)
+        qt_y = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
+        qt_c = torch.randint(low=1, high=255, size=(B, 8, 8), device=device, dtype=dtype)
         diff_jpeg_module = kornia.enhance.DiffJPEG()
         img_jpeg = diff_jpeg_module(img, jpeg_quality, qt_y, qt_c)
         assert img_jpeg is not None
