9577aef5db043ab2ba399588e1f2f7461690691b
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-02-04 10:02:37 -0300

    Introduces `benchmarks/` (#2777)

---
+++ benchmarks/color/gray_test.py
@@ -0,0 +1,30 @@
+import pytest
+import torch
+
+from kornia.color import grayscale_to_rgb
+
+
+@pytest.mark.parametrize("B", [1, 5])
+@pytest.mark.parametrize("C", [1])
+@pytest.mark.parametrize("H", [128, 237, 512])
+@pytest.mark.parametrize("W", [128, 237, 512])
+def test_grayscale_to_rgb(benchmark, device, dtype, B, C, H, W):
+    data = torch.rand(B, C, H, W, device=device, dtype=dtype)
+
+    actual = benchmark(grayscale_to_rgb, image=data)
+
+    assert actual.shape == (B, 3, H, W)
+
+
+@pytest.mark.parametrize("B", [1, 5])
+@pytest.mark.parametrize("C", [1])
+@pytest.mark.parametrize("H", [128, 237, 512])
+@pytest.mark.parametrize("W", [128, 237, 512])
+def test_grayscale_to_rgb_compile(benchmark, device, dtype, torch_optimizer, B, C, H, W):
+    data = torch.rand(B, C, H, W, device=device, dtype=dtype)
+
+    op = torch_optimizer(grayscale_to_rgb)
+
+    actual = benchmark(op, image=data)
+
+    assert actual.shape == (B, 3, H, W)
