cb01633b8d807c2b97e83f82c9f1c415fcdb2f51
Thien Tran <gau.nernst@yahoo.com.sg>
2024-02-07 17:24:25 +0700

    remove test variants due to timeout

--- tests/contrib/test_contrib.py
+++ tests/contrib/test_contrib.py
@@ -76,7 +76,7 @@ class TestVisionTransformer(BaseTester):
         out = vit(img)
         assert out.shape == (1, 197, 128)
 
-    @pytest.mark.parametrize("variant", ["vit_ti/16", "vit_s/32", "vit_b/16", "vit_l/32"])
+    @pytest.mark.parametrize("variant", ["vit_ti/16", "vit_b/16"])
     def test_from_config(self, variant):
         model = kornia.contrib.VisionTransformer.from_config(variant)
         assert isinstance(model, kornia.contrib.VisionTransformer)
