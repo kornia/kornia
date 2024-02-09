265211b774d5864098c740527f4594be103f1181
Edgar Riba <edgar.riba@gmail.com>
2024-02-09 01:28:52 +0100

    Fix robots for web crawlers (#2790)
    ---------
    
    Co-authored-by: João Gustavo A. Amorim <joaogustavoamorim@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,7 +154,7 @@ html_favicon = "_static/img/kornia_logo_favicon.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_extra_path = ["_extra"]
+html_extra_path = ["_extra/robots.txt"]
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "Kornia"
