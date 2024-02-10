137fbbb877367c5854d808f6adddbdda41f810e5
Thien Tran <gau.nernst@yahoo.com.sg>
2024-02-10 09:42:43 +0700

    fix url

--- kornia/contrib/vit.py
+++ kornia/contrib/vit.py
@@ -246,10 +246,13 @@ class VisionTransformer(Module):
         E.g. `vit_b/16` means ViT-Base, patch size 16x16. If `pretrained=True`, AugReg weights are loaded.
         The weights are hosted on HuggingFace's model hub: https://huggingface.co/kornia.
 
+        .. note::
+            The available weights are: "vit_l/16", "vit_b/16", "vit_s/16", "vit_ti/16", "vit_b/32", "vit_s/32"
+
         Args:
             variant: ViT model variant e.g. `vit_b/16`.
             pretrained: whether to load pre-trained AugReg weights.
-            kwargs: other arguments that will be passed to :func:`kornia.contrib.vit.VisionTransformer.__init__`.
+            kwargs: other keyword arguments that will be passed to :func:`kornia.contrib.vit.VisionTransformer.__init__`.
         Returns:
             The respective ViT model
 
@@ -286,4 +289,4 @@ def _get_weight_url(variant: str) -> str:
     """Return the URL of the model weights."""
     KORNIA_CHECK(variant in _AVAILABLE_WEIGHTS, f"Variant {variant} does not have pre-trained checkpoint")
     model_type, patch_size = variant.split("/")
-    return f"https://huggingface.co/kornia/vit_{model_type}{patch_size}_augreg_i21k_r224/resolve/main/vit_{model_type}-{patch_size}.pth"
+    return f"https://huggingface.co/kornia/{model_type}{patch_size}_augreg_i21k_r224/resolve/main/{model_type}-{patch_size}.pth"
