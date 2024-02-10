19290b62c357e1c36e9440a2b8b0cbfea792577c
Thien Tran <gau.nernst@yahoo.com.sg>
2024-02-10 10:26:04 +0700

    remove large model due to timeout

--- tests/contrib/test_contrib.py
+++ tests/contrib/test_contrib.py
@@ -76,7 +76,7 @@ class TestVisionTransformer(BaseTester):
         out = vit(img)
         assert out.shape == (1, 197, 128)
 
-    @pytest.mark.parametrize("variant", ["vit_ti/16", "vit_b/16", "vit_l/16"])
+    @pytest.mark.parametrize("variant", ["vit_ti/16", "vit_b/16"])
     def test_from_config(self, variant):
         model = kornia.contrib.VisionTransformer.from_config(variant)
         assert isinstance(model, kornia.contrib.VisionTransformer)
