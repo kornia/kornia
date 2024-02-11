ee003a97499d66a6230ab5248993a00d9a1a0568
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-02-10 22:30:10 -0300

    Move to BaseTester

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -5,31 +5,31 @@ from kornia.utils.misc import (
     differentiable_polynomial_rounding,
 )
 
-from testing.base import assert_close
+from testing.base import BaseTester
 
 
-class TestDifferentiableClipping:
+class TestDifferentiableClipping(BaseTester):
     def test_differentiable_clipping(self, device):
         x = tensor([1.0, 6.0, 10.0, 12.0], device=device)
         y = differentiable_clipping(x, min=5.0, max=10.0)
         y_expected = tensor([4.9804, 6.0, 10.0, 10.0173], device=device)
 
-        assert_close(y, y_expected)
+        self.assert_close(y, y_expected)
 
 
-class TestDifferentiablePolynomialRounding:
+class TestDifferentiablePolynomialRounding(BaseTester):
     def test_differentiable_polynomial_rounding(self, device):
         x = tensor([1.5], device=device)
         y = differentiable_polynomial_rounding(x)
         y_expected = tensor([1.875], device=device)
 
-        assert_close(y, y_expected)
+        self.assert_close(y, y_expected)
 
 
-class TestDifferentiablePolynomialFloor:
+class TestDifferentiablePolynomialFloor(BaseTester):
     def test_differentiable_polynomial_floor(self, device):
         x = tensor([1.5], device=device)
         y = differentiable_polynomial_floor(x)
         y_expected = tensor([1.0], device=device)
 
-        assert_close(y, y_expected)
+        self.assert_close(y, y_expected)
