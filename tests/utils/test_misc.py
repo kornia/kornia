92f009620b6b04a7a15f0faba47d3d832e6572c4
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-02-10 22:27:38 -0300

    Update tests cases

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -1,35 +1,35 @@
 import torch
 
-from kornia.core import Tensor
+from kornia.core import tensor
 from kornia.utils.misc import (
     differentiable_clipping,
     differentiable_polynomial_floor,
     differentiable_polynomial_rounding,
 )
-
+from testing.base import assert_close
 
 class TestDifferentiableClipping:
     def test_differentiable_clipping(self, device):
-        x = Tensor([1.0])
-        y = differentiable_clipping(x, min=10.0)
-        y_expected = Tensor([9.9800])
+        x = tensor([1.0, 6.0, 10.0, 12.0], device=device)
+        y = differentiable_clipping(x, min=5.0, max=10.0)
+        y_expected = tensor([4.9804, 6.0, 10.0, 10.0173], device=device)
 
-        assert (y, y_expected)
+        assert_close(y, y_expected)
 
 
 class TestDifferentiablePolynomialRounding:
     def test_differentiable_polynomial_rounding(self, device):
-        x = Tensor([1.0])
+        x = tensor([1.5], device=device)
         y = differentiable_polynomial_rounding(x)
-        y_expected = torch.round(x) + (x - torch.round(x)) ** 3
+        y_expected = tensor([1.875], device=device)
 
-        assert (y, y_expected)
+        assert_close(y, y_expected)
 
 
 class TestDifferentiablePolynomialFloor:
     def test_differentiable_polynomial_floor(self, device):
-        x = Tensor([1.0])
+        x = tensor([1.5], device=device)
         y = differentiable_polynomial_floor(x)
-        y_expected = torch.floor(x) + (x - 0.5 - torch.floor(x)) ** 3
+        y_expected = tensor([1.0], device=device)
 
-        assert (y, y_expected)
+        assert_close(y, y_expected)
