c7802d66e6b9c37e0ab534996324bbc716096347
machineko <machineko@protonmail.com>
2024-03-01 10:41:38 +0000

    fix E501 Line too long (124 > 120)

--- kornia/x/callbacks.py
+++ kornia/x/callbacks.py
@@ -118,7 +118,8 @@ class ModelCheckpoint:
     """
 
     def __init__(
-        self, filepath: str, monitor: str, filename_fcn: Optional[Callable[..., str]] = None, max_mode: bool = False
+        self, filepath: str, monitor: str, filename_fcn: Optional[Callable[..., str]] = None,
+        max_mode: bool = False
     ) -> None:
         self.filepath = filepath
         self.monitor = monitor
