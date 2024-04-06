3359120a9847b568109d0cec90e7c729ce10c05a
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-04-06 16:02:24 -0300

    Delete manual `robots.txt` from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -155,7 +155,7 @@ html_favicon = "_static/img/kornia_logo_favicon.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_extra_path = ["_extra/robots.txt"]
+html_extra_path = []
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "Kornia"
