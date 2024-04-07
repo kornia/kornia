22c40fbfbf74160e98c6d1d6c42f90d915d6e7f2
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-04-07 08:15:47 -0300

    Delete manual `robots.txt` from docs (#2869)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -161,7 +161,7 @@ gtagjs_ids = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_extra_path = ["_extra/robots.txt"]
+html_extra_path = []
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "Kornia"
