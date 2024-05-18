73c6992308a67715fe9a1282e217f50d74ac5db5
João Gustavo A. Amorim <joaogustavoamorim@gmail.com>
2024-05-18 11:45:28 -0300

    chore: skip specific dynamo tests for torch 2.3.0
    - Report in https://github.com/pytorch/pytorch/issues/126619

--- tests/filters/test_in_range.py
+++ tests/filters/test_in_range.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 
 from kornia.filters import InRange, in_range
+from kornia.utils._compat import torch_version
 
 from testing.base import BaseTester, assert_close
 
@@ -118,6 +119,8 @@ class TestInRange(BaseTester):
 
     @pytest.mark.parametrize("batch_size", [1, 2])
     def test_dynamo(self, batch_size, device, dtype, torch_optimizer):
+        if device == torch.device("cpu") and torch_version() == "2.3.0":
+            pytest.skip("Failing to compile on CPU see pytorch/pytorch#126619")
         inpt = torch.rand(batch_size, 3, 5, 5, device=device, dtype=dtype)
         op = InRange(lower=(0.2, 0.2, 0.2), upper=(0.6, 0.6, 0.6), return_mask=True)
         op_optimized = torch_optimizer(op)
