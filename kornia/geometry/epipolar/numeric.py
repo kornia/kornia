e4370f41c287443ebee4c12afb5d90e696e66012
weitong <weitong8591@163.com>
2024-05-30 15:54:20 +0200

    fix the precommit errors

--- kornia/geometry/epipolar/numeric.py
+++ kornia/geometry/epipolar/numeric.py
@@ -34,17 +34,14 @@ def matrix_cofactor_tensor(matrix: torch.Tensor) -> torch.Tensor:
     Args:
         matrix: The input matrix in the shape :math:`(*, 3, 3)`.
     """
-    try:
-        det = torch.det(matrix)
-        singular_mask = det != 0
-        if singular_mask.sum() != 0:
-            # B, 3, 3
-            cofactor = torch.linalg.inv(matrix[singular_mask]).transpose(-2, -1) * det[:, None, None]
-            # return cofactor matrix of the given matrix
-            returned_cofactor = torch.zeros_like(matrix)
-            returned_cofactor[singular_mask] = cofactor
-            return returned_cofactor
-        else:
-            raise Exception("all singular matrices")
-    except Exception as e:
-        print("could not find cofactor matrix due to", e)
+    det = torch.det(matrix)
+    singular_mask = det != 0
+    if singular_mask.sum() != 0:
+        # B, 3, 3
+        cofactor = torch.linalg.inv(matrix[singular_mask]).transpose(-2, -1) * det[:, None, None]
+        # return cofactor matrix of the given matrix
+        returned_cofactor = torch.zeros_like(matrix)
+        returned_cofactor[singular_mask] = cofactor
+        return returned_cofactor
+    else:
+        raise Exception("all singular matrices")
