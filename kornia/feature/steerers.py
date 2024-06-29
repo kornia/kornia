e0d7e225f99cafac0002af45e815a8eee26c76c9
Georg Bökman <georgbokman@outlook.com>
2024-06-29 17:34:33 +0200

    Change default steerer_order from None to 8

--- kornia/feature/steerers.py
+++ kornia/feature/steerers.py
@@ -37,7 +37,7 @@ class DiscreteSteerer(Module):
     def from_pretrained(
         cls,
         generator_weights: str = "G-C4",
-        steerer_order: Optional[int] = None,
+        steerer_order: Optional[int] = 8,
     ) -> Module:
         r"""Loads a pretrained DeDoDe steerer from the paper https://arxiv.org/abs/2312.02152.
 
