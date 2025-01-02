617d80447eee539c2daaa10735bf230d10266ec4
João G. Atkinson A. <20444345+johnnv1@users.noreply.github.com>
2025-01-02 10:10:30 -0300

    chore (pre-commit): hook to add license header (#3094)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/geometry/camera/__init__.py
+++ kornia/geometry/camera/__init__.py
@@ -1,3 +1,20 @@
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright 2018 Kornia Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from .distortion_affine import distort_points_affine, dx_distort_points_affine, undistort_points_affine
 from .distortion_kannala_brandt import (
     distort_points_kannala_brandt,
