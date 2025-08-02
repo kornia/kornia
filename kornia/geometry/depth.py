4d4ac7910856edbe562ee0405dc054ea598d5312
Meggison Oritsemisan <meggison92@gmail.com>
2025-08-02 22:51:07 +0100

    Added None type Error Handling

--- kornia/geometry/depth.py
+++ kornia/geometry/depth.py
@@ -397,6 +397,10 @@ class DepthWarper(Module):
         Szeliski, Richard, and Daniel Scharstein. "Symmetric sub-pixel stereo matching." European Conference on Computer
         Vision. Springer Berlin Heidelberg, 2002.
         """
+        # Add None check first
+        if self._dst_proj_src is None:
+            raise ValueError("Please, call compute_projection_matrix.")
+        
         delta_d = 0.01
         center_x = self.width / 2
         center_y = self.height / 2
