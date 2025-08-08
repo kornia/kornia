0c98eb45e69de883fe2ff77f56e920efe72d2154
Sidd <85678601+sidd-27@users.noreply.github.com>
2025-08-09 01:22:05 +0530

    optimized strip_hash

--- kornia/utils/image_print.py
+++ kornia/utils/image_print.py
@@ -303,13 +303,8 @@ RGB2SHORT_DICT = {v: k for k, v in SHORT2RGB_DICT.items()}
 def _str2hex(hexstr: str) -> int:
     return int(hexstr, 16)
 
-
 def _strip_hash(rgb: str) -> str:
-    # Strip leading `#` if exists.
-    if rgb.startswith("#"):
-        rgb = rgb.lstrip("#")
-    return rgb
-
+    return rgb.removeprefix("#")
 
 def short2rgb(short: str) -> str:
     """Convert short to RGB code."""
