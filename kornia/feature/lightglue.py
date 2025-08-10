9b7e0d3657f533162934c889c9f0f2ae361eed45
Sidd <85678601+sidd-27@users.noreply.github.com>
2025-08-10 13:21:18 +0530

    removed unncessary clamping
    the clamping is unnecessary for all values of layer index and conf layers which are positive, as it does nothing

--- kornia/feature/lightglue.py
+++ kornia/feature/lightglue.py
@@ -671,9 +671,8 @@ class LightGlue(Module):
 
     def confidence_threshold(self, layer_index: int) -> float:
         """Scaled confidence threshold."""
-        threshold = 0.8 + 0.1 * math.exp(-4.0 * layer_index / self.conf.n_layers)
-        return min(max(threshold, 0), 1)
-
+        return 0.8 + 0.1 * math.exp(-4.0 * layer_index / self.conf.n_layers)
+    
     def get_pruning_mask(self, confidences: Tensor, scores: Tensor, layer_index: int) -> Tensor:
         """Mask points which should be removed."""
         keep = scores > (1 - self.conf.width_confidence)
