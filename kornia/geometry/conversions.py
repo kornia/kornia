a8fce8af26a8add6a63cda1e67374548c175f680
Sidd <85678601+sidd-27@users.noreply.github.com>
2025-09-19 16:22:22 +0530

    fixed doctest

--- kornia/geometry/conversions.py
+++ kornia/geometry/conversions.py
@@ -300,7 +300,7 @@ def axis_angle_to_rotation_matrix(axis_angle: Tensor) -> Tensor:
 
     Example:
         >>> input = tensor([[0., 0., 0.]])
-        >>> axis_angle_to_rotation_matrix(input)
+        >>> axis_angle_to_rotation_matrix(input).round()
         tensor([[[1., 0., 0.],
                  [0., 1., 0.],
                  [0., 0., 1.]]])
