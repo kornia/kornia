15c8327eb22af8a5ce611dfb8106985ca3d9fa35
Soumita Das <dassoumita065@gmail.com>
2025-12-22 19:41:57 +0530

    Docs: add module docstrings(D100) for sample and draw utilities (#3390)
    * Docs: add module docstrings for sample and draw utilities
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kornia/utils/draw.py
+++ kornia/utils/draw.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""Utilities for drawing on images."""
 
 from typing import List, Optional, Tuple, Union
 
--- kornia/utils/sample.py
+++ kornia/utils/sample.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""Utilities for downloading and loading sample images."""
 
 import logging
 import os
