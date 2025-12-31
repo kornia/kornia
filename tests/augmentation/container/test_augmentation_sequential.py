7918f953c30901e5328d20b820ec2f4e32aa3229
mathurojus <mathurojus17@gmail.com>
2025-12-31 13:08:08 +0530

    fix(augmentation): ensure consistent interpolation in test_random_crops

--- tests/augmentation/container/test_augmentation_sequential.py
+++ tests/augmentation/container/test_augmentation_sequential.py
@@ -215,7 +215,7 @@ class TestAugmentationSequential:
         points = torch.tensor([[[0.0, 0.0], [1.0, 1.0]]], device=device, dtype=dtype).expand(3, -1, -1)
         aug = K.AugmentationSequential(
             K.RandomCrop((3, 3), padding=1, cropping_mode="resample", fill=0),
-            K.RandomAffine((360.0, 360.0), p=1.0),
+            K.RandomAffine((360.0, 360.0), resample="nearest", p=1.0),
             data_keys=["input", "mask", "bbox_xyxy", "keypoints"],
             extra_args={},
         )
