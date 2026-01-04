af88b57d42ab2295f59db5d2f818b49713165c8d
Edgar Riba <edgar.riba@gmail.com>
2026-01-04 09:05:31 +0100

    Update batch_size parameterization in smoke test
    Reduced the batch sizes tested in the smoke test.

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -19,7 +19,7 @@ import pytest
 import torch
 
 
-@pytest.mark.parametrize("batch_size", [1, 2, 5])
+@pytest.mark.parametrize("batch_size", [1, 2])
 def test_smoke(batch_size):
     x = torch.rand(batch_size, 2, 3)
     assert x.shape == (batch_size, 2, 3), x.shape
