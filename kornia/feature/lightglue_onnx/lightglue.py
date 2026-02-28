84cf0ec68994a3b074ad61d9ffbbb6445e1dbea9
varun sai <varunsaip2005@gmail.com>
2026-02-28 19:20:19 +0530

    added fallback to numpy for backward compatibility and added comments

--- kornia/feature/lightglue_onnx/lightglue.py
+++ kornia/feature/lightglue_onnx/lightglue.py
@@ -177,7 +177,8 @@ class OnnxLightGlue:
         self.session.run_with_iobinding(binding)
 
         matches, mscores = binding.get_outputs()
-        
+
+        # use DLPack for zero-copy ORT to torch conversion
         outputs = {
             "matches": dlpack.from_dlpack(matches.to_dlpack()),
             "scores": dlpack.from_dlpack(mscores.to_dlpack()),
